323e91d90ad15bbc86ffba14b149edd5bba02c76
dkovacevic <dejankov@gmail.com>
2020-11-07 19:18:02 +0100

    Removed dep to org.slf4j

--- src/test/java/com/wire/xenon/DAOTest.java
+++ src/test/java/com/wire/xenon/DAOTest.java
@@ -1,6 +1,7 @@
 package com.wire.xenon;
 
 import com.wire.xenon.crypto.storage.IdentitiesDAO;
+import com.wire.xenon.state.StatesDAO;
 import org.apache.log4j.BasicConfigurator;
 import org.flywaydb.core.Flyway;
 import org.jdbi.v3.core.Jdbi;
@@ -12,8 +13,8 @@ import java.sql.Driver;
 import java.sql.DriverManager;
 import java.util.UUID;
 
-public class IdentitiesDAOTest {
-    private static final String url = "jdbc:postgresql://localhost/lithium";
+public class DAOTest {
+    private static final String url = "jdbc:postgresql://localhost/xenon";
     private static final Jdbi jdbi = Jdbi.create(url)
             .installPlugin(new SqlObjectPlugin());
 
@@ -34,12 +35,23 @@ public class IdentitiesDAOTest {
     }
 
     @Test
-    public void test() {
+    public void testIdentitiesDAO() {
         final IdentitiesDAO identitiesDAO = jdbi.onDemand(IdentitiesDAO.class);
         final String id = UUID.randomUUID().toString();
 
         final int insert = identitiesDAO.insert(id, id.getBytes());
         final byte[] bytes = identitiesDAO.get(id).data;
         final int delete = identitiesDAO.delete(id);
+    }
+
+    @Test
+    public void testStatesDAO() {
+        final StatesDAO statesDAO = jdbi.onDemand(StatesDAO.class);
+        final UUID id = UUID.randomUUID();
+        final String text = "{\"some\" : \"text\"}";
+
+        final int insert = statesDAO.insert(id, text);
+        final String dbText = statesDAO.get(id);
+        final int delete = statesDAO.delete(id);
     }
 }
