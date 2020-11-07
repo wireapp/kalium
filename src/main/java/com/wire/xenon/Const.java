824c77e8b4e89119adc353a1f2e6d8f64035c10a
dkovacevic <dejankov@gmail.com>
2020-11-07 14:32:49 +0100

    Bumped jdbi version

--- src/main/java/com/wire/xenon/Const.java
+++ src/main/java/com/wire/xenon/Const.java
@@ -1,13 +1,9 @@
 package com.wire.xenon;
 
-/**
- * Created with IntelliJ IDEA.
- * User: dejankovacevic
- * Date: 23/10/2020
- * Time: 12:45
- */
 public class Const {
     public static final String WIRE_BOTS_SDK_TOKEN = "wire.bots.sdk.token";
     public static final String WIRE_BOTS_SDK_API = "wire.bots.sdk.api";
     public static final String WIRE_BOTS_SDK_WS = "wire.bots.sdk.ws";
+    public String API_HOST = "https://prod-nginz-https.wire.com";
+
 }
