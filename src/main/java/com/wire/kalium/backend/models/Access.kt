c09703c943b2a72fd1324057a6a829e2bd82309f
dkovacevic <dejankov@gmail.com>
2021-11-11 21:03:24 +0100

    Commented out Cookies and token renewal

--- src/main/java/com/wire/kalium/backend/models/Access.kt
+++ src/main/java/com/wire/kalium/backend/models/Access.kt
@@ -2,12 +2,14 @@ package com.wire.kalium.backend.models
 
 import com.wire.kalium.tools.UUIDSerializer
 import kotlinx.serialization.Serializable
-import java.util.UUID
+import java.util.*
 
 @Serializable
 data class Access(
-        var cookie: Cookie,
-        @Serializable(with = UUIDSerializer::class) val userId: UUID,
+        //@JsonIgnore
+        //var cookie: Cookie,
+
+        @Serializable(with = UUIDSerializer::class) val user: UUID,
         val access_token: String,
         val expires_in: Int,
         val token_type: String,
