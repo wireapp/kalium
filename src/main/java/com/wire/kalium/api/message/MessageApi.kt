f925ad6805f663fd4d8d0f5fce77821f835f4a89
Tommaso Piazza <196761+tmspzz@users.noreply.github.com>
2021-11-19 18:07:22 +0100

    refactor: Beautify and complete sendMessage API

--- src/main/java/com/wire/kalium/api/message/MessageApi.kt
+++ src/main/java/com/wire/kalium/api/message/MessageApi.kt
@@ -2,20 +2,39 @@ package com.wire.kalium.api.message
 
 import com.wire.kalium.api.KaliumHttpResult
 import com.wire.kalium.models.outbound.otr.OtrMessage
+import kotlinx.serialization.SerialName
 
 interface MessageApi {
 
+    sealed interface SendMessageOptionsInterface
+    sealed class MessageOption(): SendMessageOptionsInterface {
+        object IgnoreAll: MessageOption()
+        object ReportAll: MessageOption()
+        data class IgnoreSome(val userIDs: List<String>): MessageOption()
+        data class ReposeSome(val userIDs: List<String>): MessageOption()
+    }
+
+    sealed interface SendMessageParameters
+    sealed class Parameters(): SendMessageParameters {
+        data class DefaultParameters(
+           val sender: String,
+           val `data`: String,
+           val nativePush: Boolean,
+           val recipients: HashMap<String, HashMap<String, String>>,
+           val transient: Boolean,
+           val priority: String = "low"): Parameters()
+    }
+
     suspend fun sendMessage(
-            sendMessageRequest: SendMessageRequest,
+            parameters: Parameters.DefaultParameters,
             conversationId: String,
-            ignoreMissing: Boolean,
+            option: MessageOption,
             token: String
     ): KaliumHttpResult<SendMessageResponse>
-
-    suspend fun sendPartialMessage(otrMessage: OtrMessage, conversationID: String, userId: String): KaliumHttpResult<SendMessageResponse>
 
     companion object {
         const val PATH_OTR_MESSAGE = "/otr/messages"
         const val QUERY_IGNORE_MISSING = "ignore_missing"
+        const val QUERY_REPORT_MISSING = "report_missing"
     }
 }
