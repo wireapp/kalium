0bd2e7908b3d894f9967e3110ee492927a9d1e50
Mohamad Jaara <jaara.moh@gmail.com>
2021-11-19 12:10:43 +0100

    fix: add call to get getAllNotifications (#121)

--- src/main/java/com/wire/kalium/api/notification/NotificationApi.kt
+++ src/main/java/com/wire/kalium/api/notification/NotificationApi.kt
@@ -7,4 +7,9 @@ interface NotificationApi {
 
     suspend fun notificationsByBatch(querySize: Int, queryClient: String,  querySince: String): KaliumHttpResult<NotificationPageResponse>
 
+    /**
+     * request Notifications from the beginning of time
+     */
+    suspend fun getAllNotifications(querySize: Int, queryClient: String): KaliumHttpResult<NotificationPageResponse>
+
 }
