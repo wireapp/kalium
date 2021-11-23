70237268d52ff84aa01cf8d14b9444d4b1a51ef4
Mohamad Jaara <mohamad.jaara@wire.com>
2021-11-23 16:11:45 +0100

    bug fix in MessageApiImp.kt

--- src/main/java/com/wire/kalium/api/message/MessageApi.kt
+++ src/main/java/com/wire/kalium/api/message/MessageApi.kt
@@ -6,33 +6,27 @@ interface MessageApi {
 
     sealed interface SendMessageOptionsInterface
     sealed class MessageOption : SendMessageOptionsInterface {
-        object IgnoreAll: MessageOption()
-        object ReportAll: MessageOption()
-        data class IgnoreSome(val userIDs: List<String>): MessageOption()
-        data class ReposeSome(val userIDs: List<String>): MessageOption()
+        object IgnoreAll : MessageOption()
+        object ReportAll : MessageOption()
+        data class IgnoreSome(val userIDs: List<String>) : MessageOption()
+        data class ReposeSome(val userIDs: List<String>) : MessageOption()
     }
 
     sealed interface SendMessageParameters
     sealed class Parameters : SendMessageParameters {
         data class DefaultParameters(
-           val sender: String,
-           val `data`: String,
-           val nativePush: Boolean,
-           val recipients: HashMap<String, HashMap<String, String>>,
-           val transient: Boolean,
-           val priority: String = "low"): Parameters()
+                val sender: String,
+                val `data`: String,
+                val nativePush: Boolean,
+                val recipients: HashMap<String, HashMap<String, String>>,
+                val transient: Boolean,
+                val priority: String = "low"
+        ) : Parameters()
     }
 
     suspend fun sendMessage(
             parameters: Parameters.DefaultParameters,
             conversationId: String,
-            option: MessageOption,
-            token: String
+            option: MessageOption
     ): KaliumHttpResult<SendMessageResponse>
-
-    companion object {
-        const val PATH_OTR_MESSAGE = "/otr/messages"
-        const val QUERY_IGNORE_MISSING = "ignore_missing"
-        const val QUERY_REPORT_MISSING = "report_missing"
-    }
 }
