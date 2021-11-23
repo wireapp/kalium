2674417b10ec90bb01d77511cdb9f445275cb6c5
Mohamad Jaara <mohamad.jaara@wire.com>
2021-11-23 14:33:27 +0100

    handle api errors on wrapKaliumResponse

--- src/main/java/com/wire/kalium/api/KtorHttpClient.kt
+++ src/main/java/com/wire/kalium/api/KtorHttpClient.kt
@@ -1,9 +1,14 @@
 package com.wire.kalium.api
 
+import com.wire.kalium.exceptions.HttpException
 import com.wire.kalium.tools.HostProvider
 import io.ktor.client.HttpClient
 import io.ktor.client.call.receive
 import io.ktor.client.engine.HttpClientEngine
+import io.ktor.client.features.ClientRequestException
+import io.ktor.client.features.RedirectResponseException
+import io.ktor.client.features.ResponseException
+import io.ktor.client.features.ServerResponseException
 import io.ktor.client.features.auth.Auth
 import io.ktor.client.features.auth.providers.BearerTokens
 import io.ktor.client.features.auth.providers.bearer
@@ -59,15 +64,36 @@ class KtorHttpClient(
 class KaliumKtorResult<BodyType : Any>(private val httpResponse: HttpResponse, private val body: BodyType) : KaliumHttpResult<BodyType> {
     override val httpStatusCode: Int
         get() = httpResponse.status.value
-    override val headers: Headers
-        get() = httpResponse.headers
+    override val headers: Set<Map.Entry<String, List<String>>>
+        get() = httpResponse.headers.entries()
     override val resultBody: BodyType
         get() = body
 }
 
 suspend inline fun <reified BodyType : Any> wrapKaliumResponse(performRequest: () -> HttpResponse): KaliumHttpResult<BodyType> {
-    val result = performRequest()
-    return KaliumKtorResult(result, result.receive())
+    try {
+        val result = performRequest()
+        return KaliumKtorResult(result, result.receive())
+    } catch (e: ResponseException) {
+        when(e) {
+            is RedirectResponseException -> {
+                // 300..399
+                throw e
+            }
+            is ClientRequestException -> {
+                // 400..499
+                throw e
+            }
+            is ServerResponseException ->{
+                // 500..599
+                throw e
+            }
+            else -> {
+                // other ResponseException
+                throw e
+            }
+        }
+    }
 }
 
 fun HttpResponse.isSuccessful(): Boolean = this.status.value in 200..299
