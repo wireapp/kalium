ae3737a5080749014c92d4e131c285c29180590c
Mohamad Jaara <mohamad.jaara@wire.com>
2021-11-23 15:08:27 +0100

    ClientApi.kt test

--- src/main/java/com/wire/kalium/api/user/client/ClientApiImp.kt
+++ src/main/java/com/wire/kalium/api/user/client/ClientApiImp.kt
@@ -1,9 +1,7 @@
 package com.wire.kalium.api.user.client
 
 import com.wire.kalium.api.KaliumHttpResult
-import com.wire.kalium.api.user.client.ClientApi.Companion.PATH_CLIENTS
 import com.wire.kalium.api.wrapKaliumResponse
-import com.wire.kalium.exceptions.AuthException
 import io.ktor.client.HttpClient
 import io.ktor.client.call.receive
 import io.ktor.client.request.post
@@ -16,9 +14,8 @@ class ClientApiImp(private val httpClient: HttpClient) : ClientApi {
                 httpClient.post<HttpResponse>(path = PATH_CLIENTS) {
                     body = registerClientRequest
                 }.receive()
-            }.also {
-                if (it.httpStatusCode == 400 or 401) {
-                    throw AuthException(code = it.httpStatusCode)
-                }
             }
+    private companion object {
+        const val PATH_CLIENTS = "clients"
+    }
 }
