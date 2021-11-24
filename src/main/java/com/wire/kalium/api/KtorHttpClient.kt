46ec9b18992e331ba4752bf0130b79666e9dd2fa
Mohamad Jaara <mohamad.jaara@wire.com>
2021-11-24 17:56:37 +0100

    return error type when doing network call

--- src/main/java/com/wire/kalium/api/KtorHttpClient.kt
+++ src/main/java/com/wire/kalium/api/KtorHttpClient.kt
@@ -1,12 +1,14 @@
 package com.wire.kalium.api
 
-import com.wire.kalium.exceptions.HttpException
+import com.sun.xml.internal.ws.wsdl.writer.document.soap12.BodyType
+import com.wire.kalium.exceptions.ApiErrorException
+import com.wire.kalium.exceptions.InvalidRequestException
+import com.wire.kalium.exceptions.OtherException
 import com.wire.kalium.tools.HostProvider
 import io.ktor.client.HttpClient
 import io.ktor.client.call.receive
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.features.ClientRequestException
-import io.ktor.client.features.HttpResponseValidator
 import io.ktor.client.features.RedirectResponseException
 import io.ktor.client.features.ResponseException
 import io.ktor.client.features.ServerResponseException
@@ -20,8 +22,8 @@ import io.ktor.client.request.header
 import io.ktor.client.request.host
 import io.ktor.client.statement.HttpResponse
 import io.ktor.http.ContentType
-import io.ktor.http.Headers
 import io.ktor.http.URLProtocol
+import kotlinx.serialization.SerializationException
 import kotlinx.serialization.json.Json
 
 class KtorHttpClient(
@@ -32,7 +34,6 @@ class KtorHttpClient(
 
     val provideKtorHttpClient by lazy {
         HttpClient(engine) {
-            expectSuccess = false
             defaultRequest {
                 header("Content-Type", "application/json")
                 host = hostProvider.host
@@ -59,35 +60,11 @@ class KtorHttpClient(
                 })
                 accept(ContentType.Application.Json)
             }
-            HttpResponseValidator {
-                handleResponseException { exception ->
-                    if (exception is ResponseException) {
-                        when(exception) {
-                            is RedirectResponseException -> {
-                                // 300..399
-                                throw exception
-                            }
-                            is ClientRequestException -> {
-                                // 400..499
-                                throw exception
-                            }
-                            is ServerResponseException ->{
-                                // 500..599
-                                throw exception
-                            }
-                            else -> {
-                                // other ResponseException
-                                throw exception
-                            }
-                        }
-                    }
-                }
-            }
         }
     }
 }
 
-class KaliumKtorResult<BodyType : Any>(private val httpResponse: HttpResponse, private val body: BodyType) : KaliumHttpResult<BodyType> {
+class KaliumKtorResult<out BodyType : Any, out ErrorType>(private val httpResponse: HttpResponse, private val body: BodyType) : KaliumHttpResult<BodyType> {
     override val httpStatusCode: Int
         get() = httpResponse.status.value
     override val headers: Set<Map.Entry<String, List<String>>>
@@ -96,9 +73,43 @@ class KaliumKtorResult<BodyType : Any>(private val httpResponse: HttpResponse, p
         get() = body
 }
 
-suspend inline fun <reified BodyType : Any> wrapKaliumResponse(performRequest: () -> HttpResponse): KaliumHttpResult<BodyType> {
+sealed class NetworkResponse<out T : Any, out E : Any> {
+    data class Success<out T : Any>(val code: Int, val body: T) : NetworkResponse<T, Nothing>()
+    data class ServerError<out E : Any>(val code: Int, val body: E) : NetworkResponse<Nothing, E>()
+    object InvalidRequest : NetworkResponse<Nothing, Nothing>()
+    data class GenericError(val throwable: Throwable) : NetworkResponse<Nothing, Nothing>()
+}
+
+suspend inline fun <reified BodyType : Any, reified ErrorType : Any> wrapKaliumResponse(performRequest: () -> HttpResponse): NetworkResponse<BodyType, ErrorType> {
+    try {
         val result = performRequest()
-        return KaliumKtorResult(result, result.receive())
+        return NetworkResponse.Success(result.status.value, result.receive())
+    } catch (e: ResponseException) { // ktor exception
+        when (e) {
+            is RedirectResponseException -> {
+                // 300 .. 399
+                return NetworkResponse.GenericError(e)
+            }
+            is ClientRequestException -> {
+                return if (e.response.status.value == 400) {
+                    // 400
+                    NetworkResponse.InvalidRequest
+                } else {
+                    // 401 .. 499
+                    NetworkResponse.ServerError(code = e.response.status.value, body = e.response.receive())
+                }
+            }
+            is ServerResponseException -> {
+                // 500 .. 599
+                return NetworkResponse.GenericError(e)
+            }
+            else -> {
+                return NetworkResponse.GenericError(e)
+            }
+        }
+    } catch (e: SerializationException) {
+        return NetworkResponse.GenericError(e)
+    }
 }
 
 fun HttpResponse.isSuccessful(): Boolean = this.status.value in 200..299
