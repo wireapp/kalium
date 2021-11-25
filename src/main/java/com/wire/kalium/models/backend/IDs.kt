105424d5104ff1f68a9891289844735cd59bc2ab
Mohamad Jaara <mohamad.jaara@wire.com>
2021-11-25 11:24:31 +0100

    move user and conversation id model to its own file

---
+++ src/main/java/com/wire/kalium/models/backend/IDs.kt
@@ -0,0 +1,17 @@
+package com.wire.kalium.models.backend
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+typealias ConversationId = QualifiedID
+typealias UserId = QualifiedID
+
+
+@Serializable
+data class QualifiedID(
+        @SerialName("id")
+        val value: String,
+
+        @SerialName("domain")
+        val domain: String
+)
