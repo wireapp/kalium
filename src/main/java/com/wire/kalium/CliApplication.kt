b99f26a4522d3d98aeddd5cc0a1f44cb12be3b3a
Mohamad Jaara <jaara.moh@gmail.com>
2021-11-25 09:05:00 +0100

    chore: set up the http client to attache the access token to header (#123)

--- src/main/java/com/wire/kalium/CliApplication.kt
+++ src/main/java/com/wire/kalium/CliApplication.kt
@@ -3,6 +3,7 @@ package com.wire.kalium
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.required
+import com.wire.kalium.api.AuthenticationManager
 import com.wire.kalium.api.KtorHttpClient
 import com.wire.kalium.api.user.client.ClientApiImp
 import com.wire.kalium.api.user.client.RegisterClientRequest
@@ -12,23 +13,61 @@ import com.wire.kalium.api.user.login.LoginWithEmailRequest
 import com.wire.kalium.crypto.Crypto
 import com.wire.kalium.crypto.CryptoFile
 import com.wire.kalium.models.outbound.otr.PreKey
+import com.wire.kalium.tools.HostProvider
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.features.defaultRequest
+import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.features.json.serializer.KotlinxSerializer
+import io.ktor.client.request.header
+import io.ktor.client.request.host
+import io.ktor.http.ContentType
+import io.ktor.http.URLProtocol
 import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.json.Json
+import okhttp3.logging.HttpLoggingInterceptor
 
+private class AuthenticationManagerImp(private val accessToken: String, private val tokenType: String, val refreshToken: String) : AuthenticationManager {
+    override fun accessToken(): String {
+        return accessToken
+    }
+    override fun refreshToken(): String = refreshToken
+}
 
 class CliApplication() : CliktCommand() {
-    val convid: String by option(help="conversation id").required()
-    val message: String by option(help="message").required()
+    val convid: String by option(help = "conversation id").required()
+    val message: String by option(help = "message").required()
 
     //val email: String by option(help = "wire account email").prompt(text = "email ")
     //val password: String by option(help = "wire account password").prompt(text = "password ")
     override fun run() {
         runBlocking {
-            val httpClient = KtorHttpClient().ktorHttpClient
-            val loginApi: LoginApi = LoginApiImp(httpClient)
+            val okHttp = OkHttp.create {
+                val interceptor = HttpLoggingInterceptor()
+                interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+                addInterceptor(interceptor)
+            }
+            val loginApi: LoginApi = LoginApiImp(HttpClient(OkHttp) {
+                expectSuccess = false
+                defaultRequest {
+                    header("Content-Type", "application/json")
+                    host = HostProvider.host
+                    url.protocol = URLProtocol.HTTPS
+                }
+                install(JsonFeature) {
+                    serializer = KotlinxSerializer(Json {
+                        prettyPrint = true
+                        isLenient = true
+                        ignoreUnknownKeys = true
+                    })
+                    accept(ContentType.Application.Json)
+                }
+            })
             val body = LoginWithEmailRequest(email = "dejan@wire.com", password = "12345678", label = "ktor")
             val crypto: Crypto = CryptoFile("./data/joker")
             val loginResult = loginApi.emailLogin(body, false)
-            println(loginResult.resultBody)
+            println("login: ${loginResult.resultBody} ")
+            val authenticationManager = AuthenticationManagerImp(loginResult.resultBody.accessToken, loginResult.resultBody.tokenType, "")
             val preKeys: ArrayList<PreKey> = crypto.newPreKeys(0, 20)
             val lastKey: PreKey = crypto.newLastPreKey()
             val deviceClass = "tablet"
@@ -41,35 +80,11 @@ class CliApplication() : CliktCommand() {
                     preKeys = preKeys,
                     lastKey = lastKey
             )
-            val registerClient = ClientApiImp(httpClient).registerClient(registerClientRequest, loginResult.resultBody.accessToken)
+            val httpClient = KtorHttpClient(HostProvider, okHttp, authenticationManager).provideKtorHttpClient
+            val registerClient = ClientApiImp(httpClient).registerClient(registerClientRequest)
             println(registerClient.resultBody)
         }
-
-        // login stuff here
-        /*val conversationId = UUID.fromString(convid)
-        val httpClient: Client = ClientBuilder
-                .newClient()
-        val loggingFeature = LoggingFeature(Logger.getLOGGER(), Level.INFO, null, null)
-        httpClient.register(loggingFeature)
-
-        val crypto = CryptoFile("/tmp/joker")
-        val app = Application("dejan+joker@wire.com", "12345678")
-                .addClient(httpClient)
-                .addCrypto(crypto)
-
-        // Login, create device if needed, setup token refresh timer, pull missed messages and more
-        app.start()
-
-        // Create WireClient for this conversationId
-        val wireClient = app.getWireClient(conversationId)
-
-        // Send text
-        wireClient.send(MessageText("Hi there from Kotlin!"))
-
-        app.stop()
-
-         */
     }
 }
 
-fun main(args: Array<String>) =  CliApplication().main(args)
+fun main(args: Array<String>) = CliApplication().main(args)
