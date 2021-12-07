c42979c12afaf58bcd15c78ec782909c858f9e65
Mohamad Jaara <mohamad.jaara@wire.com>
2021-12-07 16:04:18 +0100

    add dependencies

--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -4,10 +4,21 @@ plugins {
 }
 
 dependencies {
+    val cryptoboxVersion = "1.0.0"
+    val genericMessageProtoVersion = "1.35.0"
+
     implementation(project(":network"))
 
     implementation(Dependencies.Cli.cliKt)
+    implementation(Dependencies.OkHttp.loggingInterceptor)
+
+    // ktor
     implementation(Dependencies.Ktor.core)
     implementation(Dependencies.Ktor.okHttp)
-    implementation(Dependencies.OkHttp.loggingInterceptor)
+    implementation(Dependencies.Ktor.json)
+    implementation(Dependencies.Ktor.serialization)
+
+    // to be removed later when the crypto module is done
+    implementation("com.wire:generic-message-proto:$genericMessageProtoVersion")
+    implementation("com.wire:cryptobox4j:$cryptoboxVersion")
 }
