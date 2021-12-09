4d0c81e5a368a2d548277fb48373a4c997edb66b
Mohamad Jaara <mohamad.jaara@wire.com>
2021-12-09 12:18:20 +0100

    feat: get self user info request

---
+++ network/src/commonMain/kotlin/com/wire/kalium/network/api/user/self/SelfApi.kt
@@ -0,0 +1,20 @@
+package com.wire.kalium.network.api.user.self
+
+import com.wire.kalium.network.api.KaliumHttpResult
+import com.wire.kalium.network.api.wrapKaliumResponse
+import io.ktor.client.HttpClient
+import io.ktor.client.call.receive
+import io.ktor.client.request.get
+import io.ktor.client.statement.HttpResponse
+
+class SelfApi(private val httpClient: HttpClient) {
+
+    suspend fun getSelfInfo(): KaliumHttpResult<SelfUserInfoResponse> =
+        wrapKaliumResponse {
+        httpClient.get<HttpResponse>(path = PATH_SELF).receive()
+    }
+
+    private companion object {
+        const val PATH_SELF = "self"
+    }
+}
---
+++ network/src/commonMain/kotlin/com/wire/kalium/network/api/user/self/UserSsoId.kt
@@ -0,0 +1,13 @@
+package com.wire.kalium.network.api.user.self
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class UserSsoId(
+    @SerialName("scim_external_id")
+    val scimExternalId: String?,
+    @SerialName("subjecßt")
+    val subject: String,
+    @SerialName("tenant")
+    val tenant: String?
+)
