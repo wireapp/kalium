b25f38e0b582ce556d720e0aa6f7ceee021c6fd7
Mohamad Jaara <jaara.moh@gmail.com>
2021-12-09 18:36:39 +0100

    chore: user explicitNulls in KtxSerializer.kt (#166)

--- network/src/commonMain/kotlin/com/wire/kalium/network/tools/KtxSerializer.kt
+++ network/src/commonMain/kotlin/com/wire/kalium/network/tools/KtxSerializer.kt
@@ -1,11 +1,16 @@
 package com.wire.kalium.network.tools
 
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 
+@ExperimentalSerializationApi
 object KtxSerializer {
     val json = Json {
         isLenient = true
         ignoreUnknownKeys = true
         encodeDefaults = true
+        // explicitNulls, defines whether null property
+        // values should be included in the serialized JSON string.
+        explicitNulls = false
     }
 }
