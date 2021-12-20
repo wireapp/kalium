6c7e05fad19c30762e70e737092771bf75d76768
Mohamad Jaara <mohamad.jaara@wire.com>
2021-12-20 13:56:52 +0100

    fix: delete deprecated getUsersPreKey function

--- network/src/commonMain/kotlin/com/wire/kalium/network/api/prekey/PreKeyApiImpl.kt
+++ network/src/commonMain/kotlin/com/wire/kalium/network/api/prekey/PreKeyApiImpl.kt
@@ -1,6 +1,5 @@
 package com.wire.kalium.network.api.prekey
 
-import com.wire.kalium.network.api.message.UserIdToClientMap
 import com.wire.kalium.network.utils.NetworkResponse
 import com.wire.kalium.network.utils.wrapKaliumResponse
 import io.ktor.client.HttpClient
@@ -8,11 +7,6 @@ import io.ktor.client.request.get
 import io.ktor.client.request.post
 
 class PreKeyApiImpl(private val httpClient: HttpClient) : PreKeyApi {
-    override suspend fun getUsersPreKey(users: UserIdToClientMap): NetworkResponse<UserClientsToPreKeyMap> = wrapKaliumResponse {
-        httpClient.post(path = "$PATH_USERS$PATH_PRE_KEY") {
-            body = users
-        }
-    }
 
     override suspend fun getUsersPreKey(users: DomainToUserIdToClientsMap): NetworkResponse<DomainToUserIdToClientsToPreykeyMap> =
         wrapKaliumResponse {
