14a46f410b739aa228999a412640b2e4c87d113e
Vitor Hugo Schwaab <vitor@schwaab.xyz>
2021-12-20 17:52:56 +0100

    feat: add Either classes for internal usage in `logic` module

---
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/failure/AuthenticationFailure.kt
@@ -0,0 +1,7 @@
+package com.wire.kalium.logic.failure
+
+import com.wire.kalium.logic.CoreFailure
+
+sealed class AuthenticationFailure : CoreFailure.FeatureFailure()
+
+object InvalidCredentials: AuthenticationFailure()
