c4f339b8787601781c33a05254d91c7ba43d4a38
Mohamad Jaara <jaara.moh@gmail.com>
2022-01-04 19:19:00 +0100

    Update logic/src/commonMain/kotlin/com/wire/kalium/logic/data/prekey/PreyKeyMapper.kt
    Co-authored-by: Jacob Persson <7156+typfel@users.noreply.github.com>

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/data/prekey/PreyKeyMapper.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/data/prekey/PreyKeyMapper.kt
@@ -2,7 +2,7 @@ package com.wire.kalium.logic.data.prekey
 
 import com.wire.kalium.network.api.prekey.PreKeyDTO
 
-class PreyKeyMapper {
+class PreKeyMapper {
     fun fromPreKeyDTO(preyKeyDTO: PreKeyDTO): PreKey = PreKey(id = preyKeyDTO.id, key = preyKeyDTO.key)
 
     fun toPreKeyDTO(preKey: PreKey): PreKeyDTO = PreKeyDTO(id = preKey.id, key = preKey.key)
