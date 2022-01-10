78980ad48c8d454a71cd033c35be71d34136f1aa
Mohamad Jaara <mohamad.jaara@wire.com>
2022-01-10 23:27:33 +0100

    use cases

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/data/session/SessionRepository.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/data/session/SessionRepository.kt
@@ -1,19 +1,45 @@
 package com.wire.kalium.logic.data.session
 
+import com.wire.kalium.logic.CoreFailure
+import com.wire.kalium.logic.failure.SessionFailure
 import com.wire.kalium.logic.feature.auth.AuthSession
+import com.wire.kalium.logic.functional.Either
+import com.wire.kalium.persistence.client.SessionLocalDataSource
+import com.wire.kalium.persistence.model.DataStoreResult
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 
 interface SessionRepository {
-    fun storeSession(session: AuthSession)
-    fun getSessions(): List<AuthSession>
+    suspend fun storeSession(autSession: AuthSession)
+    suspend fun getSessions(): Flow<Either<CoreFailure, List<AuthSession>>>
 }
 
 class InMemorySessionRepository : SessionRepository {
     private val sessions = hashMapOf<String, AuthSession>()
 
-    override fun storeSession(session: AuthSession) {
-        sessions[session.userId] = session
+    override suspend fun storeSession(autSession: AuthSession) {
+        sessions[autSession.userId] = autSession
     }
 
-    override fun getSessions(): List<AuthSession> = sessions.values.toList()
+    override suspend fun getSessions(): Flow<Either<CoreFailure, List<AuthSession>>> = flow { emit(Either.Right(sessions.values.toList())) }
 
+}
+
+class SessionRepositoryImpl(
+    private val sessionLocalDataSource: SessionLocalDataSource,
+    private val sessionMapper: SessionMapper
+) : SessionRepository {
+    override suspend fun storeSession(autSession: AuthSession) {
+        sessionLocalDataSource.addSession(sessionMapper.toPersistenceSession(autSession))
+    }
+
+    override suspend fun getSessions(): Flow<Either<CoreFailure, List<AuthSession>>> =
+        sessionLocalDataSource.allSessions().map { result ->
+            when (result) {
+                is DataStoreResult.Success -> return@map Either.Right(
+                    result.data.values.toList().map { sessionMapper.fromPersistenceSession(it) })
+                is DataStoreResult.DataNotFound -> return@map Either.Left(SessionFailure.NoSessionFound)
+            }
+        }
 }
--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/UserSessionScope.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/UserSessionScope.kt
@@ -12,16 +12,31 @@ import com.wire.kalium.logic.data.conversation.ConversationRepository
 import com.wire.kalium.logic.data.location.LocationMapper
 import com.wire.kalium.logic.data.message.MessageRepository
 import com.wire.kalium.logic.data.prekey.PreKeyMapper
+import com.wire.kalium.logic.data.session.SessionMapper
+import com.wire.kalium.logic.data.session.SessionMapperImpl
+import com.wire.kalium.logic.data.session.SessionRepository
+import com.wire.kalium.logic.data.session.SessionRepositoryImpl
 import com.wire.kalium.logic.feature.auth.AuthSession
 import com.wire.kalium.logic.feature.client.ClientScope
 import com.wire.kalium.logic.feature.conversation.ConversationScope
 import com.wire.kalium.logic.feature.message.MessageScope
+import com.wire.kalium.logic.feature.session.SessionScope
+import com.wire.kalium.persistence.client.SessionLocalDataSource
 
 class UserSessionScope(
     private val userSession: AuthSession,
     private val authenticatedDataSourceSet: AuthenticatedDataSourceSet,
-    private val clientConfig: ClientConfig
+    // TODO: change this class so there is no need to pass clientConfig & sessionLocalDataSource
+    private val clientConfig: ClientConfig,
+    private val sessionLocalDataSource: SessionLocalDataSource
 ) {
+
+    private val sessionMapper: SessionMapper get() = SessionMapperImpl()
+    private val sessionRepository: SessionRepository
+        get() = SessionRepositoryImpl(
+            sessionMapper = sessionMapper,
+            sessionLocalDataSource = sessionLocalDataSource
+        )
 
     private val conversationMapper: ConversationMapper get() = ConversationMapper()
 
@@ -47,4 +62,5 @@ class UserSessionScope(
     val client: ClientScope get() = ClientScope(clientRepository)
     val conversations: ConversationScope get() = ConversationScope(conversationRepository)
     val messages: MessageScope get() = MessageScope(messageRepository)
+    val session: SessionScope get() = SessionScope(sessionRepository)
 }
