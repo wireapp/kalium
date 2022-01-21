a56a596401cab1f66fb248fe34d6ad6aa470d7f7
Jacob Persson <7156+typfel@users.noreply.github.com>
2022-01-21 14:25:31 +0100

    chore: remove redundant public

--- persistence/src/commonMain/kotlin/com/wire/kalium/persistence/dao/UserDAO.kt
+++ persistence/src/commonMain/kotlin/com/wire/kalium/persistence/dao/UserDAO.kt
@@ -2,18 +2,18 @@ package com.wire.kalium.persistence.dao
 
 import kotlinx.coroutines.flow.Flow
 
-public data class QualifiedID(
+data class QualifiedID(
     val value: String,
     val domain: String
 )
 
-public data class User(
-    public val id: QualifiedID,
-    public val name: String?,
-    public val handle: String?
+data class User(
+    val id: QualifiedID,
+    val name: String?,
+    val handle: String?
 ) { }
 
-public interface UserDAO {
+interface UserDAO {
     suspend fun insertUser(user: User)
     suspend fun insertUsers(users: List<User>)
     suspend fun updateUser(user: User)
