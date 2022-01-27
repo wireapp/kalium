b4415102ec61a638dcff18fe2a439e43ca2488ce
vitorhugods <vitor@schwaab.xyz>
2022-01-27 22:39:31 +0100

    refactor: make ConversationsApplication use CoreLogic instead of Network directly

--- cli/src/main/kotlin/com/wire/kalium/cli/ConversationsApplication.kt
+++ cli/src/main/kotlin/com/wire/kalium/cli/ConversationsApplication.kt
@@ -3,15 +3,9 @@ package com.wire.kalium.cli
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.required
-import com.wire.kalium.cli.CLIUtils.getResource
-import com.wire.kalium.cryptography.utils.calcMd5
-import com.wire.kalium.network.AuthenticatedNetworkContainer
-import com.wire.kalium.network.LoginNetworkContainer
-import com.wire.kalium.network.api.SessionCredentials
-import com.wire.kalium.network.api.model.AssetMetadata
-import com.wire.kalium.network.api.model.AssetRetentionType
-import com.wire.kalium.network.api.user.login.LoginApi
-import com.wire.kalium.network.utils.isSuccessful
+import com.wire.kalium.logic.CoreLogic
+import com.wire.kalium.logic.feature.auth.AuthenticationResult
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
 
 class ConversationsApplication : CliktCommand() {
@@ -20,40 +14,27 @@ class ConversationsApplication : CliktCommand() {
 
     override fun run(): Unit = runBlocking {
 
-        val loginContainer = LoginNetworkContainer()
+        val coreLogic = CoreLogic("Sample Kalium CLI App", ".proteus")
 
-        val loginResult = loginContainer.loginApi.login(
-            LoginApi.LoginParam.LoginWithEmail(email = email, password = password, label = "ktor"), false)
+        val session = coreLogic.authenticationScope {
+            val result = loginUsingEmail(email, password, false)
 
-        if (!loginResult.isSuccessful()) {
-            println("There was an error on the login :( check the credentials and the internet connection and try again please")
-        } else {
-            val sessionData = loginResult.value
-            //TODO: Get them 🍪 refresh token
-            val sessionCredentials = SessionCredentials(sessionData.tokenType, sessionData.accessToken, "refreshToken")
-            val networkModule = AuthenticatedNetworkContainer(sessionCredentials)
-            val conversationsResponse = networkModule.conversationApi.conversationsByBatch(null, 100)
-
-            if (!conversationsResponse.isSuccessful()) {
-                println("There was an error loading the conversations :( check the internet connection and try again please")
-            } else {
-                println("Your conversations:")
-                conversationsResponse.value.conversations.forEach {
-                    println("ID:${it.id}, Name: ${it.name}")
-                }
+            if (result !is AuthenticationResult.Success) {
+                throw RuntimeException(
+                    "There was an error on the login :(" +
+                            "Check the credentials and the internet connection and try again"
+                )
             }
-
-            uploadTestAsset(networkModule)
+            result.userSession
         }
-    }
 
-    private suspend fun uploadTestAsset(networkModule: AuthenticatedNetworkContainer) {
-        val imageBytes: ByteArray = getResource("moon1.jpg")
-        val uploadResult = networkModule.assetApi.uploadAsset(
-            AssetMetadata("image/jpeg", true, AssetRetentionType.ETERNAL, calcMd5(imageBytes)),
-            imageBytes
-        )
-        println("The upload result is -> $uploadResult")
+        coreLogic.sessionScope(session) {
+            val conversations = conversations.getConversations().first()
+            println("Your conversations:")
+            conversations.forEach {
+                println("ID:${it.id}, Name: ${it.name}")
+            }
+        }
     }
 }
 
