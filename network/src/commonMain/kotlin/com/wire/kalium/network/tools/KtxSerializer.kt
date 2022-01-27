8d8884cadc75f183a0679e9e1de283271d39f08b
vitorhugods <vitor@schwaab.xyz>
2022-01-27 22:33:44 +0100

    feat: add support for overriding unsolvable json fields with default parameters

--- network/src/commonMain/kotlin/com/wire/kalium/network/tools/KtxSerializer.kt
+++ network/src/commonMain/kotlin/com/wire/kalium/network/tools/KtxSerializer.kt
@@ -12,5 +12,9 @@ object KtxSerializer {
         // explicitNulls, defines whether null property
         // values should be included in the serialized JSON string.
         explicitNulls = false
+
+        // If API returns null or unknown values for Enums, we can use default constructor parameter to override it
+        // https://github.com/Kotlin/kotlinx.serialization/blob/master/docs/json.md#coercing-input-values
+        coerceInputValues = true
     }
 }
