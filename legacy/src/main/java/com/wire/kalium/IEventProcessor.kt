0eab7ae0f0fcaf5c5f43c3a2c602f2361b8d3b5f
Mohamad Jaara <mohamad.jaara@wire.com>
2022-01-27 12:52:56 +0100

    nuke

--- legacy/src/main/java/com/wire/kalium/IEventProcessor.kt
+++
@@ -1,8 +0,0 @@
-package com.wire.kalium
-
-import com.wire.kalium.models.backend.Payload
-import java.util.UUID
-
-interface IEventProcessor {
-    fun processEvent(eventId: UUID, payload: Payload, wireClient: IWireClient)
-}
