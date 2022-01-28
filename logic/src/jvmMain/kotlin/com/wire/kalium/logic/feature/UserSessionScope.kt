b4a410c8d1fd461b2c98fec05d1de6ca3b29696e
Jacob Persson <7156+typfel@users.noreply.github.com>
2022-01-28 16:21:35 +0100

    chore: add missing jvm implementation

--- logic/src/jvmMain/kotlin/com/wire/kalium/logic/feature/UserSessionScope.kt
+++ logic/src/jvmMain/kotlin/com/wire/kalium/logic/feature/UserSessionScope.kt
@@ -2,9 +2,12 @@ package com.wire.kalium.logic.feature
 
 import com.wire.kalium.logic.AuthenticatedDataSourceSet
 import com.wire.kalium.logic.configuration.ClientConfig
+import com.wire.kalium.persistence.db.Database
 
 actual class UserSessionScope(
     authenticatedDataSourceSet: AuthenticatedDataSourceSet
 ) : UserSessionScopeCommon(authenticatedDataSourceSet) {
     override val clientConfig: ClientConfig get() = ClientConfig()
+    override val database: Database
+        get() = Database()
 }
