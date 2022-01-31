a5ba477227220011e0ceffd0c05062918270d445
vitorhugods <vitor@schwaab.xyz>
2022-01-31 16:24:52 +0100

    refactor: [WIP] change event response format

--- network/src/commonMain/kotlin/com/wire/kalium/network/api/notification/Event.kt
+++ network/src/commonMain/kotlin/com/wire/kalium/network/api/notification/Event.kt
@@ -2,14 +2,11 @@ package com.wire.kalium.network.api.notification
 
 import com.wire.kalium.network.api.ConversationId
 import com.wire.kalium.network.api.UserId
-import com.wire.kalium.network.api.user.client.NewClientEvent
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 sealed class Event {
-    @SerialName("time")
-    abstract val time: String
 
     @Serializable
     sealed class Conversation : Event() {
@@ -21,8 +18,8 @@ sealed class Event {
         data class NewMessage(
             override val qualifiedConversation: ConversationId,
             @SerialName("qualified_from") val qualifiedFrom: UserId,
-            override val time: String,
-            @SerialName("data ") val data: MessageEventData,
+            val time: String,
+            @SerialName("data") val data: MessageEventData,
         ) : Conversation()
     }
 
@@ -31,8 +28,7 @@ sealed class Event {
 
         @SerialName("user.client-add")
         data class NewClient(
-            @SerialName("client") val client: NewClientEvent,
-            override val time: String
+            @SerialName("client") val client: NewClientEventData,
         ) : User()
     }
 }
