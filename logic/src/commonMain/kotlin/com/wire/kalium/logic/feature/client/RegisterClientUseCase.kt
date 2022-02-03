a0f33033004acf496ace330f1e77f9a57ebc2ad2
vitorhugods <vitor@schwaab.xyz>
2022-02-03 13:38:15 +0100

    fix: only return success on registration if storage succeeded

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/client/RegisterClientUseCase.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/client/RegisterClientUseCase.kt
@@ -3,33 +3,25 @@ package com.wire.kalium.logic.feature.client
 import com.wire.kalium.logic.data.client.ClientRepository
 import com.wire.kalium.logic.data.client.RegisterClientParam
 import com.wire.kalium.logic.failure.AuthenticationFailure
-import com.wire.kalium.logic.functional.Either
 import com.wire.kalium.logic.functional.suspending
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 
 
-class RegisterClientUseCase(
-    private val clientRepository: ClientRepository
-) {
-    suspend operator fun invoke(param: RegisterClientParam): Flow<RegisterClientResult> = flow {
+class RegisterClientUseCase(private val clientRepository: ClientRepository) {
+
+    suspend operator fun invoke(param: RegisterClientParam): RegisterClientResult = suspending {
         //TODO Should we fail here if the client is already registered?
-        when (val result = clientRepository.registerClient(param)) {
-            is Either.Right -> {
-                suspending {
-                    result.flatMap {
-                        clientRepository.persistClientId(it.clientId)
-                    }
-                }
-                emit(RegisterClientResult.Success(result.value))
-            }
-
-            is Either.Left -> {
-                if (result.value is AuthenticationFailure)
-                    emit(RegisterClientResult.Failure.InvalidCredentials)
-                else
-                    emit(RegisterClientResult.Failure.Generic(result.value))
+        clientRepository.registerClient(param).flatMap { client ->
+            clientRepository.persistClientId(client.clientId).map {
+                client
             }
         }
-    }
+    }.fold({ failure ->
+        if (failure is AuthenticationFailure)
+            RegisterClientResult.Failure.InvalidCredentials
+        else
+            RegisterClientResult.Failure.Generic(failure)
+    }, { client ->
+        RegisterClientResult.Success(client)
+    })!!
+
 }
