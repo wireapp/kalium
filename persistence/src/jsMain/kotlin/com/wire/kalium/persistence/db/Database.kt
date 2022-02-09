5fe26282f05c99ccfd515a94ad4262af52d3e6ae
Mohamad Jaara <mohamad.jaara@wire.com>
2022-02-09 15:24:55 +0100

    feat: fetch selfUser registered clients

--- persistence/src/jsMain/kotlin/com/wire/kalium/persistence/db/Database.kt
+++ persistence/src/jsMain/kotlin/com/wire/kalium/persistence/db/Database.kt
@@ -1,11 +1,14 @@
 package com.wire.kalium.persistence.db
 
 import com.wire.kalium.persistence.dao.ConversationDAO
+import com.wire.kalium.persistence.dao.MetadataDAO
 import com.wire.kalium.persistence.dao.UserDAO
 
 actual class Database {
     actual val userDAO: UserDAO
         get() = TODO("Not yet implemented")
     actual val conversationDAO: ConversationDAO
+        get() = TODO("Not yet implemented")
+    actual val metadataDAO: MetadataDAO
         get() = TODO("Not yet implemented")
 }
