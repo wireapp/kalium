729a66c701512868b50b7ee1e8e8f3f922b5d3a5
Vitor Hugo Schwaab <vitor@schwaab.xyz>
2022-02-14 13:54:48 +0100

    test: test splitting of requests for user details

--- network/src/commonTest/kotlin/com/wire/kalium/api/tools/json/api/user/details/UserDetailsApiTest.kt
+++ network/src/commonTest/kotlin/com/wire/kalium/api/tools/json/api/user/details/UserDetailsApiTest.kt
@@ -3,7 +3,11 @@ package com.wire.kalium.api.tools.json.api.user.details
 import com.wire.kalium.api.ApiTest
 import com.wire.kalium.api.tools.json.model.QualifiedHandleSample
 import com.wire.kalium.api.tools.json.model.QualifiedIDSamples
+import com.wire.kalium.network.api.QualifiedHandle
+import com.wire.kalium.network.api.QualifiedID
 import com.wire.kalium.network.api.user.details.ListUserRequest
+import com.wire.kalium.network.api.user.details.QualifiedHandleListRequest
+import com.wire.kalium.network.api.user.details.QualifiedUserIdListRequest
 import com.wire.kalium.network.api.user.details.UserDetailsApi
 import com.wire.kalium.network.api.user.details.UserDetailsApiImp
 import com.wire.kalium.network.api.user.details.qualifiedHandles
@@ -14,6 +18,7 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import kotlinx.serialization.encodeToString
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 @ExperimentalCoroutinesApi
 class UserDetailsApiTest : ApiTest {
@@ -39,6 +44,25 @@ class UserDetailsApiTest : ApiTest {
     }
 
     @Test
+    fun givenListOfMoreThan64QualifiedIds_whenGettingListOfUsers_thenRequestsShouldBeSplitIntoChunks() = runTest {
+        val chunksCount = 3
+        val params = getIdsList(chunksCount)
+        var callCount = 0
+        val httpClient = mockAuthenticatedHttpClient(
+            ListUsersRequestJson.validIdsJsonProvider.rawJson,
+            statusCode = HttpStatusCode.Created,
+            assertion = {
+                callCount++
+            }
+        )
+        val userDetailsApi: UserDetailsApi = UserDetailsApiImp(httpClient)
+
+        userDetailsApi.getMultipleUsers(params)
+        assertEquals(chunksCount, callCount)
+    }
+
+
+    @Test
     fun givenListOfQualifiedHandles_whenGettingListOfUsers_thenBodyShouldSerializeCorrectly() = runTest {
         val params = ListUserRequest.qualifiedHandles(listOf(QualifiedHandleSample.one, QualifiedHandleSample.two))
         val expectedRequestBody = KtxSerializer.json.encodeToString(params)
@@ -52,6 +76,24 @@ class UserDetailsApiTest : ApiTest {
         val userDetailsApi: UserDetailsApi = UserDetailsApiImp(httpClient)
 
         userDetailsApi.getMultipleUsers(params)
+    }
+
+    @Test
+    fun givenListOfMoreThan64QualifiedHandles_whenGettingListOfUsers_thenRequestsShouldBeSplitIntoChunks() = runTest {
+        val chunksCount = 3
+        val params = getHandlesList(chunksCount)
+        var callCount = 0
+        val httpClient = mockAuthenticatedHttpClient(
+            ListUsersRequestJson.validIdsJsonProvider.rawJson,
+            statusCode = HttpStatusCode.Created,
+            assertion = {
+                callCount++
+            }
+        )
+        val userDetailsApi: UserDetailsApi = UserDetailsApiImp(httpClient)
+
+        userDetailsApi.getMultipleUsers(params)
+        assertEquals(chunksCount, callCount)
     }
 
     @Test
@@ -71,7 +113,26 @@ class UserDetailsApiTest : ApiTest {
         userDetailsApi.getMultipleUsers(ListUserRequest.qualifiedIds(listOf()))
     }
 
-    private companion object{
+    private fun getIdsList(chunksCount: Int): QualifiedUserIdListRequest {
+        val idsList = mutableListOf<QualifiedID>()
+        repeat(MAX_QUERY_SIZE * chunksCount) {
+            idsList += QualifiedIDSamples.one.copy(domain = it.toString())
+        }
+        val params = ListUserRequest.qualifiedIds(idsList)
+        return params
+    }
+
+    private fun getHandlesList(chunksCount: Int): QualifiedHandleListRequest {
+        val handlesList = mutableListOf<QualifiedHandle>()
+        repeat(MAX_QUERY_SIZE * chunksCount) {
+            handlesList += QualifiedHandleSample.one.copy(domain = it.toString())
+        }
+        val params = ListUserRequest.qualifiedHandles(handlesList)
+        return params
+    }
+
+    private companion object {
         const val PATH_LIST_USERS = "/list-users"
+        const val MAX_QUERY_SIZE = 64
     }
 }
