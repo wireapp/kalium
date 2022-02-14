5a8adb6783e720e49bfa2776b8fc8464f55579b4
Mohamad Jaara <mohamad.jaara@wire.com>
2022-02-14 14:07:53 +0000

    feat: update current session after a new login (#210)
    * feat: update current session after a new login
    
    * update test
    
    * fix: typo

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/client/ClientScope.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/client/ClientScope.kt
@@ -2,7 +2,6 @@ package com.wire.kalium.logic.feature.client
 
 import com.wire.kalium.cryptography.ProteusClient
 import com.wire.kalium.logic.data.client.ClientRepository
-import com.wire.kalium.logic.data.prekey.PreKeyMapper
 
 class ClientScope(
     private val clientRepository: ClientRepository,
