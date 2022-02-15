709a09dff1c6408ecdf4fe7ef148c033c5e3a88b
Mojtaba Chenani <chenani@outlook.com>
2022-02-15 07:52:58 +0100

    refactor: move all related classes in one file

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/client/DeleteClientUseCase.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/client/DeleteClientUseCase.kt
@@ -3,13 +3,26 @@ package com.wire.kalium.logic.feature.client
 import com.wire.kalium.logic.CoreFailure
 import com.wire.kalium.logic.data.client.ClientRepository
 import com.wire.kalium.logic.data.client.DeleteClientParam
-import com.wire.kalium.logic.functional.Either
+import com.wire.kalium.logic.functional.suspending
 
 interface DeleteClientUseCase {
-    suspend operator fun invoke(param: DeleteClientParam): Either<CoreFailure, Unit>
+    suspend operator fun invoke(param: DeleteClientParam): DeleteClientResult
 }
 
 class DeleteClientUseCaseImpl(private val clientRepository: ClientRepository) : DeleteClientUseCase {
-    override suspend operator fun invoke(param: DeleteClientParam) =
+    override suspend operator fun invoke(param: DeleteClientParam): DeleteClientResult = (suspending {
         clientRepository.deleteClient(param)
+    }.fold({ failure ->
+        DeleteClientResult.Failure.Generic(failure)
+    }, {
+        DeleteClientResult.Success
+    }))!!
+}
+
+sealed class DeleteClientResult {
+    object Success : DeleteClientResult()
+
+    sealed class Failure : DeleteClientResult() {
+        class Generic(val genericFailure: CoreFailure) : Failure()
+    }
 }
