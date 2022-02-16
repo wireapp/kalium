999c06fc0377124610e11717aca1a82408470b7c
Vitor Hugo Schwaab <vitor@schwaab.xyz>
2022-02-16 03:03:41 +0100

    test: disable one test as it can't currently be written

--- logic/src/commonTest/kotlin/com/wire/kalium/logic/data/event/EventRepositoryTest.kt
+++ logic/src/commonTest/kotlin/com/wire/kalium/logic/data/event/EventRepositoryTest.kt
@@ -12,20 +12,21 @@ import io.mockative.given
 import io.mockative.mock
 import kotlinx.coroutines.test.runTest
 import kotlin.test.BeforeTest
+import kotlin.test.Ignore
 import kotlin.test.Test
 
 class EventRepositoryTest {
 
     @Mock
-    private val notificationApi: NotificationApi = mock(classOf())
+    private val notificationApi: NotificationApi = mock(classOf<NotificationApi>())
 
     @Mock
-    private val eventInfoStorage: EventInfoStorage = configure(mock(classOf())){
+    private val eventInfoStorage: EventInfoStorage = configure(mock(classOf<EventInfoStorage>())) {
         stubsUnitByDefault = true
     }
 
     @Mock
-    private val clientRepository: ClientRepository = mock(classOf())
+    private val clientRepository: ClientRepository = mock(classOf<ClientRepository>())
 
     @Mock
     private val eventMapper: EventMapper = EventMapper(IdMapperImpl())
@@ -37,6 +38,7 @@ class EventRepositoryTest {
         eventRepository = EventDataSource(notificationApi, eventInfoStorage, clientRepository, eventMapper)
     }
 
+    @Ignore
     @Test
     fun givenNoEventWasProcessedBefore_whenGettingEvents_thenGetAllNotificationsIsCalled() = runTest {
         given(eventInfoStorage)
@@ -44,14 +46,14 @@ class EventRepositoryTest {
             .whenInvoked()
             .then { null }
 
-        given(notificationApi)
-            .suspendFunction(notificationApi::getAllNotifications)
-            .whenInvokedWith(any(), any())
+//        given(notificationApi)
+//            .suspendFunction(notificationApi::getAllNotifications)
+//            .whenInvokedWith(any(), any())
 //            .then {
 ////                NetworkResponse.Success() TODO: Can't mock Network Response!
 //            }
 
-        eventRepository.events()
+//        eventRepository.events()
 
     }
 }
