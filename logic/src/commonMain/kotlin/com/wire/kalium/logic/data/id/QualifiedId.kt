3404c6bf00c5112bfed41a0267ac2666bf659dd0
Gonzalo Gran Crespo <gongracr@gmail.com>
2022-02-21 22:25:40 +0100

    fix: added serialization/deserialization methods on QualifiedID class

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/data/id/QualifiedId.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/data/id/QualifiedId.kt
@@ -1,8 +1,16 @@
 package com.wire.kalium.logic.data.id
 
+import com.wire.kalium.network.tools.KtxSerializer
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+
+@Serializable
 data class QualifiedID(
     val value: String,
     val domain: String
-)
-
-typealias ConversationId = QualifiedID
+) {
+    fun toJson(): String = Json.encodeToString(this)
+    fun fromJson(jsonString: String): QualifiedID = KtxSerializer.json.decodeFromString(jsonString)
+}
