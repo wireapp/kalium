7de51037a47c77ff3cbe39b754c7920bfc12075e
Mohamad Jaara <mohamad.jaara@wire.com>
2022-02-23 14:49:25 +0100

    using wrapCryptoRequest in ConversationEventReceiver.kt

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/sync/ConversationEventReceiver.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/sync/ConversationEventReceiver.kt
@@ -4,13 +4,14 @@ import com.wire.kalium.cryptography.CryptoClientId
 import com.wire.kalium.cryptography.CryptoSessionId
 import com.wire.kalium.cryptography.ProteusClient
 import com.wire.kalium.cryptography.UserId
-import com.wire.kalium.cryptography.exceptions.ProteusException
 import com.wire.kalium.logic.data.event.Event
 import com.wire.kalium.logic.data.message.Message
 import com.wire.kalium.logic.data.message.MessageRepository
 import com.wire.kalium.logic.data.message.PlainMessageBlob
 import com.wire.kalium.logic.data.message.ProtoContentMapper
+import com.wire.kalium.logic.functional.suspending
 import com.wire.kalium.logic.util.Base64
+import com.wire.kalium.logic.wrapCryptoRequest
 import io.ktor.utils.io.core.toByteArray
 
 class ConversationEventReceiver(
@@ -29,27 +30,27 @@ class ConversationEventReceiver(
 
         //TODO Use domain when creating CryptoSession too
         val cryptoSessionId = CryptoSessionId(UserId(event.senderUserId.value), CryptoClientId(event.senderClientId.value))
-        val decryptedContentBytes = try {
-            proteusClient.decrypt(decodedContentBytes, cryptoSessionId)
-        } catch (e: ProteusException) {
-            e.printStackTrace()
-            //TODO: Insert a failed message into the database to notify user that encryption is kaputt
-            return
+        suspending {
+            wrapCryptoRequest { proteusClient.decrypt(decodedContentBytes, cryptoSessionId) }.map { PlainMessageBlob(it) }
+
+                .onFailure {
+                    //TODO: Insert a failed message into the database to notify user that encryption is kaputt
+                    it.proteusException.printStackTrace()
+                }.onSuccess { plainMessageBlob ->
+                    val protoContent = protoContentMapper.decodeFromProtobuf(plainMessageBlob)
+                    val message = Message(
+                        protoContent.messageUid,
+                        protoContent.messageContent,
+                        event.conversationId,
+                        event.time,
+                        event.senderUserId,
+                        event.senderClientId,
+                        Message.Status.SENT
+                    )
+                    //TODO Multiplatform logging
+                    println("Message received: $message")
+                    messageRepository.persistMessage(message)
+                }
         }
-        val protoContent = protoContentMapper.decodeFromProtobuf(PlainMessageBlob(decryptedContentBytes))
-
-        val message = Message(
-            protoContent.messageUid,
-            protoContent.messageContent,
-            event.conversationId,
-            event.time,
-            event.senderUserId,
-            event.senderClientId,
-            Message.Status.SENT
-        )
-
-        //TODO Multiplatform logging
-        println("Message received: $message")
-        messageRepository.persistMessage(message)
     }
 }
