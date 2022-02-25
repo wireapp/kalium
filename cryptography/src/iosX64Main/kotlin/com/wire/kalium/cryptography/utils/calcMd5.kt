a66ed6232715dcdd49b3bf11da0ae03b2cc0eefa
Jacob Persson <7156+typfel@users.noreply.github.com>
2022-02-25 13:33:32 +0100

    feat: add calcMd5 implementation for the iOS target

--- cryptography/src/iosX64Main/kotlin/com/wire/kalium/cryptography/utils/calcMd5.kt
+++ cryptography/src/iosX64Main/kotlin/com/wire/kalium/cryptography/utils/calcMd5.kt
@@ -1,5 +1,22 @@
 package com.wire.kalium.cryptography.utils
 
+import kotlinx.cinterop.allocArrayOf
+import kotlinx.cinterop.memScoped
+import kotlinx.cinterop.refTo
+import platform.CoreCrypto.CC_MD5
+import platform.CoreCrypto.CC_MD5_DIGEST_LENGTH
+import platform.Foundation.NSData
+import platform.Foundation.base64Encoding
+import platform.Foundation.create
+
 actual fun calcMd5(bytes: ByteArray): String {
-    TODO("Not yet implemented")
+    val digestData = UByteArray(CC_MD5_DIGEST_LENGTH)
+    val data = toData(bytes)
+    CC_MD5(data.bytes, data.length.toUInt(), digestData.refTo(0))
+
+    return toData(digestData.asByteArray()).base64Encoding()
+}
+
+private fun toData(data: ByteArray): NSData = memScoped {
+    NSData.create(bytes = allocArrayOf(data), length = data.size.toULong())
 }
