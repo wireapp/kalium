6a0f970af8dfd1d2f74e7f1a06e2ebb40cebbd71
Mohamad Jaara <mohamad.jaara@wire.com>
2022-03-01 12:01:39 +0100

    address PR comments

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/message/MessageSender.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/message/MessageSender.kt
@@ -7,6 +7,7 @@ import com.wire.kalium.logic.data.message.MessageEnvelope
 import com.wire.kalium.logic.data.message.MessageRepository
 import com.wire.kalium.logic.failure.SendMessageFailure
 import com.wire.kalium.logic.functional.Either
+import com.wire.kalium.logic.functional.isRight
 import com.wire.kalium.logic.functional.suspending
 import com.wire.kalium.logic.sync.SyncManager
 
@@ -52,19 +53,20 @@ class MessageSenderImpl(
         envelope: MessageEnvelope,
         messageUuid: String,
     ): Either<CoreFailure, Unit> = suspending {
-        messageRepository.sendEnvelope(conversationId, envelope).coFold(
-            { failure ->
-                when (failure) {
-                    is SendMessageFailure.ClientsHaveChanged -> {
-                        messageSendFailureHandler.handleClientsHaveChangedFailure(failure).flatMap {
-                            getRecipientsAndAttemptSend(conversationId, messageUuid)
-                        }
-                    }
-                    SendMessageFailure.NoNetworkConnection -> Either.Left(CoreFailure.NoNetworkConnection)
-                    else -> Either.Left(failure)
+        val sendResult = messageRepository.sendEnvelope(conversationId, envelope)
+
+        if (sendResult.isRight()) {
+            return@suspending Either.Right(Unit)
+        }
+
+        when (val failure = sendResult.value) {
+            is SendMessageFailure.ClientsHaveChanged -> {
+                messageSendFailureHandler.handleClientsHaveChangedFailure(failure).flatMap {
+                    getRecipientsAndAttemptSend(conversationId, messageUuid)
                 }
-            }, {
-                Either.Right(Unit)
-            })
+            }
+            SendMessageFailure.NoNetworkConnection -> Either.Left(CoreFailure.NoNetworkConnection)
+            else -> Either.Left(failure)
+        }
     }
 }
