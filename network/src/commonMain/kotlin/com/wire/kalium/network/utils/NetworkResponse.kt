7be01fe6d116f5a0f39834063d8300cfbebf04bb
Mohamad Jaara <mohamad.jaara@wire.com>
2022-03-01 16:58:04 +0100

    fix: login failing

--- network/src/commonMain/kotlin/com/wire/kalium/network/utils/NetworkResponse.kt
+++ network/src/commonMain/kotlin/com/wire/kalium/network/utils/NetworkResponse.kt
@@ -16,13 +16,16 @@ sealed class NetworkResponse<out T : Any> {
     ) : NetworkResponse<T>() {
         internal constructor(value: T, httpResponse: HttpResponse) : this(
             value,
+            // small issue here where keys are converted to small case letters
+            // this is an issue for ktor to solve
             httpResponse.headers.toMap()
                 .mapValues { headerEntry -> headerEntry.value.firstOrNull() }, // Ignore header duplication on purpose
             httpResponse.status.value
         )
 
         val cookies: Map<String, String> by lazy {
-            this.headers[HttpHeaders.SetCookie]?.splitSetCookieHeader()?.flatMap { it.splitSetCookieHeader() }
+            // don't use HttpHeaders.SetCookie until the case sensitivity issue is solved
+            this.headers["set-cookie"]?.splitSetCookieHeader()?.flatMap { it.splitSetCookieHeader() }
                 ?.map { parseServerSetCookieHeader(it) }?.associate {
                     it.name to it.value
                 } ?: mapOf()
