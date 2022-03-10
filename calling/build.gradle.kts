4b66c415f5fa50079603c08a5c802befdcfb7378
Oussama Hassine <oussama.has100@gmail.com>
2022-03-10 18:34:04 +0100

    feat: create calling module [AR-1245] (#259)

---
+++ calling/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    Plugins.androidLibrary(this)
+    Plugins.multiplatform(this)
+}
+
+group = "com.wire.kalium"
+version = "0.0.1-SNAPSHOT"
+
+android {
+    compileSdk = Android.Sdk.compile
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    defaultConfig {
+        minSdk = Android.Sdk.min
+        targetSdk = Android.Sdk.target
+        consumerProguardFiles("consumer-proguard-rules.pro")
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+kotlin {
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "1.8"
+            kotlinOptions.freeCompilerArgs += "-Xopt-in=kotlin.RequiresOptIn"
+        }
+        testRuns["test"].executionTask.configure {
+            useJUnit()
+        }
+    }
+    android()
+
+    sourceSets {
+        val androidMain by getting {
+            dependencies {
+
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+    }
+}
+
