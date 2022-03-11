6a15c772e4ae636392c25f009b42a9be52714f96
Mojtaba Chenani <chenani@outlook.com>
2022-03-11 15:57:03 +0100

    chore: handle add/delete members to/from conversations events

--- network/src/commonMain/kotlin/com/wire/kalium/network/api/notification/EventContentDTO.kt
+++ network/src/commonMain/kotlin/com/wire/kalium/network/api/notification/EventContentDTO.kt
@@ -2,6 +2,8 @@ package com.wire.kalium.network.api.notification
 
 import com.wire.kalium.network.api.ConversationId
 import com.wire.kalium.network.api.UserId
+import com.wire.kalium.network.api.conversation.ConversationMembers
+import com.wire.kalium.network.api.conversation.ConversationUsers
 import com.wire.kalium.network.api.notification.conversation.MessageEventData
 import com.wire.kalium.network.api.notification.user.NewClientEventData
 import kotlinx.serialization.SerialName
@@ -28,6 +30,28 @@ sealed class EventContentDTO {
             @SerialName("qualified_from") val qualifiedFrom: UserId,
             val time: String,
             @SerialName("data") val data: MessageEventData,
+        ) : Conversation()
+
+        @Serializable
+        @SerialName("conversation.member-join")
+        data class MemberJoinDTO(
+            @SerialName("qualified_conversation")
+            val qualifiedConversation: ConversationId,
+            @SerialName("qualified_from") val qualifiedFrom: UserId,
+            val time: String,
+            @SerialName("data") val members: ConversationMembers,
+            @SerialName("from") val from: String
+        ) : Conversation()
+
+        @Serializable
+        @SerialName("conversation.member-leave")
+        data class MemberLeaveDTO(
+            @SerialName("qualified_conversation")
+            val qualifiedConversation: ConversationId,
+            @SerialName("qualified_from") val qualifiedFrom: UserId,
+            val time: String,
+            @SerialName("data") val members: ConversationUsers,
+            @SerialName("from") val from: String
         ) : Conversation()
     }
 
