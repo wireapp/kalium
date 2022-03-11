553ebd966ed57719e42ea8ea4eb554266762b07f
Jacob Persson <7156+typfel@users.noreply.github.com>
2022-03-11 14:34:16 +0100

    feat: add basic CLI application for sending and receiving messages

---
+++ cli/src/main/kotlin/com/wire/kalium/cli/SendReceiveApplication.kt
@@ -0,0 +1,90 @@
+package com.wire.kalium.cli
+
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.required
+import com.wire.kalium.logic.CoreLogic
+import com.wire.kalium.logic.configuration.ServerConfig
+import com.wire.kalium.logic.data.message.MessageContent
+import com.wire.kalium.logic.feature.auth.AuthSession
+import com.wire.kalium.logic.feature.auth.AuthenticationResult
+import com.wire.kalium.logic.feature.client.RegisterClientResult
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+class SendReceiveApplication : CliktCommand() {
+
+    private val email: String by option(help = "Wire account email").required()
+    private val password: String by option(help = "Wire account password").required()
+    private val environment: String? by option(help = "Choose backend environment: can be production or staging")
+    private val coreLogic = CoreLogic("Kalium CLI", ".proteus")
+
+    private val serverConfig: ServerConfig by lazy {
+        if (environment == "production") {
+            ServerConfig.PRODUCTION
+        } else {
+            ServerConfig.DEFAULT
+        }
+    }
+
+    override fun run() = runBlocking {
+        val authSession = login(email, password)
+        val userSession = coreLogic.getSessionScope(authSession)
+
+        when (userSession.client.register(password, emptyList())) {
+            is RegisterClientResult.Failure -> throw RuntimeException("Client registration failed")
+            is RegisterClientResult.Success -> Unit
+        }
+
+        val conversations = userSession.conversations.getConversations().first()
+
+        conversations.forEachIndexed { index, conversation ->
+            println("$index) ${conversation.id.value}  Name: ${conversation.name}")
+        }
+
+        print("Enter conversation index: ")
+        val conversationIndex = readLine()!!.toInt()
+        val conversationID = conversations.get(conversationIndex).id
+
+        launch(Dispatchers.Default) {
+            userSession.listenToEvents()
+        }
+
+        launch(Dispatchers.Default) {
+            userSession.messages.getRecentMessages(conversationID, limit = 1).collect {
+                for (message in it) {
+                    val content = message.content
+
+                    when (content) {
+                        is MessageContent.Text -> println("> ${content.value}")
+                        else -> println("<unknown message>")
+                    }
+                }
+            }
+        }
+
+        while (true) {
+            val message = readLine()!!
+            userSession.messages.sendTextMessage(conversationID, message)
+        }
+    }
+
+    private suspend fun login(username: String, password: String): AuthSession {
+        val result = coreLogic.getAuthenticationScope().login(username, password, false, serverConfig)
+
+        if (result !is AuthenticationResult.Success) {
+            throw RuntimeException(
+                "There was an error on the login :(" +
+                        "Check the credentials and the internet connection and try again"
+            )
+        }
+
+        return result.userSession
+    }
+
+}
+
+fun main(args: Array<String>) = SendReceiveApplication().main(args)
