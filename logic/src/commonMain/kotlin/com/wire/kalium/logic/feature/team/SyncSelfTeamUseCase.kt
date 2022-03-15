3d721e7320ceb17128592b1db073b65dd480ba22
alexandreferris <ferris.alexandre@gmail.com>
2022-03-15 14:45:23 +0100

    feat: creation of SyncSelfTeamUseCase and TeamScope to be used in slow sync worker

---
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/team/SyncSelfTeamUseCase.kt
@@ -0,0 +1,22 @@
+package com.wire.kalium.logic.feature.team
+
+import com.wire.kalium.logic.CoreFailure
+import com.wire.kalium.logic.data.team.TeamRepository
+import com.wire.kalium.logic.data.user.UserRepository
+import com.wire.kalium.logic.functional.Either
+import kotlinx.coroutines.flow.first
+
+interface SyncSelfTeamUseCase {
+    suspend operator fun invoke(): Either<CoreFailure, Unit>
+}
+
+class SyncSelfTeamUseCaseImpl(
+    private val userRepository: UserRepository,
+    private val teamRepository: TeamRepository
+) : SyncSelfTeamUseCase {
+
+    override suspend fun invoke(): Either<CoreFailure, Unit> =
+        userRepository.getSelfUser().first().team?.let { teamId ->
+            teamRepository.fetchTeamById(teamId = teamId)
+        } ?: Either.Right(Unit)
+}
