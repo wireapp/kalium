c59934da1cf607022f3d6de17229fb7be1569a4e
Mohamad Jaara <mohamad.jaara@wire.com>
2022-03-15 14:07:57 +0100

    TeamDTO.kt

--- network/src/commonMain/kotlin/com/wire/kalium/network/api/teams/TeamsApi.kt
+++ network/src/commonMain/kotlin/com/wire/kalium/network/api/teams/TeamsApi.kt
@@ -1,9 +1,9 @@
 package com.wire.kalium.network.api.teams
 
-import com.wire.kalium.network.api.AssetId
 import com.wire.kalium.network.api.NonQualifiedConversationId
-import com.wire.kalium.network.api.TeamId
 import com.wire.kalium.network.api.NonQualifiedUserId
+import com.wire.kalium.network.api.TeamId
+import com.wire.kalium.network.api.model.TeamDTO
 import com.wire.kalium.network.api.user.LegalHoldStatusResponse
 import com.wire.kalium.network.utils.NetworkResponse
 import kotlinx.serialization.SerialName
@@ -13,26 +13,15 @@ interface TeamsApi {
 
     @Serializable
     data class TeamsResponse(
-        @SerialName("has_more") val hasMore: Boolean,
-        val teams: List<Team>
+        @SerialName("has_more") val hasMore: Boolean, val convTeamInfos: List<TeamDTO>
     )
 
-    @Serializable
-    data class Team(
-        val creator: String,
-        val icon: AssetId,
-        val name: String,
-        val id: TeamId,
-        @SerialName("icon_key") val iconKey: AssetId?,
-        val binding: Boolean
-    )
 
     @Serializable
     data class TeamMemberList(
         // Please note that this is intentionally cased differently form the has_more in TeamsResponse
         // because the backend response contains a different casing
-        @SerialName("hasMore") val hasMore: Boolean,
-        val members: List<TeamMember>
+        @SerialName("hasMore") val hasMore: Boolean, val members: List<TeamMember>
     )
 
     @Serializable
@@ -46,8 +35,7 @@ interface TeamsApi {
 
     @Serializable
     data class Permissions(
-        val copy: Int,
-        @SerialName("self") val own: Int
+        val copy: Int, @SerialName("self") val own: Int
     )
 
     sealed interface GetTeamsOptionsInterface
@@ -85,5 +73,5 @@ interface TeamsApi {
      */
     suspend fun getTeams(size: Int?, option: GetTeamsOption?): NetworkResponse<TeamsResponse>
     suspend fun getTeamMembers(teamId: TeamId, limitTo: Int?): NetworkResponse<TeamMemberList>
-    suspend fun getTeamInfo(teamId: TeamId): NetworkResponse<Team>
+    suspend fun getTeamInfo(teamId: TeamId): NetworkResponse<TeamDTO>
 }
