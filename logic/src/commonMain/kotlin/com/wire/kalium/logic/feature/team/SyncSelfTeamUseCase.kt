5b84d2dbf19337d3e771eae4fcee4ca20e9ca50f
alexandreferris <ferris.alexandre@gmail.com>
2022-03-15 17:47:36 +0100

    feat: add logger to when team sync is skipped due to user not having a team

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/team/SyncSelfTeamUseCase.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/team/SyncSelfTeamUseCase.kt
@@ -4,6 +4,7 @@ import com.wire.kalium.logic.CoreFailure
 import com.wire.kalium.logic.data.team.TeamRepository
 import com.wire.kalium.logic.data.user.UserRepository
 import com.wire.kalium.logic.functional.Either
+import com.wire.kalium.logic.kaliumLogger
 import kotlinx.coroutines.flow.first
 
 internal interface SyncSelfTeamUseCase {
@@ -15,14 +16,18 @@ internal class SyncSelfTeamUseCaseImpl(
     private val teamRepository: TeamRepository
 ) : SyncSelfTeamUseCase {
 
-    override suspend fun invoke(): Either<CoreFailure, Unit> =
-        userRepository.getSelfUser().first().let { user ->
-            user.team?.let { teamId ->
-                teamRepository.fetchTeamById(teamId = teamId)
-                teamRepository.fetchMembersByTeamId(
-                    teamId = teamId,
-                    userDomain = user.id.domain
-                )
-            }
-        } ?: Either.Right(Unit)
+    override suspend fun invoke(): Either<CoreFailure, Unit> {
+        val user = userRepository.getSelfUser().first()
+
+        return user.team?.let { teamId ->
+            teamRepository.fetchTeamById(teamId = teamId)
+            teamRepository.fetchMembersByTeamId(
+                teamId = teamId,
+                userDomain = user.id.domain
+            )
+        } ?: run {
+            kaliumLogger.i("Skipping team sync because user doesn't belong to a team")
+            Either.Right(Unit)
+        }
+    }
 }
