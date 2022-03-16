8867dafe68df281afe12f433a4900b6e7c60d25b
Vitor Hugo Schwaab <vitor@schwaab.xyz>
2022-03-16 18:30:13 +0100

    chore(Android): Add missing type adapter to DB initialization

--- persistence/src/androidMain/kotlin/com/wire/kalium/persistence/db/Database.kt
+++ persistence/src/androidMain/kotlin/com/wire/kalium/persistence/db/Database.kt
@@ -52,7 +52,7 @@ actual class Database(private val context: Context, private val name: String, ka
         database = AppDatabase(
             driver,
             Client.Adapter(user_idAdapter = QualifiedIDAdapter()),
-            Conversation.Adapter(qualified_idAdapter = QualifiedIDAdapter()),
+            Conversation.Adapter(qualified_idAdapter = QualifiedIDAdapter(), typeAdapter = EnumColumnAdapter()),
             Member.Adapter(userAdapter = QualifiedIDAdapter(), conversationAdapter = QualifiedIDAdapter()),
             Message.Adapter(
                 conversation_idAdapter = QualifiedIDAdapter(),
