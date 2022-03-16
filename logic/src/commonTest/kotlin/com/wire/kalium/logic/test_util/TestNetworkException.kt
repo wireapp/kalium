44b9ba07f25fa5e7d94f389013de0d904e8cf18f
Mohamad Jaara <mohamad.jaara@wire.com>
2022-03-16 21:53:54 +0100

    SetUserHandleUseCaseTest

--- logic/src/commonTest/kotlin/com/wire/kalium/logic/test_util/TestNetworkException.kt
+++ logic/src/commonTest/kotlin/com/wire/kalium/logic/test_util/TestNetworkException.kt
@@ -6,34 +6,26 @@ import com.wire.kalium.network.exceptions.KaliumException
 object TestNetworkException {
 
     val generic = KaliumException.InvalidRequestError(
-        ErrorResponse(
-            400,
-            message = "generic test error",
-            label = "generic-test-error"
-        )
+        ErrorResponse(400, message = "generic test error", label = "generic-test-error")
     )
 
     val tooManyClient = KaliumException.InvalidRequestError(
-        ErrorResponse(
-            403,
-            message = "too many clients",
-            label = "too-many-clients"
-        )
+        ErrorResponse(403, message = "too many clients", label = "too-many-clients")
     )
 
     val missingAuth = KaliumException.InvalidRequestError(
-        ErrorResponse(
-            403,
-            message = "missing auth",
-            label = "missing-auth"
-        )
+        ErrorResponse(403, message = "missing auth", label = "missing-auth")
     )
 
     val invalidCredentials = KaliumException.InvalidRequestError(
-        ErrorResponse(
-            403,
-            message = "invalid credentials",
-            label = "invalid-credentials"
-        )
+        ErrorResponse(403, message = "invalid credentials", label = "invalid-credentials")
+    )
+
+    val invalidHandle = KaliumException.InvalidRequestError(
+        ErrorResponse(400, message = "invalid handle", label = "invalid-handle")
+    )
+
+    val handleExists = KaliumException.InvalidRequestError(
+        ErrorResponse(409, message = "handle exists", label = "handle-exists")
     )
 }
