04ff7c4e632d5cb71cdc6d70d325ad518a7ed520
Mohamad Jaara <mohamad.jaara@wire.com>
2022-03-16 14:32:10 +0100

    handle register specific errors

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/register/RegisterAccountUseCase.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/register/RegisterAccountUseCase.kt
@@ -6,6 +6,14 @@ import com.wire.kalium.logic.data.register.RegisterAccountRepository
 import com.wire.kalium.logic.data.session.SessionRepository
 import com.wire.kalium.logic.data.user.SelfUser
 import com.wire.kalium.logic.feature.auth.AuthSession
+import com.wire.kalium.network.exceptions.KaliumException
+import com.wire.kalium.network.exceptions.isBlackListedEmail
+import com.wire.kalium.network.exceptions.isDomainBlockedForRegistration
+import com.wire.kalium.network.exceptions.isInvalidCode
+import com.wire.kalium.network.exceptions.isInvalidEmail
+import com.wire.kalium.network.exceptions.isKeyExists
+import com.wire.kalium.network.exceptions.isTooMAnyMembers
+import com.wire.kalium.network.exceptions.isUserCreationRestricted
 
 sealed class RegisterParam(
     firstName: String,
@@ -41,18 +49,46 @@ class RegisterAccountUseCase(
             }
         }.fold(
             {
-                RegisterResult.Failure.Generic(it)
+                if (it is NetworkFailure.ServerMiscommunication && it.kaliumException is KaliumException.InvalidRequestError) {
+                    handleSpecialErrors(it.kaliumException)
+                } else {
+                    RegisterResult.Failure.Generic(it)
+                }
             }, {
                 sessionRepository.storeSession(it.second)
                 sessionRepository.updateCurrentSession(it.second.userId)
                 RegisterResult.Success(it)
             })
     }
+
+    private fun handleSpecialErrors(error: KaliumException.InvalidRequestError) =
+        with(error) {
+            when {
+                isInvalidEmail() -> RegisterResult.Failure.InvalidEmail
+                isInvalidCode() -> RegisterResult.Failure.InvalidActivationCode
+                isKeyExists() -> RegisterResult.Failure.AccountAlreadyExists
+                isBlackListedEmail() -> RegisterResult.Failure.BlackListed
+                isUserCreationRestricted() -> RegisterResult.Failure.UserCreationRestricted
+                isTooMAnyMembers() -> RegisterResult.Failure.TeamMaxMembers
+                isDomainBlockedForRegistration() -> RegisterResult.Failure.EmailDomainBlocked
+                else -> RegisterResult.Failure.Generic(NetworkFailure.ServerMiscommunication(this))
+            }
+        }
+
+
 }
+
 
 sealed class RegisterResult {
     class Success(val value: Pair<SelfUser, AuthSession>) : RegisterResult()
     sealed class Failure : RegisterResult() {
-        class Generic(val failure: NetworkFailure) : RegisterResult()
+        object EmailDomainBlocked : Failure()
+        object AccountAlreadyExists : Failure()
+        object InvalidActivationCode : Failure()
+        object UserCreationRestricted : Failure()
+        object TeamMaxMembers : Failure()
+        object BlackListed : Failure()
+        object InvalidEmail : Failure()
+        class Generic(val failure: NetworkFailure) : Failure()
     }
 }
