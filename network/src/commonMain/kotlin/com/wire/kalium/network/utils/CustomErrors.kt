57fe67d9dbf4716ac369234c7272edcbf0d9c2c3
Mohamad Jaara <mohamad.jaara@wire.com>
2022-03-18 11:08:32 +0100

    extract error label

--- network/src/commonMain/kotlin/com/wire/kalium/network/utils/CustomErrors.kt
+++ network/src/commonMain/kotlin/com/wire/kalium/network/utils/CustomErrors.kt
@@ -2,8 +2,17 @@ package com.wire.kalium.network.utils
 
 import com.wire.kalium.network.api.ErrorResponse
 import com.wire.kalium.network.exceptions.KaliumException
+import com.wire.kalium.network.exceptions.NetworkErrorLabel
 
 object CustomErrors {
     val MISSING_REFRESH_TOKEN =
-        NetworkResponse.Error(KaliumException.ServerError(ErrorResponse(500, "no cookie was found", "missing-refreshToken")))
+        NetworkResponse.Error(
+            KaliumException.ServerError(
+                ErrorResponse(
+                    500,
+                    "no cookie was found",
+                    NetworkErrorLabel.KaliumCustom.MISSING_REFRESH_TOKEN
+                )
+            )
+        )
 }
