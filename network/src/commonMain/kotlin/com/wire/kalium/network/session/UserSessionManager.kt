b0edc5f57dc35d0727e3688c1c6006ceb37e563b
Mohamad Jaara <mohamad.jaara@wire.com>
2022-03-21 16:34:25 +0100

    onSessionExpiry

--- network/src/commonMain/kotlin/com/wire/kalium/network/session/UserSessionManager.kt
+++ network/src/commonMain/kotlin/com/wire/kalium/network/session/UserSessionManager.kt
@@ -14,6 +14,7 @@ import io.ktor.client.plugins.auth.providers.bearer
 interface UserSessionManager {
     fun userConfig(): Pair<SessionDTO, BackendConfig>
     fun updateSession(accessToken: AccessTokenDTO, refreshTokenDTO: RefreshTokenDTO?): SessionDTO
+    fun onSessionExpiry()
 }
 
 
