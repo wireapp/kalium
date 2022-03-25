2bb8420fdac303738bb23443d30b9f01c004f4e9
alexandreferris <ferris.alexandre@gmail.com>
2022-03-25 10:36:40 +0100

    fix: resolve client id and user id blocking

--- logic/src/androidMain/kotlin/com/wire/kalium/logic/feature/call/CallManager.kt
+++ logic/src/androidMain/kotlin/com/wire/kalium/logic/feature/call/CallManager.kt
@@ -16,128 +16,115 @@ import com.wire.kalium.logic.data.user.UserId
 import com.wire.kalium.logic.data.user.UserRepository
 import com.wire.kalium.logic.kaliumLogger
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 
 actual class CallManager(
     private val calling: Calling,
     private val callRepository: CallRepository,
     private val userRepository: UserRepository,
     private val clientRepository: ClientRepository
-): CallConfigRequestHandler {
+) : CallConfigRequestHandler {
 
-    private val handle: Handle
     private val job = SupervisorJob() // TODO clear job method
     private val scope = CoroutineScope(job + Dispatchers.IO)
+    private val deferredHandle: Deferred<Handle>
 
-    private val clientId by lazy {
-        runBlocking {
-            clientRepository.currentClientId().fold({
-               TODO("adjust correct variable calling")
-            }, {
-                it
-            })
-        }
+    private val clientId: Deferred<ClientId> = scope.async(start = CoroutineStart.LAZY) {
+        clientRepository.currentClientId().fold({
+            TODO("adjust correct variable calling")
+        }, {
+            kaliumLogger.d("CallManager - clientId $it")
+            it
+        })
     }
-    private val userId by lazy {
-        runBlocking {
-            userRepository.getSelfUser().first().id
+    private val userId: Deferred<UserId> = scope.async(start = CoroutineStart.LAZY) {
+        userRepository.getSelfUser().first().id.also {
+            kaliumLogger.d("CallManager - userId $it")
         }
     }
 
     init {
-         handle = startHandle()
+        deferredHandle = startHandleAsync()
     }
 
-    private fun startHandle() = calling.wcall_create(
-        userId = userId.asString(),
-        clientId = clientId.value,
-        readyHandler = { version: Int, arg: Pointer? ->
-            kaliumLogger.d("WCALL_CREATE -> readyHandler")
-        },
-        sendHandler = { context: Pointer?, conversationId: String, userIdSelf: String, clientIdSelf: String, userIdDestination: String?,
-                        clientIdDestination: String?, data: Pointer?, length: Size_t, isTransient: Boolean, arg: Pointer? ->
-            kaliumLogger.d("WCALL_CREATE -> sendHandler")
-            0
-        },
-        sftRequestHandler = { ctx: Pointer?, url: String, data: Pointer?, length: Size_t, arg: Pointer? ->
-            kaliumLogger.d("WCALL_CREATE -> sftRequestHandler")
-            0
-        },
-        incomingCallHandler = { conversationId: String,
-                                messageTime: Uint32_t,
-                                userId: String,
-                                clientId: String,
-                                isVideoCall: Boolean,
-                                shouldRing: Boolean,
-                                conversationType: Int,
-                                arg: Pointer? ->
-            kaliumLogger.d("WCALL_CREATE -> incomingCallHandler")
-        },
-        missedCallHandler = { conversationId: String,
-                              messageTime: Uint32_t,
-                              userId: String,
-                              isVideoCall: Boolean,
-                              arg: Pointer? ->
-            kaliumLogger.d("WCALL_CREATE -> missedCallHandler")
-        },
-        answeredCallHandler = { conversationId: String, arg: Pointer? ->
-            kaliumLogger.d("WCALL_CREATE -> answeredCallHandler")
-        },
-        establishedCallHandler = { conversationId: String, userId: String, clientId: String, arg: Pointer? ->
-            kaliumLogger.d("WCALL_CREATE -> establishedCallHandler")
-        },
-        closeCallHandler = { reason: Int,
-                             conversationId: String,
-                             messageTime: Uint32_t,
-                             userId: String,
-                             clientId: String,
-                             arg: Pointer? ->
-            kaliumLogger.d("WCALL_CREATE -> closeCallHandler")
-        },
-        metricsHandler = { conversationId: String, metricsJson: String, arg: Pointer? ->
-            kaliumLogger.d("WCALL_CREATE -> metricsHandler")
-        },
-        callConfigRequestHandler = this,
-        constantBitRateStateChangeHandler = { userId: String,
-                                              clientId: String,
-                                              isEnabled: Boolean,
-                                              arg: Pointer? ->
-            kaliumLogger.d("WCALL_CREATE -> constantBitRateStateChangeHandler")
-        },
-        videoReceiveStateHandler = { conversationId: String,
-                                     userId: String,
-                                     clientId: String,
-                                     state: Int,
-                                     arg: Pointer? ->
-            kaliumLogger.d("WCALL_CREATE -> videoReceiveStateHandler")
-        },
-        arg = null
-    )
-
-    actual suspend fun onCallingMessageReceived(message: Message, content: MessageContent.Calling) {
-        kaliumLogger.d("Arrived in onCallingMessageReceived()")
-        val msg = content.value.toByteArray()
-        calling.wcall_recv_msg(
-            inst = handle,
-            msg = msg,
-            len = msg.size,
-            curr_time = Uint32_t(value = System.currentTimeMillis() / 1000),
-            msg_time = Uint32_t(value = (System.currentTimeMillis() / 1000) + 10), // TODO: add correct variable
-            convId = message.conversationId.asString(),
-            userId = message.senderUserId.asString(),
-            clientId = message.senderClientId.value
-        )
-        kaliumLogger.d("Passed through onCallingMessageReceived()")
+    private fun startHandleAsync() = scope.async(start = CoroutineStart.LAZY) {
+        calling.wcall_create(
+            userId = userId.await().asString(),
+            clientId = clientId.await().value,
+            readyHandler = { version: Int, arg: Pointer? ->
+                kaliumLogger.i("startHandleAsync -> readyHandler")
+            },
+            sendHandler = { context: Pointer?, conversationId: String, userIdSelf: String, clientIdSelf: String, userIdDestination: String?,
+                            clientIdDestination: String?, data: Pointer?, length: Size_t, isTransient: Boolean, arg: Pointer? ->
+                kaliumLogger.i("startHandleAsync -> sendHandler")
+                0
+            },
+            sftRequestHandler = { ctx: Pointer?, url: String, data: Pointer?, length: Size_t, arg: Pointer? ->
+                kaliumLogger.i("startHandleAsync -> sftRequestHandler")
+                0
+            },
+            incomingCallHandler = { conversationId: String, messageTime: Uint32_t, userId: String, clientId: String, isVideoCall: Boolean,
+                                    shouldRing: Boolean, conversationType: Int, arg: Pointer? ->
+                kaliumLogger.i("startHandleAsync -> incomingCallHandler")
+            },
+            missedCallHandler = { conversationId: String, messageTime: Uint32_t, userId: String, isVideoCall: Boolean, arg: Pointer? ->
+                kaliumLogger.i("startHandleAsync -> missedCallHandler")
+            },
+            answeredCallHandler = { conversationId: String, arg: Pointer? ->
+                kaliumLogger.i("startHandleAsync -> answeredCallHandler")
+            },
+            establishedCallHandler = { conversationId: String, userId: String, clientId: String, arg: Pointer? ->
+                kaliumLogger.i("startHandleAsync -> establishedCallHandler")
+            },
+            closeCallHandler = { reason: Int, conversationId: String, messageTime: Uint32_t, userId: String, clientId: String,
+                                 arg: Pointer? ->
+                kaliumLogger.i("startHandleAsync -> closeCallHandler")
+            },
+            metricsHandler = { conversationId: String, metricsJson: String, arg: Pointer? ->
+                kaliumLogger.i("startHandleAsync -> metricsHandler")
+            },
+            callConfigRequestHandler = this@CallManager,
+            constantBitRateStateChangeHandler = { userId: String, clientId: String, isEnabled: Boolean, arg: Pointer? ->
+                kaliumLogger.i("startHandleAsync -> constantBitRateStateChangeHandler")
+            },
+            videoReceiveStateHandler = { conversationId: String, userId: String, clientId: String, state: Int, arg: Pointer? ->
+                kaliumLogger.i("startHandleAsync -> videoReceiveStateHandler")
+            },
+            arg = null
+        ).also {
+            kaliumLogger.d("CallManager - initialized with $it")
+        }
     }
+
+    private suspend fun <T> withCalling(action: suspend Calling.(handle: Handle) -> T): T {
+        val handle = deferredHandle.await()
+        return calling.action(handle)
+    }
+
+    actual suspend fun onCallingMessageReceived(message: Message, content: MessageContent.Calling) =
+        withCalling {
+            val msg = content.value.toByteArray()
+            wcall_recv_msg(
+                inst = deferredHandle.await(),
+                msg = msg,
+                len = msg.size,
+                curr_time = Uint32_t(value = System.currentTimeMillis() / 1000),
+                msg_time = Uint32_t(value = (System.currentTimeMillis() / 1000) + 10), // TODO: add correct variable
+                convId = message.conversationId.asString(),
+                userId = message.senderUserId.asString(),
+                clientId = message.senderClientId.value
+            )
+            kaliumLogger.d("onCallingMessageReceived -> Passed through")
+        }
 
     override fun onConfigRequest(inst: Handle, arg: Pointer?): Int {
-        kaliumLogger.d("WCALL_CREATE -> callConfigRequestHandler")
         scope.launch {
-            kaliumLogger.d("SCOPE_LAUNCH -> getCallConfigResponse")
             val config = callRepository.getCallConfigResponse(limit = null)
                 .fold({
                     TODO("")
@@ -145,15 +132,16 @@ actual class CallManager(
                     it
                 })
 
-            kaliumLogger.d("SCOPE_LAUNCH -> wcall_config_update")
-            calling.wcall_config_update(
-                inst = inst,
-                error = 0, // TODO: http error from internal json
-                jsonString = config
-            )
+            withCalling {
+                wcall_config_update(
+                    inst = inst,
+                    error = 0, // TODO: http error from internal json
+                    jsonString = config
+                )
+                kaliumLogger.i("onConfigRequest -> wcall_config_update")
+            }
         }
 
-        kaliumLogger.d("onConfigRequest -> returning 0")
         return 0
     }
 }
