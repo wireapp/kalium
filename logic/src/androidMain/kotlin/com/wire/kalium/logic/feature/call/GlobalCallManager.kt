ff785fdb70a07f539e6903348fdbe2384f65bf97
alexandreferris <ferris.alexandre@gmail.com>
2022-03-25 10:49:49 +0100

    chore: add logs for callManager creation

--- logic/src/androidMain/kotlin/com/wire/kalium/logic/feature/call/GlobalCallManager.kt
+++ logic/src/androidMain/kotlin/com/wire/kalium/logic/feature/call/GlobalCallManager.kt
@@ -3,15 +3,12 @@ package com.wire.kalium.logic.feature.call
 import android.content.Context
 import com.sun.jna.Pointer
 import com.waz.call.FlowManager
-import com.waz.call.RequestHandler
 import com.waz.media.manager.MediaManager
 import com.wire.kalium.calling.Calling
 import com.wire.kalium.calling.ENVIRONMENT_DEFAULT
 import com.wire.kalium.calling.callbacks.LogHandler
 import com.wire.kalium.logic.data.call.CallRepository
 import com.wire.kalium.logic.data.client.ClientRepository
-import com.wire.kalium.logic.data.conversation.ClientId
-import com.wire.kalium.logic.data.user.UserId
 import com.wire.kalium.logic.data.user.UserRepository
 import com.wire.kalium.logic.kaliumLogger
 import com.waz.log.LogHandler as NativeLogHandler
@@ -32,10 +29,13 @@ actual class GlobalCallManager(
                 logHandler = LogHandlerImpl,
                 arg = null
             )
+            kaliumLogger.i("GlobalCallManager -> wcall_init")
         }
     }
-
-    // TODO have a storage to make sure we have only one instance of CallManger per user client combination
+    
+    /**
+     * Get a [CallManager] for a session, shouldn't be instantiated more than one CallManager for a single session.
+     */
     actual fun getCallManagerForClient(
         callRepository: CallRepository,
         userRepository: UserRepository,
@@ -56,19 +56,17 @@ actual class GlobalCallManager(
             appContext
         ) { manager, path, method, ctype, content, ctx ->
             // TODO("Not yet implemented")
-            kaliumLogger.d("FlowManager -> RequestHandler -> $path : $method")
+            kaliumLogger.i("FlowManager -> RequestHandler -> $path : $method")
             0
         }.also {
             it.setEnableLogging(true)
             it.setLogHandler(object : NativeLogHandler {
                 override fun append(msg: String?) {
-                    // TODO("Not yet implemented")
-                    kaliumLogger.d("FlowManager -> Logger -> Append -> $msg")
+                    kaliumLogger.i("FlowManager -> Logger -> Append -> $msg")
                 }
 
                 override fun upload() {
-                    // TODO("Not yet implemented")
-                    kaliumLogger.d("FlowManager -> Logger -> upload")
+                    kaliumLogger.i("FlowManager -> Logger -> upload")
                 }
             })
         }
--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/call/GlobalCallManager.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/call/GlobalCallManager.kt
@@ -2,8 +2,6 @@ package com.wire.kalium.logic.feature.call
 
 import com.wire.kalium.logic.data.call.CallRepository
 import com.wire.kalium.logic.data.client.ClientRepository
-import com.wire.kalium.logic.data.conversation.ClientId
-import com.wire.kalium.logic.data.user.UserId
 import com.wire.kalium.logic.data.user.UserRepository
 
 expect class GlobalCallManager {
