354141ab742449c6db790c1d1e7da6b653b6a731
Mateusz <m.pachulski94@gmail.com>
2022-03-25 10:59:29 +0100

    Update logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/wireuser/search/WireUserSearchResult.kt
    Co-authored-by: Jacob Persson <7156+typfel@users.noreply.github.com>

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/wireuser/search/WireUserSearchResult.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/wireuser/search/WireUserSearchResult.kt
@@ -3,4 +3,4 @@ package com.wire.kalium.logic.feature.wireuser.search
 import com.wire.kalium.logic.data.wireuser.model.WireUser
 
 //TODO: this model could be extended later on if we want to include more info into the search result
-data class WireUserSearchResult(val wireUsers: List<WireUser>)
+data class UserSearchResult(val wireUsers: List<WireUser>)
