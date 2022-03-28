a6d5df976c204744a3aa8b902fa3e86021596cd6
Mohamad Jaara <mohamad.jaara@wire.com>
2022-03-28 12:41:01 +0200

    testing daisy-chained network requests

--- network/src/commonTest/kotlin/com/wire/kalium/api/ApiTest.kt
+++ network/src/commonTest/kotlin/com/wire/kalium/api/ApiTest.kt
@@ -22,11 +22,11 @@ import io.ktor.http.content.TextContent
 import io.ktor.http.headersOf
 import io.ktor.utils.io.ByteReadChannel
 import kotlinx.serialization.json.buildJsonObject
-import kotlin.test.assertContains
 import kotlin.test.assertEquals
 import kotlin.test.assertFalse
 import kotlin.test.assertIs
 import kotlin.test.assertTrue
+import kotlin.test.fail
 
 class TestSessionManager : SessionManager {
     private val serverConfig = TEST_BACKEND_CONFIG
@@ -127,6 +127,39 @@ interface ApiTest {
                 status = statusCode,
                 headers = HeadersImpl(head)
             )
+        }
+        return LoginNetworkContainer(
+            engine = mockEngine
+        ).anonymousHttpClient
+    }
+
+    class TestRequestHandler(
+        val path: String,
+        val responseBody: String,
+        val statusCode: HttpStatusCode,
+        val assertion: (HttpRequestData.() -> Unit) = {},
+        val headers: Map<String, String>? = null
+    )
+
+    fun mockUnauthenticatedHttpClient(
+        expectedRequests: List<TestRequestHandler>
+    ): HttpClient {
+        val mockEngine = MockEngine { currentRequest ->
+            expectedRequests.forEach { request ->
+                val head: Map<String, List<String>> = (request.headers?.let {
+                    mutableMapOf(HttpHeaders.ContentType to "application/json").plus(request.headers).mapValues { listOf(it.value) }
+                } ?: run {
+                    mapOf(HttpHeaders.ContentType to "application/json").mapValues { listOf(it.value) }
+                })
+                if (request.path == currentRequest.url.encodedPath) {
+                    return@MockEngine respond(
+                        content = ByteReadChannel(request.responseBody),
+                        status = request.statusCode,
+                        headers = HeadersImpl(head)
+                    )
+                }
+            }
+            fail("no expected response was found for ${currentRequest.method.value}:${currentRequest.url}")
         }
         return LoginNetworkContainer(
             engine = mockEngine
