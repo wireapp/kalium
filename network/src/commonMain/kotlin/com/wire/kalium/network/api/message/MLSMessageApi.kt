1129458c7f97240e68066998ae712b8914079742
Jacob Persson <7156+typfel@users.noreply.github.com>
2022-03-28 09:55:20 +0200

    chore: add docs

--- network/src/commonMain/kotlin/com/wire/kalium/network/api/message/MLSMessageApi.kt
+++ network/src/commonMain/kotlin/com/wire/kalium/network/api/message/MLSMessageApi.kt
@@ -5,12 +5,31 @@ import kotlin.jvm.JvmInline
 
 interface MLSMessageApi {
 
-    @JvmInline
-    value class Message(val value: ByteArray)
+    /**
+     * A welcome message allows an MLS client to join an existing MLS group.
+     */
     @JvmInline
     value class WelcomeMessage(val value: ByteArray)
 
+    /**
+     * An MLS message can either be an application message containing user content or
+     * a handshake message which updates the group state (adding, removing clients).
+     */
+    @JvmInline
+    value class Message(val value: ByteArray)
+
+    /**
+     * Send an MLS message to a MLS group. The destination group is encoded into the message itself.
+     *
+     * @param message MLS Message
+     */
     suspend fun sendMessage(message: Message): NetworkResponse<Unit>
+
+    /**
+     * Send an MLS welcome message to a client(s) which you've added to a MLS group.
+     *
+     * @param message MLS welcome Message
+     */
     suspend fun sendWelcomeMessage(message: WelcomeMessage): NetworkResponse<Unit>
 
 }
