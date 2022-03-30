816fa3518aa0892051e27a080375f777f36693db
Mohamad Jaara <mohamad.jaara@wire.com>
2022-03-30 16:19:17 +0200

    fix: UpdateCurrentSessionUseCaseTest

--- logic/src/commonTest/kotlin/com/wire/kalium/logic/feature/session/UpdateCurrentSessionUseCaseTest.kt
+++ logic/src/commonTest/kotlin/com/wire/kalium/logic/feature/session/UpdateCurrentSessionUseCaseTest.kt
@@ -2,6 +2,7 @@ package com.wire.kalium.logic.feature.session
 
 import com.wire.kalium.logic.data.session.SessionRepository
 import com.wire.kalium.logic.data.user.UserId
+import com.wire.kalium.logic.functional.Either
 import io.mockative.ConfigurationApi
 import io.mockative.Mock
 import io.mockative.classOf
@@ -31,7 +32,7 @@ class UpdateCurrentSessionUseCaseTest {
     @Test
     fun givenAUserId_whenUpdateCurrentSessionUseCaseIsInvoked_thenUpdateCurrentSessionIsCalled() = runTest {
         val userId = UserId("user_id", "domain.de")
-        given(sessionRepository).invocation { updateCurrentSession(userId) }
+        given(sessionRepository).invocation { updateCurrentSession(userId) }.then { Either.Right(Unit) }
 
         updateCurrentSessionUseCase(userId)
 
