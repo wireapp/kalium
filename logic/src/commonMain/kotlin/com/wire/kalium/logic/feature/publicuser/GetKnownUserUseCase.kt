d33a08a6fbda228c66e97e5b64be08a5a4360f0e
Mateusz <m.pachulski94@gmail.com>
2022-03-30 12:17:22 +0200

    feat: get known user by id  (#330)
    * expose the use case
    
    * expose the use case
    
    * remove unneeded mapper
    
    Co-authored-by: mateusz.pachulski <mateusz.pachulski@appunite.com>

---
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/publicuser/GetKnownUserUseCase.kt
@@ -0,0 +1,19 @@
+package com.wire.kalium.logic.feature.publicuser
+
+import com.wire.kalium.logic.data.publicuser.model.PublicUser
+import com.wire.kalium.logic.data.user.UserId
+import com.wire.kalium.logic.data.user.UserRepository
+import kotlinx.coroutines.flow.Flow
+
+interface GetKnownUserUseCase {
+    suspend operator fun invoke(userId: UserId): Flow<PublicUser?>
+}
+
+class GetKnownUserUseCaseImpl(private val userRepository: UserRepository) : GetKnownUserUseCase {
+
+    //TODO: once we return Either here we could map the fact that the user is nullable to custom error
+    // indicating it for example . NoUserFound
+    override suspend fun invoke(userId: UserId): Flow<PublicUser?> = userRepository.getKnownUser(userId)
+
+}
+
