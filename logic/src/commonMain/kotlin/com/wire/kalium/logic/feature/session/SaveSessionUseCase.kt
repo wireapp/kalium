ce81bfd87b39994b4ce6d98e00b53a0cf3487f4a
Mohamad Jaara <mohamad.jaara@wire.com>
2022-03-30 12:24:34 +0200

    refactor: revert changes from #297

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/session/SaveSessionUseCase.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/session/SaveSessionUseCase.kt
@@ -3,6 +3,7 @@ package com.wire.kalium.logic.feature.session
 import com.wire.kalium.logic.data.session.SessionRepository
 import com.wire.kalium.logic.feature.auth.AuthSession
 
+@Deprecated("unsafe API", replaceWith = ReplaceWith("com.wire.kalium.logic.feature.auth.AddAuthenticatedUserUseCase", "com.wire.kalium.logic.feature.auth.AddAuthenticatedUserUseCase"))
 class SaveSessionUseCase(
     private val sessionRepository: SessionRepository
 ) {
