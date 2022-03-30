5d659ce15ce939bd12e25b013e68f2838365b324
Michał Saleniuk <saleniuk@gmail.com>
2022-03-30 09:48:19 +0200

    add tests

--- logic/src/commonTest/kotlin/com/wire/kalium/logic/feature/user/SetUserHandleUseCaseTest.kt
+++ logic/src/commonTest/kotlin/com/wire/kalium/logic/feature/user/SetUserHandleUseCaseTest.kt
@@ -4,6 +4,7 @@ import com.wire.kalium.logic.NetworkFailure
 import com.wire.kalium.logic.data.user.UserRepository
 import com.wire.kalium.logic.feature.auth.ValidateUserHandleUseCase
 import com.wire.kalium.logic.functional.Either
+import com.wire.kalium.logic.sync.SyncManager
 import com.wire.kalium.logic.test_util.TestNetworkException
 import com.wire.kalium.network.exceptions.KaliumException
 import io.mockative.Mock
@@ -28,15 +29,18 @@ class SetUserHandleUseCaseTest {
     @Mock
     private val userRepository = mock(classOf<UserRepository>())
 
+    @Mock
+    private val syncManager = mock(classOf<SyncManager>())
+
     private lateinit var setUserHandleUseCase: SetUserHandleUseCase
 
     @BeforeTest
     fun setup() {
-        setUserHandleUseCase = SetUserHandleUseCase(userRepository, validateHandleUseCase)
+        setUserHandleUseCase = SetUserHandleUseCase(userRepository, validateHandleUseCase, syncManager)
     }
 
     @Test
-    fun givenValidHandleAndRepositorySuccess_thenSuccessIsPropagated() = runTest {
+    fun givenValidHandleAndRepositorySuccess_whenSlowSyncIsCompleted_thenLocalDataUpdatedAndSuccessIsPropagated() = runTest {
         val handle = "user_handle"
         given(validateHandleUseCase)
             .function(validateHandleUseCase::invoke)
@@ -45,6 +49,15 @@ class SetUserHandleUseCaseTest {
         given(userRepository)
             .coroutine { updateSelfHandle(handle) }
             .then { Either.Right(Unit) }
+        given(userRepository)
+            .coroutine { updateLocalSelfUserHandle(handle) }
+            .then { }
+        given(syncManager)
+            .coroutine { syncManager.isSlowSyncOngoing() }
+            .then { false }
+        given(syncManager)
+            .coroutine { syncManager.isSlowSyncCompleted() }
+            .then { true }
 
         val actual = setUserHandleUseCase(handle)
 
@@ -56,6 +69,82 @@ class SetUserHandleUseCaseTest {
         verify(userRepository)
             .coroutine { updateSelfHandle(handle) }
             .wasInvoked(exactly = once)
+        verify(userRepository)
+            .coroutine { updateLocalSelfUserHandle(handle) }
+            .wasInvoked(exactly = once)
+    }
+
+    @Test
+    fun givenValidHandleAndRepositorySuccess_whenSlowSyncIsOngoing_thenLocalDataUpdatedAfterSlowSyncAndSuccessIsPropagated() = runTest {
+        val handle = "user_handle"
+        given(validateHandleUseCase)
+            .function(validateHandleUseCase::invoke)
+            .whenInvokedWith(any())
+            .then { true }
+        given(userRepository)
+            .coroutine { updateSelfHandle(handle) }
+            .then { Either.Right(Unit) }
+        given(userRepository)
+            .coroutine { updateLocalSelfUserHandle(handle) }
+            .then { }
+        given(syncManager)
+            .coroutine { syncManager.isSlowSyncOngoing() }
+            .then { true }
+        given(syncManager)
+            .coroutine { syncManager.isSlowSyncCompleted() }
+            .then { true }
+        given(syncManager)
+            .coroutine { syncManager.waitForSlowSyncToComplete() }
+            .then { }
+
+        val actual = setUserHandleUseCase(handle)
+
+        assertIs<SetUserHandleResult.Success>(actual)
+
+        verify(validateHandleUseCase)
+            .invocation { invoke(handle) }
+            .wasInvoked(exactly = once)
+        verify(userRepository)
+            .coroutine { updateSelfHandle(handle) }
+            .wasInvoked(exactly = once)
+        verify(userRepository)
+            .coroutine { updateLocalSelfUserHandle(handle) }
+            .wasInvoked(exactly = once)
+        verify(syncManager)
+            .coroutine { waitForSlowSyncToComplete() }
+            .wasInvoked(exactly = once)
+    }
+
+    @Test
+    fun givenValidHandleAndRepositorySuccess_whenSlowSyncIsNotStarted_thenLocalDataNotUpdatedAndSuccessIsPropagated() = runTest {
+        val handle = "user_handle"
+        given(validateHandleUseCase)
+            .function(validateHandleUseCase::invoke)
+            .whenInvokedWith(any())
+            .then { true }
+        given(userRepository)
+            .coroutine { updateSelfHandle(handle) }
+            .then { Either.Right(Unit) }
+        given(syncManager)
+            .coroutine { syncManager.isSlowSyncOngoing() }
+            .then { false }
+        given(syncManager)
+            .coroutine { syncManager.isSlowSyncCompleted() }
+            .then { false }
+
+        val actual = setUserHandleUseCase(handle)
+
+        assertIs<SetUserHandleResult.Success>(actual)
+
+        verify(validateHandleUseCase)
+            .invocation { invoke(handle) }
+            .wasInvoked(exactly = once)
+        verify(userRepository)
+            .coroutine { updateSelfHandle(handle) }
+            .wasInvoked(exactly = once)
+        verify(userRepository)
+            .coroutine { updateLocalSelfUserHandle(handle) }
+            .wasNotInvoked()
     }
 
     @Test
@@ -107,10 +196,12 @@ class SetUserHandleUseCaseTest {
     }
 
     @Test
-    fun givenValidHandleAndRepositoryFailWithInvalidHandle_thenInvalidHandleIsPropagated() = testErrors(TestNetworkException.invalidHandle, SetUserHandleResult.Failure.InvalidHandle)
+    fun givenValidHandleAndRepositoryFailWithInvalidHandle_thenInvalidHandleIsPropagated() =
+        testErrors(TestNetworkException.invalidHandle, SetUserHandleResult.Failure.InvalidHandle)
 
     @Test
-    fun givenValidHandleAndRepositoryFailWithHandleExists_thenHandleExistsPropagated() = testErrors(TestNetworkException.handleExists, SetUserHandleResult.Failure.HandleExists)
+    fun givenValidHandleAndRepositoryFailWithHandleExists_thenHandleExistsPropagated() =
+        testErrors(TestNetworkException.handleExists, SetUserHandleResult.Failure.HandleExists)
 
 
     private fun testErrors(kaliumException: KaliumException, expectedError: SetUserHandleResult.Failure) = runTest {
