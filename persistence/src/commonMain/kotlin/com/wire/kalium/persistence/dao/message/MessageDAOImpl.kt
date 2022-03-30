4205f18c6a4b109253c90a69f689634c8f97a424
Mojtaba Chenani <chenani@outlook.com>
2022-03-30 16:07:49 +0200

    Update persistence/src/commonMain/kotlin/com/wire/kalium/persistence/dao/message/MessageDAOImpl.kt
    Co-authored-by: Jacob Persson <7156+typfel@users.noreply.github.com>

--- persistence/src/commonMain/kotlin/com/wire/kalium/persistence/dao/message/MessageDAOImpl.kt
+++ persistence/src/commonMain/kotlin/com/wire/kalium/persistence/dao/message/MessageDAOImpl.kt
@@ -30,8 +30,8 @@ class MessageDAOImpl(private val queries: MessagesQueries) : MessageDAO {
 
     override suspend fun deleteMessage(id: String) = queries.deleteMessageById(id)
 
-    override suspend fun updateMessageVisibility(visibility: MessageEntity.Visibility, conversationId: QualifiedIDEntity, content: String?, id: String) =
-        queries.updateMessageVisibility(visibility, content, id)
+    override suspend fun updateMessageVisibility(visibility: MessageEntity.Visibility, conversationId: QualifiedIDEntity, id: String) =
+        queries.updateMessageVisibility(visibility, "", id)
 
     override suspend fun deleteAllMessages() = queries.deleteAllMessages()
 
