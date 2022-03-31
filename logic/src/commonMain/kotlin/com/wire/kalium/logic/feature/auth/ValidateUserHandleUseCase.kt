fc166a5af172210dacf7ba860c726e07924ff230
Michał Saleniuk <saleniuk@gmail.com>
2022-03-31 17:10:43 +0200

    feat: update ValidateUserHandleUseCase to return also handleWithoutInvalidChars

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/auth/ValidateUserHandleUseCase.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/auth/ValidateUserHandleUseCase.kt
@@ -1,24 +1,21 @@
 package com.wire.kalium.logic.feature.auth
 
 interface ValidateUserHandleUseCase {
-    operator fun invoke(handle: String): Boolean
+    operator fun invoke(handle: String): ValidateUserHandleResult
 }
 class ValidateUserHandleUseCaseImpl: ValidateUserHandleUseCase  {
-    override operator fun invoke(handle: String): Boolean {
-        return when {
-            isHandleTooShort(handle) -> false
-            !validateHandle(handle) -> false
-            else -> true
-        }
+    override operator fun invoke(handle: String): ValidateUserHandleResult {
+        val handleWithoutInvalidChars = handle.replace(Regex(HANDLE_FORBIDDEN_CHARACTERS_REGEX), "")
+        val hasValidChars = handle == handleWithoutInvalidChars
+        val hasValidLength = handleWithoutInvalidChars.length in HANDLE_MIN_LENGTH..HANDLE_MAX_LENGTH
+        return ValidateUserHandleResult(hasValidChars && hasValidLength, handleWithoutInvalidChars)
     }
-
-    private fun validateHandle(handle: String) =
-        handle.matches(HANDLE_REGEX)
-
-    private fun isHandleTooShort(handle: String) = handle.length < HANDEL_MIN_LENGTH
 
     private companion object {
-        private const val HANDEL_MIN_LENGTH = 2
-        private val HANDLE_REGEX = """^[a-z0-9_]*$""".toRegex()
+        private const val HANDLE_FORBIDDEN_CHARACTERS_REGEX = "[^a-z0-9_]"
+        private const val HANDLE_MIN_LENGTH = 2
+        private const val HANDLE_MAX_LENGTH = 255
     }
 }
+
+data class ValidateUserHandleResult(val isValid: Boolean, val handleWithoutInvalidChars: String)
