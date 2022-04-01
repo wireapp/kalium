9d12900333a14233dd5af3059fd4f642a6b24514
Oussama Hassine <oussama.has100@gmail.com>
2022-04-01 11:23:15 +0100

    fix wrong method name
    Co-authored-by: Alexandre Ferris <ferris.alexandre@gmail.com>

--- logic/src/androidMain/kotlin/com/wire/kalium/logic/feature/call/SendCallingMessage.kt
+++ logic/src/androidMain/kotlin/com/wire/kalium/logic/feature/call/SendCallingMessage.kt
@@ -5,7 +5,7 @@ import com.wire.kalium.logic.data.message.Message
 import com.wire.kalium.logic.data.message.MessageContent
 import java.util.UUID
 
-suspend fun CallManager.senCallingMessage(conversationId: ConversationId, data: String) {
+suspend fun CallManager.sendCallingMessage(conversationId: ConversationId, data: String) {
     val messageContent =  MessageContent.Calling(data)
     val message =  Message(UUID.randomUUID().toString(), messageContent, conversationId)
     messageSender.getRecipientsAndAttemptSend(conversationId, message)
