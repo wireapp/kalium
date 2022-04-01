2efa9ce37f0cf9392e54c2c7630c511e5d91e073
Gonzo <gongracr@gmail.com>
2022-04-01 16:16:18 +0200

    feat: add `SendImageMessageUseCase` [AR-1307]  (#327)
    * Added calcSHA256 method
    
    * Added AssetProtoContent and initial mappers
    
    * Added SendImageUseCase
    
    * Calculate SHA from encrypted data
    
    * Added extra asset fields on Message table
    
    * Added contentType field on Message object and added messageContent field on MessageDao
    
    * Cleaned code and fixed issues in ProtoContentMapper
    
    * Smart cast
    
    * Correct metadata types
    
    * Addressed more PR comments
    
    * Declared contentType as internal
    
    * Removed Message.contentType field from logic module
    
    * Removed contentType from MessageEntity too
    
    * Added basic testing

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/data/message/MessageMapper.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/data/message/MessageMapper.kt
@@ -2,8 +2,13 @@ package com.wire.kalium.logic.data.message
 
 import com.wire.kalium.logic.data.conversation.ClientId
 import com.wire.kalium.logic.data.id.IdMapper
-import com.wire.kalium.logic.kaliumLogger
+import com.wire.kalium.logic.data.message.AssetContent.AssetMetadata.Audio
+import com.wire.kalium.logic.data.message.AssetContent.AssetMetadata.Image
+import com.wire.kalium.logic.data.message.AssetContent.AssetMetadata.Video
+import com.wire.kalium.logic.di.MapperProvider
 import com.wire.kalium.persistence.dao.message.MessageEntity
+import com.wire.kalium.persistence.dao.message.MessageEntity.MessageEntityContent.AssetMessageContent
+import com.wire.kalium.persistence.dao.message.MessageEntity.MessageEntityContent.TextMessageContent
 
 interface MessageMapper {
     fun fromMessageToEntity(message: Message): MessageEntity
@@ -12,37 +17,65 @@ interface MessageMapper {
 
 class MessageMapperImpl(private val idMapper: IdMapper) : MessageMapper {
     override fun fromMessageToEntity(message: Message): MessageEntity {
-        val stringContent = when (val content = message.content) {
-            is MessageContent.Text -> content.value
-            is MessageContent.Calling -> {
-                kaliumLogger.w("fromMessageToEntity - Calling")
-                null
-            }
-            is MessageContent.DeleteMessage -> content.messageId
-            is MessageContent.DeleteForMe -> content.messageId
-            MessageContent.Unknown -> null
-        }
         val status = when (message.status) {
             Message.Status.PENDING -> MessageEntity.Status.PENDING
             Message.Status.SENT -> MessageEntity.Status.SENT
             Message.Status.READ -> MessageEntity.Status.READ
             Message.Status.FAILED -> MessageEntity.Status.FAILED
         }
+        val messageContent = when (message.content) {
+            is MessageContent.Text -> TextMessageContent(messageBody = message.content.value)
+            is MessageContent.DeleteMessage -> TextMessageContent(messageBody = message.content.messageId)
+            is MessageContent.DeleteForMe -> TextMessageContent(messageBody = message.content.messageId)
+            is MessageContent.Asset -> {
+                with(message.content.value) {
+                    AssetMessageContent(
+                        assetMimeType = mimeType,
+                        assetSize = size,
+                        assetName = name,
+                        assetImageWidth = metadata?.let { if (it is Image) it.width else null },
+                        assetImageHeight = metadata?.let { if (it is Image) it.height else null },
+                        assetVideoWidth = metadata?.let { if (it is Video) it.width else null },
+                        assetVideoHeight = metadata?.let { if (it is Video) it.height else null },
+                        assetVideoDurationMs = metadata?.let { if (it is Video) it.durationMs else null },
+                        assetAudioDurationMs = metadata?.let { if (it is Audio) it.durationMs else null },
+                        assetAudioNormalizedLoudness = metadata?.let { if (it is Audio) it.normalizedLoudness else null },
+                        assetOtrKey = remoteData.otrKey,
+                        assetSha256Key = remoteData.sha256,
+                        assetId = remoteData.assetId,
+                        assetEncryptionAlgorithm = remoteData.encryptionAlgorithm?.name
+                    )
+                }
+            }
+            else -> TextMessageContent(messageBody = "")
+        }
+
         return MessageEntity(
-            message.id,
-            stringContent,
-            idMapper.toDaoModel(message.conversationId),
-            message.date,
-            idMapper.toDaoModel(message.senderUserId),
-            message.senderClientId.value,
-            status
+            content = messageContent,
+            id = message.id,
+            conversationId = idMapper.toDaoModel(message.conversationId),
+            date = message.date,
+            senderUserId = idMapper.toDaoModel(message.senderUserId),
+            senderClientId = message.senderClientId.value,
+            status = status
         )
     }
 
     override fun fromEntityToMessage(message: MessageEntity): Message {
-        val content = when (val stringContent = message.content) {
-            null -> MessageContent.Unknown
-            else -> MessageContent.Text(stringContent)
+        val content = when (val messageContent = message.content) {
+            // It's a text message
+            is TextMessageContent -> {
+                MessageContent.Text(messageContent.messageBody)
+            }
+
+            // It's an asset message
+            is AssetMessageContent -> {
+                MessageContent.Asset(
+                    MapperProvider.assetMapper().fromAssetEntityToAssetContent(messageContent)
+                )
+            }
+
+            else -> MessageContent.Unknown
         }
         val status = when (message.status) {
             MessageEntity.Status.PENDING -> Message.Status.PENDING
