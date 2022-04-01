418dcd7c0f910751b51159ead27b041ae31bd53b
Jacob Persson <7156+typfel@users.noreply.github.com>
2022-04-01 12:33:44 +0200

    chore: fix bad merge

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/client/RegisterClientUseCase.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/client/RegisterClientUseCase.kt
@@ -1,6 +1,5 @@
 package com.wire.kalium.logic.feature.client
 
-import com.wire.kalium.cryptography.MLSClientImpl
 import com.wire.kalium.logic.CoreFailure
 import com.wire.kalium.logic.NetworkFailure
 import com.wire.kalium.logic.data.client.Client
@@ -10,7 +9,6 @@ import com.wire.kalium.logic.data.client.MLSClientProvider
 import com.wire.kalium.logic.data.client.RegisterClientParam
 import com.wire.kalium.logic.data.keypackage.KeyPackageRepository
 import com.wire.kalium.logic.data.prekey.PreKeyRepository
-import com.wire.kalium.logic.data.user.UserRepository
 import com.wire.kalium.logic.feature.client.RegisterClientUseCase.Companion.FIRST_KEY_ID
 import com.wire.kalium.logic.functional.Either
 import com.wire.kalium.logic.functional.flatMap
@@ -18,7 +16,6 @@ import com.wire.kalium.logic.functional.suspending
 import com.wire.kalium.network.exceptions.KaliumException
 import com.wire.kalium.network.exceptions.isMissingAuth
 import com.wire.kalium.network.exceptions.isTooManyClients
-import kotlinx.coroutines.flow.first
 
 sealed class RegisterClientResult {
     class Success(val client: Client) : RegisterClientResult()
@@ -29,7 +26,6 @@ sealed class RegisterClientResult {
         class Generic(val genericFailure: CoreFailure) : Failure()
     }
 }
-
 
 interface RegisterClientUseCase {
     suspend operator fun invoke(
@@ -51,29 +47,6 @@ class RegisterClientUseCaseImpl(
     private val mlsClientProvider: MLSClientProvider
 ) : RegisterClientUseCase {
 
-    override suspend operator fun invoke(
-        password: String,
-        capabilities: List<ClientCapability>?,
-        preKeysToSend: Int
-    ): RegisterClientResult = suspending {
-
-        preKeyRepository.generateNewPreKeys(FIRST_KEY_ID, preKeysToSend).flatMap { preKeys ->
-            preKeyRepository.generateNewLastKey().flatMap { lastKey ->
-                Either.Right(
-                    RegisterClientParam(
-                        password = password,
-                        capabilities = capabilities,
-                        preKeys = preKeys,
-                        lastKey = lastKey
-                    )
-                )
-            }
-        }.coFold({
-            RegisterClientResult.Failure.Generic(it)
-        }, { registerClientParam ->
-            clientRepository.registerClient(registerClientParam).flatMap { client ->
-                // TODO when https://github.com/wireapp/core-crypto/issues/11 is implemented we
-                // can remove registerMLSClient() and supply the MLS public key in registerClient().
     override suspend operator fun invoke(
         password: String,
         capabilities: List<ClientCapability>?,
@@ -123,21 +96,5 @@ class RegisterClientUseCaseImpl(
                 )
             )
         }
-    }
-                    .flatMap { keyPackageRepository.uploadNewKeyPackages(client.clientId) }
-                    .flatMap { clientRepository.persistClientId(client.clientId) }
-                    .map { client }
-            }.fold({ failure ->
-                if(failure is NetworkFailure.ServerMiscommunication && failure.kaliumException is KaliumException.InvalidRequestError)
-                    when {
-                        failure.kaliumException.isTooManyClients() -> RegisterClientResult.Failure.TooManyClients
-                        failure.kaliumException.isMissingAuth() -> RegisterClientResult.Failure.InvalidCredentials
-                        else -> RegisterClientResult.Failure.Generic(failure)
-                    }
-                else RegisterClientResult.Failure.Generic(failure)
-            }, { client ->
-                RegisterClientResult.Success(client)
-            })
-        })
     }
 }
