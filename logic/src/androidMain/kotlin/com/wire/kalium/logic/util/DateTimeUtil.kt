2a3ed72bffde833fbb708d0156e7eb6ace28fde5
yamilmedina <yamilmedina@users.noreply.github.com>
2022-04-04 15:48:05 -0400

    fix: address lint issue, format required to use api > 24, current is 23

--- logic/src/androidMain/kotlin/com/wire/kalium/logic/util/DateTimeUtil.kt
+++ logic/src/androidMain/kotlin/com/wire/kalium/logic/util/DateTimeUtil.kt
@@ -2,6 +2,6 @@ package com.wire.kalium.logic.util
 
 import java.text.SimpleDateFormat
 
-private val serverDateTimeFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
+private val serverDateTimeFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
 
 fun String.toTimeInMillis(): Long = serverDateTimeFormat.parse(this).time
