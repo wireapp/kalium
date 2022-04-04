6732bbc1c392ac8ad59c9f9f617e7f8a126746c6
Vitor Hugo Schwaab <vitor@schwaab.xyz>
2022-04-04 12:12:37 +0200

    feat(conversations): observe details of a conversation

--- logic/src/commonTest/kotlin/com/wire/kalium/logic/framework/TestConversation.kt
+++ logic/src/commonTest/kotlin/com/wire/kalium/logic/framework/TestConversation.kt
@@ -1,11 +1,17 @@
 package com.wire.kalium.logic.framework
 
+import com.wire.kalium.logic.data.conversation.Conversation
+import com.wire.kalium.logic.data.conversation.ConversationDetails
 import com.wire.kalium.logic.data.id.ConversationId
 import com.wire.kalium.persistence.dao.ConversationEntity
 import com.wire.kalium.persistence.dao.QualifiedIDEntity
 
 object TestConversation {
     val ID = ConversationId("valueConvo", "domainConvo")
+
+    val ONE_ON_ONE = Conversation(ID, "ONE_ON_ONE Name", Conversation.Type.ONE_ON_ONE, TestTeam.TEAM_ID)
+    val SELF = Conversation(ID, "SELF Name", Conversation.Type.SELF, TestTeam.TEAM_ID)
+    val GROUP = Conversation(ID, "GROUP Name", Conversation.Type.GROUP, TestTeam.TEAM_ID)
 
     val ENTITY_ID = QualifiedIDEntity("valueConversation", "domainConversation")
     val ENTITY = ConversationEntity(ENTITY_ID, "convo name", ConversationEntity.Type.SELF, "teamId")
---
+++ logic/src/commonTest/kotlin/com/wire/kalium/logic/framework/TestTeam.kt
@@ -0,0 +1,7 @@
+package com.wire.kalium.logic.framework
+
+import com.wire.kalium.logic.data.id.TeamId
+
+object TestTeam {
+    val TEAM_ID = TeamId("Some-Team")
+}
