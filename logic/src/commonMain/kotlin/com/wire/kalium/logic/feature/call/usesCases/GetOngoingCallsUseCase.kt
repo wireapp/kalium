9dae670de8a5467a4dad43b0bc0b005c74335ab0
Oussama Hassine <oussama.has100@gmail.com>
2022-04-05 18:02:40 +0100

    rename package
    Co-authored-by: Vitor Hugo Schwaab <vitor@schwaab.xyz>

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/call/usesCases/GetOngoingCallsUseCase.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/call/usesCases/GetOngoingCallsUseCase.kt
@@ -1,4 +1,4 @@
-package com.wire.kalium.logic.feature.call.usesCases
+package com.wire.kalium.logic.feature.call.usescase
 
 import com.wire.kalium.logic.feature.call.Call
 import com.wire.kalium.logic.feature.call.CallManager
