dd6df9140bb919636241fc143a1b2a4464148388
Oussama Hassine <oussama.has100@gmail.com>
2022-04-05 18:06:19 +0100

    remove unnecessary methods
    Co-authored-by: Vitor Hugo Schwaab <vitor@schwaab.xyz>

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/call/CallManager.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/call/CallManager.kt
@@ -13,13 +13,7 @@ interface CallManager {
     val allCalls: StateFlow<List<Call>>
 }
 
-expect class CallManagerImpl: CallManager {
-
-    override suspend fun onCallingMessageReceived(message: Message, content: MessageContent.Calling)
-    override val allCalls: StateFlow<List<Call>>
-    override suspend fun startCall(conversationId: ConversationId, callType: Int, conversationType: Int, isAudioCbr: Boolean) //TODO Audio CBR
-}
-
+expect class CallManagerImpl: CallManager
 val CallManager.ongoingCalls get() = allCalls.map {
     it.filter { call ->
         call.status in listOf(
