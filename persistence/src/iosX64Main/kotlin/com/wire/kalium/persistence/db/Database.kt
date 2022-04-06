763fceb58b03e7fdf6f48ed32516eaff0dc4f188
Jacob Persson <7156+typfel@users.noreply.github.com>
2022-04-06 15:59:49 +0200

    chore: fix iOS target not compiling

--- persistence/src/iosX64Main/kotlin/com/wire/kalium/persistence/db/Database.kt
+++ persistence/src/iosX64Main/kotlin/com/wire/kalium/persistence/db/Database.kt
@@ -31,7 +31,12 @@ actual class Database(userId: UserIDEntity, passphrase: String) {
         database = AppDatabase(
             driver,
             Client.Adapter(user_idAdapter = QualifiedIDAdapter()),
-            Conversation.Adapter(qualified_idAdapter = QualifiedIDAdapter(), typeAdapter = EnumColumnAdapter()),
+            Conversation.Adapter(
+                qualified_idAdapter = QualifiedIDAdapter(),
+                typeAdapter = EnumColumnAdapter(),
+                group_stateAdapter = EnumColumnAdapter(),
+                protocolAdapter = EnumColumnAdapter()
+            ),
             Member.Adapter(userAdapter = QualifiedIDAdapter(), conversationAdapter = QualifiedIDAdapter()),
             Message.Adapter(
                 conversation_idAdapter = QualifiedIDAdapter(),
