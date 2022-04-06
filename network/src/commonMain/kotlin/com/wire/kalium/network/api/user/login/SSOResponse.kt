3a1702fdeb5ea485160438c7197161440fcf1fa0
Mohamad Jaara <mohamad.jaara@wire.com>
2022-04-06 12:54:49 +0200

    delete unnecessary AuthnRequestSSO

--- network/src/commonMain/kotlin/com/wire/kalium/network/api/user/login/SSOResponse.kt
+++ network/src/commonMain/kotlin/com/wire/kalium/network/api/user/login/SSOResponse.kt
@@ -7,71 +7,8 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class SSOResponse(
     @SerialName("uri")
-    val uri: String?,
-    @SerialName("xml")
-    val xml: AuthnRequestSSO?
+    val uri: String?
 )
-
-@Serializable
-data class AuthnRequestSSO(
-    @SerialName("iD")
-    val id: SSOID,
-    @SerialName("issueInstant")
-    val issueInstant: IssueInstant,
-    @SerialName("issuer")
-    val issuer: String,
-    @SerialName("nameIDPolicy")
-    val nameIDPolicy: NameIDPolicy?
-)
-
-@Serializable
-data class IssueInstant(
-    @SerialName("time")
-    val time: String
-)
-
-
-@Serializable
-data class SSOID(
-    @SerialName("iD")
-    val iD: XmlText
-)
-
-@Serializable
-data class XmlText(
-    @SerialName("fromXmlText")
-    val fromXmlText: String
-)
-
-@Serializable
-data class NameIDPolicy(
-    @SerialName("allowCreate")
-    val allowCreate: Boolean,
-    @SerialName("format")
-    val format: NameIDFormat,
-    @SerialName("spNameQualifier")
-    val spNameQualifier: XmlText?
-)
-
-@Serializable
-enum class NameIDFormat {
-    @SerialName("NameIDFUnspecified")
-    NameIDFUnspecified,
-    @SerialName("NameIDFEmail")
-    NameIDFEmail,
-    @SerialName("NameIDFX509")
-    NameIDFX509,
-    @SerialName("NameIDFWindows")
-    NameIDFWindows,
-    @SerialName("NameIDFKerberos")
-    NameIDFKerberos,
-    @SerialName("NameIDFEntity")
-    NameIDFEntity,
-    @SerialName("NameIDFPersistent")
-    NameIDFPersistent,
-    @SerialName("NameIDFTransient")
-    NameIDFTransient
-}
 
 @Serializable
 data class SSOSettingsResponse(
--- network/src/commonTest/kotlin/com/wire/kalium/api/tools/json/api/user/login/SSOResponseJson.kt
+++ network/src/commonTest/kotlin/com/wire/kalium/api/tools/json/api/user/login/SSOResponseJson.kt
@@ -4,46 +4,17 @@ import com.wire.kalium.api.tools.json.ValidJsonProvider
 import com.wire.kalium.network.api.user.login.SSOResponse
 import kotlinx.serialization.json.buildJsonObject
 import kotlinx.serialization.json.put
-import kotlinx.serialization.json.putJsonObject
 
 object SSOResponseJson {
     private val jsonProvider = { serializable: SSOResponse ->
         buildJsonObject {
             with(serializable) {
                 uri?.let { put("uri", it) }
-                xml?.let {
-                    putJsonObject("xml") {
-                        putJsonObject("iD") {
-                            putJsonObject("fromXmlText") {
-                                it.id.iD.fromXmlText
-                            }
-                        }
-                        putJsonObject("issueInstant") {
-                            put("time", it.issueInstant.time)
-                        }
-                        put("issuer", it.issuer)
-                        it.nameIDPolicy?.let {
-                            putJsonObject("nameIDPolicy") {
-                                put("allowCreate", it.allowCreate)
-                                put("format", it.format.toString())
-
-                                it.spNameQualifier?.let {
-                                    putJsonObject("spNameQualifier") {
-                                        putJsonObject("fromXmlText") {
-                                            it.fromXmlText
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
             }
         }.toString()
     }
 
     val valid = ValidJsonProvider(
-        SSOResponse(uri = "sso.login.de", null), jsonProvider
+        SSOResponse(uri = "sso.login.de"), jsonProvider
     )
-
 }
