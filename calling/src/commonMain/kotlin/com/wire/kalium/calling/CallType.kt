8c731fa3105bbb57321bfffd8cae40093bfaa373
Oussama <oussama.has100@gmail.com>
2022-04-07 08:17:33 +0100

    chore: add some comments

--- calling/src/commonMain/kotlin/com/wire/kalium/calling/CallType.kt
+++ calling/src/commonMain/kotlin/com/wire/kalium/calling/CallType.kt
@@ -1,5 +1,9 @@
 package com.wire.kalium.calling
 
+/**
+ * NORMAL for audio call
+ * VIDEO for video cal
+ */
 enum class CallType(val avsValue: Int) {
     NORMAL(avsValue = 0),
     VIDEO(avsValue = 1),
