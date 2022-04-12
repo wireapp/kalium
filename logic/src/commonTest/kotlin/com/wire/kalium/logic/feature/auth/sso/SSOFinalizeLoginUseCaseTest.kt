f6a9d89821df158c87df574222c94c3db851e0a6
Michał Saleniuk <30429749+saleniuk@users.noreply.github.com>
2022-04-12 19:52:56 +0200

    feat: implement sso use cases [AR-1160] (#389)
    * feat: add SSO usecases
    
    * add missing tests
    
    * remove unneeded imports
    
    * fix wrongly written test
    
    * fix wrongly written test
    
    * review changes
    
    * review changes
    
    * review changes

---
+++ logic/src/commonTest/kotlin/com/wire/kalium/logic/feature/auth/sso/SSOFinalizeLoginUseCaseTest.kt
@@ -0,0 +1,66 @@
+package com.wire.kalium.logic.feature.auth.sso
+
+import com.wire.kalium.logic.NetworkFailure
+import com.wire.kalium.logic.data.auth.login.SSOLoginRepository
+import com.wire.kalium.logic.functional.Either
+import com.wire.kalium.logic.test_util.TestNetworkException
+import com.wire.kalium.logic.test_util.TestServerConfig
+import com.wire.kalium.logic.test_util.serverMiscommunicationFailure
+import com.wire.kalium.network.api.ErrorResponse
+import com.wire.kalium.network.exceptions.KaliumException
+import io.ktor.http.HttpStatusCode
+import io.mockative.Mock
+import io.mockative.classOf
+import io.mockative.given
+import io.mockative.mock
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertIs
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class SSOFinalizeLoginUseCaseTest {
+
+    @Mock
+    val ssoLoginRepository = mock(classOf<SSOLoginRepository>())
+    lateinit var ssoFinalizeLoginUseCase: SSOFinalizeLoginUseCase
+
+    @BeforeTest
+    fun setup() {
+        ssoFinalizeLoginUseCase = SSOFinalizeLoginUseCaseImpl(ssoLoginRepository)
+    }
+
+    @Test
+    fun givenApiReturnsInvalidCookie_whenFinalizing_thenReturnInvalidCookie() =
+        runTest {
+            given(ssoLoginRepository).coroutine { finalize(TEST_COOKIE, TestServerConfig.generic) }
+                .then { Either.Left(serverMiscommunicationFailure(code = HttpStatusCode.BadRequest.value)) }
+            val result = ssoFinalizeLoginUseCase(TEST_COOKIE, TestServerConfig.generic)
+            assertEquals(result, SSOFinalizeLoginResult.Failure.InvalidCookie)
+        }
+
+    @Test
+    fun givenApiReturnsGenericError_whenFinalizing_thenReturnGenericFailure() =
+        runTest {
+            val expected = serverMiscommunicationFailure(code = HttpStatusCode.Forbidden.value)
+            given(ssoLoginRepository).coroutine { finalize(TEST_COOKIE, TestServerConfig.generic) }.then { Either.Left(expected) }
+            val result = ssoFinalizeLoginUseCase(TEST_COOKIE, TestServerConfig.generic)
+            assertIs<SSOFinalizeLoginResult.Failure.Generic>(result)
+            assertEquals(expected, result.genericFailure)
+        }
+
+    @Test
+    fun givenApiReturnsSuccess_whenFinalizing_thenReturnSuccess() =
+        runTest {
+            given(ssoLoginRepository).coroutine { finalize(TEST_COOKIE, TestServerConfig.generic) }.then { Either.Right(TEST_RESPONSE) }
+            val result = ssoFinalizeLoginUseCase(TEST_COOKIE, TestServerConfig.generic)
+            assertEquals(result, SSOFinalizeLoginResult.Success(TEST_RESPONSE))
+        }
+
+    private companion object {
+        const val TEST_COOKIE = "cookie"
+        const val TEST_RESPONSE = "wire/response"
+    }
+}
