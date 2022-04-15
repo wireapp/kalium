289340775d748224dbd08e76564b2ed333e6e199
mateusz.pachulski <mateusz.pachulski@appunite.com>
2022-04-15 10:47:01 +0200

    add use case and unit tests

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/message/SendTextMessageUseCase.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/message/SendTextMessageUseCase.kt
@@ -1,16 +1,13 @@
 package com.wire.kalium.logic.feature.message
 
 import com.benasher44.uuid.uuid4
-import com.wire.kalium.logic.CoreFailure
 import com.wire.kalium.logic.data.client.ClientRepository
 import com.wire.kalium.logic.data.id.ConversationId
 import com.wire.kalium.logic.data.message.Message
 import com.wire.kalium.logic.data.message.MessageContent
 import com.wire.kalium.logic.data.message.MessageRepository
 import com.wire.kalium.logic.data.user.UserRepository
-import com.wire.kalium.logic.functional.Either
 import com.wire.kalium.logic.functional.suspending
-import com.wire.kalium.logic.kaliumLogger
 import com.wire.kalium.logic.sync.SyncManager
 import kotlinx.coroutines.flow.first
 import kotlinx.datetime.Clock
@@ -20,18 +17,17 @@ class SendTextMessageUseCase(
     private val userRepository: UserRepository,
     private val clientRepository: ClientRepository,
     private val syncManager: SyncManager,
-    private val messageSender: MessageSender
+    private val messageSender: MessageSender,
 ) {
 
-    suspend operator fun invoke(conversationId: ConversationId, text: String): Either<CoreFailure, Unit> {
+    suspend operator fun invoke(conversationId: ConversationId, text: String): SendTextMessageResult {
         syncManager.waitForSlowSyncToComplete()
         val selfUser = userRepository.getSelfUser().first()
+        val generatedMessageUuid = uuid4().toString()
 
         return suspending {
-            val generatedMessageUuid = uuid4().toString()
-
-            clientRepository.currentClientId().flatMap { currentClientId ->
-                val message = Message(
+            clientRepository.currentClientId().map { currentClientId ->
+                Message(
                     id = generatedMessageUuid,
                     content = MessageContent.Text(text),
                     conversationId = conversationId,
@@ -40,16 +36,24 @@ class SendTextMessageUseCase(
                     senderClientId = currentClientId,
                     status = Message.Status.PENDING
                 )
+            }.flatMap { message ->
                 messageRepository.persistMessage(message)
-            }.flatMap {
-                messageSender.trySendingOutgoingMessageById(conversationId, generatedMessageUuid)
-            }.onFailure {
-                kaliumLogger.e("There was an error trying to send the message $it")
-                if (it is CoreFailure.Unknown) {
-                    //TODO Did I write multiplatform logging today?
-                    it.rootCause?.printStackTrace()
-                }
-            }
+                    .flatMap {
+                        messageSender.trySendingOutgoingMessageById(conversationId, generatedMessageUuid)
+                    }.flatMap {
+                        messageRepository.persistMessage(message.copy(status = Message.Status.SENT))
+                    }
+            }.onFailure { SendTextMessageResult.Failure("test") }
+                .onSuccess { SendTextMessageResult.Success }
+                .coFold(
+                    { SendTextMessageResult.Failure("test") },
+                    { SendTextMessageResult.Success }
+                )
         }
     }
+}
+
+sealed class SendTextMessageResult {
+    object Success : SendTextMessageResult()
+    data class Failure(val messageId: String) : SendTextMessageResult()
 }
---
+++ logic/src/commonTest/kotlin/com/wire/kalium/logic/feature/message/SendTextMessageUseCaseTest.kt
@@ -0,0 +1,234 @@
+package com.wire.kalium.logic.feature.message
+
+import com.wire.kalium.logic.CoreFailure
+import com.wire.kalium.logic.data.client.ClientRepository
+import com.wire.kalium.logic.data.conversation.ClientId
+import com.wire.kalium.logic.data.id.ConversationId
+import com.wire.kalium.logic.data.id.PlainId
+import com.wire.kalium.logic.data.message.Message
+import com.wire.kalium.logic.data.message.MessageContent
+import com.wire.kalium.logic.data.message.MessageRepository
+import com.wire.kalium.logic.data.user.UserId
+import com.wire.kalium.logic.data.user.UserRepository
+import com.wire.kalium.logic.framework.TestUser
+import com.wire.kalium.logic.functional.Either
+import com.wire.kalium.logic.sync.SyncManager
+import io.mockative.Mock
+import io.mockative.Times
+import io.mockative.anything
+import io.mockative.eq
+import io.mockative.given
+import io.mockative.mock
+import io.mockative.verify
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import kotlinx.datetime.Clock
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertIs
+
+class SendTextMessageUseCaseTest {
+
+    @Mock
+    private val messageRepository = mock(MessageRepository::class)
+
+    @Mock
+    private val userRepository = mock(UserRepository::class)
+
+    @Mock
+    private val clientRepository = mock(ClientRepository::class)
+
+    @Mock
+    private val syncManager = mock(SyncManager::class)
+
+    @Mock
+    private val messageSender: MessageSender = mock(MessageSender::class)
+
+    lateinit var sendTextMessageUseCase: SendTextMessageUseCase
+
+    @BeforeTest
+    fun setup() {
+        sendTextMessageUseCase = SendTextMessageUseCase(
+            messageRepository = messageRepository,
+            userRepository = userRepository,
+            clientRepository = clientRepository,
+            syncManager = syncManager,
+            messageSender = messageSender
+        )
+    }
+
+    @Test
+    fun givenRecipients_whenCreatingAnEnvelope_thenProteusClientShouldBeUsedToEncryptForEachClient() = runTest {
+        //given
+        given(syncManager)
+            .suspendFunction(syncManager::waitForSlowSyncToComplete)
+            .whenInvoked()
+            .thenReturn(Unit)
+
+        given(userRepository)
+            .suspendFunction(userRepository::getSelfUser)
+            .whenInvoked()
+            .thenReturn(flowOf(TestUser.SELF))
+
+        given(clientRepository)
+            .suspendFunction(clientRepository::currentClientId)
+            .whenInvoked()
+            .thenReturn(Either.Left(CoreFailure.Unknown(IllegalArgumentException())))
+        //when
+        val result = sendTextMessageUseCase(ConversationId("test", "test"), "text")
+
+        //then
+        verify(messageRepository)
+            .suspendFunction(messageRepository::persistMessage)
+            .with(anything())
+            .wasNotInvoked()
+
+        verify(messageSender)
+            .suspendFunction(messageSender::trySendingOutgoingMessageById)
+            .with(anything(), anything())
+            .wasNotInvoked()
+
+        assertIs<SendTextMessageResult.Failure>(result)
+    }
+
+    @Test
+    fun givenRecipients_whenCreatingAnEnvelope_thenProteusClientShouldBeUsedToEncryptForEachClient123() = runTest {
+        //given
+        given(syncManager)
+            .suspendFunction(syncManager::waitForSlowSyncToComplete)
+            .whenInvoked()
+            .thenReturn(Unit)
+
+        given(userRepository)
+            .suspendFunction(userRepository::getSelfUser)
+            .whenInvoked()
+            .thenReturn(flowOf(TestUser.SELF))
+
+        given(clientRepository)
+            .suspendFunction(clientRepository::currentClientId)
+            .whenInvoked()
+            .thenReturn(Either.Right(PlainId("test")))
+
+        given(messageRepository)
+            .suspendFunction(messageRepository::persistMessage)
+            .whenInvokedWith(anything())
+            .thenReturn(Either.Left(CoreFailure.Unknown(IllegalArgumentException())))
+        //when
+        val result = sendTextMessageUseCase(ConversationId("test", "test"), "text")
+
+        //then
+        verify(messageRepository)
+            .suspendFunction(messageRepository::persistMessage)
+            .with(anything())
+            .wasInvoked(Times(1))
+
+        verify(messageSender)
+            .suspendFunction(messageSender::trySendingOutgoingMessageById)
+            .with(anything(), anything())
+            .wasNotInvoked()
+
+        assertIs<SendTextMessageResult.Failure>(result)
+    }
+
+    @Test
+    fun givenRecipients_whenCreatingAnEnvelope_thenProteusClientShouldBeUsedToEncryptForEachClient1234() = runTest {
+        //given
+        given(syncManager)
+            .suspendFunction(syncManager::waitForSlowSyncToComplete)
+            .whenInvoked()
+            .thenReturn(Unit)
+
+        given(userRepository)
+            .suspendFunction(userRepository::getSelfUser)
+            .whenInvoked()
+            .thenReturn(flowOf(TestUser.SELF))
+
+        given(clientRepository)
+            .suspendFunction(clientRepository::currentClientId)
+            .whenInvoked()
+            .thenReturn(Either.Right(PlainId("test")))
+
+        given(messageRepository)
+            .suspendFunction(messageRepository::persistMessage)
+            .whenInvokedWith(anything())
+            .thenReturn(Either.Right(Unit))
+
+        given(messageSender)
+            .suspendFunction(messageSender::trySendingOutgoingMessageById)
+            .whenInvokedWith(anything(), anything())
+            .thenReturn(Either.Left(CoreFailure.Unknown(IllegalArgumentException())))
+        //when
+        val result = sendTextMessageUseCase(ConversationId("test", "test"), "text")
+
+        //then
+        verify(messageRepository)
+            .suspendFunction(messageRepository::persistMessage)
+            .with(anything())
+            .wasInvoked(Times(1))
+
+        verify(messageSender)
+            .suspendFunction(messageSender::trySendingOutgoingMessageById)
+            .with(anything(), anything())
+            .wasInvoked(Times(1))
+
+        assertIs<SendTextMessageResult.Failure>(result)
+    }
+
+    @Test
+    fun givenRecipients_whenCreatingAnEnvelope_thenProteusClientShouldBeUsedToEncryptForEachClient12345() = runTest {
+        //given
+        given(syncManager)
+            .suspendFunction(syncManager::waitForSlowSyncToComplete)
+            .whenInvoked()
+            .thenReturn(Unit)
+
+        given(userRepository)
+            .suspendFunction(userRepository::getSelfUser)
+            .whenInvoked()
+            .thenReturn(flowOf(TestUser.SELF))
+
+        given(clientRepository)
+            .suspendFunction(clientRepository::currentClientId)
+            .whenInvoked()
+            .thenReturn(Either.Right(PlainId("test")))
+
+        given(messageRepository)
+            .suspendFunction(messageRepository::persistMessage)
+            .whenInvokedWith(anything())
+            .thenReturn(Either.Right(Unit))
+
+        given(messageSender)
+            .suspendFunction(messageSender::trySendingOutgoingMessageById)
+            .whenInvokedWith(anything(), anything())
+            .thenReturn(Either.Right(Unit))
+        //when
+        val result = sendTextMessageUseCase(ConversationId("test", "test"), "text")
+
+        //then
+        verify(messageRepository)
+            .suspendFunction(messageRepository::persistMessage)
+            .with(eq(TEST_MESSAGE))
+            .wasInvoked(Times(3))
+
+
+        verify(messageSender)
+            .suspendFunction(messageSender::trySendingOutgoingMessageById)
+            .with(anything(), anything())
+            .wasInvoked(Times(1))
+
+        assertIs<SendTextMessageResult.Success>(result)
+    }
+
+    companion object {
+        val TEST_MESSAGE = Message(
+            id = "test",
+            content = MessageContent.Text("test"),
+            conversationId = ConversationId("test", "test"),
+            date = Clock.System.now().toString(),
+            senderUserId = UserId("test", "test"),
+            senderClientId = ClientId("test"),
+            status = Message.Status.PENDING
+        )
+    }
+
+}
