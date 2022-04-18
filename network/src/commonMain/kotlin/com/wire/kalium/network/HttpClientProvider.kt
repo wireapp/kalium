b1362001f26888cd86d008a4919d662138c1b382
Gonzalo Gran Crespo <gongracr@gmail.com>
2022-04-18 18:08:24 +0200

    Revert "Bumped ktor to 2.0.0 version"
    This reverts commit aa2a0e22cc0ceaa74a3332efcc37c9cc312d928a.

--- network/src/commonMain/kotlin/com/wire/kalium/network/HttpClientProvider.kt
+++ network/src/commonMain/kotlin/com/wire/kalium/network/HttpClientProvider.kt
@@ -5,7 +5,7 @@ import com.wire.kalium.network.tools.ServerConfigDTO
 import io.ktor.client.HttpClient
 import io.ktor.client.HttpClientConfig
 import io.ktor.client.engine.HttpClientEngine
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
