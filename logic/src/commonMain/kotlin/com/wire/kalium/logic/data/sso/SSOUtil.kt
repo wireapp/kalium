44e1ee9ce17425c3bec455370db302c721307629
Mojtaba Chenani <chenani@outlook.com>
2022-04-20 00:21:39 +0200

    chore: update cookie param

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/data/sso/SSOUtil.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/data/sso/SSOUtil.kt
@@ -2,12 +2,12 @@ package com.wire.kalium.logic.data.sso
 
 object SSOUtil {
     internal fun generateSuccessRedirect(serverConfigId: String) =
-        "wire://$SSO_LOGIN_HOST/$SUCCESS_PATH/?$QUERY_COOKIES=\$cookie&$QUERY_USER_ID=\$userid\$$QUERY_SERVER_CONFIG=${serverConfigId}"
+        "wire://$SSO_LOGIN_HOST/$SUCCESS_PATH/?$QUERY_COOKIE=\$cookie&$QUERY_USER_ID=\$userid\$$QUERY_SERVER_CONFIG=${serverConfigId}"
 
     internal fun generateErrorRedirect() = "wire://$SSO_LOGIN_HOST/$ERROR_PATH/?$QUERY_ERROR=\$label"
 
     const val QUERY_USER_ID = "userId"
-    const val QUERY_COOKIES = "cookies"
+    const val QUERY_COOKIE = "cookie"
     const val QUERY_SERVER_CONFIG = "location"
     const val QUERY_ERROR = "errorCode"
     const val SSO_LOGIN_HOST = "sso-login"
