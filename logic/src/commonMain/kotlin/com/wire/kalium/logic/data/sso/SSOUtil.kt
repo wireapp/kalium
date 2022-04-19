90a30f3eb633989dd10d40515444756a3594da4c
Mojtaba Chenani <chenani@outlook.com>
2022-04-19 16:57:02 +0200

    feat(sso): fix userId query param

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/data/sso/SSOUtil.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/data/sso/SSOUtil.kt
@@ -2,11 +2,11 @@ package com.wire.kalium.logic.data.sso
 
 object SSOUtil {
     internal fun generateSuccessRedirect(serverConfigId: String) =
-        "wire://$SUCCESS_PATH/?$QUERY_COOKIES=\$cookie&$QUERY_USER_ID=\$user\$$QUERY_SERVER_CONFIG=${serverConfigId}"
+        "wire://$SUCCESS_PATH/?$QUERY_COOKIES=\$cookie&$QUERY_USER_ID=\$userId\$$QUERY_SERVER_CONFIG=${serverConfigId}"
 
     internal fun generateErrorRedirect() = "wire://$SSO_LOGIN_HOST/$ERROR_PATH/?$QUERY_ERROR=\$label"
 
-    const val QUERY_USER_ID = "userId"
+    const val QUERY_USER_ID = "user"
     const val QUERY_COOKIES = "cookies"
     const val QUERY_SERVER_CONFIG = "location"
     const val QUERY_ERROR = "error"
