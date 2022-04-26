738e1d2c38d9406d9ab825ee56e8b3ccdb0f1103
Michał Saleniuk <30429749+saleniuk@users.noreply.github.com>
2022-04-26 11:46:45 +0200

    feat: add GetSelfTeamUseCase (#440)

---
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/team/GetSelfTeamUseCase.kt
@@ -0,0 +1,26 @@
+package com.wire.kalium.logic.feature.team
+
+import com.wire.kalium.logic.data.team.Team
+import com.wire.kalium.logic.data.team.TeamRepository
+import com.wire.kalium.logic.data.user.UserRepository
+import com.wire.kalium.logic.sync.SyncManager
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flow
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class GetSelfTeamUseCase(
+    private val userRepository: UserRepository,
+    private val teamRepository: TeamRepository,
+    private val syncManager: SyncManager
+) {
+    suspend operator fun invoke(): Flow<Team?> {
+        syncManager.waitForSlowSyncToComplete()
+        return userRepository.getSelfUser()
+            .flatMapLatest {
+                if (it.team != null) teamRepository.getTeam(it.team)
+                else flow { emit(it.team) }
+            }
+    }
+}
