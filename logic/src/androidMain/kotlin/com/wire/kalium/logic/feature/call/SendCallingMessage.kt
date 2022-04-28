4af87436d5782ff75ac0257940bf43ee0c59f71b
alexandreferris <ferris.alexandre@gmail.com>
2022-04-28 11:18:07 +0200

    chore: add send http response message

--- logic/src/androidMain/kotlin/com/wire/kalium/logic/feature/call/SendCallingMessage.kt
+++ logic/src/androidMain/kotlin/com/wire/kalium/logic/feature/call/SendCallingMessage.kt
@@ -1,16 +1,18 @@
 package com.wire.kalium.logic.feature.call
 
+import com.wire.kalium.logic.CoreFailure
 import com.wire.kalium.logic.data.conversation.ClientId
 import com.wire.kalium.logic.data.id.ConversationId
 import com.wire.kalium.logic.data.message.Message
 import com.wire.kalium.logic.data.message.MessageContent
 import com.wire.kalium.logic.data.user.UserId
+import com.wire.kalium.logic.functional.Either
 import kotlinx.datetime.Clock
 import java.util.UUID
 
-suspend fun CallManagerImpl.sendCallingMessage(conversationId: ConversationId, userId: UserId, clientId: ClientId, data: String) {
+suspend fun CallManagerImpl.sendCallingMessage(conversationId: ConversationId, userId: UserId, clientId: ClientId, data: String): Either<CoreFailure, Unit> {
     val messageContent = MessageContent.Calling(data)
     val date = Clock.System.now().toString()
     val message = Message(UUID.randomUUID().toString(), messageContent, conversationId, date, userId, clientId, Message.Status.SENT)
-    messageSender.trySendingOutgoingMessage(conversationId, message)
+    return messageSender.trySendingOutgoingMessage(conversationId, message)
 }
