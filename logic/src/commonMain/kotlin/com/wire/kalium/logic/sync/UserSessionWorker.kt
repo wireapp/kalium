610a5c9ca783ed782113583c425de46eb97a70b8
Vitor Hugo Schwaab <vitor@schwaab.xyz>
2022-05-03 00:59:29 +0200

    chore: add TODO about refactoring `UserSessionWorker` to enable testing

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/sync/UserSessionWorker.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/sync/UserSessionWorker.kt
@@ -8,6 +8,9 @@ sealed class Result {
     object Retry : Result()
 }
 
+// TODO: 03/05/2022 Remove mandatory UserSessionScope to enable testing of workers
+//  We can have specialised factories to create workers if needed, passing only
+//  dependencies instead of a full UserSessionScope, see how Android schedules PendingMessagesSenderWorker
 abstract class UserSessionWorker(val userSessionScope: UserSessionScope) {
 
     abstract suspend fun doWork(): Result
