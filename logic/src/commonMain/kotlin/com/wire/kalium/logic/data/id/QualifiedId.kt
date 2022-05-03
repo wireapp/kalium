978fac91a2b1b91d8f0979de1d8a7b4d9461bfec
Gonzalo Gran Crespo <gongracr@gmail.com>
2022-05-03 14:33:23 +0200

    Added exception if parsing fails

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/data/id/QualifiedId.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/data/id/QualifiedId.kt
@@ -33,5 +33,6 @@ fun String.toConversationId(): ConversationId {
 
 fun String.parseIntoQualifiedID(): QualifiedID {
     val components = split("@")
+    if (components.size < 2) throw IllegalStateException("The string trying to parse is not a valid one")
     return QualifiedID(components.last(), components.first())
 }
