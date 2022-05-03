bd3d7b9b0d546b688e4c7cc9ec85729ee9def40e
vitorhugods <vitor@schwaab.xyz>
2022-05-03 10:50:05 +0000

    Deploying to gh-pages from @ wireapp/kalium@55547e656be1a4a90c837c9460816b4d96763a38 🚀

---
+++ docs/calling/html/scripts/sourceset_dependencies.js
@@ -0,0 +1,1 @@
+sourceset_dependencies='{":calling:dokkaHtml/androidAndroidTestRelease":[],":calling:dokkaHtml/androidDebug":[],":calling:dokkaHtml/androidMain":[":calling:dokkaHtml/commonMain"],":calling:dokkaHtml/androidRelease":[],":calling:dokkaHtml/commonMain":[],":calling:dokkaHtml/jvmMain":[":calling:dokkaHtml/commonMain"]}'
