6812e5826a73a4db00452f8534bc120b4cb8fa78
alexandreferris <ferris.alexandre@gmail.com>
2022-05-04 16:59:33 +0200

    chore: move extension function to private fun where it is only used

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/util/StringExtension.kt
+++
@@ -1,7 +0,0 @@
-package com.wire.kalium.logic.util
-
-private const val DOMAIN_SEPARATOR = "@"
-
-fun String.removeDomain() = if (contains(DOMAIN_SEPARATOR)) split(DOMAIN_SEPARATOR).first() else ""
-
-fun String.getDomain() = if (contains(DOMAIN_SEPARATOR)) split(DOMAIN_SEPARATOR).last() else ""
