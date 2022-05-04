8ce62c82c59b00da225a5e741ef33899d9b6e452
alexandreferris <ferris.alexandre@gmail.com>
2022-05-04 16:59:11 +0200

    chore: rework avsclientlist to json string

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/data/call/AvsClient.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/data/call/AvsClient.kt
@@ -1,17 +1,17 @@
 package com.wire.kalium.logic.data.call
 
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+
+@Serializable
 data class AvsClient(
     val userId: String,
     val clientId: String
 )
 
+@Serializable
 data class AvsClientList(
     val clients: List<AvsClient>
 ) {
-    fun toJsonString(): String {
-        val clientsListContent = clients.joinToString(separator = ",") {
-            """{"userid": "${it.userId}","clientid": "${it.clientId}"}"""
-        }
-        return """{"clients":[$clientsListContent]}"""
-    }
+    fun toJsonString(): String = Json { isLenient = true }.encodeToString(serializer(), this)
 }
