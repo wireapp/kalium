3b8a5f9dd6a7cb63a854cea17a2eededc57f3da6
alexandreferris <ferris.alexandre@gmail.com>
2022-05-04 15:35:44 +0200

    chore: rework AvsClient toJsonString function

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/data/call/AvsClient.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/data/call/AvsClient.kt
@@ -9,17 +9,9 @@ data class AvsClientList(
     val clients: List<AvsClient>
 ) {
     fun toJsonString(): String {
-        val start = "{"
-        var startClients = "clients:["
-
-        clients.forEachIndexed { index, avsClient ->
-            startClients += if (index > 0) "," else ""
-            startClients += "{userid: ${avsClient.userId},clientid: ${avsClient.clientId}}"
+        val clientsListContent = clients.joinToString(separator = ",") {
+            """{"userid": "${it.userId}","clientid": "${it.clientId}"}"""
         }
-
-        val endClients = "]"
-        val end = "}"
-
-        return "$start$startClients$endClients$end"
+        return """{"clients":[$clientsListContent]}"""
     }
 }
