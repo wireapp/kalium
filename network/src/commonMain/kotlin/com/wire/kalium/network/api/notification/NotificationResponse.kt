9d4a355e516d43178a228caf8486a58cabc7028f
Vitor Hugo Schwaab <vitor@schwaab.xyz>
2022-05-04 20:22:37 +0200

    chore: add missing @Serializable annotation

--- network/src/commonMain/kotlin/com/wire/kalium/network/api/notification/NotificationResponse.kt
+++ network/src/commonMain/kotlin/com/wire/kalium/network/api/notification/NotificationResponse.kt
@@ -1,8 +1,10 @@
 package com.wire.kalium.network.api.notification
 
 import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.Transient
 
+@Serializable
 data class NotificationResponse(
     @SerialName("time") val time: String,
     @SerialName("has_more") val hasMore: Boolean,
