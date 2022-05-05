0998ccc434228723250ebeeb460c2735a3218a74
Alexandre Ferris <ferris.alexandre@gmail.com>
2022-05-05 16:41:37 +0200

    chore: Add AVS Participants Changed (#469)
    * chore: add new data classes and new Call attributes
    
    * chore: add new handler and native calling function
    
    * chore: add implementation of participants changed handler
    
    * chore: add todos for next steps on calling instance ready
    
    * fix: adjust toString method
    
    * chore: remove implemented todo
    
    * chore: rework AvsClient toJsonString function
    
    * chore: rework avsclientlist to json string
    
    * chore: move extension function to private fun where it is only used
    
    * chore: add proper participants changed mapper
    
    * chore: rename from avs to call prefix
    
    * chore: rename functions from avs naming prefix
    
    * test: add CallMapper tests

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/call/Call.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/call/Call.kt
@@ -1,5 +1,6 @@
 package com.wire.kalium.logic.feature.call
 
+import com.wire.kalium.logic.data.call.Participant
 import com.wire.kalium.logic.data.id.ConversationId
 
 enum class CallStatus {
@@ -13,5 +14,7 @@ enum class CallStatus {
 
 data class Call(
     val conversationId: ConversationId,
-    val status: CallStatus
+    val status: CallStatus,
+    val participants: List<Participant> = emptyList(),
+    val maxParticipants: Int = 0 // Was used for tracking
 )
