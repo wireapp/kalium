24faea29f95ae7c0b0d69b317bd6d4f1737aebe6
yamilmedina <yamilmedina@users.noreply.github.com>
2022-05-05 11:23:39 +0200

    feat: add send connection req use case

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/connection/ConnectionScope.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/connection/ConnectionScope.kt
@@ -1,13 +1,15 @@
 package com.wire.kalium.logic.feature.connection
 
 import com.wire.kalium.logic.data.connection.ConnectionRepository
-import com.wire.kalium.logic.data.user.UserRepository
 
 class ConnectionScope(
     private val connectionRepository: ConnectionRepository
 ) {
 
-    internal val syncConnections: SyncConnectionsUseCase get() = SyncConnectionsUseCaseImpl(
-        connectionRepository = connectionRepository
-    )
+    internal val syncConnections: SyncConnectionsUseCase
+        get() = SyncConnectionsUseCaseImpl(
+            connectionRepository = connectionRepository
+        )
+
+    val sendConnectionRequestUseCase: SendConnectionRequestUseCase get() = SendConnectionRequestUseCaseImpl(connectionRepository)
 }
