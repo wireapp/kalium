a825e787799caaa90ad948c510c4ad430d08a283
Yamil Medina <yamilmedina@users.noreply.github.com>
2022-05-06 05:34:26 -0400

    feat: send connection request usecase (AR-1267) (#474)
    * feat: add send connection req use case
    
    * feat: extract userId for tests to variable
    
    * feat: extract userId for tests to variable
    
    * feat: extract userId for tests to variable
    
    * Update logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/connection/ConnectionScope.kt
    
    Co-authored-by: Alexandre Ferris <ferris.alexandre@gmail.com>
    
    Co-authored-by: Alexandre Ferris <ferris.alexandre@gmail.com>

---
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/connection/SendConnectionRequestUseCase.kt
@@ -0,0 +1,40 @@
+package com.wire.kalium.logic.feature.connection
+
+import com.wire.kalium.logic.CoreFailure
+import com.wire.kalium.logic.data.connection.ConnectionRepository
+import com.wire.kalium.logic.data.user.UserId
+import com.wire.kalium.logic.functional.suspending
+import com.wire.kalium.logic.kaliumLogger
+
+/**
+ * Use Case that allows a user send a connection request to connect with another User
+ */
+fun interface SendConnectionRequestUseCase {
+    /**
+     * Use case [SendConnectionRequestUseCase] operation
+     *
+     * @param userId the target user to connect with
+     * @return a [SendConnectionRequestResult] indicating the operation result
+     */
+    suspend operator fun invoke(userId: UserId): SendConnectionRequestResult
+}
+
+internal class SendConnectionRequestUseCaseImpl(
+    private val connectionRepository: ConnectionRepository
+) : SendConnectionRequestUseCase {
+
+    override suspend fun invoke(userId: UserId): SendConnectionRequestResult = suspending {
+        connectionRepository.sendUserConnection(userId)
+            .coFold({ coreFailure ->
+                kaliumLogger.e("An error occurred when sending a connection request to $userId")
+                SendConnectionRequestResult.Failure(coreFailure)
+            }, {
+                SendConnectionRequestResult.Success
+            })
+    }
+}
+
+sealed class SendConnectionRequestResult {
+    object Success : SendConnectionRequestResult()
+    class Failure(val coreFailure: CoreFailure) : SendConnectionRequestResult()
+}
