b845968667193b0e174c622b007af253a8ef420d
vitorhugods <vitor@schwaab.xyz>
2022-05-10 19:12:41 +0200

    chore: fix recursive type resolution

--- logic/src/commonTest/kotlin/com/wire/kalium/logic/util/EitherAssertions.kt
+++ logic/src/commonTest/kotlin/com/wire/kalium/logic/util/EitherAssertions.kt
@@ -9,7 +9,7 @@ inline infix fun <L, R> Either<L, R>.shouldSucceed(crossinline successAssertion:
 
 fun <L> Either<L, Unit>.shouldSucceed() = shouldSucceed { }
 
-inline infix fun <L, R> Either<L, R>.shouldFail(crossinline failAssertion: (L) -> Unit) =
+inline infix fun <L, R> Either<L, R>.shouldFail(crossinline failAssertion: (L) -> Unit): Unit =
     this.fold({ failAssertion(it) }) { fail("Expected a Left value but got Right") }
 
-fun <L> Either<L, Unit>.shouldFail() = shouldFail { }
+fun <L> Either<L, Unit>.shouldFail(): Unit = shouldFail { Unit }
