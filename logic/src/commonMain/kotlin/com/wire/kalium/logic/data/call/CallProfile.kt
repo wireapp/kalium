20f1dd10d2921957dbe444e641e881433c4f9148
Oussama <oussama.has100@gmail.com>
2022-05-10 10:34:45 +0200

    refactor: remove unused methods

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/data/call/CallProfile.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/data/call/CallProfile.kt
@@ -6,11 +6,5 @@ import com.wire.kalium.logic.feature.call.CallStatus
 data class CallProfile(
     val calls: Map<String, Call>
 ) {
-
-    private val callsList get() = calls.values
-
     operator fun get(conversationId: String): Call? = calls[conversationId]
-
-    fun incomingCalls(): List<Call> = callsList
-        .filter { it.status == CallStatus.INCOMING }
 }
