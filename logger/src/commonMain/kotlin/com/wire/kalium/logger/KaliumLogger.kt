bcf836d8ae3adfe935da4b16ef78b4e26458e652
Mohammed Mokresh <mokresh.mohammed@gmail.com>
2022-05-11 17:05:51 +0200

    feat: add log writer to the kermit config [AR-1450] (#485)
    * add logwriter to kalium logger
    
    * empty commit
    
    * add comments
    
    * return the result from save notifications use case
    
    * create enable logging shared prefrence
    
    * add is loggin available use case
    
    * update IsLoggingEnabledUseCase
    
    * update set loggin level
    
    * revert kermit version to 1.0.0
    
    * extract usecases interfaces
    
    Co-authored-by: Mohamad Mukresh <mohamadmukresh@Mohamads-MacBook-Pro.local>
    Co-authored-by: Mohamad Mukresh <mohamadmukresh@Mohamads-MBP.home>
    Co-authored-by: Mohamad Jaara <mohamad.jaara@wire.com>

--- logger/src/commonMain/kotlin/com/wire/kalium/logger/KaliumLogger.kt
+++ logger/src/commonMain/kotlin/com/wire/kalium/logger/KaliumLogger.kt
@@ -1,5 +1,6 @@
 package com.wire.kalium.logger
 
+import co.touchlab.kermit.LogWriter
 import co.touchlab.kermit.Severity
 import co.touchlab.kermit.StaticConfig
 import co.touchlab.kermit.Logger as KermitLogger
@@ -41,17 +42,31 @@ enum class KaliumLogLevel {
     DISABLED
 }
 
-class KaliumLogger(config: Config) {
+/**
+ * the logWriter is to create a custom writer other than the existing log writers from kermit to intercept the logs
+ * in the android case we use it to write the logs on file
+ *
+ */
+class KaliumLogger(config: Config, logWriter: LogWriter? = null) {
 
     private val kermitLogger: KermitLogger
 
     init {
-        kermitLogger = KermitLogger(
-            config = StaticConfig(
-                minSeverity = config.severityLevel
-            ),
-            tag = config.tag
-        )
+        kermitLogger = if (logWriter == null) {
+            KermitLogger(
+                config = StaticConfig(
+                    minSeverity = config.severityLevel
+                ),
+                tag = config.tag
+            )
+        } else {
+            KermitLogger(
+                config = StaticConfig(
+                    minSeverity = config.severityLevel, listOf(logWriter)
+                ),
+                tag = config.tag
+            )
+        }
     }
 
     val severity = config.severity
