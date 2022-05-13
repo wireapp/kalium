f50aed9d36c2375c2e6ae85eda9d7498e0cab59c
Gonzalo Gran Crespo <gongracr@gmail.com>
2022-05-13 10:23:17 +0200

    code cleaning

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/asset/UpdateAssetMessageDownloadStatusUseCase.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/asset/UpdateAssetMessageDownloadStatusUseCase.kt
@@ -2,21 +2,22 @@ package com.wire.kalium.logic.feature.asset
 
 import com.wire.kalium.logic.CoreFailure
 import com.wire.kalium.logic.data.id.ConversationId
-import com.wire.kalium.logic.data.message.Message
+import com.wire.kalium.logic.data.message.Message.DownloadStatus
 import com.wire.kalium.logic.data.message.MessageRepository
+import com.wire.kalium.logic.functional.fold
 
 interface UpdateAssetMessageDownloadStatusUseCase {
-    suspend operator fun invoke(downloadStatus: Message.DownloadStatus, conversationId: ConversationId, messageId: String): Result
+    suspend operator fun invoke(downloadStatus: DownloadStatus, conversationId: ConversationId, messageId: String): Result
 }
 
 class UpdateAssetMessageDownloadStatusUseCaseImpl(
     private val messageRepository: MessageRepository
 ) : UpdateAssetMessageDownloadStatusUseCase {
 
-    override suspend operator fun invoke(downloadStatus: Message.DownloadStatus, conversationId: ConversationId, messageId: String): Result {
+    override suspend operator fun invoke(downloadStatus: DownloadStatus, conversationId: ConversationId, messageId: String): Result {
         return messageRepository.updateAssetMessageDownloadStatus(downloadStatus, conversationId, messageId).fold({
             Result.Failure(it)
-        },{
+        }, {
             Result.Success
         })
     }
