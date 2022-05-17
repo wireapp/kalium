0d7cf3df7bf57966d8bd20518bc1f884e819aafd
Vitor Hugo Schwaab <vitor@schwaab.xyz>
2022-05-17 18:25:35 +0200

    refactor: hold the sync state in a repository (AR-1438) (#504)

---
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/data/sync/SyncRepository.kt
@@ -0,0 +1,19 @@
+package com.wire.kalium.logic.data.sync
+
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.updateAndGet
+
+interface SyncRepository {
+    val syncState: StateFlow<SyncState>
+    fun updateSyncState(updateBlock: (currentState: SyncState) -> SyncState): SyncState
+}
+
+internal class InMemorySyncRepository : SyncRepository {
+    private val _syncState = MutableStateFlow(SyncState.WAITING)
+
+    override val syncState: StateFlow<SyncState> get() = _syncState
+
+    override fun updateSyncState(updateBlock: (currentState: SyncState) -> SyncState): SyncState =
+        _syncState.updateAndGet(updateBlock)
+}
