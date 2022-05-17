24ded5993e262d7a79057cf30c4a8c8e35b6a8cf
Mateusz <m.pachulski94@gmail.com>
2022-05-17 10:45:18 +0200

    add unit tests to search known user use case

---
+++ logic/src/commonTest/kotlin/com/wire/kalium/logic/feature/user/SearchKnownUserUseCaseTest.kt
@@ -0,0 +1,121 @@
+package com.wire.kalium.logic.feature.user
+
+import com.wire.kalium.logic.data.id.QualifiedID
+import com.wire.kalium.logic.data.publicuser.SearchUserRepository
+import com.wire.kalium.logic.data.publicuser.model.OtherUser
+import com.wire.kalium.logic.data.publicuser.model.UserSearchResult
+import com.wire.kalium.logic.data.user.ConnectionState
+import com.wire.kalium.logic.feature.publicuser.SearchKnownUsersUseCase
+import com.wire.kalium.logic.feature.publicuser.SearchKnownUsersUseCaseImpl
+import io.ktor.client.plugins.convertLongTimeoutToLongWithInfiniteAsZero
+import io.mockative.Mock
+import io.mockative.Times
+import io.mockative.anything
+import io.mockative.classOf
+import io.mockative.eq
+import io.mockative.given
+import io.mockative.mock
+import io.mockative.verify
+import kotlinx.coroutines.test.runTest
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+
+class SearchKnownUserUseCaseTest {
+
+    @Mock
+    private val searchUserRepository = mock(classOf<SearchUserRepository>())
+
+    private lateinit var searchKnownUsersUseCase: SearchKnownUsersUseCase
+
+    @BeforeTest
+    fun setUp() {
+        searchKnownUsersUseCase = SearchKnownUsersUseCaseImpl(searchUserRepository)
+    }
+
+    @Test
+    fun whenSearchingWithQueryInHandleFormat_ThenSearchOnlyByHandle() = runTest {
+        //when
+        val handleSearchQuery = "@someHandle"
+
+        given(searchUserRepository)
+            .suspendFunction(searchUserRepository::searchKnownUsersByHandle)
+            .whenInvokedWith(eq(handleSearchQuery))
+            .thenReturn(
+                UserSearchResult(
+                    listOf(
+                        OtherUser(
+                            id = QualifiedID(
+                                value = "someValue",
+                                domain = "someDomain",
+                            ),
+                            name = null,
+                            handle = null,
+                            email = null,
+                            phone = null,
+                            accentId = 0,
+                            team = null,
+                            connectionStatus = ConnectionState.ACCEPTED,
+                            previewPicture = null,
+                            completePicture = null
+                        )
+                    )
+                )
+            )
+        //given
+        searchKnownUsersUseCase(handleSearchQuery)
+        //then
+        verify(searchUserRepository)
+            .suspendFunction(searchUserRepository::searchKnownUsersByHandle)
+            .with(eq(handleSearchQuery))
+            .wasInvoked()
+
+        verify(searchUserRepository)
+            .suspendFunction(searchUserRepository::searchKnownUsersByNameOrHandleOrEmail)
+            .with(anything())
+            .wasNotInvoked()
+    }
+
+    @Test
+    fun whenSearchingQueryHasNotHandleFormat_ThenSearchByNameOrHandleOrEmail() = runTest {
+        //when
+        val searchQuery = "someSearchQuery"
+
+        given(searchUserRepository)
+            .suspendFunction(searchUserRepository::searchKnownUsersByNameOrHandleOrEmail)
+            .whenInvokedWith(eq(searchQuery))
+            .thenReturn(
+                UserSearchResult(
+                    listOf(
+                        OtherUser(
+                            id = QualifiedID(
+                                value = "someValue",
+                                domain = "someDomain",
+                            ),
+                            name = null,
+                            handle = null,
+                            email = null,
+                            phone = null,
+                            accentId = 0,
+                            team = null,
+                            connectionStatus = ConnectionState.ACCEPTED,
+                            previewPicture = null,
+                            completePicture = null
+                        )
+                    )
+                )
+            )
+        //given
+        searchKnownUsersUseCase(searchQuery)
+        //then
+        verify(searchUserRepository)
+            .suspendFunction(searchUserRepository::searchKnownUsersByHandle)
+            .with(anything())
+            .wasNotInvoked()
+
+        verify(searchUserRepository)
+            .suspendFunction(searchUserRepository::searchKnownUsersByNameOrHandleOrEmail)
+            .with(eq(searchQuery))
+            .wasInvoked()
+    }
+
+}
