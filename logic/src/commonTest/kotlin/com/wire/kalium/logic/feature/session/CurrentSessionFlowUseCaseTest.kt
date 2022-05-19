92b37652d52c001a2066d930af70d3809d25bd38
Michał Saleniuk <saleniuk@gmail.com>
2022-05-19 10:32:02 +0200

    merge fix

--- logic/src/commonTest/kotlin/com/wire/kalium/logic/feature/session/CurrentSessionFlowUseCaseTest.kt
+++ logic/src/commonTest/kotlin/com/wire/kalium/logic/feature/session/CurrentSessionFlowUseCaseTest.kt
@@ -2,7 +2,7 @@ package com.wire.kalium.logic.feature.session
 
 import app.cash.turbine.test
 import com.wire.kalium.logic.StorageFailure
-import com.wire.kalium.logic.configuration.ServerConfig
+import com.wire.kalium.logic.configuration.server.ServerConfig
 import com.wire.kalium.logic.data.session.SessionRepository
 import com.wire.kalium.logic.data.user.UserId
 import com.wire.kalium.logic.feature.auth.AuthSession
