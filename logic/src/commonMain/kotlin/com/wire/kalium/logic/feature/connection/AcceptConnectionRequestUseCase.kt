c2a3a8c5bff63003641c2f58477f4f54f390f30f
yamilmedina <yamilmedina@users.noreply.github.com>
2022-05-20 12:19:18 +0200

    feat: connect usecase to repo for accepting a connection and tests

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/connection/AcceptConnectionRequestUseCase.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/connection/AcceptConnectionRequestUseCase.kt
@@ -2,8 +2,9 @@ package com.wire.kalium.logic.feature.connection
 
 import com.wire.kalium.logic.CoreFailure
 import com.wire.kalium.logic.data.connection.ConnectionRepository
+import com.wire.kalium.logic.data.user.ConnectionState
 import com.wire.kalium.logic.data.user.UserId
-import com.wire.kalium.logic.kaliumLogger
+import com.wire.kalium.logic.functional.fold
 
 /**
  * Use Case that allows a user accept a connection request to connect with another User
@@ -23,8 +24,12 @@ internal class AcceptConnectionRequestUseCaseImpl(
 ) : AcceptConnectionRequestUseCase {
 
     override suspend fun invoke(userId: UserId): AcceptConnectionRequestUseCaseResult {
-        kaliumLogger.d("accepting a connection request")
-        return AcceptConnectionRequestUseCaseResult.Success
+        return connectionRepository.updateConnectionStatus(userId, ConnectionState.ACCEPTED)
+            .fold({
+                AcceptConnectionRequestUseCaseResult.Failure(it)
+            }, {
+                AcceptConnectionRequestUseCaseResult.Success
+            })
     }
 }
 
