ce398b73cc774e705aa9a7d4747590a81095b99e
Vitor Hugo Schwaab <vitor@schwaab.xyz>
2022-05-24 16:27:03 +0200

    style: suppress old LongParameterList issue

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/AuthenticatedDataSourceSet.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/AuthenticatedDataSourceSet.kt
@@ -8,6 +8,7 @@ import com.wire.kalium.persistence.db.UserDatabaseProvider
 import com.wire.kalium.persistence.kmm_settings.EncryptedSettingsHolder
 import com.wire.kalium.persistence.kmm_settings.KaliumPreferencesSettings
 
+@Suppress("LongParameterList") // Suppressed as it's an old issue
 class AuthenticatedDataSourceSet(
     val authenticatedRootDir: String,
     val authenticatedNetworkContainer: AuthenticatedNetworkContainer,
