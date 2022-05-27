46f474edbe031c74e9f960295f08f07f39a221bf
Vitor Hugo Schwaab <vitor@schwaab.xyz>
2022-05-27 19:24:24 +0200

    test: use `TestKaliumDispatcher` to create `SyncManager`

--- logic/src/commonTest/kotlin/com/wire/kalium/logic/sync/SyncManagerTest.kt
+++ logic/src/commonTest/kotlin/com/wire/kalium/logic/sync/SyncManagerTest.kt
@@ -54,7 +54,8 @@ class SyncManagerTest {
             workScheduler,
             eventRepository,
             syncRepository,
-            conversationEventReceiver
+            conversationEventReceiver,
+            TestKaliumDispatcher
         )
     }
 
