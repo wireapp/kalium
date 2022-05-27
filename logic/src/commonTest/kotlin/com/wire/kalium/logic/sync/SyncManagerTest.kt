3eb3e4e81ed7f4481add392c3933daa8f0af5bae
Vitor Hugo Schwaab <vitor@schwaab.xyz>
2022-05-27 14:42:37 +0200

    test: move sync tests from UseCase to the SyncManager

--- logic/src/commonTest/kotlin/com/wire/kalium/logic/sync/SyncManagerTest.kt
+++ logic/src/commonTest/kotlin/com/wire/kalium/logic/sync/SyncManagerTest.kt
@@ -2,18 +2,27 @@ package com.wire.kalium.logic.sync
 
 import app.cash.turbine.test
 import com.wire.kalium.logic.NetworkFailure
+import com.wire.kalium.logic.data.event.Event
 import com.wire.kalium.logic.data.event.EventRepository
 import com.wire.kalium.logic.data.sync.InMemorySyncRepository
 import com.wire.kalium.logic.data.sync.SyncRepository
 import com.wire.kalium.logic.data.sync.SyncState
+import com.wire.kalium.logic.framework.TestConversation
+import com.wire.kalium.logic.framework.TestUser
 import com.wire.kalium.logic.functional.Either
 import com.wire.kalium.logic.test_util.TestKaliumDispatcher
+import com.wire.kalium.network.api.conversation.ConversationMembers
 import io.mockative.Mock
+import io.mockative.configure
+import io.mockative.eq
 import io.mockative.given
 import io.mockative.mock
 import io.mockative.once
 import io.mockative.verify
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
@@ -29,10 +38,11 @@ class SyncManagerTest {
     private val workScheduler = FakeWorkScheduler()
 
     @Mock
-    private val eventRepository: EventRepository = mock(EventRepository::class)
+    private val eventRepository: EventRepository = configure(mock(EventRepository::class)) { stubsUnitByDefault = true }
 
     @Mock
-    private val conversationEventReceiver: ConversationEventReceiver = mock(ConversationEventReceiver::class)
+    private val conversationEventReceiver: ConversationEventReceiver =
+        configure(mock(ConversationEventReceiver::class)) { stubsUnitByDefault = true }
 
     private lateinit var syncRepository: SyncRepository
     private lateinit var syncManager: SyncManager
@@ -167,6 +177,138 @@ class SyncManagerTest {
 
         verify(eventRepository)
             .suspendFunction(eventRepository::liveEvents)
+            .wasInvoked(exactly = once)
+    }
+
+    @Test
+    fun givenSlowSyncCompletedAndAPendingEvent_whenSyncing_thenTheLastProcessedEventIdIsUpdated() = runTest(TestKaliumDispatcher.default) {
+        //Given
+        val eventId = "eventId"
+        val event = Event.Conversation.MemberJoin(
+            eventId,
+            TestConversation.ID,
+            TestUser.USER_ID,
+            ConversationMembers(listOf(), listOf()),
+            "from"
+        )
+
+        given(eventRepository)
+            .suspendFunction(eventRepository::pendingEvents)
+            .whenInvoked()
+            .thenReturn(flowOf(Either.Right(event)))
+
+        given(eventRepository)
+            .suspendFunction(eventRepository::liveEvents)
+            .whenInvoked()
+            .thenReturn(Either.Right(emptyFlow()))
+
+        //When
+        syncManager.onSlowSyncComplete()
+        advanceUntilIdle()
+
+        //Then
+        verify(eventRepository)
+            .suspendFunction(eventRepository::updateLastProcessedEventId)
+            .with(eq(eventId))
+            .wasInvoked(exactly = once)
+    }
+
+    @Test
+    fun givenSlowSyncCompletedAndAPendingEvent_whenSyncing_thenTheEventReceiverIsCalled() = runTest(TestKaliumDispatcher.default) {
+        //Given
+        val eventId = "eventId"
+        val event = Event.Conversation.MemberJoin(
+            eventId,
+            TestConversation.ID,
+            TestUser.USER_ID,
+            ConversationMembers(listOf(), listOf()),
+            "from"
+        )
+        given(eventRepository)
+            .suspendFunction(eventRepository::pendingEvents)
+            .whenInvoked()
+            .thenReturn(flowOf(Either.Right(event)))
+
+        given(eventRepository)
+            .suspendFunction(eventRepository::liveEvents)
+            .whenInvoked()
+            .thenReturn(Either.Right(emptyFlow()))
+
+        //When
+        syncManager.onSlowSyncComplete()
+        advanceUntilIdle()
+
+        //Then
+        verify(conversationEventReceiver)
+            .suspendFunction(conversationEventReceiver::onEvent)
+            .with(eq(event))
+            .wasInvoked(exactly = once)
+    }
+
+    @Test
+    fun givenSlowSyncCompletedAndALiveEvent_whenSyncing_thenTheLastProcessedEventIdIsUpdated() = runTest(TestKaliumDispatcher.default) {
+        //Given
+        val eventId = "eventId"
+        val event = Event.Conversation.MemberJoin(
+            eventId, TestConversation.ID, TestUser.USER_ID, ConversationMembers(
+                listOf(), listOf()
+            ), "from"
+        )
+        val liveEventsChannel = Channel<Event>()
+        given(eventRepository)
+            .suspendFunction(eventRepository::pendingEvents)
+            .whenInvoked()
+            .thenReturn(flowOf(Either.Right(event)))
+
+        given(eventRepository)
+            .suspendFunction(eventRepository::liveEvents)
+            .whenInvoked()
+            .thenReturn(Either.Right(liveEventsChannel.receiveAsFlow()))
+
+        //When
+        syncManager.onSlowSyncComplete()
+        advanceUntilIdle()
+
+        liveEventsChannel.send(event)
+        //Then
+        verify(eventRepository)
+            .suspendFunction(eventRepository::updateLastProcessedEventId)
+            .with(eq(eventId))
+            .wasInvoked(exactly = once)
+    }
+
+    @Test
+    fun givenSlowSyncCompletedAndALiveEvent_whenSyncing_thenTheEventReceiverIsCalled() = runTest(TestKaliumDispatcher.default) {
+        //Given
+        val eventId = "eventId"
+        val event = Event.Conversation.MemberJoin(
+            eventId, TestConversation.ID, TestUser.USER_ID, ConversationMembers(
+                listOf(), listOf()
+            ), "from"
+        )
+        val liveEventsChannel = Channel<Event>()
+        given(eventRepository)
+            .suspendFunction(eventRepository::pendingEvents)
+            .whenInvoked()
+            .thenReturn(flowOf(Either.Right(event)))
+
+        given(eventRepository)
+            .suspendFunction(eventRepository::liveEvents)
+            .whenInvoked()
+            .then {
+                Either.Right(liveEventsChannel.receiveAsFlow())
+            }
+
+        //When
+        syncManager.onSlowSyncComplete()
+        advanceUntilIdle()
+
+        liveEventsChannel.send(event)
+
+        //Then
+        verify(conversationEventReceiver)
+            .suspendFunction(conversationEventReceiver::onEvent)
+            .with(eq(event))
             .wasInvoked(exactly = once)
     }
 }
