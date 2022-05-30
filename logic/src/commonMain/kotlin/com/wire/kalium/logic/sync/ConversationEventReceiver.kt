af3697c940cce5c297f17db6441dc171fb20ac8e
Mateusz <m.pachulski94@gmail.com>
2022-05-30 17:43:25 +0200

    add description

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/sync/ConversationEventReceiver.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/sync/ConversationEventReceiver.kt
@@ -202,7 +202,7 @@ class ConversationEventReceiver(
                 messageRepository.updateTextMessageContent(
                     conversationId = message.conversationId,
                     messageId = message.content.messageId,
-                    newTextContent = MessageContent.Text(message.content.newContent)
+                    newTextContent = MessageContent.Text(message.content.newContent),
                 ).flatMap {
                     messageRepository.markMessageAsEdited(
                         messageUuid = message.content.messageId,
@@ -210,6 +210,14 @@ class ConversationEventReceiver(
                         timeStamp = message.date
                     )
                 }.flatMap {
+                    // whenever "other" client updates the content message, it discards the previous id
+                    // and replaces it by a new id
+                    // in order to still point to the same message on our device and every other device displaying the message,
+                    // the "other client" sends us the "old" id, which will be equal to the
+                    // one we currently have, and a "new" id. we, first adjust the changes to the status and content
+                    // using the old id and after that we update the id which the one provided by the "other" client
+                    // when the update happens again we repeat the process, by doing it we always reference to the id
+                    // that the "other" client references, so that we can talk about the same message reference
                     messageRepository.updateMessageId(
                         conversationId = message.conversationId,
                         oldMessageId = message.content.messageId,
