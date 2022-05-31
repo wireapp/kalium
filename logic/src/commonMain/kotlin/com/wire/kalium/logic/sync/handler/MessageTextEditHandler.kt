f9f7b718d6c1c9a6eedf89f75a63199da36479da
Mateusz <m.pachulski94@gmail.com>
2022-05-31 13:30:03 +0200

    fix

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/sync/handler/MessageTextEditHandler.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/sync/handler/MessageTextEditHandler.kt
@@ -10,32 +10,30 @@ class MessageTextEditHandler(private val messageRepository: MessageRepository) {
     suspend fun handle(
         message: Message,
         messageContent: MessageContent.TextEdited
-    ) {
-        messageRepository.updateTextMessageContent(
+    ) = messageRepository.updateTextMessageContent(
+        conversationId = message.conversationId,
+        messageId = messageContent.messageId,
+        newTextContent = messageContent.newContent,
+    ).flatMap {
+        messageRepository.markMessageAsEdited(
+            messageUuid = messageContent.messageId,
             conversationId = message.conversationId,
-            messageId = messageContent.messageId,
-            newTextContent = messageContent.newContent,
-        ).flatMap {
-            messageRepository.markMessageAsEdited(
-                messageUuid = messageContent.messageId,
-                conversationId = message.conversationId,
-                timeStamp = message.date
-            )
-        }.flatMap {
-            // whenever "other" client updates the content message, it discards the previous id
-            // and replaces it by a new id
-            // in order to still point to the same message on our device and every other device displaying the message,
-            // the "other client" sends us the "old" id, which will be equal to the
-            // one we currently have, and a "new" id. we, first adjust the changes to the status and content
-            // using the old id and after that we update the id which the one provided by the "other" client
-            // when the update happens again we repeat the process, by doing it we always reference to the id
-            // that the "other" client references, so that we can talk about the same message reference
-            messageRepository.updateMessageId(
-                conversationId = message.conversationId,
-                oldMessageId = messageContent.messageId,
-                newMessageId = message.id
-            )
-        }
+            timeStamp = message.date
+        )
+    }.flatMap {
+        // whenever "other" client updates the content message, it discards the previous id
+        // and replaces it by a new id
+        // in order to still point to the same message on our device and every other device displaying the message,
+        // the "other client" sends us the "old" id, which will be equal to the
+        // one we currently have, and a "new" id. we, first adjust the changes to the status and content
+        // using the old id and after that we update the id which the one provided by the "other" client
+        // when the update happens again we repeat the process, by doing it we always reference to the id
+        // that the "other" client references, so that we can talk about the same message reference
+        messageRepository.updateMessageId(
+            conversationId = message.conversationId,
+            oldMessageId = messageContent.messageId,
+            newMessageId = message.id
+        )
     }
 
 }
