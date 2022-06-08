9a23d659f691cb8fb896d1ff9d5beb91662112c8
Mohamad Jaara <mohamad.jaara@wire.com>
2022-06-08 13:49:26 +0200

    add DeregisterTokenUseCase to logout usecase

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/session/DeregisterTokenUseCase.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/session/DeregisterTokenUseCase.kt
@@ -6,20 +6,25 @@ import com.wire.kalium.logic.data.client.ClientRepository
 import com.wire.kalium.logic.functional.flatMap
 import com.wire.kalium.logic.functional.fold
 
-class DeregisterTokenUseCase(
+interface DeregisterTokenUseCase {
+    suspend operator fun invoke()
+}
+class DeregisterTokenUseCaseImpl(
     private val clientRepository: ClientRepository,
     private val notificationTokenRepository: NotificationTokenRepository
-) {
+): DeregisterTokenUseCase {
     sealed class Result {
         object Success : Result()
         sealed class Failure : Result() {
+            object NotFound : Failure()
             data class Generic(val coreFailure: CoreFailure) : Failure()
         }
     }
 
-    suspend operator fun invoke(): Result = notificationTokenRepository.getNotificationToken().flatMap { notiToken ->
+    override suspend operator fun invoke(): Result = notificationTokenRepository.getNotificationToken().flatMap { notiToken ->
         clientRepository.deregisterToken(notiToken.token)
     }.fold({
+        TODO("check the error lable for token not found")
         Result.Failure.Generic(it)
     }, {
         Result.Success
