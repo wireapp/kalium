c5acbfb1531a0ccc04ad984ef0566899726b122a
yamilmedina <yamilmedina@users.noreply.github.com>
2022-06-10 10:13:39 +0200

    feat: fix compilation and tests

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/asset/DownloadAssetMessageMetadata.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/asset/DownloadAssetMessageMetadata.kt
@@ -1,9 +1,8 @@
 package com.wire.kalium.logic.feature.asset
 
-data class DownloadAssetMessageMetadata(
+class DownloadAssetMessageMetadata(
     val assetId: String,
     val assetDomain: String?,
     val assetToken: String?,
     val assetEncryptionKey: ByteArray
-) {
-}
+)
