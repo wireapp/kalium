f799d0ebe6df571b342a0edec5e46de7f704baee
vitorhugods <vitor@schwaab.xyz>
2022-06-15 14:20:19 +0200

    docs: remove unnecessary comment

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/sync/SyncManager.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/sync/SyncManager.kt
@@ -118,7 +118,6 @@ internal class SyncManagerImpl(
     private val eventProcessingScope = CoroutineScope(processingSupervisorJob + eventProcessingDispatcher + coroutineExceptionHandler)
     private var processingJob: Job? = null
 
-    // Do not access this variable directly of I will cut off your hands
     private val offlineEventBuffer = PendingEventsBuffer()
 
     private var processingEventFlow = MutableSharedFlow<Event>()
