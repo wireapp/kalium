4db3185adc6678d000796f4aac347580c8581361
Boris Safonov <boris.safonov@appunite.com>
2022-06-16 11:01:08 +0300

    feat: Apply UserStatus to IncomingCalls

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/call/CallsScope.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/call/CallsScope.kt
@@ -1,6 +1,8 @@
 package com.wire.kalium.logic.feature.call
 
 import com.wire.kalium.logic.data.call.CallRepository
+import com.wire.kalium.logic.data.conversation.ConversationRepository
+import com.wire.kalium.logic.data.user.UserRepository
 import com.wire.kalium.logic.feature.call.usecase.EndCallUseCase
 import com.wire.kalium.logic.feature.call.usecase.GetAllCallsUseCase
 import com.wire.kalium.logic.feature.call.usecase.GetIncomingCallsUseCase
@@ -20,6 +22,8 @@ import com.wire.kalium.logic.sync.SyncManager
 class CallsScope(
     private val callManager: Lazy<CallManager>,
     private val callRepository: CallRepository,
+    private val conversationRepository: ConversationRepository,
+    private val userRepository: UserRepository,
     private val flowManagerService: FlowManagerService,
     private val mediaManagerService: MediaManagerService,
     private val syncManager: SyncManager
@@ -40,7 +44,9 @@ class CallsScope(
     val getIncomingCalls: GetIncomingCallsUseCase
         get() = GetIncomingCallsUseCaseImpl(
             callRepository = callRepository,
-            syncManager = syncManager
+            syncManager = syncManager,
+            conversationRepository = conversationRepository,
+            userRepository = userRepository
         )
 
     val startCall: StartCallUseCase get() = StartCallUseCase(callManager)
