d583dcff5429a2a7bd8e9db54c12ff0c7a973edf
Mohamad Jaara <mohamad.jaara@wire.com>
2022-06-21 01:00:55 +0200

    use ktor util as base for Base64

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/util/Base64.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/util/Base64.kt
@@ -1,10 +1,16 @@
 package com.wire.kalium.logic.util
 
+import io.ktor.util.decodeBase64Bytes
+import io.ktor.util.encodeBase64
+import io.ktor.utils.io.core.toByteArray
+
 /**
  *
  * TODO(qol): Move to a utils module?
  */
-expect object Base64 {
-    fun encodeToBase64(originalString: ByteArray): ByteArray
-    fun decodeFromBase64(encoded: ByteArray): ByteArray
+object Base64 {
+    fun encodeToBase64(originalString: ByteArray): ByteArray = originalString.encodeBase64().toByteArray()
+    fun decodeFromBase64(encoded: ByteArray): ByteArray = encoded.decodeToString().decodeBase64Bytes()
+    fun encode(originalString: ByteArray): String = originalString.encodeBase64()
+    fun decode(encoded: String): ByteArray = encoded.decodeBase64Bytes()
 }
