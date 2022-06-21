5e785e8fc2a44f7f50ef506ebc7743c68d6e78ee
alexandreferris <ferris.alexandre@gmail.com>
2022-06-21 11:02:12 +0200

    chore: remove unused imports

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/call/Call.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/call/Call.kt
@@ -2,10 +2,7 @@ package com.wire.kalium.logic.feature.call
 
 import com.wire.kalium.logic.data.call.Participant
 import com.wire.kalium.logic.data.conversation.Conversation
-import com.wire.kalium.logic.data.conversation.ConversationDetails
 import com.wire.kalium.logic.data.id.ConversationId
-import com.wire.kalium.logic.data.publicuser.model.OtherUser
-import com.wire.kalium.logic.data.team.Team
 
 enum class CallStatus {
     STARTED,
