90b4f5871472e53c0d5dc26d2a54596af5c9645a
Vitor Hugo Schwaab <vitor@schwaab.xyz>
2022-06-22 15:16:35 +0200

    refactor: stop using WorkManager for SlowSync

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/sync/SlowSyncWorker.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/sync/SlowSyncWorker.kt
@@ -13,14 +13,20 @@ class SlowSyncWorker(
 ) : DefaultWorker {
 
     override suspend fun doWork(): Result {
+        kaliumLogger.d("Sync: Starting SlowSync")
 
-        val result = userSessionScope.users.syncSelfUser()
-            .flatMap { userSessionScope.conversations.syncConversations() }
-            .flatMap { userSessionScope.connection.syncConnections() }
-            .flatMap { userSessionScope.team.syncSelfTeamUseCase() }
-            .flatMap { userSessionScope.users.syncContacts() }
-            .onSuccess { userSessionScope.syncManager.onSlowSyncComplete() }
-            .onFailure { userSessionScope.syncManager.onSlowSyncFailure(it) }
+        val result = try{
+            userSessionScope.users.syncSelfUser()
+                .flatMap { userSessionScope.conversations.syncConversations() }
+                .flatMap { userSessionScope.connection.syncConnections() }
+                .flatMap { userSessionScope.team.syncSelfTeamUseCase() }
+                .flatMap { userSessionScope.users.syncContacts() }
+                .onSuccess { userSessionScope.syncManager.onSlowSyncComplete() }
+                .onFailure { userSessionScope.syncManager.onSlowSyncFailure(it) }
+        }catch (e: Exception){
+            kaliumLogger.e("An unexpected error happening during SlowSync", e)
+            Either.Left(CoreFailure.Unknown(e))
+        }
 
         return when (result) {
             is Either.Left -> {
