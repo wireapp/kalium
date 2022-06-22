7b56b6ae01d0c37a1f73bf9aa2b8c2fab1b6f0c9
Mohamad Jaara <mohamad.jaara@wire.com>
2022-06-22 13:23:59 +0200

    fix test

--- persistence/src/androidAndroidTest/kotlin/com/wire/kalium/persistence/GlobalDBBaseTest.kt
+++ persistence/src/androidAndroidTest/kotlin/com/wire/kalium/persistence/GlobalDBBaseTest.kt
@@ -2,20 +2,18 @@ package com.wire.kalium.persistence
 
 import android.content.Context
 import androidx.test.core.app.ApplicationProvider
-import com.russhwolf.settings.MockSettings
 import com.wire.kalium.persistence.db.GlobalDatabaseProvider
-import com.wire.kalium.persistence.kmm_settings.KaliumPreferencesSettings
+import com.wire.kalium.persistence.db.GlobalDatabaseSecret
 import com.wire.kalium.persistence.util.FileNameUtil
 
 actual abstract class GlobalDBBaseTest {
-
-    private val preferences = KaliumPreferencesSettings(MockSettings())
 
     actual fun deleteDatabase() {
         val context: Context = ApplicationProvider.getApplicationContext()
         context.deleteDatabase(FileNameUtil.globalDBName())
     }
 
-    actual fun createDatabase(): GlobalDatabaseProvider = GlobalDatabaseProvider(ApplicationProvider.getApplicationContext(), preferences)
+    actual fun createDatabase(): GlobalDatabaseProvider =
+        GlobalDatabaseProvider(ApplicationProvider.getApplicationContext(), GlobalDatabaseSecret("test_db_secret".toByteArray()))
 
 }
