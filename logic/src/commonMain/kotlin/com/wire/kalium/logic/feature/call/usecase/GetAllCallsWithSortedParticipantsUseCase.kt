6ae37ecad51551b2a4f385c7defafd14eef63df2
Oussama Hassine <oussama.has100@gmail.com>
2022-06-27 12:48:44 +0200

    feat(calling): sort participants alphabetically (AR-1148) (#624)

---
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/call/usecase/GetAllCallsWithSortedParticipantsUseCase.kt
@@ -0,0 +1,24 @@
+package com.wire.kalium.logic.feature.call.usecase
+
+import com.wire.kalium.logic.data.call.CallRepository
+import com.wire.kalium.logic.feature.call.Call
+import com.wire.kalium.logic.feature.call.ParticipantsOrder
+import com.wire.kalium.logic.sync.SyncManager
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class GetAllCallsWithSortedParticipantsUseCase(
+    private val callRepository: CallRepository,
+    private val syncManager: SyncManager,
+    private val participantsOrder: ParticipantsOrder
+) {
+    operator fun invoke(): Flow<List<Call>> {
+        syncManager.startSyncIfIdle()
+        return callRepository.callsFlow().map { calls ->
+            calls.map { call ->
+                val sortedParticipants = participantsOrder.reorderItems(call.participants)
+                call.copy(participants = sortedParticipants)
+            }
+        }
+    }
+}
