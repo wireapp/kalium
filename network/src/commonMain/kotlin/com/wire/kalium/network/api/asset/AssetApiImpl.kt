9b8b35e67615d3a97f275db6390341b1c0641a0c
Gonzalo Gran Crespo <gongracr@gmail.com>
2022-07-04 18:42:06 +0200

    removed legacy asset upload method

--- network/src/commonMain/kotlin/com/wire/kalium/network/api/asset/AssetApiImpl.kt
+++ network/src/commonMain/kotlin/com/wire/kalium/network/api/asset/AssetApiImpl.kt
@@ -17,7 +17,6 @@ import io.ktor.utils.io.close
 import io.ktor.utils.io.core.toByteArray
 import okio.Source
 import okio.buffer
-import okio.use
 
 interface AssetApi {
     suspend fun downloadAsset(assetId: AssetId, assetToken: String?): NetworkResponse<ByteArray>
@@ -25,10 +24,6 @@ interface AssetApi {
         metadata: AssetMetadataRequest,
         encryptedDataSource: Source,
         encryptedDataSize: Long
-    ): NetworkResponse<AssetResponse>
-    suspend fun uploadAsset(
-        metadata: AssetMetadataRequest,
-        encryptedData: ByteArray
     ): NetworkResponse<AssetResponse>
 }
 
@@ -78,83 +73,6 @@ class AssetApiImpl internal constructor(
                 setBody(StreamAssetContent(metadata, encryptedDataSize, encryptedDataSource))
             }
         }
-
-    override suspend fun uploadAsset(
-        metadata: AssetMetadataRequest,
-        encryptedData: ByteArray
-    ): NetworkResponse<AssetResponse> =
-        wrapKaliumResponse {
-            httpClient.post(PATH_PUBLIC_ASSETS_V3) {
-                contentType(ContentType.MultiPart.Mixed)
-                setBody(provideLegacyRequestBody(metadata, encryptedData))
-            }
-        }
-
-    private fun provideDataBody(metadata: AssetMetadataRequest, encryptedDataSize: Long, encryptedDataSource: Source): ByteArray {
-        val body = StringBuilder()
-        val closingArray = "\r\n--frontier--\r\n".toByteArray(UTF_8)
-
-        // Part 1
-        val strMetadata = "{\"public\": ${metadata.public}, \"retention\": \"${metadata.retentionType.name.lowercase()}\"}"
-
-        body.append("--frontier\r\n")
-        body.append("Content-Type: application/json;charset=utf-8\r\n")
-        body.append("Content-Length: ")
-            .append(strMetadata.length)
-            .append("\r\n\r\n")
-        body.append(strMetadata)
-            .append("\r\n")
-
-        // Part 2
-        body.append("--frontier\r\n")
-        body.append("Content-Type: application/octet-stream")
-            .append("\r\n")
-        body.append("Content-Length: ")
-            .append(encryptedDataSize)
-            .append("\r\n")
-        body.append("Content-MD5: ")
-            .append(metadata.md5)
-            .append("\r\n\r\n")
-
-        val openingData = body.toString().toByteArray(UTF_8)
-        val encodedData = encryptedDataSource.buffer().use {
-            it.readByteArray()
-        }
-
-        return openingData + encodedData + closingArray
-    }
-
-    private fun provideLegacyRequestBody(metadata: AssetMetadataRequest, encryptedData: ByteArray): ByteArray {
-        val body = StringBuilder()
-
-        // Part 1
-        val strMetadata = "{\"public\": ${metadata.public}, \"retention\": \"${metadata.retentionType.name.lowercase()}\"}"
-
-        body.append("--frontier\r\n")
-        body.append("Content-Type: application/json;charset=utf-8\r\n")
-        body.append("Content-Length: ")
-            .append(strMetadata.length)
-            .append("\r\n\r\n")
-        body.append(strMetadata)
-            .append("\r\n")
-
-        // Part 2
-        body.append("--frontier\r\n")
-        body.append("Content-Type: application/octet-stream")
-            .append("\r\n")
-        body.append("Content-Length: ")
-            .append(encryptedData.size)
-            .append("\r\n")
-        body.append("Content-MD5: ")
-            .append(metadata.md5)
-            .append("\r\n\r\n")
-
-        val bodyArray = body.toString().toByteArray(UTF_8)
-        val closingArray = "\r\n--frontier--\r\n".toByteArray(UTF_8)
-
-        // Merge all sections on the request
-        return bodyArray + encryptedData + closingArray
-    }
 
     private companion object {
         const val PATH_PUBLIC_ASSETS_V3 = "assets/v3"
