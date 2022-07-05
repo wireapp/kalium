5d64bcaa00dd33640f2236ab2af8d6dd954d4004
yamilmedina <yamilmedina@users.noreply.github.com>
2022-07-05 19:31:08 +0200

    feat: add compression support for ktor

--- network/src/commonMain/kotlin/com/wire/kalium/network/NetworkClient.kt
+++ network/src/commonMain/kotlin/com/wire/kalium/network/NetworkClient.kt
@@ -11,12 +11,12 @@ import io.ktor.client.HttpClient
 import io.ktor.client.HttpClientConfig
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.plugins.ContentNegotiation
-import io.ktor.client.plugins.compression.ContentEncoding
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.plugins.logging.SIMPLE
 import io.ktor.client.plugins.websocket.WebSockets
+import io.ktor.http.HttpHeaders.ContentEncoding
 import io.ktor.serialization.kotlinx.json.json
 
 /**
@@ -110,7 +110,7 @@ internal fun provideBaseHttpClient(
     }
 
     if (installCompression) {
-        install(ContentEncoding) { gzip(0.9f) }
+        install(ContentEncoding) { gzip(null) }
     }
 
     install(ContentNegotiation) {
