00d178983e5a8b6a790c2d3a7189dea505bff3ba
alexandreferris <ferris.alexandre@gmail.com>
2022-07-11 15:41:21 +0200

    chore: add calls queries and DAO interface and implementation

---
+++ persistence/src/commonMain/kotlin/com/wire/kalium/persistence/dao/call/CallDAOImpl.kt
@@ -0,0 +1,89 @@
+package com.wire.kalium.persistence.dao.call
+
+import com.squareup.sqldelight.runtime.coroutines.asFlow
+import com.squareup.sqldelight.runtime.coroutines.mapToList
+import com.squareup.sqldelight.runtime.coroutines.mapToOne
+import com.wire.kalium.persistence.CallsQueries
+import com.wire.kalium.persistence.dao.QualifiedIDEntity
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.flow.map
+import kotlinx.datetime.Clock
+import com.wire.kalium.persistence.Call as SQLDelightCall
+
+internal class CallMapper {
+    fun toModel(dbEntry: SQLDelightCall) = CallEntity(
+        conversationId = dbEntry.conversation_id,
+        id = dbEntry.id,
+        status = dbEntry.status,
+        callerId = dbEntry.caller_id,
+        conversationType = dbEntry.conversation_type
+    )
+}
+
+internal class CallDAOImpl(private val callsQueries: CallsQueries) : CallDAO {
+    val mapper = CallMapper()
+
+    override suspend fun insertCall(call: CallEntity) {
+        val createdTime: Long = Clock.System.now().epochSeconds
+
+        callsQueries.insertCall(
+            conversation_id = call.conversationId,
+            id = call.id,
+            status = call.status,
+            caller_id = call.callerId,
+            conversation_type = call.conversationType,
+            created_at = createdTime.toString()
+        )
+    }
+
+    override suspend fun getCalls(): Flow<List<CallEntity>> =
+        callsQueries.selectAllCalls()
+            .asFlow()
+            .mapToList()
+            .map { calls -> calls.map(mapper::toModel) }
+
+    override suspend fun getIncomingCalls(): Flow<List<CallEntity>> =
+        callsQueries.selectIncomingCalls()
+            .asFlow()
+            .mapToList()
+            .map { calls -> calls.map(mapper::toModel) }
+
+    override suspend fun getEstablishedCalls(): Flow<List<CallEntity>> =
+        callsQueries.selectEstablishedCalls()
+            .asFlow()
+            .mapToList()
+            .map { calls -> calls.map(mapper::toModel) }
+
+    override suspend fun getOngoingCalls(): Flow<List<CallEntity>> =
+        callsQueries.selectOngoingCalls()
+            .asFlow()
+            .mapToList()
+            .map { calls -> calls.map(mapper::toModel) }
+
+    override suspend fun updateLastCallStatusByConversationId(status: CallEntity.Status, conversationId: QualifiedIDEntity) {
+        callsQueries.updateLastCallStatusByConversationId(
+            status,
+            conversationId
+        )
+    }
+
+    override suspend fun getCallerIdByConversationId(conversationId: QualifiedIDEntity): String =
+        callsQueries.selectLastCallByConversationId(conversationId)
+            .asFlow()
+            .mapToOne()
+            .map { it.caller_id }
+            .first()
+
+    override suspend fun getCallStatusByConversationId(conversationId: QualifiedIDEntity): CallEntity.Status? =
+        callsQueries.selectLastCallByConversationId(conversationId)
+            .asFlow()
+            .mapToOne()
+            .map { mapper.toModel(dbEntry = it).status }
+            .firstOrNull()
+
+    override suspend fun deleteAllCalls() {
+        callsQueries.deleteAllCalls()
+    }
+}
