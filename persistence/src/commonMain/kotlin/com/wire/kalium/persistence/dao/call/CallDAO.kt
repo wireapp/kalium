fe271a76049a52381b61ed7a0bb57e4775dbc26b
Alexandre Ferris <ferris.alexandre@gmail.com>
2022-07-12 12:33:48 +0200

    chore: add calls to database persistence (#668)
    * chore: add gradle date time dependency
    
    * chore: add calls queries and DAO interface and implementation
    
    * chore: add callDAO to user database provider
    
    * chore: add correct prefix for db flows

---
+++ persistence/src/commonMain/kotlin/com/wire/kalium/persistence/dao/call/CallDAO.kt
@@ -0,0 +1,35 @@
+package com.wire.kalium.persistence.dao.call
+
+import com.wire.kalium.persistence.dao.ConversationEntity
+import com.wire.kalium.persistence.dao.QualifiedIDEntity
+import kotlinx.coroutines.flow.Flow
+
+data class CallEntity(
+    val conversationId: QualifiedIDEntity,
+    val id: String,
+    val status: Status,
+    val callerId: String,
+    val conversationType: ConversationEntity.Type
+) {
+    enum class Status {
+        STARTED,
+        INCOMING,
+        MISSED,
+        ANSWERED,
+        ESTABLISHED,
+        STILL_ONGOING,
+        CLOSED
+    }
+}
+
+interface CallDAO {
+    suspend fun insertCall(call: CallEntity)
+    suspend fun observeCalls(): Flow<List<CallEntity>>
+    suspend fun observeIncomingCalls(): Flow<List<CallEntity>>
+    suspend fun observeEstablishedCalls(): Flow<List<CallEntity>>
+    suspend fun observeOngoingCalls(): Flow<List<CallEntity>>
+    suspend fun updateLastCallStatusByConversationId(status: CallEntity.Status, conversationId: QualifiedIDEntity)
+    suspend fun getCallerIdByConversationId(conversationId: QualifiedIDEntity): String
+    suspend fun getCallStatusByConversationId(conversationId: QualifiedIDEntity): CallEntity.Status?
+    suspend fun deleteAllCalls()
+}
