bcf8f9e19713e6224c6a5fb33db61d4898d4863e
alexandreferris <ferris.alexandre@gmail.com>
2022-07-13 11:24:39 +0200

    chore: add delete all calls use case docs

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/call/usecase/DeleteAllCallsUseCase.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/call/usecase/DeleteAllCallsUseCase.kt
@@ -2,6 +2,13 @@ package com.wire.kalium.logic.feature.call.usecase
 
 import com.wire.kalium.logic.data.call.CallRepository
 
+/**
+ * Delete All Calls Use Case
+ *
+ * This UseCase will only be used in the Debug menu, until we have a fully working calling that is handling all the edge cases.
+ *
+ * This UseCase will be deleted after the statement above is done. As we don't have any functionality that requires the deletion of calls.
+ */
 interface DeleteAllCallsUseCase {
     suspend operator fun invoke()
 }
