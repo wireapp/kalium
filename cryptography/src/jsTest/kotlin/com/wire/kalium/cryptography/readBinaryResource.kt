641fd4c42997518bbc458e26f79efc4f915eaa99
Gonzalo Gran Crespo <gongracr@gmail.com>
2022-07-14 16:25:16 +0200

    Added dummy implementation

--- cryptography/src/jsTest/kotlin/com/wire/kalium/cryptography/readBinaryResource.kt
+++ cryptography/src/jsTest/kotlin/com/wire/kalium/cryptography/readBinaryResource.kt
@@ -2,5 +2,6 @@ package com.wire.kalium.cryptography
 
 /** Read the given resource as binary data. */
 actual fun readBinaryResource(resourceName: String): ByteArray {
-    TODO("Not yet implemented")
+    // TODO("Not yet implemented")
+    return ByteArray(0)
 }
