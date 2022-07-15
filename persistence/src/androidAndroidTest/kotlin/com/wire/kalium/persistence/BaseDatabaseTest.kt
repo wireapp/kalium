c9d8755e998983cae0396fb1a8eb4c1c2e01c9fd
Gonzalo Gran Crespo <gongracr@gmail.com>
2022-07-15 10:50:25 +0200

    Fixed test

--- persistence/src/androidAndroidTest/kotlin/com/wire/kalium/persistence/BaseDatabaseTest.kt
+++ persistence/src/androidAndroidTest/kotlin/com/wire/kalium/persistence/BaseDatabaseTest.kt
@@ -6,6 +6,7 @@ import com.wire.kalium.persistence.dao.UserIDEntity
 import com.wire.kalium.persistence.db.UserDBSecret
 import com.wire.kalium.persistence.db.UserDatabaseProvider
 import com.wire.kalium.persistence.util.FileNameUtil
+import java.io.File
 
 actual open class BaseDatabaseTest actual constructor() {
     private val userId = UserIDEntity("78dd6502-ab84-40f7-a8b3-1e7e1eb4cc8c", "user_12_domain")
@@ -16,6 +17,16 @@ actual open class BaseDatabaseTest actual constructor() {
     }
 
     actual fun createDatabase(): UserDatabaseProvider {
-        return UserDatabaseProvider(ApplicationProvider.getApplicationContext(), userId, UserDBSecret("db_secret".toByteArray()))
+        val rootStoragePath = "Users/me/storage"
+        val rootCachePath = "Users/me/cache"
+        val storageFile = File(rootStoragePath)
+        val cacheFile = File(rootCachePath)
+        return UserDatabaseProvider(
+            ApplicationProvider.getApplicationContext(),
+            userId,
+            storageFile,
+            cacheFile,
+            UserDBSecret("db_secret".toByteArray())
+        )
     }
 }
