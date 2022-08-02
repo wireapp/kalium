e546b3ebc258fa6e57eec44be6e7f686bb6c7b6b
Oussama <oussama.has100@gmail.com>
2022-08-02 11:15:06 +0200

    fix: formatting

--- logic/src/commonTest/kotlin/com/wire/kalium/logic/feature/call/UpdateVideoStateUseCaseTest.kt
+++ logic/src/commonTest/kotlin/com/wire/kalium/logic/feature/call/UpdateVideoStateUseCaseTest.kt
@@ -41,8 +41,6 @@ class UpdateVideoStateUseCaseTest {
             .suspendFunction(callManager::updateVideoState)
             .whenInvokedWith(any(), any())
             .thenReturn(Unit)
-
-
     }
 
     @Test
