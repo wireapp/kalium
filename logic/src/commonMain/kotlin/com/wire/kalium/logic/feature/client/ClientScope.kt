51dbf51f82330b2f25d561e395c651636c42cf04
Jacob Persson <7156+typfel@users.noreply.github.com>
2022-08-02 10:30:45 +0200

    feat: support lower the key package limits for testing purposes (#723)
    * feat: support lower the key package limits for testing purposes
    
    * fix detekt
    
    Co-authored-by: Tommaso Piazza <196761+tmspzz@users.noreply.github.com>

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/client/ClientScope.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/client/ClientScope.kt
@@ -3,10 +3,9 @@ package com.wire.kalium.logic.feature.client
 import com.wire.kalium.logic.configuration.notification.NotificationTokenRepository
 import com.wire.kalium.logic.data.client.ClientRepository
 import com.wire.kalium.logic.data.client.MLSClientProvider
+import com.wire.kalium.logic.data.keypackage.KeyPackageLimitsProvider
 import com.wire.kalium.logic.data.keypackage.KeyPackageRepository
 import com.wire.kalium.logic.data.prekey.PreKeyRepository
-import com.wire.kalium.logic.feature.keypackage.MLSKeyPackageCountUseCase
-import com.wire.kalium.logic.feature.keypackage.MLSKeyPackageCountUseCaseImpl
 import com.wire.kalium.logic.feature.keypackage.RefillKeyPackagesUseCase
 import com.wire.kalium.logic.feature.keypackage.RefillKeyPackagesUseCaseImpl
 import com.wire.kalium.logic.feature.session.DeregisterTokenUseCase
@@ -18,6 +17,7 @@ class ClientScope(
     private val clientRepository: ClientRepository,
     private val preKeyRepository: PreKeyRepository,
     private val keyPackageRepository: KeyPackageRepository,
+    private val keyPackageLimitsProvider: KeyPackageLimitsProvider,
     private val mlsClientProvider: MLSClientProvider,
     private val notificationTokenRepository: NotificationTokenRepository
 
@@ -27,6 +27,7 @@ class ClientScope(
             clientRepository,
             preKeyRepository,
             keyPackageRepository,
+            keyPackageLimitsProvider,
             mlsClientProvider
         )
     val selfClients: SelfClientsUseCase get() = SelfClientsUseCaseImpl(clientRepository)
@@ -34,7 +35,9 @@ class ClientScope(
     val needsToRegisterClient: NeedsToRegisterClientUseCase get() = NeedsToRegisterClientUseCaseImpl(clientRepository)
     val registerPushToken: RegisterTokenUseCase get() = RegisterTokenUseCaseImpl(clientRepository, notificationTokenRepository)
     val deregisterNativePushToken: DeregisterTokenUseCase get() = DeregisterTokenUseCaseImpl(clientRepository, notificationTokenRepository)
-    val mlsKeyPackageCountUseCase: MLSKeyPackageCountUseCase
-        get() = MLSKeyPackageCountUseCaseImpl(keyPackageRepository, clientRepository)
-    val refillKeyPackages: RefillKeyPackagesUseCase get() = RefillKeyPackagesUseCaseImpl(keyPackageRepository, clientRepository)
+    val refillKeyPackages: RefillKeyPackagesUseCase get() = RefillKeyPackagesUseCaseImpl(
+        keyPackageRepository,
+        keyPackageLimitsProvider,
+        clientRepository
+    )
 }
