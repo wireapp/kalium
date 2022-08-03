88b28ad6b890673d804150a2c4cc3b606746d3e1
Michał Saleniuk <saleniuk@gmail.com>
2022-08-03 13:54:21 +0200

    fix: QualifiedIdMapper crashes

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/data/id/QualifiedIdMapper.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/data/id/QualifiedIdMapper.kt
@@ -11,21 +11,25 @@ class QualifiedIdMapperImpl(
 ) : QualifiedIdMapper {
     override fun fromStringToQualifiedID(id: String): QualifiedID {
         val components = id.split(VALUE_DOMAIN_SEPARATOR).filter { it.isNotBlank() }
-
         val count = id.count { it == VALUE_DOMAIN_SEPARATOR }
-        if (id.isEmpty()) {
-            QualifiedID(value = "", domain = "")
-        }
-        return if (count > 1) {
-            val value = id.substringBeforeLast(VALUE_DOMAIN_SEPARATOR)
-            val domain = id.substringAfterLast(VALUE_DOMAIN_SEPARATOR)
-            QualifiedID(value = value, domain = domain)
-
-        } else if (count == 1) {
-            QualifiedID(value = components.first(), domain = components.last())
-        } else {
-            val selfUserDomain = userRepository?.getSelfUserId()?.domain ?: run { "" }
-            QualifiedID(value = components.first(), domain = selfUserDomain)
+        return when {
+            id.isEmpty() -> {
+                QualifiedID(value = "", domain = "")
+            }
+            count > 1 -> {
+                val value = id.substringBeforeLast(VALUE_DOMAIN_SEPARATOR)
+                val domain = id.substringAfterLast(VALUE_DOMAIN_SEPARATOR)
+                QualifiedID(value = value, domain = domain)
+            }
+            count == 1 -> {
+                QualifiedID(value = components.first(), domain = components.last())
+            }
+            else -> {
+                val selfUserDomain = userRepository?.getSelfUserId()?.domain ?: run { "" }
+                QualifiedID(value = components.first(), domain = selfUserDomain)
+            }
         }
     }
 }
+
+fun String.toQualifiedID(mapper: QualifiedIdMapper): QualifiedID = mapper.fromStringToQualifiedID(this)
