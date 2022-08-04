04b6405e0da897ff0aac90f993310f08c4a3e2e1
Mohamad Jaara <mohamad.jaara@wire.com>
2022-08-04 18:19:31 +0200

    fix styling

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/auth/ValidateEmailUseCase.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/auth/ValidateEmailUseCase.kt
@@ -18,7 +18,7 @@ class ValidateEmailUseCaseImpl : ValidateEmailUseCase {
 
     private companion object {
         private const val EMAIL_MIN_LENGTH = 5
-        private val EMAIL_REGEX = //RFC5322-compliant regex that covers 99.99% of input email addresses. http://emailregex.com/
+        private val EMAIL_REGEX = // RFC5322-compliant regex that covers 99.99% of input email addresses. http://emailregex.com/
             ("(?:[a-z0-9!#\$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#\$%&'*+/=?^_`{|}~-]+)*|\"" +
                     "(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@" +
                     "(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?" +
