ebef25ef57c0827920093a2c2eba2eaca681bb47
Mateusz <m.pachulski94@gmail.com>
2022-08-04 16:53:01 +0200

    fix

--- logic/src/commonTest/kotlin/com/wire/kalium/logic/feature/message/GetNotificationsUseCaseTest.kt
+++ logic/src/commonTest/kotlin/com/wire/kalium/logic/feature/message/GetNotificationsUseCaseTest.kt
@@ -499,7 +499,7 @@ class GetNotificationsUseCaseTest {
             mutedStatus,
             TIME_EARLIER,
             TIME_EARLIER,
-            lastReadDate = null,
+            lastReadDate = "2000-01-01T12:00:00.000Z",
             access = listOf(Conversation.Access.CODE, Conversation.Access.INVITE),
             accessRole = listOf(Conversation.AccessRole.NON_TEAM_MEMBER, Conversation.AccessRole.GUEST)
         )
