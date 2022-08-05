e18b10386da0fbaa7a4605788d501239519b1191
Mohamad Jaara <mohamad.jaara@wire.com>
2022-08-05 16:18:27 +0000

    feat: handle user SAML settings (#737)
    * parse self user sso id
    
    * store ssoId in MetadataDAO
    
    * return user ssoId on LoginUseCase
    
    * return user ssoId on SSOLoginSessionResult
    
    * store SSO id with the AuthSession
    
    * remove TODO in KaliumPreferences
    
    * update self user Id
    
    * IsPasswordRequiredUseCase
    
    * add IsPasswordRequiredUseCase to UserScope
    
    * fix typo
    
    * fix styling
    
    * fix merge issues
    
    * fix styling
    
    * update tokens only when session expires
    
    * delete deprecated SaveSessionUseCase.kt
    
    * revert renaming valueByKey to observeValueByKey in MetadataDAO
    
    * fix styling
    
    * fix CLIApplication
    
    * fix MainActivity
    
    * fix tests
    
    * fix LoginApiTest
    
    * return ssoId in RegisterAccountUseCase
    
    * add tests cases for IsPasswordRequiredUseCase
    
    * fix RegisterAccountApp
    
    * fix styling
    
    Co-authored-by: Gonzo <gongracr@gmail.com>

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/UserSessionScope.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/UserSessionScope.kt
@@ -189,7 +189,8 @@ abstract class UserSessionScopeCommon(
             userDatabaseProvider.metadataDAO,
             userDatabaseProvider.clientDAO,
             authenticatedDataSourceSet.authenticatedNetworkContainer.selfApi,
-            authenticatedDataSourceSet.authenticatedNetworkContainer.userDetailsApi
+            authenticatedDataSourceSet.authenticatedNetworkContainer.userDetailsApi,
+            sessionRepository
         )
 
     private val teamRepository: TeamRepository
@@ -446,7 +447,9 @@ abstract class UserSessionScopeCommon(
             assetRepository,
             teamRepository,
             connectionRepository,
-            qualifiedIdMapper
+            qualifiedIdMapper,
+            sessionRepository,
+            userId,
         )
     val logout: LogoutUseCase
         get() = LogoutUseCaseImpl(
--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/auth/sso/SSOLoginScope.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/auth/sso/SSOLoginScope.kt
@@ -3,14 +3,11 @@ package com.wire.kalium.logic.feature.auth.sso
 import com.wire.kalium.logic.configuration.server.ServerConfig
 import com.wire.kalium.logic.configuration.server.ServerConfigRepository
 import com.wire.kalium.logic.data.auth.login.SSOLoginRepository
-import com.wire.kalium.logic.data.session.SessionMapper
-import com.wire.kalium.logic.di.MapperProvider
 
 class SSOLoginScope internal constructor(
     private val ssoLoginRepository: SSOLoginRepository,
     private val serverLinks: ServerConfig.Links,
-    private val serverConfigRepository: ServerConfigRepository,
-    private val sessionMapper: SessionMapper = MapperProvider.sessionMapper()
+    private val serverConfigRepository: ServerConfigRepository
 ) {
     private val validateSSOCodeUseCase: ValidateSSOCodeUseCase get() = ValidateSSOCodeUseCaseImpl()
     val initiate: SSOInitiateLoginUseCase
@@ -21,7 +18,7 @@ class SSOLoginScope internal constructor(
             serverConfigRepository
         )
     val finalize: SSOFinalizeLoginUseCase get() = SSOFinalizeLoginUseCaseImpl(ssoLoginRepository)
-    val getLoginSessionGet: GetSSOLoginSessionUseCase get() = GetSSOLoginSessionUseCaseImpl(ssoLoginRepository, sessionMapper, serverLinks)
+    val getLoginSessionGet: GetSSOLoginSessionUseCase get() = GetSSOLoginSessionUseCaseImpl(ssoLoginRepository, serverLinks)
     val metaData: SSOMetaDataUseCase get() = SSOMetaDataUseCaseImpl(ssoLoginRepository)
     val settings: SSOSettingsUseCase get() = SSOSettingsUseCaseImpl(ssoLoginRepository)
 }
