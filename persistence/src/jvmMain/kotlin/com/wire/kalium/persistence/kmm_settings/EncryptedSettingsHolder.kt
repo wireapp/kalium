c29ff9d360c11f6b1b6a0aabc6cd179cce83a521
Sven Jost <sven@wire.com>
2022-08-15 19:33:59 +0200

    fix: save settings in rootPath instead of os-dependant java pref

--- persistence/src/jvmMain/kotlin/com/wire/kalium/persistence/kmm_settings/EncryptedSettingsHolder.kt
+++ persistence/src/jvmMain/kotlin/com/wire/kalium/persistence/kmm_settings/EncryptedSettingsHolder.kt
@@ -1,10 +1,12 @@
 package com.wire.kalium.persistence.kmm_settings
 
 import com.russhwolf.settings.ExperimentalSettingsImplementation
-import com.russhwolf.settings.JvmPreferencesSettings
+import com.russhwolf.settings.JvmPropertiesSettings
 import com.russhwolf.settings.Settings
-import com.wire.kalium.PackagePathProvider
-import java.util.prefs.Preferences
+import java.io.File
+import java.io.FileInputStream
+import java.nio.file.Paths
+import java.util.Properties
 
 /**
  * the java implementation is not yet encrypted
@@ -12,9 +14,24 @@ import java.util.prefs.Preferences
 
 @OptIn(ExperimentalSettingsImplementation::class)
 actual class EncryptedSettingsHolder(
+    rootPath: String,
     options: SettingOptions
 ) {
-    private val preferences: Preferences = Preferences.userNodeForPackage(PackagePathProvider::class.java).node(options.fileName)
+
+    val file: File = File(Paths.get(rootPath, options.fileName).toString())
+    val properties = createOrLoad(rootPath, file)
+
     // TODO(jvm): JvmPreferencesSettings is not encrypted
-    actual val encryptedSettings: Settings = JvmPreferencesSettings(preferences)
+    actual val encryptedSettings: Settings = JvmPropertiesSettings(properties)
+
+    private fun createOrLoad(rootPath: String, file: File): Properties {
+        val properties = Properties()
+        File(rootPath).mkdirs()
+        if (!file.exists()) {
+            System.out.println(file.absolutePath)
+            file.createNewFile()
+        }
+        properties.load(FileInputStream(file))
+        return properties
+    }
 }
