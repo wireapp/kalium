dda279e10ad605d1bd1bc85503d74f2901c38bc0
Vitor Hugo Schwaab <vitor@schwaab.xyz>
2022-08-15 10:33:15 -0300

    fix: websocket not receiving messages after some time (#791)
    * fix: websocket not receiving messages after some time
    
    * chore: reduce ping interval to 20s
    
    Co-authored-by: Mohamad Jaara <mohamad.jaara@wire.com>

--- network/src/commonJvmAndroid/kotlin/com/wire/kalium/network/HttpEngine.kt
+++ network/src/commonJvmAndroid/kotlin/com/wire/kalium/network/HttpEngine.kt
@@ -2,5 +2,14 @@ package com.wire.kalium.network
 
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.engine.okhttp.OkHttp
+import okhttp3.OkHttpClient
+import java.util.concurrent.TimeUnit
 
-actual fun defaultHttpEngine(): HttpClientEngine = OkHttp.create()
+actual fun defaultHttpEngine(): HttpClientEngine = OkHttp.create {
+    // OkHttp doesn't support configuring ping intervals dynamically,
+    // so they must be set when creating the Engine
+    // See https://youtrack.jetbrains.com/issue/KTOR-4752
+    val client = OkHttpClient.Builder().pingInterval(WEBSOCKET_PING_INTERVAL_MILLIS, TimeUnit.MILLISECONDS).build()
+    preconfigured = client
+    webSocketFactory = KaliumWebSocketFactory(client)
+}
