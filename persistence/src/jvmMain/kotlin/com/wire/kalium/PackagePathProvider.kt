32ad19579510b74056bc5b72513bb668d18dff34
Sven Jost <mythsunwind@users.noreply.github.com>
2022-08-16 13:38:04 +0200

    fix: save settings in rootPath on JVM instead of os-specific location (#799)

--- persistence/src/jvmMain/kotlin/com/wire/kalium/PackagePathProvider.kt
+++
@@ -1,8 +0,0 @@
-package com.wire.kalium;
-
-/**
- * IMPORTANT: this class only exist to provide a permanent package path for
- * the Java Preferences API and must **NOT** be moved to a different package.
- * Doing so will result in data loss for users.
- */
-internal class PackagePathProvider
