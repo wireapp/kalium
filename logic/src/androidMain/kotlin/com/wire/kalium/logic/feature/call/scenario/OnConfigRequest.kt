df4fbef1988ca40c5be3ce8c9f2b1236902d53d1
Gonzalo Gran Crespo <gongracr@gmail.com>
2022-08-23 13:33:58 +0200

    feat: fix crash when hanging up and no connection

--- logic/src/androidMain/kotlin/com/wire/kalium/logic/feature/call/scenario/OnConfigRequest.kt
+++ logic/src/androidMain/kotlin/com/wire/kalium/logic/feature/call/scenario/OnConfigRequest.kt
@@ -4,15 +4,15 @@ import com.sun.jna.Pointer
 import com.wire.kalium.calling.Calling
 import com.wire.kalium.calling.callbacks.CallConfigRequestHandler
 import com.wire.kalium.calling.types.Handle
+import com.wire.kalium.logic.CoreFailure
 import com.wire.kalium.logic.callingLogger
 import com.wire.kalium.logic.data.call.CallRepository
 import com.wire.kalium.logic.feature.call.AvsCallBackError
-import com.wire.kalium.logic.feature.call.CallManagerImpl
 import com.wire.kalium.logic.functional.fold
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
-//TODO(testing): create unit test
+// TODO(testing): create unit test
 class OnConfigRequest(
     private val calling: Calling,
     private val callRepository: CallRepository,
@@ -21,20 +21,18 @@ class OnConfigRequest(
     override fun onConfigRequest(inst: Handle, arg: Pointer?): Int {
         callingLogger.i("[OnConfigRequest] - STARTED")
         callingScope.launch {
-            val config = callRepository.getCallConfigResponse(limit = null)
+            callRepository.getCallConfigResponse(limit = null)
                 .fold({
                     callingLogger.i("[OnConfigRequest] - Error: $it")
-                    TODO("Not yet implemented.")
-                }, {
-                    it
+                    // TODO("Not yet implemented.")
+                }, { config ->
+                    calling.wcall_config_update(
+                        inst = inst,
+                        error = 0, // TODO(calling): http error from internal json
+                        jsonString = config
+                    )
+                    callingLogger.i("[OnConfigRequest] - wcall_config_update()")
                 })
-
-            calling.wcall_config_update(
-                inst = inst,
-                error = 0, // TODO(calling): http error from internal json
-                jsonString = config
-            )
-            callingLogger.i("[OnConfigRequest] - wcall_config_update()")
         }
 
         return AvsCallBackError.NONE.value
