49670f53e9790555787e458b2b3395697f7de081
Gonzalo Gran Crespo <gongracr@gmail.com>
2022-08-24 15:09:02 +0200

    fix: launch EndCallUseCase with io dispatcher

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/call/usecase/EndCallUseCase.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/call/usecase/EndCallUseCase.kt
@@ -2,10 +2,13 @@ package com.wire.kalium.logic.feature.call.usecase
 
 import com.wire.kalium.logic.data.id.ConversationId
 import com.wire.kalium.logic.feature.call.CallManager
+import com.wire.kalium.util.KaliumDispatcher
+import kotlinx.coroutines.withContext
 
-class EndCallUseCase(private val callManager: Lazy<CallManager>) {
+class EndCallUseCase(private val callManager: Lazy<CallManager>,
+                     private val dispatchers: KaliumDispatcher) {
 
-    suspend operator fun invoke(conversationId: ConversationId) {
+    suspend operator fun invoke(conversationId: ConversationId) = withContext(dispatchers.io) {
         callManager.value.endCall(conversationId)
     }
 }
