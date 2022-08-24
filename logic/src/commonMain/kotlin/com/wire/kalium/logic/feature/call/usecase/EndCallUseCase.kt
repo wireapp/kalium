e09adcbbc935a50179be73e8255c577dc385dcc0
Gonzalo Gran Crespo <gongracr@gmail.com>
2022-08-24 16:00:08 +0200

    Fixed tests

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/call/usecase/EndCallUseCase.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/call/usecase/EndCallUseCase.kt
@@ -3,10 +3,12 @@ package com.wire.kalium.logic.feature.call.usecase
 import com.wire.kalium.logic.data.id.ConversationId
 import com.wire.kalium.logic.feature.call.CallManager
 import com.wire.kalium.util.KaliumDispatcher
+import com.wire.kalium.util.KaliumDispatcherImpl
 import kotlinx.coroutines.withContext
 
 class EndCallUseCase(private val callManager: Lazy<CallManager>,
-                     private val dispatchers: KaliumDispatcher) {
+                     private val dispatchers: KaliumDispatcher = KaliumDispatcherImpl
+) {
 
     suspend operator fun invoke(conversationId: ConversationId) = withContext(dispatchers.io) {
         callManager.value.endCall(conversationId)
