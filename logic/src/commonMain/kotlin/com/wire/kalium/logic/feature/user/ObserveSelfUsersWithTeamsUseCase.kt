82dbe5215aa6e3628852c6035753e692197f5c97
Michał Saleniuk <saleniuk@gmail.com>
2022-08-26 14:22:18 +0200

    change use case to return also team

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/user/ObserveSelfUsersWithTeamsUseCase.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/user/ObserveSelfUsersWithTeamsUseCase.kt
@@ -1,6 +1,7 @@
 package com.wire.kalium.logic.feature.user
 
 import com.wire.kalium.logic.data.session.SessionRepository
+import com.wire.kalium.logic.data.team.Team
 import com.wire.kalium.logic.data.user.SelfUser
 import com.wire.kalium.logic.feature.UserSessionScopeProvider
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -8,21 +9,26 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapLatest
 
-interface ObserveSelfUsersListUseCase {
+interface ObserveSelfUsersWithTeamsUseCase {
 
-    suspend operator fun invoke(): Flow<List<SelfUser>>
+    suspend operator fun invoke(): Flow<List<Pair<SelfUser, Team?>>>
 }
 
-internal class ObserveSelfUsersListUseCaseImpl internal constructor(
+internal class ObserveSelfUsersWithTeamsUseCaseImpl internal constructor(
     private val sessionRepository: SessionRepository,
     private val userSessionScopeProvider: UserSessionScopeProvider
-) : ObserveSelfUsersListUseCase {
+) : ObserveSelfUsersWithTeamsUseCase {
 
     @OptIn(ExperimentalCoroutinesApi::class)
-    override suspend fun invoke(): Flow<List<SelfUser>> =
+    override suspend fun invoke(): Flow<List<Pair<SelfUser, Team?>>> =
         sessionRepository.allValidSessionsFlow().flatMapLatest {
             val flowsOfSelfUsers = it.map {
-                userSessionScopeProvider.get(it.session.userId).users.getSelfUser()
+                userSessionScopeProvider.get(it.session.userId).let {
+                    combine(
+                        it.users.getSelfUser(),
+                        it.team.getSelfTeamUseCase()
+                    ) { selfUser, team -> selfUser to team }
+                }
             }
             combine(flowsOfSelfUsers) { it.asList() }
         }
