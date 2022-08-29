b9e7604b5a8f9f808d5ba401ae0292c351d4f886
Mohamad Jaara <mohamad.jaara@wire.com>
2022-08-29 16:29:14 +0200

    feat: account switching

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/session/UpdateCurrentSessionUseCase.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/session/UpdateCurrentSessionUseCase.kt
@@ -1,8 +1,17 @@
 package com.wire.kalium.logic.feature.session
 
+import com.wire.kalium.logic.StorageFailure
 import com.wire.kalium.logic.data.session.SessionRepository
 import com.wire.kalium.logic.data.user.UserId
+import com.wire.kalium.logic.functional.fold
 
-class UpdateCurrentSessionUseCase(private val sessionRepository: SessionRepository) {
-    operator fun invoke(userId: UserId) = sessionRepository.updateCurrentSession(userId)
+class UpdateCurrentSessionUseCase internal constructor(private val sessionRepository: SessionRepository) {
+    operator suspend fun invoke(userId: UserId) =
+        sessionRepository.updateCurrentSession(userId).fold({ Result.Failure(it) }, { Result.Success })
+
+    sealed class Result {
+        object Success : Result()
+        data class Failure(val cause: StorageFailure) : Result()
+    }
+
 }
