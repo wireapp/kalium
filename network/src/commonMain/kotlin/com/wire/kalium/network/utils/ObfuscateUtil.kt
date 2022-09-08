d24b4757bf42297ef62412a9a3ccca628295ff52
Mohamad Mukresh <mokresh.mohammed@gmail.com>
2022-09-08 16:55:02 +0200

    update obfuscate util

--- network/src/commonMain/kotlin/com/wire/kalium/network/utils/ObfuscateUtil.kt
+++ network/src/commonMain/kotlin/com/wire/kalium/network/utils/ObfuscateUtil.kt
@@ -22,7 +22,6 @@ fun obfuscateAndLogMessage(text: String) {
         } else {
             logObfuscatedJsonElement(obj)
         }
-        obj.toString()
     } catch (e: Exception) {
         "error the body content while logging "
     }
@@ -65,34 +64,31 @@ fun obfuscatePath(url: Url): String {
     return requestToLog
 }
 
-fun deleteSensitiveItemsFromJson(text: String) {
+fun deleteSensitiveItemsFromJson(text: String): String {
+    var logMessage = ""
     try {
         val obj = (Json.decodeFromString(text) as JsonElement)
-        if (obj.jsonArray.size > 0) {
-            obj.jsonArray.map {
-                itemsToKeep(it)
+        obj.jsonObject.entries.toMutableSet().map {
+            if (notSensitiveJsonArray.contains(it.key.lowercase())) {
+                if (it.value.jsonArray.size > 0) {
+                    it.value.jsonArray[0].jsonObject.entries.toMutableSet().map {
+                        if (notSensitiveJsonKeys.contains(it.key.lowercase())) {
+                            logMessage += " ${it.key} : ${it.value}"
+                        }
+                    }
+                }
             }
-        } else {
-            itemsToKeep(obj)
         }
-        obj.toString()
+        return logMessage
+
     } catch (e: Exception) {
-        "error the body content while logging "
-    }
-
-}
-
-fun itemsToKeep(obj: JsonElement) {
-    obj.jsonObject.entries.toMutableSet().map {
-        when {
-            notSensitiveJsonKeys.contains(it.key.lowercase()) -> {
-                kaliumLogger.v("${it.key} : ${it.value}")
-            }
-        }
+        return "error while logging "
     }
 }
+
 
 val sensitiveJsonKeys by lazy { listOf("password", "authorization", "set-cookie", "location", "x-amz-meta-user") }
 private val sensitiveJsonIdKeys by lazy { listOf("conversation", "id", "user", "team") }
 private val sensitiveJsonObjects by lazy { listOf("qualified_id") }
 private val notSensitiveJsonKeys by lazy { listOf("type", "time") }
+private val notSensitiveJsonArray by lazy { listOf("payload") }
