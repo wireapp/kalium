f52d058bd8addb0fc6e16a222ef38ad79582eec8
Mohamad Jaara <mohamad.jaara@wire.com>
2022-09-08 17:15:12 +0200

    DeleteSessionUseCaseTest

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/session/DeleteSessionUseCase.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/session/DeleteSessionUseCase.kt
@@ -1,8 +1,11 @@
 package com.wire.kalium.logic.feature.session
 
+import com.wire.kalium.logic.StorageFailure
 import com.wire.kalium.logic.data.session.SessionRepository
 import com.wire.kalium.logic.data.user.UserId
 import com.wire.kalium.logic.feature.UserSessionScopeProvider
+import com.wire.kalium.logic.functional.fold
+import com.wire.kalium.logic.functional.onSuccess
 
 /**
  * This class is responsible for deleting a user session and freeing up all the resources.
@@ -11,8 +14,17 @@ class DeleteSessionUseCase internal constructor(
     private val sessionRepository: SessionRepository,
     private val userSessionScopeProvider: UserSessionScopeProvider
 ) {
-    operator fun invoke(userId: UserId) {
-        sessionRepository.deleteSession(userId)
-        userSessionScopeProvider.delete(userId)
+    operator fun invoke(userId: UserId) = sessionRepository.deleteSession(userId)
+        .onSuccess {
+            userSessionScopeProvider.delete(userId)
+        }.fold({
+            Result.Failure(it)
+        }, {
+            Result.Success
+        })
+
+    sealed class Result {
+        object Success : Result()
+        data class Failure(val cause: StorageFailure) : Result()
     }
 }
