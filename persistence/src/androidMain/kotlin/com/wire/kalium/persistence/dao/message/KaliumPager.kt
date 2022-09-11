1a722e294c59a9d52639ffdd4abdeafb570a3a99
Vitor Hugo Schwaab <vitor@schwaab.xyz>
2022-09-12 01:54:40 +0200

    style: fix styling issues

--- persistence/src/androidMain/kotlin/com/wire/kalium/persistence/dao/message/KaliumPager.kt
+++ persistence/src/androidMain/kotlin/com/wire/kalium/persistence/dao/message/KaliumPager.kt
@@ -11,11 +11,11 @@ import kotlinx.coroutines.flow.map
  * Exposes a [pagingDataFlow] that can be used in Android UI components to display paginated data.
  *
  */
-class KaliumPager<RowType: Any, EntityType: Any>(
+class KaliumPager<RowType : Any, EntityType : Any>(
     private val pager: Pager<Long, RowType>,
     internal val pagingSource: PagingSource<Long, RowType>,
     private val mapperFunction: (RowType) -> EntityType
-){
+) {
     val pagingDataFlow: Flow<PagingData<EntityType>>
         get() = pager.flow.map { pagingData ->
             pagingData.map { item ->
