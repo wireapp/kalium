390f2d546c396ec50106723ac0ad4204d4790713
Vitor Hugo Schwaab <vitor@schwaab.xyz>
2022-09-17 03:44:26 +0200

    fix: slow performance when fetching user IDs repeatedly

--- persistence/src/jsTest/kotlin/com/wire/kalium/persistence/BaseDatabaseTest.kt
+++ persistence/src/jsTest/kotlin/com/wire/kalium/persistence/BaseDatabaseTest.kt
@@ -1,8 +1,12 @@
 package com.wire.kalium.persistence
 
 import com.wire.kalium.persistence.db.UserDatabaseProvider
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.test.StandardTestDispatcher
 
 actual open class BaseDatabaseTest actual constructor() {
+
+    protected actual val dispatcher: CoroutineDispatcher = StandardTestDispatcher()
 
     actual fun deleteDatabase() {
         // TODO delete test database
--- persistence/src/jvmTest/kotlin/com/wire/kalium/persistence/BaseDatabaseTest.kt
+++ persistence/src/jvmTest/kotlin/com/wire/kalium/persistence/BaseDatabaseTest.kt
@@ -1,9 +1,13 @@
 package com.wire.kalium.persistence
 
 import com.wire.kalium.persistence.db.UserDatabaseProvider
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.TestDispatcher
 import java.nio.file.Files
 
 actual open class BaseDatabaseTest actual constructor() {
+
+    protected actual val dispatcher: TestDispatcher = StandardTestDispatcher()
 
     private val databaseFile  = Files.createTempDirectory("test-storage").toFile().resolve("test.db")
 
@@ -12,7 +16,7 @@ actual open class BaseDatabaseTest actual constructor() {
     }
 
     actual fun createDatabase(): UserDatabaseProvider {
-        return UserDatabaseProvider(databaseFile)
+        return UserDatabaseProvider(databaseFile, dispatcher = dispatcher)
     }
 
 }
