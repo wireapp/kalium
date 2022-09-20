b69562b5202c1efdda26324d827f4d870c372e44
Mohamad Jaara <mohamad.jaara@wire.com>
2022-09-20 12:36:26 +0200

    fix cli app not building

--- logic/src/jvmMain/kotlin/com/wire/kalium/logic/feature/UserSessionScopeProviderImpl.kt
+++ logic/src/jvmMain/kotlin/com/wire/kalium/logic/feature/UserSessionScopeProviderImpl.kt
@@ -15,10 +15,9 @@ import com.wire.kalium.logic.featureFlags.KaliumConfigs
 import com.wire.kalium.logic.network.SessionManagerImpl
 import com.wire.kalium.logic.sync.UserSessionWorkSchedulerImpl
 import com.wire.kalium.network.AuthenticatedNetworkContainer
-import com.wire.kalium.persistence.client.AuthTokenStorage
 import com.wire.kalium.persistence.db.UserDatabaseProvider
 import com.wire.kalium.persistence.kmmSettings.EncryptedSettingsHolder
-import com.wire.kalium.persistence.kmmSettings.KaliumPreferences
+import com.wire.kalium.persistence.kmmSettings.GlobalPrefProvider
 import com.wire.kalium.persistence.kmmSettings.KaliumPreferencesSettings
 import com.wire.kalium.persistence.kmmSettings.SettingOptions
 import com.wire.kalium.util.KaliumDispatcherImpl
@@ -30,7 +29,7 @@ actual class UserSessionScopeProviderImpl(
     private val rootPath: String,
     private val globalScope: GlobalKaliumScope,
     private val kaliumConfigs: KaliumConfigs,
-    private val globalPreferences: KaliumPreferences,
+    private val globalPreferences: GlobalPrefProvider,
     private val globalCallManager: GlobalCallManager,
     private val idMapper: IdMapper
 ) : UserSessionScopeProviderCommon() {
@@ -43,7 +42,7 @@ actual class UserSessionScopeProviderImpl(
         val rootCachePath = CacheFolder("$rootAccountPath/cache")
         val dataStoragePaths = DataStoragePaths(rootFileSystemPath, rootCachePath)
         val networkContainer = AuthenticatedNetworkContainer(
-            SessionManagerImpl(globalScope.sessionRepository, userId, tokenStorage = AuthTokenStorage(globalPreferences)),
+            SessionManagerImpl(globalScope.sessionRepository, userId, tokenStorage = globalPreferences.authTokenStorage),
             ServerMetaDataManagerImpl(globalScope.serverConfigRepository),
             developmentApiEnabled = kaliumConfigs.developmentApiEnabled
         )
