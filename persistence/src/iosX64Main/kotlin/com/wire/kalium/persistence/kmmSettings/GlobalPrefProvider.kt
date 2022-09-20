eb2b2d6d91f1217b4bd9da8e4c3521ed7a9482ba
Mohamad Jaara <mohamad.jaara@wire.com>
2022-09-20 15:51:17 +0200

    missing actual class GlobalPrefProvider for iOS

---
+++ persistence/src/iosX64Main/kotlin/com/wire/kalium/persistence/kmmSettings/GlobalPrefProvider.kt
@@ -0,0 +1,20 @@
+package com.wire.kalium.persistence.kmmSettings
+
+import com.wire.kalium.persistence.client.AuthTokenStorage
+import com.wire.kalium.persistence.client.TokenStorage
+import com.wire.kalium.persistence.client.UserConfigStorage
+import com.wire.kalium.persistence.dbPassphrase.PassphraseStorage
+
+actual class GlobalPrefProvider {
+    actual val authTokenStorage: AuthTokenStorage
+        get() = TODO("Not yet implemented")
+    actual val passphraseStorage: PassphraseStorage
+        get() = TODO("Not yet implemented")
+    actual val tokenStorage: TokenStorage
+        get() = TODO("Not yet implemented")
+
+    @Deprecated("must be moved to user specific storage")
+    actual val userConfigStorage: UserConfigStorage
+        get() = TODO("Not yet implemented")
+
+}
