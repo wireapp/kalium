80a4e685e69491e2164a63d589e7958a89ab1f23
Jacob Persson <7156+typfel@users.noreply.github.com>
2022-09-21 10:47:42 +0200

    chore: fix detekt

--- logic/src/commonTest/kotlin/com/wire/kalium/logic/corefailure/WrapMLSRequestTest.kt
+++ logic/src/commonTest/kotlin/com/wire/kalium/logic/corefailure/WrapMLSRequestTest.kt
@@ -1,9 +1,7 @@
-package com.wire.kalium.logic.core_failure
+package com.wire.kalium.logic.corefailure
 
-import com.wire.kalium.cryptography.PreKeyCrypto
 import com.wire.kalium.logic.functional.Either
 import com.wire.kalium.logic.util.shouldFail
-import com.wire.kalium.logic.wrapCryptoRequest
 import com.wire.kalium.logic.wrapMLSRequest
 import kotlin.test.Test
 import kotlin.test.assertEquals
