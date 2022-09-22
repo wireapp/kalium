59fa924f16fd3bb84399cd5bb41fccb464897495
Mohammed Mokresh <mokresh.mohammed@gmail.com>
2022-09-22 18:48:37 +0200

    fix: app crash when call after login (#955)
    * fix: app crash when call after login
    
    * fix detekt
    
    * fix: add missing initialization of UserSessionScopeProviderCommon with global call manager
    
    Co-authored-by: alexandreferris <ferris.alexandre@gmail.com>

--- logic/src/jvmMain/kotlin/com/wire/kalium/logic/feature/UserSessionScopeProviderImpl.kt
+++ logic/src/jvmMain/kotlin/com/wire/kalium/logic/feature/UserSessionScopeProviderImpl.kt
@@ -31,7 +31,7 @@ actual class UserSessionScopeProviderImpl(
     private val globalPreferences: GlobalPrefProvider,
     private val globalCallManager: GlobalCallManager,
     private val idMapper: IdMapper
-) : UserSessionScopeProviderCommon() {
+) : UserSessionScopeProviderCommon(globalCallManager) {
 
     override fun create(userId: UserId): UserSessionScope {
         val rootAccountPath = "$rootPath/${userId.domain}/${userId.value}"
