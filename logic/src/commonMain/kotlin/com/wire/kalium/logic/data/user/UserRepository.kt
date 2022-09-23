b33e06fcb812c8bc0824b8bba7bea3431ad6e2d5
yamilmedina <yamilmedina@users.noreply.github.com>
2022-09-23 16:17:49 +0200

    feat: pr comments

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/data/user/UserRepository.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/data/user/UserRepository.kt
@@ -298,7 +298,7 @@ internal class UserDataSource internal constructor(
     override suspend fun updateUserFromEvent(event: Event.User.Update): Either<CoreFailure, Unit> = wrapStorageRequest {
         val userId = qualifiedIdMapper.fromStringToQualifiedID(event.userId)
         val user = userDAO.getUserByQualifiedID(idMapper.toDaoModel(userId)).firstOrNull()
-        if (user != null) {
+        if (user == null) {
             Either.Left(StorageFailure.DataNotFound)
         }
         userDAO.updateUser(userMapper.toUpdateDaoFromEvent(event, user!!))
