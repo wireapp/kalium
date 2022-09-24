ac455369ee922baa7ebc918b759faf0168987577
Boris Safonov <boris.safonov@appunite.com>
2022-09-24 16:26:53 +0300

    chore: Added logs in register firebase token

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/notificationToken/PushTokenUpdater.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/notificationToken/PushTokenUpdater.kt
@@ -1,10 +1,13 @@
 package com.wire.kalium.logic.feature.notificationToken
 
+import com.wire.kalium.logger.obfuscateId
+import com.wire.kalium.logic.kaliumLogger
 import com.wire.kalium.logic.configuration.notification.NotificationTokenRepository
 import com.wire.kalium.logic.data.client.ClientRepository
 import com.wire.kalium.logic.data.notification.PushTokenRepository
 import com.wire.kalium.logic.functional.flatMap
 import com.wire.kalium.logic.functional.isRight
+import com.wire.kalium.logic.functional.onFailure
 import com.wire.kalium.network.api.base.model.PushTokenBody
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.filter
@@ -35,9 +38,22 @@ internal class PushTokenUpdater(
                             )
                         )
                     }
+                    .onFailure {
+                        kaliumLogger.i(
+                            "$TAG Error while registering Firebase token " +
+                                    "for the client: ${clientId.toString().obfuscateId()} error: $it"
+                        )
+                    }
             }
             .collect { result ->
-                if (result.isRight()) pushTokenRepository.setUpdateFirebaseTokenFlag(false)
+                if (result.isRight()) {
+                    kaliumLogger.i("$TAG Firebase registered successfully")
+                    pushTokenRepository.setUpdateFirebaseTokenFlag(false)
+                }
             }
+    }
+
+    companion object {
+        private const val TAG = "PushTokenUpdater"
     }
 }
