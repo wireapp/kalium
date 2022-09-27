33a9c4bf9c9ac5ee71ac4f8fba68c0c04c1c3571
Oussama <oussama.has100@gmail.com>
2022-09-27 11:55:54 +0200

    chore: bump AVS version to 8.2.14

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -32,7 +32,7 @@ object Versions {
     const val sqlDelight = "2.0.0-alpha01"
     const val pbandk = "0.14.1"
     const val turbine = "0.7.0"
-    const val avs = "8.2.12"
+    const val avs = "8.2.14"
     const val jna = "5.6.0@aar"
     const val mlsClient = "0.4.2"
     const val desugarJdk = "1.1.5"
