aeb9e1176672e0518f70d94d5654db8243794303
Mohamad Jaara <mohamad.jaara@wire.com>
2022-09-27 17:49:52 +0200

    test: order api unit tests and add tests for v2  (#986)
    * add base v2 classes
    
    * add v2 changes
    
    * reorder api unit tests
    
    * add unit tests for v2 apis
    
    * fix missing imports
    
    * fix styling
    
    * fix test
    
    * fix styling
    
    * revert changes to InstanceService

---
+++ network/src/commonTest/kotlin/com/wire/kalium/api/json/model/QualifiedIDSamples.kt
@@ -0,0 +1,8 @@
+package com.wire.kalium.api.json.model
+
+import com.wire.kalium.network.api.base.model.QualifiedID
+
+object QualifiedIDSamples {
+    val one = QualifiedID("someValue", "someDomain")
+    val two = QualifiedID("anotherValue", "anotherDomain")
+}
