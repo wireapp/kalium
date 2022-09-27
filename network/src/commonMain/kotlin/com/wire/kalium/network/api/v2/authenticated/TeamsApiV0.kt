776ab6cc0521a4abe6b948a4ea132be2ddfa5b66
Mohamad Jaara <mohamad.jaara@wire.com>
2022-09-27 14:24:43 +0200

    add unit tests for v2 apis

--- network/src/commonMain/kotlin/com/wire/kalium/network/api/v2/authenticated/TeamsApiV0.kt
+++
@@ -1,8 +0,0 @@
-package com.wire.kalium.network.api.v2.authenticated
-
-import com.wire.kalium.network.AuthenticatedNetworkClient
-import com.wire.kalium.network.api.v0.authenticated.TeamsApiV0
-
-internal open class TeamsApiV2 internal constructor(
-    authenticatedNetworkClient: AuthenticatedNetworkClient
-) : TeamsApiV0(authenticatedNetworkClient)
