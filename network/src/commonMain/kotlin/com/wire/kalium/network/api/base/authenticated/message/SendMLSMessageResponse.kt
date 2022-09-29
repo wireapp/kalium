a0fb8a321f11da54c7ffd4f63c9d95b84368c078
Jacob Persson <7156+typfel@users.noreply.github.com>
2022-09-29 11:38:12 +0200

    chore: fix detekt

--- network/src/commonMain/kotlin/com/wire/kalium/network/api/base/authenticated/message/SendMLSMessageResponse.kt
+++ network/src/commonMain/kotlin/com/wire/kalium/network/api/base/authenticated/message/SendMLSMessageResponse.kt
@@ -1,10 +1,13 @@
 package com.wire.kalium.network.api.base.authenticated.message
 
 import com.wire.kalium.network.api.base.authenticated.notification.EventResponse
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class SendMLSMessageResponse(
+    @SerialName("name")
     val time: String,
+    @SerialName("events")
     val events: List<EventResponse>
 )
