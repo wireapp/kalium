3eb0f266a6aa88326fdf89709bfd9b285c4be3f1
Jakub Żerko <zerasop@gmail.com>
2022-10-05 08:26:28 +0100

    Removed unused imports

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/client/ClientScope.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/client/ClientScope.kt
@@ -1,12 +1,10 @@
 package com.wire.kalium.logic.feature.client
 
 import com.wire.kalium.cryptography.ProteusClient
-import com.wire.kalium.logic.CoreFailure
 import com.wire.kalium.logic.configuration.notification.NotificationTokenRepository
 import com.wire.kalium.logic.data.client.ClientRepository
 import com.wire.kalium.logic.data.client.MLSClientProvider
 import com.wire.kalium.logic.data.client.remote.ClientRemoteRepository
-import com.wire.kalium.logic.data.conversation.ClientId
 import com.wire.kalium.logic.data.keypackage.KeyPackageLimitsProvider
 import com.wire.kalium.logic.data.keypackage.KeyPackageRepository
 import com.wire.kalium.logic.data.prekey.PreKeyRepository
@@ -20,8 +18,6 @@ import com.wire.kalium.logic.feature.keypackage.RefillKeyPackagesUseCaseImpl
 import com.wire.kalium.logic.feature.session.DeregisterTokenUseCase
 import com.wire.kalium.logic.feature.session.DeregisterTokenUseCaseImpl
 import com.wire.kalium.logic.featureFlags.FeatureSupport
-import com.wire.kalium.logic.functional.Either
-import com.wire.kalium.logic.functional.onSuccess
 
 @Suppress("LongParameterList")
 class ClientScope(
