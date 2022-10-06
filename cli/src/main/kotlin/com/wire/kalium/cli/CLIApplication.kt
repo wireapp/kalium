ea4e89f301bca7268a338c40402f54fb9acbcbf6
Jacob Persson <7156+typfel@users.noreply.github.com>
2022-10-06 16:35:36 +0200

    chore: fix detekt

--- cli/src/main/kotlin/com/wire/kalium/cli/CLIApplication.kt
+++ cli/src/main/kotlin/com/wire/kalium/cli/CLIApplication.kt
@@ -149,8 +149,7 @@ class CreateGroupCommand : CliktCommand(name = "create-group") {
     private val name: String by option(help = "Name of the group").prompt()
     private val protocol: ConversationOptions.Protocol
         by option(help = "Protocol for sending messages").enum<ConversationOptions.Protocol>().default(ConversationOptions.Protocol.MLS)
-
-
+    
     override fun run() = runBlocking {
         val userSession = currentUserSession()
 
