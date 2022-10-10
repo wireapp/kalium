b8e7f03808cdbb28696b96cdb223758e7bf01f54
Dusan Stevanovic <dule@wire.com>
2022-10-10 16:00:32 +0200

    move AnsweCallUseCase to usecase

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/call/usecase/AnswerCallUseCase.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/call/usecase/AnswerCallUseCase.kt
@@ -1,6 +1,9 @@
-package com.wire.kalium.logic.feature.call
+package com.wire.kalium.logic.feature.call.usecase
 
+import com.wire.kalium.logic.data.call.CallType
+import com.wire.kalium.logic.data.call.ConversationType
 import com.wire.kalium.logic.data.id.ConversationId
+import com.wire.kalium.logic.feature.call.CallManager
 
 interface AnswerCallUseCase {
     suspend operator fun invoke(conversationId: ConversationId)
