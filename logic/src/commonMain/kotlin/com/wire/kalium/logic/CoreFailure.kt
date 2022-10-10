66a3e6131a7d843eada199fe53a6b4cdbe290325
Jacob Persson <7156+typfel@users.noreply.github.com>
2022-10-10 14:06:47 +0200

    fix: fail when trying to add a user without key packages to a group AR-2596 (#1029)
    * fix: fail when trying to add a user without key packages to a group
    
    * chore: fix detekt

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/CoreFailure.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/CoreFailure.kt
@@ -1,6 +1,7 @@
 package com.wire.kalium.logic
 
 import com.wire.kalium.cryptography.exceptions.ProteusException
+import com.wire.kalium.logic.data.user.UserId
 import com.wire.kalium.logic.functional.Either
 import com.wire.kalium.network.exceptions.KaliumException
 import com.wire.kalium.network.utils.NetworkResponse
@@ -15,6 +16,12 @@ sealed class CoreFailure {
      * The attempted operation requires that this client is registered.
      */
     object MissingClientRegistration : CoreFailure()
+
+    /**
+     * A user has no key packages available which prevents him/her from being added
+     * to an existing or new conversation.
+     */
+    data class NoKeyPackagesAvailable(val userId: UserId) : CoreFailure()
 
     data class Unknown(val rootCause: Throwable?) : CoreFailure()
 
