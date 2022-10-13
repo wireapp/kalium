f0c360013b944c772000ecc0b03e3cb2a6b0cf55
Michał Saleniuk <saleniuk@gmail.com>
2022-10-13 12:02:23 +0200

    change after review

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/auth/LogoutUseCase.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/auth/LogoutUseCase.kt
@@ -34,15 +34,22 @@ class LogoutUseCaseImpl @Suppress("LongParameterList") constructor(
         logoutRepository.logout()
         sessionRepository.logout(userId = userId, reason)
 
-        if (reason == LogoutReason.REMOVED_CLIENT || reason == LogoutReason.DELETED_ACCOUNT || reason == LogoutReason.SELF_HARD_LOGOUT) {
-            // we put this delay here to avoid a race condition when receiving web socket events at the exact time of logging put
-            delay(CLEAR_DATA_DELAY)
-            clearClientDataUseCase()
-        }
-        if (reason == LogoutReason.SELF_HARD_LOGOUT) {
-            clearUserDataUseCase() // this clears also current client id
-        } else {
-            clientRepository.clearCurrentClientId()
+        when (reason) {
+            LogoutReason.SELF_HARD_LOGOUT -> {
+                // we put this delay here to avoid a race condition when receiving web socket events at the exact time of logging put
+                delay(CLEAR_DATA_DELAY)
+                clearClientDataUseCase()
+                clearUserDataUseCase() // this clears also current client id
+            }
+            LogoutReason.REMOVED_CLIENT, LogoutReason.DELETED_ACCOUNT -> {
+                // we put this delay here to avoid a race condition when receiving web socket events at the exact time of logging put
+                delay(CLEAR_DATA_DELAY)
+                clearClientDataUseCase()
+                clientRepository.clearCurrentClientId()
+            }
+            LogoutReason.SELF_SOFT_LOGOUT, LogoutReason.SESSION_EXPIRED -> {
+                clientRepository.clearCurrentClientId()
+            }
         }
         userSessionScopeProvider.get(userId)?.cancel()
         userSessionScopeProvider.delete(userId)
