99df524ad30111674d3b0d278cd2bf8487fd68c3
Oussama <oussama.has100@gmail.com>
2022-10-14 09:29:30 +0200

    chore: address comments

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/call/usecase/IsCallRunningUseCase.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/call/usecase/IsCallRunningUseCase.kt
@@ -5,15 +5,19 @@ import com.wire.kalium.logic.feature.call.CallStatus
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 
-class IsCallRunningUseCase(
+class IsCallRunningUseCase internal constructor(
     private val callRepository: CallRepository
 ) {
     suspend operator fun invoke(): Boolean {
         return callRepository.callsFlow().map { calls ->
             val call = calls.find {
-                it.status in listOf(CallStatus.STARTED, CallStatus.INCOMING, CallStatus.ESTABLISHED)
+                it.status in runningCalls
             }
             call != null
         }.first()
+    }
+
+    companion object {
+        val runningCalls = listOf(CallStatus.STARTED, CallStatus.INCOMING, CallStatus.ESTABLISHED)
     }
 }
