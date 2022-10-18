69eacf9e0229183d9b3c68dbbcc0e45c9882195f
yamilmedina <yamilmedina@users.noreply.github.com>
2022-10-18 16:04:37 +0200

    chore: add new detekt rules for documented use cases

--- detekt-rules/src/main/java/com/wire/detekt/rules/DocumentedPublicUseCases.kt
+++ detekt-rules/src/main/java/com/wire/detekt/rules/DocumentedPublicUseCases.kt
@@ -12,19 +12,28 @@ class DocumentedPublicUseCases(config: Config = Config.empty) : Rule(config) {
 
     override val issue: Issue = Issue(
         id = javaClass.simpleName,
-        severity = Severity.Style,
+        severity = Severity.Maintainability,
         description = "Public Use Cases should have documentation for the clients.",
         debt = Debt(mins = DEBT_IN_MINUTES_PER_MISSING_ANNOTATION)
     )
 
     override fun visitClassOrObject(kClass: KtClassOrObject) {
-        if (kClass.javaClass.packageName.contains("com.wire.kalium.logic.feature") && kClass.isPublic) {
-            val hasDocumentation = kClass.docComment?.getAllSections()?.firstOrNull()?.getContent() != null
-            if (!hasDocumentation) {
+        if (isClassAPublicUseCase(kClass)) {
+            if (!hasDocumentation(kClass)) {
                 report(kClass, "The class '${kClass.name}' is a Public Use Case and doesn't provide documentation.")
             }
         }
     }
+
+    private fun hasDocumentation(kClass: KtClassOrObject) =
+        kClass.docComment?.getAllSections()?.firstOrNull()?.getContent() != null ||
+                kClass.body?.functions?.firstOrNull { it.name == "invoke" }?.docComment?.getAllSections()?.firstOrNull()
+                    ?.getContent() != null
+
+    private fun isClassAPublicUseCase(kClass: KtClassOrObject) =
+        kClass.fqName?.asString().orEmpty().contains("com.wire.kalium.logic.feature")
+                && kClass.fqName?.shortName()?.asString().orEmpty().endsWith("UseCase", ignoreCase = true)
+                && kClass.isPublic
 
     companion object {
         private const val DEBT_IN_MINUTES_PER_MISSING_ANNOTATION = 1
