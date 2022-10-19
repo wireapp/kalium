26370cc1d996f4f4e60600870b08765438eafd8e
Vitor Hugo Schwaab <vitor@schwaab.xyz>
2022-10-19 18:57:26 +0200

    feat: performing SlowSync only once a day

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/sync/slow/SlowSyncManager.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/sync/slow/SlowSyncManager.kt
@@ -4,6 +4,7 @@ import com.wire.kalium.logger.KaliumLogger.Companion.ApplicationFlow.SYNC
 import com.wire.kalium.logic.data.event.Event
 import com.wire.kalium.logic.data.sync.SlowSyncRepository
 import com.wire.kalium.logic.data.sync.SlowSyncStatus
+import com.wire.kalium.logic.functional.combine
 import com.wire.kalium.logic.kaliumLogger
 import com.wire.kalium.logic.sync.SyncExceptionHandler
 import com.wire.kalium.logic.sync.incremental.IncrementalSyncManager
@@ -16,6 +17,9 @@ import kotlinx.coroutines.flow.cancellable
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.launch
+import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
+import kotlin.time.Duration.Companion.days
 import kotlin.time.Duration.Companion.seconds
 
 /**
@@ -50,35 +54,61 @@ internal class SlowSyncManager(
         }
     })
 
-    init { startMonitoring() }
+    init {
+        startMonitoring()
+    }
 
     private fun startMonitoring() {
         scope.launch(coroutineExceptionHandler) {
             slowSyncCriteriaProvider
                 .syncCriteriaFlow()
                 .distinctUntilChanged()
+                .combine(slowSyncRepository.observeLastSlowSyncCompletionInstant())
                 // Collect latest will cancel whatever is running inside the collector when a new value is emitted
-                .collectLatest { handleCriteriaResolution(it) }
+                .collectLatest { (syncCriteriaResolution, lastTimeSlowSyncWasPerformed) ->
+                    handleCriteriaResolution(syncCriteriaResolution, lastTimeSlowSyncWasPerformed)
+                }
         }
     }
 
-    private suspend fun handleCriteriaResolution(it: SyncCriteriaResolution) {
-        if (it is SyncCriteriaResolution.Ready) {
-            // START SYNC
-            logger.i("Starting SlowSync as all criteria are met")
-            slowSyncWorker.performSlowSyncSteps().cancellable().collect { step ->
-                logger.i("Performing SlowSyncStep $step")
-                slowSyncRepository.updateSlowSyncStatus(SlowSyncStatus.Ongoing(step))
+    private suspend fun handleCriteriaResolution(syncCriteriaResolution: SyncCriteriaResolution, lastTimeSlowSyncWasPerformed: Instant?) {
+        if (syncCriteriaResolution is SyncCriteriaResolution.Ready) {
+            // START SYNC IF NEEDED
+            logger.i("SlowSync criteria ready, checking if SlowSync is needed or already performed")
+            logger.i("Last SlowSync was performed on '$lastTimeSlowSyncWasPerformed'")
+            if (isSlowSyncNeeded(lastTimeSlowSyncWasPerformed)) {
+                logger.i("Starting SlowSync as all criteria are met and it wasn't performed recently")
+                performSlowSync()
+            } else {
+                logger.i("No need to perform SlowSync. Marking as Complete")
             }
             slowSyncRepository.updateSlowSyncStatus(SlowSyncStatus.Complete)
+            slowSyncRepository.setLastSlowSyncCompletionInstant(Clock.System.now())
         } else {
             // STOP SYNC
-            logger.i("SlowSync Stopped as criteria are not met: $it")
+            logger.i("SlowSync Stopped as criteria are not met: $syncCriteriaResolution")
             slowSyncRepository.updateSlowSyncStatus(SlowSyncStatus.Pending)
+        }
+    }
+
+    private fun isSlowSyncNeeded(lastTimeSlowSyncWasPerformed: Instant?): Boolean {
+        return lastTimeSlowSyncWasPerformed?.let {
+            val currentTime = Clock.System.now()
+            val nextSlowSyncDateTime = lastTimeSlowSyncWasPerformed + MIN_TIME_BETWEEN_SLOW_SYNCS
+            logger.i("Next SlowSync should be performed on '$nextSlowSyncDateTime'")
+            currentTime > nextSlowSyncDateTime
+        } ?: true
+    }
+
+    private suspend fun performSlowSync() {
+        slowSyncWorker.performSlowSyncSteps().cancellable().collect { step ->
+            logger.i("Performing SlowSyncStep $step")
+            slowSyncRepository.updateSlowSyncStatus(SlowSyncStatus.Ongoing(step))
         }
     }
 
     private companion object {
         val RETRY_DELAY = 10.seconds
+        val MIN_TIME_BETWEEN_SLOW_SYNCS = 1.days
     }
 }
