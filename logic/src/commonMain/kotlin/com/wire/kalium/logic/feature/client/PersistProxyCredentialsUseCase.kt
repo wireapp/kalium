aa49149fe3c64af5558b769db36e59ac12111650
Mohamad Mukresh <mokresh.mohammed@gmail.com>
2022-10-26 11:20:30 +0200

    update persist proxy credentials use case

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/client/PersistProxyCredentialsUseCase.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/client/PersistProxyCredentialsUseCase.kt
@@ -9,24 +9,28 @@ import com.wire.kalium.logic.functional.fold
  * to authenticate the proxy for the rest of the API calls in the app
  */
 interface PersistProxyCredentialsUseCase {
-    suspend operator fun invoke(username: String, password: String): Result
+    suspend operator fun invoke(username: String, password: String): PersistProxyCredentialsResult
+
+    sealed class PersistProxyCredentialsResult {
+        object Success : PersistProxyCredentialsResult()
+        sealed class Failure : PersistProxyCredentialsResult() {
+            class Generic(val failure: StorageFailure) : Failure()
+        }
+    }
+
 }
 
 class PersistProxyCredentialsUseCaseImpl(
     private val proxyCredentialsRepository: ProxyCredentialsRepository
 ) : PersistProxyCredentialsUseCase {
 
-    override suspend operator fun invoke(username: String, password: String): Result =
+    override suspend operator fun invoke(username: String, password: String): PersistProxyCredentialsUseCase.PersistProxyCredentialsResult =
         proxyCredentialsRepository.persistProxyCredentials(username, password).fold({
-            Result.Failure.Generic(it)
+            PersistProxyCredentialsUseCase.PersistProxyCredentialsResult.Failure.Generic(it)
         }, {
-            Result.Success
+            PersistProxyCredentialsUseCase.PersistProxyCredentialsResult.Success
         })
+
 }
 
-sealed class Result {
-    object Success : Result()
-    sealed class Failure : Result() {
-        class Generic(val failure: StorageFailure) : Failure()
-    }
-}
+
