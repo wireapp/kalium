fd89f7e5d19ade72238f55b9847db09e2a2d09ba
Alexandre Ferris <ferris.alexandre@gmail.com>
2022-10-27 15:52:07 +0200

    test: Receive mentions (#1077)
    * feat: select mentions from db view and map correctly
    
    * feat: add migration for MessageDetailsView
    
    * feat: add mentions tests
    
    * chore: adjust detekt
    
    * chore: remove experimental
    
    * test: try out failing separated tests
    
    * test: add println
    
    * test: change logs
    
    * test: change logs
    
    * test: change test assertions to string instead of objects
    
    * ci: archive test reports
    
    * test: change logs to println again
    
    * chore: throw exception if IDs are initialised with @ in values
    
    * chore: throw exception if IDs are initialised with @ in values
    
    * chore: force remove @ when splitting user id in db
    
    * chore: remove require throwing exception when qualified id has extra @
    
    * chore: add sql change to migration and adjust tests
    
    * chore: rename test reports file
    
    * chore: remove require throwing exceptions for qualified id containing @
    
    Co-authored-by: Vitor Hugo Schwaab <vitor@schwaab.xyz>

---
+++ persistence/src/commonTest/kotlin/com/wire/kalium/persistence/dao/message/MessageMentionsTest.kt
@@ -0,0 +1,111 @@
+package com.wire.kalium.persistence.dao.message
+
+import com.wire.kalium.persistence.BaseDatabaseTest
+import com.wire.kalium.persistence.dao.ConversationDAO
+import com.wire.kalium.persistence.dao.UserDAO
+import com.wire.kalium.persistence.utils.stubs.newConversationEntity
+import com.wire.kalium.persistence.utils.stubs.newRegularMessageEntity
+import com.wire.kalium.persistence.utils.stubs.newUserEntity
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.runTest
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertIs
+
+class MessageMentionsTest : BaseDatabaseTest() {
+
+    private lateinit var messageDAO: MessageDAO
+    private lateinit var conversationDAO: ConversationDAO
+    private lateinit var userDAO: UserDAO
+
+    @BeforeTest
+    fun setUp() {
+        deleteDatabase(SELF_USER_ID)
+        val db = createDatabase(SELF_USER_ID)
+
+        messageDAO = db.messageDAO
+        conversationDAO = db.conversationDAO
+        userDAO = db.userDAO
+    }
+
+    @Test
+    fun givenMentionsAreInserted_whenGettingMessageByConversationIdAndVisibility_thenCorrectMentionsAreReturned() = runTest {
+        testTotalMentions {
+            messageDAO.getMessagesByConversationAndVisibility(TEST_MESSAGE.conversationId, 1, 0)
+                .first()
+                .first()
+        }
+    }
+
+    @Test
+    fun givenMentionsAreInserted_whenGettingMessageById_thenCorrectMentionsAreReturned() = runTest {
+        testTotalMentions {
+            messageDAO.getMessageById(TEST_MESSAGE.id, TEST_MESSAGE.conversationId).first()
+        }
+    }
+
+    private suspend fun testTotalMentions(
+        queryMessageEntity: suspend () -> MessageEntity?
+    ) {
+        // given
+        insertInitialData()
+
+        // when
+        val result = queryMessageEntity()
+
+        // then
+        val messageResult = (result?.content as MessageEntityContent.Text)
+        assertIs<MessageEntity.Regular>(result)
+        assertEquals(
+            2,
+            messageResult.mentions.size
+        )
+        assertEquals(
+            SELF_USER_ID,
+            messageResult.mentions.first().userId
+        )
+        assertEquals(
+            OTHER_USER_2.id,
+            messageResult.mentions.last().userId
+        )
+    }
+
+    private suspend fun insertInitialData() {
+        userDAO.upsertUsers(listOf(SELF_USER, OTHER_USER))
+        conversationDAO.insertConversation(TEST_CONVERSATION)
+        messageDAO.insertMessage(
+            TEST_MESSAGE.copy(
+                content = MessageEntityContent.Text(
+                    messageBody = "@${SELF_USER.name} @${OTHER_USER_2.name}",
+                    mentions = listOf(
+                        TEST_MENTION_1,
+                        TEST_MENTION_2
+                    )
+                )
+            )
+        )
+    }
+
+    private companion object {
+        val TEST_CONVERSATION = newConversationEntity("testConversation")
+        val SELF_USER = newUserEntity("selfUser").copy(name = "selfUser")
+        val OTHER_USER = newUserEntity("otherUser").copy(name = "otherUser")
+        val OTHER_USER_2 = newUserEntity("otherUser2").copy(name = "otherUser2")
+        val SELF_USER_ID = SELF_USER.id
+        val TEST_MESSAGE = newRegularMessageEntity(
+            conversationId = TEST_CONVERSATION.id,
+            senderUserId = OTHER_USER.id
+        )
+        val TEST_MENTION_1 = MessageEntity.Mention(
+            start = 0,
+            length = 9,
+            userId = SELF_USER_ID
+        )
+        val TEST_MENTION_2 = MessageEntity.Mention(
+            start = 10,
+            length = 11,
+            userId = OTHER_USER_2.id
+        )
+    }
+}
