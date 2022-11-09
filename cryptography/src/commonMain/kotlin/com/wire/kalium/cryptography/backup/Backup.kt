b6c5f64e5e8369c701c182e8dfc991c674f596b3
Gonzalo Gran Crespo <gongracr@gmail.com>
2022-11-09 10:32:29 +0100

    More detekt

--- cryptography/src/commonMain/kotlin/com/wire/kalium/cryptography/backup/Backup.kt
+++ cryptography/src/commonMain/kotlin/com/wire/kalium/cryptography/backup/Backup.kt
@@ -44,60 +44,57 @@ class Backup(val salt: UByteArray, val userId: CryptoUserID, val passphrase: Pas
         companion object {
 
             @Suppress("ComplexMethod")
+            @Throws(IOException::class)
             fun Source.readBackupHeader(): Header {
-                try {
-                    val readBuffer = Buffer()
-                    // We read the backup header and execute some sanity checks
-                    val format = this.read(readBuffer, BACKUP_HEADER_FORMAT_LENGTH).let { size ->
-                        readBuffer.readByteArray(size).decodeToString().also {
-                            if (it != format) throw IllegalStateException("Backup format is not valid")
-                            readBuffer.clear()
-                        }
+                val readBuffer = Buffer()
+                // We read the backup header and execute some sanity checks
+                val format = this.read(readBuffer, BACKUP_HEADER_FORMAT_LENGTH).let { size ->
+                    readBuffer.readByteArray(size).decodeToString().also {
+                        if (it != format) throw IllegalStateException("Backup format is not valid")
+                        readBuffer.clear()
                     }
-
-                    // We skip the extra gap
-                    read(readBuffer, BACKUP_HEADER_EXTRA_GAP_LENGTH).also { readBuffer.clear() }
-
-                    val version = read(readBuffer, BACKUP_HEADER_VERSION_LENGTH).let { size ->
-                        readBuffer.readByteArray(size).decodeToString().also {
-                            if (it != version) throw IllegalStateException("Backup version is not valid")
-                            readBuffer.clear()
-                        }
-                    }
-
-                    val salt = read(readBuffer, crypto_pwhash_SALTBYTES.toLong()).let { size ->
-                        readBuffer.readByteArray(size).toUByteArray().also { readBuffer.clear() }
-                    }
-
-                    val hashedUserId = read(readBuffer, PWD_HASH_OUTPUT_BYTES.toLong()).let { size ->
-                        readBuffer.readByteArray(size).toUByteArray().also { readBuffer.clear() }
-                    }
-
-                    val opslimit = this.read(readBuffer, UNSIGNED_INT_LENGTH).let {
-                        readBuffer.readInt().also {
-                            // TODO: Do some checking if opslimit value changes between versions?
-                            readBuffer.clear()
-                        }
-                    }
-
-                    val memlimit = this.read(readBuffer, UNSIGNED_INT_LENGTH).let {
-                        readBuffer.readInt().also {
-                            // TODO: Do some checking if opslimit value changes between versions?
-                            readBuffer.clear()
-                        }
-                    }
-
-                    return Header(
-                        format = format,
-                        version = version,
-                        salt = salt,
-                        hashedUserId = hashedUserId,
-                        opslimit = opslimit,
-                        memlimit = memlimit
-                    )
-                } catch (e: IOException) {
-                    throw IllegalStateException("Backup provided can't be read", e)
                 }
+
+                // We skip the extra gap
+                read(readBuffer, BACKUP_HEADER_EXTRA_GAP_LENGTH).also { readBuffer.clear() }
+
+                val version = read(readBuffer, BACKUP_HEADER_VERSION_LENGTH).let { size ->
+                    readBuffer.readByteArray(size).decodeToString().also {
+                        if (it != version) throw IllegalStateException("Backup version is not valid")
+                        readBuffer.clear()
+                    }
+                }
+
+                val salt = read(readBuffer, crypto_pwhash_SALTBYTES.toLong()).let { size ->
+                    readBuffer.readByteArray(size).toUByteArray().also { readBuffer.clear() }
+                }
+
+                val hashedUserId = read(readBuffer, PWD_HASH_OUTPUT_BYTES.toLong()).let { size ->
+                    readBuffer.readByteArray(size).toUByteArray().also { readBuffer.clear() }
+                }
+
+                val opslimit = this.read(readBuffer, UNSIGNED_INT_LENGTH).let {
+                    readBuffer.readInt().also {
+                        // TODO: Do some checking if opslimit value changes between versions?
+                        readBuffer.clear()
+                    }
+                }
+
+                val memlimit = this.read(readBuffer, UNSIGNED_INT_LENGTH).let {
+                    readBuffer.readInt().also {
+                        // TODO: Do some checking if opslimit value changes between versions?
+                        readBuffer.clear()
+                    }
+                }
+
+                return Header(
+                    format = format,
+                    version = version,
+                    salt = salt,
+                    hashedUserId = hashedUserId,
+                    opslimit = opslimit,
+                    memlimit = memlimit
+                )
             }
 
         }
@@ -138,7 +135,8 @@ class Backup(val salt: UByteArray, val userId: CryptoUserID, val passphrase: Pas
             initializeLibsodiumIfNeeded()
             return PasswordHash.pwhash(
                 PWD_HASH_OUTPUT_BYTES,
-                userId.value,  // TODO: Should we hash only the user id value or the whole object with domain?
+                // TODO: Should we hash only the user id value or the whole object with domain?
+                userId.value,
                 salt,
                 OPSLIMIT_INTERACTIVE_VALUE.toULong(),
                 MEMLIMIT_INTERACTIVE_VALUE,
