a9e06622c951d173a9b0a7c237f5c27687f783a2
Mateusz <m.pachulski94@gmail.com>
2022-11-11 14:27:58 +0100

    push

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/backup/ImportBackupUseCase.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/backup/ImportBackupUseCase.kt
@@ -2,10 +2,10 @@ package com.wire.kalium.logic.feature.backup
 
 import com.wire.kalium.persistence.backup.BackupImporter
 
+// TODO: User IO Dispatcher
 class ImportBackupUseCase(
     private val backupImporter: BackupImporter
-    // TODO: User IO Dispatcher
-    ) {
+) {
 
     suspend operator fun invoke(backupFilePath: String) {
         backupImporter.importFromFile(backupFilePath)
