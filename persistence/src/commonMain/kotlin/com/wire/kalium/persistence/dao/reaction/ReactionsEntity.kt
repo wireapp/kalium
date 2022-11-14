c37db90ec8bdf682c6af8773c2ed8ad0bbc9400c
alexandreferris <ferris.alexandre@gmail.com>
2022-11-14 12:04:29 +0100

    feat: add message deatils reactions view, query and mapper to DAO

--- persistence/src/commonMain/kotlin/com/wire/kalium/persistence/dao/reaction/ReactionsEntity.kt
+++ persistence/src/commonMain/kotlin/com/wire/kalium/persistence/dao/reaction/ReactionsEntity.kt
@@ -1,5 +1,10 @@
 package com.wire.kalium.persistence.dao.reaction
 
+import com.wire.kalium.persistence.dao.ConnectionEntity
+import com.wire.kalium.persistence.dao.QualifiedIDEntity
+import com.wire.kalium.persistence.dao.UserAvailabilityStatusEntity
+import com.wire.kalium.persistence.dao.UserTypeEntity
+
 data class ReactionsEntity(
     val totalReactions: ReactionsCountEntity,
     val selfUserReactions: UserReactionsEntity
@@ -8,6 +13,18 @@ data class ReactionsEntity(
         val EMPTY = ReactionsEntity(emptyMap(), emptySet())
     }
 }
+
+data class MessageReactionEntity(
+    val emoji: String,
+    val userId: QualifiedIDEntity,
+    val name: String?,
+    val handle: String?,
+    val previewAssetIdEntity: QualifiedIDEntity?,
+    val userTypeEntity: UserTypeEntity,
+    val deleted: Boolean,
+    val connectionStatus: ConnectionEntity.State,
+    val availabilityStatus: UserAvailabilityStatusEntity
+)
 
 typealias ReactionsCountEntity = Map<String, Int>
 typealias UserReactionsEntity = Set<String>
