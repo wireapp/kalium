5be16bedea827723734b9a9265ca1f9e4ece6f3c
Mateusz <m.pachulski94@gmail.com>
2022-11-14 12:43:32 +0100

    Update persistence/src/commonTest/kotlin/com/wire/kalium/persistence/backup/UserDatabaseDataGenerator.kt
    Co-authored-by: Gonzo <gongracr@gmail.com>

--- persistence/src/commonTest/kotlin/com/wire/kalium/persistence/backup/UserDatabaseDataGenerator.kt
+++ persistence/src/commonTest/kotlin/com/wire/kalium/persistence/backup/UserDatabaseDataGenerator.kt
@@ -248,7 +248,7 @@ class UserDatabaseDataGenerator(
 
         val conversationType = ConversationEntity.Type.values()[generatedCallsCount % ConversationEntity.Type.values().size]
 
-        val invalidatedConversationType =
+        val sanitizedConversationType =
             if (conversationType == ConversationEntity.Type.CONNECTION_PENDING)
                 ConversationEntity.Type.values()[(generatedCallsCount + 1) % ConversationEntity.Type.values().size]
             else conversationType
