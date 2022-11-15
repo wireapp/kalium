f0ab6f0fd4328314b12e546d06696196c2a9d59b
Jacob Persson <7156+typfel@users.noreply.github.com>
2022-11-15 15:57:34 +0100

    chore: fix detekt

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/network/ApiMigrationV3.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/network/ApiMigrationV3.kt
@@ -7,7 +7,6 @@ import com.wire.kalium.logic.feature.session.UpgradeCurrentSessionUseCase
 import com.wire.kalium.logic.functional.Either
 import com.wire.kalium.logic.functional.flatMap
 import com.wire.kalium.logic.functional.flatMapLeft
-import com.wire.kalium.logic.functional.map
 
 class ApiMigrationV3(
     private val currentClientIdProvider: CurrentClientIdProvider,
