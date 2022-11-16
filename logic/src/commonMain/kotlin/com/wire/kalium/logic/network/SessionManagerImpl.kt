99016280636a1dae6371d8385b837d4484a663fa
Mohamad Jaara <mohamad.jaara@wire.com>
2022-11-16 13:16:55 +0100

    move the token refresh logic to session manager

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/network/SessionManagerImpl.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/network/SessionManagerImpl.kt
@@ -1,6 +1,8 @@
 package com.wire.kalium.logic.network
 
 import app.cash.sqldelight.internal.Atomic
+import com.wire.kalium.logger.obfuscateId
+import com.wire.kalium.logic.NetworkFailure
 import com.wire.kalium.logic.configuration.server.ServerConfigMapper
 import com.wire.kalium.logic.data.id.IdMapper
 import com.wire.kalium.logic.data.id.QualifiedID
@@ -11,15 +13,22 @@ import com.wire.kalium.logic.di.MapperProvider
 import com.wire.kalium.logic.functional.fold
 import com.wire.kalium.logic.functional.map
 import com.wire.kalium.logic.functional.nullableFold
+import com.wire.kalium.logic.functional.onFailure
 import com.wire.kalium.logic.functional.onSuccess
+import com.wire.kalium.logic.kaliumLogger
+import com.wire.kalium.logic.wrapApiRequest
 import com.wire.kalium.logic.wrapStorageRequest
+import com.wire.kalium.network.api.base.authenticated.AccessTokenApi
 import com.wire.kalium.network.api.base.model.AccessTokenDTO
 import com.wire.kalium.network.api.base.model.ProxyCredentialsDTO
 import com.wire.kalium.network.api.base.model.RefreshTokenDTO
 import com.wire.kalium.network.api.base.model.SessionDTO
+import com.wire.kalium.network.exceptions.KaliumException
+import com.wire.kalium.network.exceptions.isUnknownClient
 import com.wire.kalium.network.session.SessionManager
 import com.wire.kalium.network.tools.ServerConfigDTO
 import com.wire.kalium.persistence.client.AuthTokenStorage
+import io.ktor.http.HttpStatusCode
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 
@@ -37,12 +46,15 @@ class SessionManagerImpl internal constructor(
     private var session: SessionDTO? = null
     private var serverConfig: Atomic<ServerConfigDTO?> = Atomic(null)
 
-    override suspend fun session(): SessionDTO = lock.withLock {
+    override suspend fun session(): SessionDTO? = lock.withLock {
         session ?: run {
             wrapStorageRequest { tokenStorage.getToken(idMapper.toDaoModel(userId)) }
                 .map { sessionMapper.fromEntityToSessionDTO(it) }
                 .onSuccess { session = it }
-            session!!
+                .onFailure { kaliumLogger.e("""SESSION MANAGER: 
+                    |"error": "missing user session",
+                    |"cause": "$it" """.trimMargin()) }
+            session
         }
     }
 
@@ -54,7 +66,7 @@ class SessionManagerImpl internal constructor(
         serverConfig.get()!!
     }
 
-    override fun updateLoginSession(newAccessTokeDTO: AccessTokenDTO, newRefreshTokenDTO: RefreshTokenDTO?): SessionDTO =
+    private fun updateLoginSession(newAccessTokeDTO: AccessTokenDTO, newRefreshTokenDTO: RefreshTokenDTO?): SessionDTO? =
         wrapStorageRequest {
             tokenStorage.updateToken(
                 userId = idMapper.toDaoModel(userId),
@@ -66,25 +78,47 @@ class SessionManagerImpl internal constructor(
             sessionMapper.fromEntityToSessionDTO(it)
         }.onSuccess {
             session = it
-        }.fold({
-            TODO("IMPORTANT! Not yet implemented")
+        }.nullableFold({
+            null
         }, {
             it
         })
 
-    override suspend fun beforeTokenUpdate() {
-        lock.lock()
+    override suspend fun updateToken(accessTokenApi: AccessTokenApi, oldAccessToken: String, oldRefreshToken: String): SessionDTO? {
+        return lock.withLock {
+            wrapApiRequest { accessTokenApi.getToken(oldRefreshToken) }.nullableFold({
+                when (it) {
+                    is NetworkFailure.NoNetworkConnection -> null
+                    is NetworkFailure.ProxyError -> null
+                    is NetworkFailure.ServerMiscommunication -> {
+                        onServerMissCommunication(it)
+                        null
+                    }
+                }
+            }, {
+                updateLoginSession(it.first, it.second)
+            })
+        }
     }
 
-    override fun afterTokenUpdate() {
-        lock.unlock()
+    private suspend fun onServerMissCommunication(serverMiscommunication: NetworkFailure.ServerMiscommunication) {
+        with(serverMiscommunication.kaliumException) {
+            if (this is KaliumException.InvalidRequestError) {
+                if (this.errorResponse.code == HttpStatusCode.Forbidden.value)
+                    onSessionExpired()
+                if (this.isUnknownClient())
+                    onClientRemoved()
+            }
+        }
     }
 
-    override suspend fun onSessionExpired() {
+    private suspend fun onSessionExpired() {
+        kaliumLogger.d("SESSION MANAGER: onSessionExpired is called for user ${userId.value.obfuscateId()}")
         sessionRepository.logout(userId, LogoutReason.SESSION_EXPIRED)
     }
 
-    override suspend fun onClientRemoved() {
+    private suspend fun onClientRemoved() {
+        kaliumLogger.d("SESSION MANAGER: onSessionExpired is called for user ${userId.value.obfuscateId()}")
         sessionRepository.logout(userId, LogoutReason.REMOVED_CLIENT)
     }
 
