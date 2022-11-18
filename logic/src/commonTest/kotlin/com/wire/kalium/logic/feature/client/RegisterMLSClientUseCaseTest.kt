b987f42752c4273739286dd2904d5e2380a39ae1
Jacob Persson <7156+typfel@users.noreply.github.com>
2022-11-18 14:37:31 +0100

    feat: register MLS client upon upgrading AR-2634 (#1132)
    * feat: expose mlsPublicKeys on the Client model
    
    * feat: add method for checking if an MLS client has been registered
    
    * fix: only run MLS managers if an MLS client has been registered
    
    * feat: register an MLS client on demand when support is enabled
    
    * feat: persist that we have registered an MLS client
    
    * chore: make tests compile
    
    * test: update LogoutUseCaseTest
    
    * test: add MLSCLientManagerTest
    
    * test: add RegisterMLSClientUseCaseTest
    
    * test: update ClientRepositoryTest
    
    * test: update ClientRegistrationStorageTest
    
    * chore: fix detekt
    
    * test: update KeyingMaterialsManagerTests and KeyPackageManagerTests
    
    * chore: fix compilation
    
    * test: fix network tests
    
    * chore: fix detekt
    
    * refactor: trigger slow sync instead of performing individual slow sync steps
    
    * chore: fix detekt
    
    Co-authored-by: Mojtaba Chenani <chenani@outlook.com>

---
+++ logic/src/commonTest/kotlin/com/wire/kalium/logic/feature/client/RegisterMLSClientUseCaseTest.kt
@@ -0,0 +1,109 @@
+package com.wire.kalium.logic.feature.client
+
+import com.wire.kalium.cryptography.MLSClient
+import com.wire.kalium.logic.CoreFailure
+import com.wire.kalium.logic.data.client.ClientRepository
+import com.wire.kalium.logic.data.client.MLSClientProvider
+import com.wire.kalium.logic.data.keypackage.KeyPackageLimitsProvider
+import com.wire.kalium.logic.data.keypackage.KeyPackageRepository
+import com.wire.kalium.logic.framework.TestClient
+import com.wire.kalium.logic.functional.Either
+import io.mockative.Mock
+import io.mockative.anything
+import io.mockative.classOf
+import io.mockative.eq
+import io.mockative.given
+import io.mockative.mock
+import io.mockative.once
+import io.mockative.verify
+import kotlinx.coroutines.test.runTest
+import kotlin.test.Test
+
+class RegisterMLSClientUseCaseTest {
+    @Test
+    fun givenRegisterMLSClientUseCase_whenInvoked_thenRegisterMLSClient() =
+        runTest() {
+            val (arrangement, registerMLSClient) = Arrangement()
+                .withGetMLSClientSuccessful()
+                .withGetPublicKey(Arrangement.MLS_PUBLIC_KEY)
+                .withRegisterMLSClient(Either.Right(Unit))
+                .withKeyPackageLimits(Arrangement.REFILL_AMOUNT)
+                .withUploadKeyPackagesSuccessful()
+                .arrange()
+
+            registerMLSClient(TestClient.CLIENT_ID)
+
+            verify(arrangement.clientRepository)
+                .suspendFunction(arrangement.clientRepository::registerMLSClient)
+                .with(eq(TestClient.CLIENT_ID), eq(Arrangement.MLS_PUBLIC_KEY))
+                .wasInvoked(exactly = once)
+
+            verify(arrangement.keyPackageRepository)
+                .suspendFunction(arrangement.keyPackageRepository::uploadNewKeyPackages)
+                .with(eq(TestClient.CLIENT_ID), eq(Arrangement.REFILL_AMOUNT))
+        }
+
+    private class Arrangement {
+
+        @Mock
+        val mlsClient = mock(classOf<MLSClient>())
+
+        @Mock
+        var mlsClientProvider = mock(classOf<MLSClientProvider>())
+
+        @Mock
+        val clientRepository = mock(classOf<ClientRepository>())
+
+        @Mock
+        val keyPackageRepository = mock(classOf<KeyPackageRepository>())
+
+        @Mock
+        val keyPackageLimitsProvider = mock(classOf<KeyPackageLimitsProvider>())
+
+        fun withRegisterMLSClient(result: Either<CoreFailure, Unit>) = apply {
+            given(clientRepository)
+                .suspendFunction(clientRepository::registerMLSClient)
+                .whenInvokedWith(anything(), anything())
+                .thenReturn(result)
+        }
+
+        fun withKeyPackageLimits(refillAmount: Int) = apply {
+            given(keyPackageLimitsProvider).function(keyPackageLimitsProvider::refillAmount)
+                .whenInvoked()
+                .thenReturn(refillAmount)
+        }
+
+        fun withUploadKeyPackagesSuccessful() = apply {
+            given(keyPackageRepository).suspendFunction(keyPackageRepository::uploadNewKeyPackages)
+                .whenInvokedWith(eq(TestClient.CLIENT_ID), anything())
+                .thenReturn(Either.Right(Unit))
+        }
+
+        fun withGetPublicKey(result: ByteArray) = apply {
+            given(mlsClient)
+                .function(mlsClient::getPublicKey)
+                .whenInvoked()
+                .thenReturn(result)
+        }
+
+        fun withGetMLSClientSuccessful() = apply {
+            given(mlsClientProvider)
+                .suspendFunction(mlsClientProvider::getMLSClient)
+                .whenInvokedWith(anything())
+                .then { Either.Right(mlsClient) }
+        }
+
+        fun arrange() = this to RegisterMLSClientUseCaseImpl(
+            mlsClientProvider,
+            clientRepository,
+            keyPackageRepository,
+            keyPackageLimitsProvider
+        )
+
+        companion object {
+            val MLS_PUBLIC_KEY = "public_key".encodeToByteArray()
+            const val REFILL_AMOUNT = 100
+        }
+
+    }
+}
