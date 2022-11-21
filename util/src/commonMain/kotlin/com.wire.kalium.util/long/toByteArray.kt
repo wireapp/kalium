650236994f19c5afb69879901daec4b4a1da979e
Mateusz <m.pachulski94@gmail.com>
2022-11-21 22:45:42 +0100

    add unit tests for now

--- util/src/commonMain/kotlin/com.wire.kalium.util/long/toByteArray.kt
+++ util/src/commonMain/kotlin/com.wire.kalium.util/long/toByteArray.kt
@@ -1,16 +1,14 @@
 package com.wire.kalium.util.long
 
 fun Long.toByteArray(): ByteArray {
-    var longConvertedToByteArray = this
-
     val result = ByteArray(Long.SIZE_BYTES)
+
+    var longConvertedToByteArray = this
 
     for (i in Long.SIZE_BYTES - 1 downTo 0) {
         result[i] = (longConvertedToByteArray and 0xFFL).toByte()
         longConvertedToByteArray = longConvertedToByteArray shr Long.SIZE_BYTES
     }
 
-    val lastEncounteredZeroIndex = result.indexOfLast { it == 0x00.toByte() }
-
-    return result.copyOfRange(lastEncounteredZeroIndex + 1, result.size)
+    return result.copyOfRange(result.indexOfLast { it == 0x00.toByte() } + 1, result.size)
 }
