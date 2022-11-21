c2adc197bcce35defc155bbd81a03d546ee5dc3e
Mateusz <m.pachulski94@gmail.com>
2022-11-21 22:48:32 +0100

    work on unit tests

--- util/src/commonTest/kotlin/com/wire/kalium/util/string/LongToByteArrayTest.kt
+++ util/src/commonTest/kotlin/com/wire/kalium/util/string/LongToByteArrayTest.kt
@@ -1,12 +1,15 @@
 package com.wire.kalium.util.string
 
+import com.wire.kalium.util.long.toByteArray
 import kotlin.test.Test
 
 class LongToByteArrayTest {
 
     @Test
     fun test() {
+        val result = 9_223_372_036_854_775_803.toByteArray()
 
+        println(result)
     }
 
 }
