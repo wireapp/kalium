d86d6bf39e96da6339f83ed00043d766c6f739b8
Mateusz <m.pachulski94@gmail.com>
2022-11-23 15:37:41 +0100

    cover ios

--- util/src/iosX64Main/kotlin/com.wire.kalium.util/string/toUTF16BEByteArray.kt
+++ util/src/iosX64Main/kotlin/com.wire.kalium.util/string/toUTF16BEByteArray.kt
@@ -1,12 +1,23 @@
 package com.wire.kalium.util.string
 
+import kotlinx.cinterop.allocArrayOf
 import kotlinx.cinterop.getBytes
+import kotlinx.cinterop.memScoped
 import kotlinx.cinterop.utf16
+import platform.Foundation.NSData
+import platform.Foundation.NSString
+import platform.Foundation.NSUTF16BigEndianStringEncoding
 
 actual fun String.toUTF16BEByteArray(): ByteArray {
     return utf16.getBytes()
 }
 
-actual fun ByteArray.toStringFromUtf16BE(): String {
-    TODO("Not yet implemented")
+actual fun ByteArray.toStringFromUtf16BE(): String = memScoped {
+    val data = NSData.create(
+        bytes = allocArrayOf(this@toStringFromUtf16BE),
+        length = this@toStringFromUtf16BE.size.toULong()
+    )
+    val string = NSString.create(data = data, encoding = NSUTF16BigEndianStringEncoding)
+    return@memScoped string
 }
+
