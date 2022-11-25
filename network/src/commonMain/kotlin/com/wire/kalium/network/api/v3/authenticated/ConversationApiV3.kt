10a355b0b1741c00d791ec01e6d807cd4fd4432b
Mojtaba Chenani <chenani@outlook.com>
2022-11-25 15:53:44 +0100

    feat(mls): join by external commit (AR-2506) (#1150)
    * feat(mls): join by external commit
    
    * test(mls): add tests
    
    * chore: convert fetchGroupState result to byteArray
    
    * fix defekts
    
    * fix tests
    
    * add joinViaExternalCommit to joinExistGroup usecase
    
    * test: update JoinExistingMLSConversationsUseCaseTest
    
    * remove unneeded code
    
    * fix spacing in code
    
    * revert unneeded changes
    
    * apply review comments
    
    * apply review comments
    
    * fix tests and defekt
    
    Co-authored-by: Jacob Persson <7156+typfel@users.noreply.github.com>

--- network/src/commonMain/kotlin/com/wire/kalium/network/api/v3/authenticated/ConversationApiV3.kt
+++ network/src/commonMain/kotlin/com/wire/kalium/network/api/v3/authenticated/ConversationApiV3.kt
@@ -1,8 +1,25 @@
 package com.wire.kalium.network.api.v3.authenticated
 
 import com.wire.kalium.network.AuthenticatedNetworkClient
+import com.wire.kalium.network.api.base.model.QualifiedID
 import com.wire.kalium.network.api.v2.authenticated.ConversationApiV2
+import com.wire.kalium.network.utils.NetworkResponse
+import com.wire.kalium.network.utils.wrapKaliumResponse
+import io.ktor.client.request.get
 
 internal open class ConversationApiV3 internal constructor(
     authenticatedNetworkClient: AuthenticatedNetworkClient
-) : ConversationApiV2(authenticatedNetworkClient)
+) : ConversationApiV2(authenticatedNetworkClient) {
+
+    override suspend fun fetchGroupInfo(conversationId: QualifiedID): NetworkResponse<ByteArray> =
+        wrapKaliumResponse {
+            httpClient.get(
+                "$PATH_CONVERSATIONS/${conversationId.domain}/${conversationId.value}/$PATH_GROUP_INFO"
+            )
+        }
+
+    protected companion object {
+        const val PATH_CONVERSATIONS = "conversations"
+        const val PATH_GROUP_INFO = "groupinfo"
+    }
+}
