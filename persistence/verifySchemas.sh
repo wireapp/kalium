da548de768179e3469966872c2f3ff9b9442ebb1
Vitor Hugo Schwaab <vitor@schwaab.xyz>
2022-12-02 14:52:57 +0100

    ci: verify modified schemas

--- persistence/verifySchemas.sh
+++ persistence/verifySchemas.sh
@@ -3,7 +3,7 @@
 MISSING_FILES=""
 for dbDir in db_global db_user
 do
-    RESULT=$(git ls-files --other --exclude-standard | grep ".*$dbDir.*.db$")
+    RESULT=$(git ls-files -m -o --exclude-standard | grep ".*$dbDir.*.db$")
     if [ -n "${RESULT}" ]; then
         RESULT="\n${RESULT}"
     fi
@@ -12,6 +12,9 @@ done
 
 if [ -n "${MISSING_FILES}" ]
 then
-    >&2 echo "Missing DB schema(s) detected. Make sure you run the appropriate ./gradlew generate[*]DatabaseSchema and commit the generated files to the repository.$MISSING_FILES"
+    >&2 echo "Missing or Modified DB schema(s) detected: $MISSING_FILES"
+    >&2 echo "- If you have modified the DB schema. Make sure you have added a new migration!"
+    >&2 echo "- If have added a new migration. Make sure you run the appropriate ./gradlew generate[*]DatabaseSchema and commit the generated files to the repository."
+    >&2 echo "- If you are modifying a migration... ☢️ That's dangerous. Are you sure about that? ☢️ "
     exit 1
 fi
