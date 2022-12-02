4775119ba5c6988de2fd1bd17f9c16d1153d08bc
Mateusz <m.pachulski94@gmail.com>
2022-12-02 18:34:22 +0100

    add logs

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/sync/incremental/IncrementalSyncRecoveryHandler.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/sync/incremental/IncrementalSyncRecoveryHandler.kt
@@ -5,18 +5,21 @@ import com.wire.kalium.logic.NetworkFailure
 import com.wire.kalium.logic.data.sync.IncrementalSyncRepository
 import com.wire.kalium.logic.data.sync.IncrementalSyncStatus
 import com.wire.kalium.logic.data.sync.SlowSyncRepository
+import com.wire.kalium.logic.kaliumLogger
 import com.wire.kalium.network.exceptions.KaliumException
 
 internal class IncrementalSyncRecoveryHandler(
     private val slowSyncRepository: SlowSyncRepository,
     private val incrementalSyncRepository: IncrementalSyncRepository
 ) {
-
     suspend fun recover(failure: CoreFailure, onRetry: suspend () -> Unit) {
+        kaliumLogger.i("$TAG Checking if we can recover from the failure: $failure")
         incrementalSyncRepository.updateIncrementalSyncState(IncrementalSyncStatus.Failed(failure))
         if (shouldRestartSlowSyncProcess(failure)) {
+            kaliumLogger.i("$TAG Cannot recover from the failure: $failure restarting slow sync")
             slowSyncRepository.clearLastSlowSyncCompletionInstant()
         } else {
+            kaliumLogger.i("$TAG Retrying to recover form the failure $failure, perform the incremental sync again")
             onRetry()
         }
     }
@@ -29,4 +32,8 @@ internal class IncrementalSyncRecoveryHandler(
         (failure is NetworkFailure.ServerMiscommunication) &&
                 (failure.kaliumException is KaliumException.InvalidRequestError) &&
                 (failure.kaliumException.errorResponse.code == 404)
+
+    private companion object {
+        private const val TAG = "IncrementalSyncRecoveryHandler"
+    }
 }
--- logic/src/commonMain/kotlin/com/wire/kalium/logic/sync/slow/SlowSyncRecoveryHandler.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/sync/slow/SlowSyncRecoveryHandler.kt
@@ -4,14 +4,28 @@ import com.wire.kalium.logic.CoreFailure
 import com.wire.kalium.logic.data.logout.LogoutReason
 import com.wire.kalium.logic.failure.SelfUserDeleted
 import com.wire.kalium.logic.feature.auth.LogoutUseCase
+import com.wire.kalium.logic.kaliumLogger
+import com.wire.kalium.logic.sync.incremental.IncrementalSyncRecoveryHandler
 
 class SlowSyncRecoveryHandler(private val logoutUseCase: LogoutUseCase) {
 
     suspend fun recover(failure: CoreFailure, onRetry: suspend () -> Unit) {
+        kaliumLogger.i("$TAG Checking if we can recover from the failure: $failure")
         when (failure) {
-            is SelfUserDeleted -> logoutUseCase(LogoutReason.DELETED_ACCOUNT)
-            else -> onRetry()
+            is SelfUserDeleted -> {
+                kaliumLogger.i("$TAG Self user has been deleted, cannot recover, performing logout: $failure")
+                logoutUseCase(LogoutReason.DELETED_ACCOUNT)
+            }
+
+            else -> {
+                kaliumLogger.i("${TAG} Retrying to recover form the failure $failure, performing the slow sync again")
+                onRetry()
+            }
         }
+    }
+
+    private companion object {
+        const val TAG = "SlowSyncRecoveryHandler"
     }
 
 }
