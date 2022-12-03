274d19b8c084d0ac953f78a5cb254448b1775502
Vitor Hugo Schwaab <vitor@schwaab.xyz>
2022-12-03 21:05:37 +0100

    chore: add convention plugins to simplify module setup (#1194)

--- persistence-test/build.gradle.kts
+++ persistence-test/build.gradle.kts
@@ -2,46 +2,14 @@
 plugins {
     id(libs.plugins.android.library.get().pluginId)
     id(libs.plugins.kotlin.multiplatform.get().pluginId)
+    id(libs.plugins.kalium.library.get().pluginId)
 }
 
-group = "com.wire.kalium"
-version = "0.0.1-SNAPSHOT"
-
-android {
-    compileSdk = Android.Sdk.compile
-    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
-    defaultConfig {
-        minSdk = Android.Sdk.min
-        targetSdk = Android.Sdk.target
-        consumerProguardFiles("consumer-proguard-rules.pro")
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
+kaliumLibrary {
+    multiplatform()
 }
 
 kotlin {
-    jvm {
-        compilations.all {
-            kotlinOptions.jvmTarget = "1.8"
-            kotlinOptions.freeCompilerArgs += "-Xopt-in=kotlin.RequiresOptIn"
-        }
-        testRuns["test"].executionTask.configure {
-            useJUnit()
-        }
-    }
-    android()
-    iosX64()
-    js(IR) {
-        browser {
-            testTask {
-                useMocha {
-                    timeout = "5s"
-                }
-            }
-        }
-    }
 
     sourceSets {
         val commonMain by getting {
