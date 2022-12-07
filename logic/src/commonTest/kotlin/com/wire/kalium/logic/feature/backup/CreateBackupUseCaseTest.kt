a75ef4dbbe9c55e03c961d3fee7fd89811cbe298
Gonzalo Gran Crespo <gongracr@gmail.com>
2022-12-07 17:40:00 +0100

    Added comment

--- logic/src/commonTest/kotlin/com/wire/kalium/logic/feature/backup/CreateBackupUseCaseTest.kt
+++ logic/src/commonTest/kotlin/com/wire/kalium/logic/feature/backup/CreateBackupUseCaseTest.kt
@@ -3,6 +3,7 @@ package com.wire.kalium.logic.feature.backup
 import kotlin.test.Test
 
 class CreateBackupUseCaseTest {
+    // TODO: Implement tests for CreateBackupUseCase
 
     @Test
     fun `given some valid data, when creating a non-encrypted backup, then the final backup file is created correctly`() {
