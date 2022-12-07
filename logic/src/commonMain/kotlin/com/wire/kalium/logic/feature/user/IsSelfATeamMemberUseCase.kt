150ab3e592954305321d87f15c7c88812765b682
Mohamad Jaara <mohamad.jaara@wire.com>
2022-12-07 16:55:16 +0100

    fix styling

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/user/IsSelfATeamMemberUseCase.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/user/IsSelfATeamMemberUseCase.kt
@@ -3,6 +3,9 @@ package com.wire.kalium.logic.feature.user
 import com.wire.kalium.logic.feature.SelfTeamIdProvider
 import com.wire.kalium.logic.functional.fold
 
+/**
+ * Return if self user is part of a team or noz
+ */
 class IsSelfATeamMemberUseCase internal constructor(
     private val selfTeamIdProvider: SelfTeamIdProvider
 ) {
