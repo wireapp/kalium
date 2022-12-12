354079e0ab73ee2286d57cce9c1a9a805943771b
Jakub Żerko <zerasop@gmail.com>
2022-12-12 16:47:25 +0100

    detekt fix

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/data/message/MessageMapper.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/data/message/MessageMapper.kt
@@ -144,6 +144,7 @@ class MessageMapperImpl(
         }
     }
 
+    @Suppress("ComplexMethod")
     override fun fromMessageToLocalNotificationMessage(
         message: NotificationMessageEntity
     ): LocalNotificationMessage =
