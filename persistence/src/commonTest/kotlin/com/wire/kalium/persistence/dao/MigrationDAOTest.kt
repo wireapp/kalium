98192dd75d2e8852dd50070267bb25d1779c98f7
Mohamad Jaara <mohamad.jaara@wire.com>
2022-12-16 17:28:07 +0100

    add unit tests

---
+++ persistence/src/commonTest/kotlin/com/wire/kalium/persistence/dao/MigrationDAOTest.kt
@@ -0,0 +1,41 @@
+package com.wire.kalium.persistence.dao
+
+import com.wire.kalium.persistence.BaseDatabaseTest
+import com.wire.kalium.persistence.utils.stubs.newConversationEntity
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class MigrationDAOTest : BaseDatabaseTest() {
+    private lateinit var migrationDAO: MigrationDAO
+    private lateinit var conversationDAO: ConversationDAO
+
+    @BeforeTest
+    fun setUp() {
+        deleteDatabase()
+        val db = createDatabase()
+        migrationDAO = db.migrationDAO
+        conversationDAO = db.conversationDAO
+    }
+
+    @Test
+    fun givenConversationAlreadyStored_whenInsertingFromMigration_thenIgnore() = runTest {
+        val conversation = newConversationEntity(id = "conversation_id").copy(type = ConversationEntity.Type.GROUP, name = "conv name")
+        val conversationFromMigration = conversation.copy(type = ConversationEntity.Type.ONE_ON_ONE, name = "migration name")
+
+        conversationDAO.insertConversation(conversation)
+        conversationDAO.getConversationByQualifiedID(conversation.id).also {
+            assertEquals(conversation.type, it?.type)
+            assertEquals(conversation.name, it?.name)
+        }
+
+        migrationDAO.insertConversation(listOf(conversationFromMigration))
+        conversationDAO.getConversationByQualifiedID(conversationFromMigration.id).also {
+            assertEquals(conversation.type, it?.type)
+            assertEquals(conversation.name, it?.name)
+        }
+    }
+}
