9b47d9ae22c080acbf99afecc5e2e9be57f9a5a1
Mohamad Jaara <mohamad.jaara@wire.com>
2022-12-19 12:33:18 +0100

    fix unresolved references

--- persistence/src/jsMain/kotlin/com/wire/kalium/persistence/kmmSettings/EncryptedSettingsHolder.kt
+++ persistence/src/jsMain/kotlin/com/wire/kalium/persistence/kmmSettings/EncryptedSettingsHolder.kt
@@ -1,12 +1,12 @@
 package com.wire.kalium.persistence.kmmSettings
 
-import com.russhwolf.settings.JsSettings
 import com.russhwolf.settings.Settings
+import com.russhwolf.settings.StorageSettings
 import org.w3c.dom.Storage
 
 internal actual fun encryptedSettingsBuilder(
     options: SettingOptions,
     param: EncryptedSettingsPlatformParam
-): Settings = JsSettings(param.storage)
+): Settings = StorageSettings(param.storage)
 
 internal actual class EncryptedSettingsPlatformParam(val storage: Storage)
