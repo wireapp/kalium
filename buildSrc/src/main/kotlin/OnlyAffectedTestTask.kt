d1370040a371dab07489796cd7d734e61c9ef9c8
yamilmedina <yamilmedina@users.noreply.github.com>
2022-12-21 16:27:25 +0100

    chore: fix cli command

--- buildSrc/src/main/kotlin/OnlyAffectedTestTask.kt
+++ buildSrc/src/main/kotlin/OnlyAffectedTestTask.kt
@@ -83,7 +83,7 @@ open class OnlyAffectedTestTask : DefaultTask() {
      * Check if we have to run all tests, by looking at changes on libs versions or buildSrc
      */
     private fun hasToRunAllTests(): Boolean {
-        val isVersionsFileChanged = "git diff --quiet ${project.rootDir}/gradle/libs.versions.toml ; echo $?".runCommandWithExitCode()
+        val isVersionsFileChanged = "bash -c 'git diff --quiet ${project.rootDir}/gradle/libs.versions.toml ; echo $?'".runCommandWithExitCode()
         return (isVersionsFileChanged != 0).also {
             if (it) {
                 println("\uD83D\uDD27 Running all tests because there are changes at the root level")
