9b38bb6e70c2672f2bc712c28ebd40602e6a3f19
yamilmedina <yamilmedina@users.noreply.github.com>
2022-12-21 19:39:28 +0100

    chore: more code documentation

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/auth/sso/ValidateSSOCodeUseCase.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/auth/sso/ValidateSSOCodeUseCase.kt
@@ -3,7 +3,14 @@ package com.wire.kalium.logic.feature.auth.sso
 import com.benasher44.uuid.uuidFrom
 import com.wire.kalium.logic.feature.auth.sso.ValidateSSOCodeUseCase.Companion.SSO_CODE_WIRE_PREFIX
 
+/**
+ * Validates a SSO code
+ */
 interface ValidateSSOCodeUseCase {
+    /**
+     * @param ssoCode the SSO code to validate
+     * @return the [ValidateSSOCodeResult] with the UUID if successful
+     */
     operator fun invoke(ssoCode: String): ValidateSSOCodeResult
 
     companion object {
@@ -11,7 +18,7 @@ interface ValidateSSOCodeUseCase {
     }
 }
 
-class ValidateSSOCodeUseCaseImpl() : ValidateSSOCodeUseCase {
+internal class ValidateSSOCodeUseCaseImpl : ValidateSSOCodeUseCase {
     override fun invoke(ssoCode: String): ValidateSSOCodeResult =
         if(!ssoCode.startsWith(SSO_CODE_WIRE_PREFIX)) ValidateSSOCodeResult.Invalid
         else ssoCode.removePrefix(SSO_CODE_WIRE_PREFIX).let { uuid ->
