237e0eae5e63a82292b1224e6a3cec373e885412
Michał Saleniuk <saleniuk@gmail.com>
2022-12-21 16:56:33 +0100

    fix tests

--- util/src/iosX64Main/kotlin/com.wire.kalium.util/PlatformDateTimeUtil.kt
+++ util/src/iosX64Main/kotlin/com.wire.kalium.util/PlatformDateTimeUtil.kt
@@ -13,5 +13,5 @@ actual open class PlatformDateTimeUtil actual constructor() {
      * @return date in ISO-8601 format (YYYY-MM-DDTHH:mm:ss.SSSZ)
      */
     actual fun fromInstantToIsoDateTimeString(instant: Instant): String =
-        TODO("Implement own iOS method")
+        instant.toString() // TODO:"Implement own iOS method"
 }
