ce63ab82297bd673c1271c6ccde2d8f14a74fe8e
yamilmedina <yamilmedina@users.noreply.github.com>
2022-12-22 12:27:10 +0100

    chore: more code documentation

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/user/UpdateSelfAvailabilityStatusUseCase.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/user/UpdateSelfAvailabilityStatusUseCase.kt
@@ -3,12 +3,19 @@ package com.wire.kalium.logic.feature.user
 import com.wire.kalium.logic.data.user.UserAvailabilityStatus
 import com.wire.kalium.logic.data.user.UserRepository
 
+/**
+ * Updates the current user's [UserAvailabilityStatus] status.
+ * @see [UserAvailabilityStatus]
+ */
 class UpdateSelfAvailabilityStatusUseCase internal constructor(
     private val userRepository: UserRepository,
 ) {
+    /**
+     * @param status the new [UserAvailabilityStatus] status.
+     */
     suspend operator fun invoke(status: UserAvailabilityStatus) {
         // TODO: Handle possibility of being offline. Storing the broadcast to be sent when Sync is done.
-        //       For now, we don't need Sync, as we do not broadcast the availability to other devices or users.
+        // For now, we don't need Sync, as we do not broadcast the availability to other devices or users.
         userRepository.updateSelfUserAvailabilityStatus(status)
     }
 }
