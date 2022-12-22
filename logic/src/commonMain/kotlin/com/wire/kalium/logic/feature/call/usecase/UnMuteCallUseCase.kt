1b5ec0f4ddecd3a96b3dd068406c781b03c37482
yamilmedina <yamilmedina@users.noreply.github.com>
2022-12-22 18:10:21 +0100

    chore: more code documentation

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/call/usecase/UnMuteCallUseCase.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/call/usecase/UnMuteCallUseCase.kt
@@ -4,17 +4,23 @@ import com.wire.kalium.logic.data.call.CallRepository
 import com.wire.kalium.logic.data.id.ConversationId
 import com.wire.kalium.logic.feature.call.CallManager
 
+/**
+ * This use case is responsible for un-mute a call.
+ */
 class UnMuteCallUseCase(
     private val callManager: Lazy<CallManager>,
     private val callRepository: CallRepository
 ) {
-
+    /**
+     * We should call AVS muting method only for established call, otherwise incoming call could mute/un-mute the current call
+     * @param conversationId the id of the conversation.
+     */
     suspend operator fun invoke(conversationId: ConversationId) {
         callRepository.updateIsMutedById(
             conversationId = conversationId.toString(),
             isMuted = false
         )
-        // We should call AVS muting method only for established call, otherwise incoming call could mute/un-mute the current call
+        // We should call AVS muting method only for established call, otherwise incoming call could mute the current call
         callRepository.getCallMetadataProfile()[conversationId.toString()]?.establishedTime?.let {
             callManager.value.muteCall(false)
         }
