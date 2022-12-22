ed0f34c752077d6a6832a3f2ddfb2743947ca4fd
yamilmedina <yamilmedina@users.noreply.github.com>
2022-12-22 23:59:50 +0100

    chore: more code documentation

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/call/usecase/AnswerCallUseCase.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/call/usecase/AnswerCallUseCase.kt
@@ -3,6 +3,9 @@ package com.wire.kalium.logic.feature.call.usecase
 import com.wire.kalium.logic.data.id.ConversationId
 import com.wire.kalium.logic.feature.call.CallManager
 
+/**
+ * This use case is responsible for answering a call.
+ */
 interface AnswerCallUseCase {
     suspend operator fun invoke(conversationId: ConversationId)
 }
@@ -11,6 +14,9 @@ internal class AnswerCallUseCaseImpl(
     private val callManager: Lazy<CallManager>
 ) : AnswerCallUseCase {
 
+    /**
+     * @param conversationId the id of the conversation.
+     */
     override suspend fun invoke(conversationId: ConversationId) {
         callManager.value.answerCall(
             conversationId = conversationId
