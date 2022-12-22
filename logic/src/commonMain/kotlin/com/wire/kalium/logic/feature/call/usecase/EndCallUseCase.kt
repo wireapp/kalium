3fc5ee05bb767863728b8986cd5373388fe0cf83
yamilmedina <yamilmedina@users.noreply.github.com>
2022-12-23 00:18:42 +0100

    chore: more code documentation

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/call/usecase/EndCallUseCase.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/call/usecase/EndCallUseCase.kt
@@ -7,12 +7,18 @@ import com.wire.kalium.util.KaliumDispatcher
 import com.wire.kalium.util.KaliumDispatcherImpl
 import kotlinx.coroutines.withContext
 
+/**
+ * This use case is responsible for ending a call.
+ */
 class EndCallUseCase(
     private val callManager: Lazy<CallManager>,
     private val callRepository: CallRepository,
     private val dispatchers: KaliumDispatcher = KaliumDispatcherImpl
 ) {
 
+    /**
+     * @param conversationId the id of the conversation for the call should be ended.
+     */
     suspend operator fun invoke(conversationId: ConversationId) = withContext(dispatchers.io) {
         callManager.value.endCall(conversationId)
         callRepository.updateIsCameraOnById(conversationId.toString(), false)
