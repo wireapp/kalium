a55e9c19756e5ed7d8379132e3eba44d10ae492f
yamilmedina <yamilmedina@users.noreply.github.com>
2022-12-23 12:04:03 +0100

    chore: more code documentation

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/client/ObserveCurrentClientIdUseCase.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/client/ObserveCurrentClientIdUseCase.kt
@@ -4,6 +4,9 @@ import com.wire.kalium.logic.data.client.ClientRepository
 import com.wire.kalium.logic.data.conversation.ClientId
 import kotlinx.coroutines.flow.Flow
 
+/**
+ * This use case will observe and return the current client id of the current user.
+ */
 interface ObserveCurrentClientIdUseCase {
     suspend operator fun invoke(): Flow<ClientId?>
 }
