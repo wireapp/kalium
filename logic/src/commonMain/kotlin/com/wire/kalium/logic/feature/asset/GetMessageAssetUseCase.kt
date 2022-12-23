1dffab08f780d93c0efaa1dd338aab74af411fc2
Mateusz <m.pachulski94@gmail.com>
2022-12-23 13:02:08 +0100

    fix the styling

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/asset/GetMessageAssetUseCase.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/asset/GetMessageAssetUseCase.kt
@@ -70,8 +70,11 @@ internal class GetMessageAssetUseCaseImpl(
                     }
                 }
                 // This should never happen
-                else -> return@fold CompletableDeferred(MessageAssetResult.Failure(
-                    CoreFailure.Unknown(IllegalStateException("The message associated to this id, was not an asset message"))))
+                else -> return@fold CompletableDeferred(
+                    MessageAssetResult.Failure(
+                        CoreFailure.Unknown(IllegalStateException("The message associated to this id, was not an asset message"))
+                    )
+                )
             }
 
             // Start progress bar for generic assets
@@ -103,6 +106,6 @@ internal class GetMessageAssetUseCaseImpl(
 }
 
 sealed class MessageAssetResult {
-    class Success(val decodedAssetPath: Path, val assetSize: Long, val assetName :String) : MessageAssetResult()
+    class Success(val decodedAssetPath: Path, val assetSize: Long, val assetName: String) : MessageAssetResult()
     class Failure(val coreFailure: CoreFailure) : MessageAssetResult()
 }
