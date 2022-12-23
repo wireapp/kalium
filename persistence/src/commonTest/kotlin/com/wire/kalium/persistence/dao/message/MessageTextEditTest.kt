9940d10cc7b035314bf5f3de7a67e5f12a9927f3
Vitor Hugo Schwaab <vitor@schwaab.xyz>
2022-12-23 17:14:51 +0100

    fix: update receipts when editing a text message

--- persistence/src/commonTest/kotlin/com/wire/kalium/persistence/dao/message/MessageTextEditTest.kt
+++ persistence/src/commonTest/kotlin/com/wire/kalium/persistence/dao/message/MessageTextEditTest.kt
@@ -1,9 +1,11 @@
 package com.wire.kalium.persistence.dao.message
 
 import app.cash.turbine.test
+import com.wire.kalium.persistence.dao.receipt.ReceiptTypeEntity
 import com.wire.kalium.persistence.utils.stubs.newRegularMessageEntity
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.test.runTest
+import kotlinx.datetime.Clock
 import kotlin.test.Test
 import kotlin.test.assertContentEquals
 import kotlin.test.assertEquals
@@ -31,10 +33,33 @@ class MessageTextEditTest : BaseMessageTest() {
     }
 
     @Test
-    fun givenTextWasInserted_whenUpdatingMessageBody_thenContentShouldHaveNewMessageBody() = runTest {
+    fun givenTextWasInsertedAndReacted_whenUpdatingMessageBody_thenContentShouldHaveNewMessageBody() = runTest {
         insertInitialData()
         reactionDAO.insertReaction(ORIGINAL_MESSAGE_ID, CONVERSATION_ID, SELF_USER_ID, "Date", "💁‍♂️")
         reactionDAO.insertReaction(ORIGINAL_MESSAGE_ID, CONVERSATION_ID, OTHER_USER_2.id, "Date", "🤌️")
+
+        val newMessageBody = "newBody"
+
+        messageDAO.updateTextMessageContent(
+            editTimeStamp = "newDate",
+            conversationId = CONVERSATION_ID,
+            currentMessageId = ORIGINAL_MESSAGE_ID,
+            newTextContent = ORIGINAL_CONTENT.copy(messageBody = newMessageBody),
+            newMessageId = NEW_MESSAGE_ID
+        )
+
+        val result = messageDAO.getMessageById(NEW_MESSAGE_ID, CONVERSATION_ID).first()!!
+
+        val content = result.content
+        assertIs<MessageEntityContent.Text>(content)
+
+        assertEquals(newMessageBody, content.messageBody)
+    }
+
+    @Test
+    fun givenTextWasInsertedAndWithReceipts_whenUpdatingMessageBody_thenContentShouldHaveNewMessageBody() = runTest {
+        insertInitialData()
+        receiptDAO.insertReceipts(OTHER_USER.id, CONVERSATION_ID, Clock.System.now(), ReceiptTypeEntity.READ, listOf(ORIGINAL_MESSAGE_ID))
 
         val newMessageBody = "newBody"
 
@@ -93,6 +118,29 @@ class MessageTextEditTest : BaseMessageTest() {
         reactionDAO.observeMessageReactions(CONVERSATION_ID, NEW_MESSAGE_ID).test {
             val reactions = awaitItem()
             assertTrue(reactions.isEmpty())
+        }
+    }
+
+    @Test
+    fun givenTextWasInsertedAndReceiptsAttached_whenUpdatingContent_thenReceiptsRemainAfterBeingEdited() = runTest {
+        insertInitialData()
+        val instant = Clock.System.now()
+        receiptDAO.insertReceipts(OTHER_USER.id, CONVERSATION_ID, instant, ReceiptTypeEntity.READ, listOf(ORIGINAL_MESSAGE_ID))
+
+        messageDAO.updateTextMessageContent(
+            editTimeStamp = "newDate",
+            conversationId = CONVERSATION_ID,
+            currentMessageId = ORIGINAL_MESSAGE_ID,
+            newTextContent = ORIGINAL_CONTENT.copy(messageBody = "Howdy"),
+            newMessageId = NEW_MESSAGE_ID
+        )
+
+        receiptDAO.observeDetailedReceiptsForMessage(CONVERSATION_ID, NEW_MESSAGE_ID, ReceiptTypeEntity.READ).test {
+            val receipts = awaitItem()
+            assertEquals(1, receipts.size)
+
+            assertEquals(OTHER_USER.id, receipts.first().userId)
+            assertEquals(instant, receipts.first().date)
         }
     }
 
