c6260c9f4b50655973b50e570e58781a577f6d79
Mohamad Jaara <mohamad.jaara@wire.com>
2023-01-02 17:53:08 +0100

    fix: app stuck at loading screen after migration (#1292)
    * clean the websocket url before inserting
    
    * fix missing brackets
    
    * add unit tests
    
    * fix styling
    
    * remove await only if it is at the end of the path segments

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/server/StoreServerConfigUseCase.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/server/StoreServerConfigUseCase.kt
@@ -4,6 +4,7 @@ import com.wire.kalium.logic.CoreFailure
 import com.wire.kalium.logic.configuration.server.ServerConfig
 import com.wire.kalium.logic.configuration.server.ServerConfigRepository
 import com.wire.kalium.logic.functional.fold
+import io.ktor.http.URLBuilder
 
 /**
  * Stores the server configuration metadata, like main urls and flags for this server.
@@ -21,9 +22,17 @@ internal class StoreServerConfigUseCaseImpl(
     private val configRepository: ServerConfigRepository
 ) : StoreServerConfigUseCase {
 
-    override suspend fun invoke(links: ServerConfig.Links, versionInfo: ServerConfig.VersionInfo): StoreServerConfigResult =
-        configRepository.storeConfig(links, versionInfo)
+    override suspend fun invoke(links: ServerConfig.Links, versionInfo: ServerConfig.VersionInfo): StoreServerConfigResult {
+        val cleanWsLink = URLBuilder(links.webSocket).apply {
+            pathSegments = pathSegments.toMutableList().apply {
+                if (lastOrNull() == "await") removeLast()
+            }
+        }.buildString()
+
+        val cleanLinks = links.copy(webSocket = cleanWsLink)
+        return configRepository.storeConfig(cleanLinks, versionInfo)
             .fold({ StoreServerConfigResult.Failure.Generic(it) }, { StoreServerConfigResult.Success(it) })
+    }
 }
 
 sealed class StoreServerConfigResult {
