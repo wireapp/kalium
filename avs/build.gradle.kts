19c8e5919502d2608b33d4fb07e45e7dded34d48
yamilmedina <yamilmedina@users.noreply.github.com>
2023-01-10 14:07:36 -0300

    feat: fix dependency add local tasks

--- avs/build.gradle.kts
+++ avs/build.gradle.kts
@@ -1,2 +1,24 @@
-configurations.maybeCreate("default")
-artifacts.add("default", file("avs-9-0-2.aar"))
+// DISCLAIMER: This file should be removed when the avs dependency is on the proper artifactory.
+plugins {
+    id("maven-publish")
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    maven(file("libs"))
+}
+
+project.afterEvaluate {
+    publishing {
+        publications {
+            group = "com.wire"
+            version = "9.0.2-rc1"
+
+            create<MavenPublication>("maven") {
+                artifactId = "avs"
+                artifact(file("avs-9-0-2.aar"))
+            }
+        }
+    }
+}
