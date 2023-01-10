79ad8c82a5e9fe3f7354a372cb296fc19a451844
Jacob Persson <7156+typfel@users.noreply.github.com>
2023-01-10 17:12:47 +0100

    feat: implement provideEnvelopeProtoMapper for darwin

---
+++ network/src/darwinMain/kotlin/com/wire/kalium/network/api/base/authenticated/message/OtrUserIdMapper.kt
@@ -0,0 +1,32 @@
+package com.wire.kalium.network.api.base.authenticated.message
+
+import com.wire.kalium.protobuf.otr.UserId
+import kotlinx.cinterop.addressOf
+import kotlinx.cinterop.reinterpret
+import kotlinx.cinterop.usePinned
+import pbandk.ByteArr
+import platform.Foundation.NSUUID
+
+class OtrUserIdMapper {
+
+    fun toOtrUserId(userId: String): UserId {
+        val uuid = NSUUID(userId)
+
+        val nativeBytes = ByteArray(USER_UID_BYTE_COUNT)
+        nativeBytes.usePinned {
+            uuid.getUUIDBytes(it.addressOf(0).reinterpret())
+        }
+        return UserId(uuid = (ByteArr(nativeBytes)))
+    }
+
+    fun fromOtrUserId(otrUserId: UserId): String {
+        val uuid = otrUserId.uuid.array.usePinned {
+            NSUUID(it.addressOf(0).reinterpret())
+        }
+        return uuid.UUIDString.lowercase()
+    }
+
+    companion object {
+        private const val USER_UID_BYTE_COUNT = 16
+    }
+}
