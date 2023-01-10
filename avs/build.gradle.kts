af26b118bc2c353f38789fc8bf3afa681f684331
yamilmedina <yamilmedina@users.noreply.github.com>
2023-01-10 14:26:04 -0300

    chore: fix dependency add local tasks

--- avs/build.gradle.kts
+++ avs/build.gradle.kts
@@ -1,23 +1,9 @@
-// DISCLAIMER: This file should be removed when the avs dependency is on the proper artifactory.
-plugins {
-    id("maven-publish")
-}
-
-repositories {
-    mavenLocal()
-    mavenCentral()
-    maven(file("libs"))
-}
-
-project.afterEvaluate {
-    publishing {
-        publications {
-            group = "com.wire"
-            version = "9.0.2-rc1"
-
-            create<MavenPublication>("maven") {
-                artifactId = "avs"
-                artifact(file("avs-9-0-2.aar"))
+// DISCLAIMER: Delete this whole "avs" directory when the publication is available at the proper artifactory.
+rootProject.gradle.afterProject {
+    allprojects {
+        repositories {
+            maven {
+                url = uri("$rootDir/avs/localrepo/")
             }
         }
     }
