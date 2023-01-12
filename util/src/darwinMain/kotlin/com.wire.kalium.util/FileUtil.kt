2110e9a054e1d7f68a82078c551576cd9d1a01ad
Jacob Persson <7156+typfel@users.noreply.github.com>
2023-01-12 22:07:23 +0100

    feat: add missing darwin implementation for FileUtil

--- util/src/darwinMain/kotlin/com.wire.kalium.util/FileUtil.kt
+++ util/src/darwinMain/kotlin/com.wire.kalium.util/FileUtil.kt
@@ -1,7 +1,9 @@
 package com.wire.kalium.util
 
+import platform.Foundation.NSFileManager
+
 actual object FileUtil {
-    actual fun mkDirs(path: String): Boolean {
-        TODO("Not yet implemented")
-    }
+    actual fun mkDirs(path: String): Boolean =
+        // TODO setup a logger for util and log on error?
+        NSFileManager.defaultManager.createDirectoryAtPath(path, false, null, null)
 }
