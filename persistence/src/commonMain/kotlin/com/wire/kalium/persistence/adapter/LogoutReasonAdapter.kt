9da6c6bf52a39dfde0ee2735aa6820f2cf63c8a8
Vitor Hugo Schwaab <vitor@schwaab.xyz>
2023-01-13 20:51:39 +0100

    refactor: move column adapters to a dedicated package (#1335)

--- persistence/src/commonMain/kotlin/com/wire/kalium/persistence/adapter/LogoutReasonAdapter.kt
+++ persistence/src/commonMain/kotlin/com/wire/kalium/persistence/adapter/LogoutReasonAdapter.kt
@@ -1,9 +1,9 @@
-package com.wire.kalium.persistence.daokaliumdb
+package com.wire.kalium.persistence.adapter
 
 import app.cash.sqldelight.ColumnAdapter
 import com.wire.kalium.persistence.model.LogoutReason
 
-object LogoutReasonAdapter : ColumnAdapter<LogoutReason, String> {
+internal object LogoutReasonAdapter : ColumnAdapter<LogoutReason, String> {
     override fun decode(databaseValue: String): LogoutReason = LogoutReason.valueOf(databaseValue)
 
     override fun encode(value: LogoutReason): String = value.name
