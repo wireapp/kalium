1264d0e9bb39ca75722b0494d53df6e4d466a485
Jacob Persson <7156+typfel@users.noreply.github.com>
2023-01-19 12:32:50 +0100

    chore: delete actual fun which moved to common

--- cryptography/src/jsMain/kotlin/com/wire/kalium/cryptography/utils/CryptoUtils.kt
+++ cryptography/src/jsMain/kotlin/com/wire/kalium/cryptography/utils/CryptoUtils.kt
@@ -3,22 +3,6 @@ package com.wire.kalium.cryptography.utils
 import okio.Sink
 import okio.Source
 
-actual fun calcMd5(bytes: ByteArray): String {
-    TODO("Not yet implemented")
-}
-
-actual fun calcSHA256(bytes: ByteArray): ByteArray {
-    TODO("Not yet implemented")
-}
-
-actual fun calcFileMd5(dataSource: Source): String? {
-    TODO("Not yet implemented")
-}
-
-actual fun calcFileSHA256(dataSource: Source): ByteArray? {
-    TODO("Not yet implemented")
-}
-
 actual fun encryptDataWithAES256(data: PlainData, key: AES256Key): EncryptedData {
     TODO("Not yet implemented")
 }
