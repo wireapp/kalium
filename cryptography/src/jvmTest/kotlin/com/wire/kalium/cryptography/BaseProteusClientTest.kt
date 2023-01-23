5e090a2eaa739bb8b528db4ce4f37d3392c9e38a
Mohamad Jaara <mohamad.jaara@wire.com>
2023-01-23 18:10:46 +0100

    use TestCoroutineScheduler

--- cryptography/src/jvmTest/kotlin/com/wire/kalium/cryptography/BaseProteusClientTest.kt
+++ cryptography/src/jvmTest/kotlin/com/wire/kalium/cryptography/BaseProteusClientTest.kt
@@ -1,8 +1,10 @@
 package com.wire.kalium.cryptography
 
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.StandardTestDispatcher
 import java.nio.file.Files
 
+@OptIn(ExperimentalCoroutinesApi::class)
 actual open class BaseProteusClientTest {
 
     private val standardScope = StandardTestDispatcher()
@@ -16,5 +18,4 @@ actual open class BaseProteusClientTest {
     actual fun createProteusClient(proteusStore: ProteusStoreRef, databaseKey: ProteusDBSecret?): ProteusClient {
         return ProteusClientImpl(proteusStore.value, databaseKey, standardScope, standardScope)
     }
-
 }
