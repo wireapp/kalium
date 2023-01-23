c212713e80c2671d6fb5423a49845f872681a17d
Oussama <oussama.has100@gmail.com>
2023-01-23 10:47:34 +0100

    chore: missing params for jvm sample

--- samples/src/jvmMain/kotlin/samples/cryptography/ProteusClientSamples.kt
+++ samples/src/jvmMain/kotlin/samples/cryptography/ProteusClientSamples.kt
@@ -1,7 +1,9 @@
 package samples.cryptography
 
 import com.wire.kalium.cryptography.ProteusClientImpl
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 
 fun jvmInitialization() {
-    val proteusClient = ProteusClientImpl("rootDirectory", null)
+    val proteusClient = ProteusClientImpl("rootDirectory", null, Dispatchers.IO, Dispatchers.Default)
 }
