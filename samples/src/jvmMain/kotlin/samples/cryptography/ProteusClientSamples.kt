fd191a0d2bcb73eb00d03fa9477e856586bf1918
Oussama <oussama.has100@gmail.com>
2023-01-23 11:01:46 +0100

    chore: remove unused imports

--- samples/src/jvmMain/kotlin/samples/cryptography/ProteusClientSamples.kt
+++ samples/src/jvmMain/kotlin/samples/cryptography/ProteusClientSamples.kt
@@ -1,7 +1,6 @@
 package samples.cryptography
 
 import com.wire.kalium.cryptography.ProteusClientImpl
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 
 fun jvmInitialization() {
