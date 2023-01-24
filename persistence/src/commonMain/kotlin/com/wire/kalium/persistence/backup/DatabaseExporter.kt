7684566dba272344491d4667cf30ab7b9c659f42
Mohamad Jaara <mohamad.jaara@wire.com>
2023-01-24 18:59:57 +0100

    dump WAL content to main DB before migration

---
+++ persistence/src/commonMain/kotlin/com/wire/kalium/persistence/backup/DatabaseExporter.kt
@@ -0,0 +1,19 @@
+package com.wire.kalium.persistence.backup
+
+import app.cash.sqldelight.db.SqlDriver
+
+class DatabaseExporter internal constructor(private val sqlDriver: SqlDriver) {
+
+    /*
+    https://www.sqlite.org/c3ref/c_checkpoint_full.html
+
+    #define SQLITE_CHECKPOINT_PASSIVE  0  /* Do as much as possible w/o blocking */
+    #define SQLITE_CHECKPOINT_FULL     1  /* Wait for writers, then checkpoint */
+    #define SQLITE_CHECKPOINT_RESTART  2  /* Like FULL but wait for readers */
+    #define SQLITE_CHECKPOINT_TRUNCATE 3  /* Like RESTART but also truncate WAL */
+     */
+    fun beforeBackup() {
+        sqlDriver.executeQuery(null, "PRAGMA wal_checkpoint(3)", {}, 0)
+    }
+
+}
