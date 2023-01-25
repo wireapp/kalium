42015165c85f80294a83b3db6bc803f146443f08
Jacob Persson <7156+typfel@users.noreply.github.com>
2023-01-25 22:26:31 +0100

    feat: implement crypto utils for Darwin (#1361)
    * refactor: move sha256 and md5 calculation to common
    
    * feat: implement AES256 encryption for the darwin target
    
    * fix: reading binary test resources for the darwin targets
    
    * chore: enable darwin for CryptoUtilsTests
    
    * chore: delete actual fun which moved to common
    
    * fix: Duplicate JVM class name compile error
    
    * chore: re-enable previously disabled tests which relied on CryptoUtils
    
    * fix: toUTF16BEByteArray for darwin not being big endian
    
    * chore: enable for darwin tests
    
    * refactor: introduce CryptohraphyException
    
    * chore: fix detekt
    
    * chore: fix detekt
    
    * refactor: move gradle copy test resources setup into a shared extension
    
    * refactor: also forward the rootCause exception
    
    * fix: make sure buffers are closed by using use {}
    
    * fix: make sure temporary buffers are also closed after use
    
    * chore: fix detekt
    
    * chore: revert including iv size in return value

---
+++ cryptography/src/commonMain/kotlin/com/wire/kalium/cryptography/exceptions/CryptographyException.kt
@@ -0,0 +1,3 @@
+package com.wire.kalium.cryptography.exceptions
+
+class CryptographyException(override val message: String, val rootCause: Throwable? = null) : Exception(message, rootCause)
--- util/src/darwinMain/kotlin/com.wire.kalium.util/string/toUTF16BEByteArray.kt
+++ util/src/darwinMain/kotlin/com.wire.kalium.util/string/toUTF16BEByteArray.kt
@@ -18,18 +18,19 @@
 
 package com.wire.kalium.util.string
 
+import kotlinx.cinterop.addressOf
 import kotlinx.cinterop.allocArrayOf
-import kotlinx.cinterop.getBytes
 import kotlinx.cinterop.memScoped
-import kotlinx.cinterop.utf16
+import kotlinx.cinterop.usePinned
 import platform.Foundation.NSData
 import platform.Foundation.NSString
 import platform.Foundation.NSUTF16BigEndianStringEncoding
 import platform.Foundation.create
+import platform.Foundation.dataUsingEncoding
+import platform.Foundation.getBytes
 
-actual fun String.toUTF16BEByteArray(): ByteArray {
-    return utf16.getBytes()
-}
+actual fun String.toUTF16BEByteArray(): ByteArray =
+    (this as NSString).dataUsingEncoding(NSUTF16BigEndianStringEncoding)!!.toByteArray()
 
 actual fun ByteArray.toStringFromUtf16BE(): String = memScoped {
     val data = NSData.create(
@@ -38,4 +39,12 @@ actual fun ByteArray.toStringFromUtf16BE(): String = memScoped {
     )
     val string = NSString.create(data = data, encoding = NSUTF16BigEndianStringEncoding)
     return@memScoped string as String
+}
+
+fun NSData.toByteArray(): ByteArray {
+    val buffer = ByteArray(length.toInt())
+    buffer.usePinned {
+        getBytes(it.addressOf(0))
+    }
+    return buffer
 }
