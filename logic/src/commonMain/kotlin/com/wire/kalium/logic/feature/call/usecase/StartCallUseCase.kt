11927713b80d74dc1f6f75fcf8fb3e94b156a559
Jacob Persson <7156+typfel@users.noreply.github.com>
2023-01-26 14:55:26 +0100

    feat(calling): join subconversation (#1400)
    * feat: add network API for subconversations
    
    * feat: add use case for joining subconversation
    
    * feat: support starting a MLS conference call (incomplete)
    
    * test: add network tests for subconversations
    
    * test: add JoinSubconversationUseCase tests
    
    * chore: fix detekt
    
    * chore: document use case
    
    * refactor: move duration calculation into extension
    
    * refactor: introduce a SubconversationId type for better type safety
    
    * chore: remove redundant comment
    
    * chore: fix detekt
    
    Co-authored-by: Mojtaba Chenani <chenani@outlook.com>

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/call/usecase/StartCallUseCase.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/call/usecase/StartCallUseCase.kt
@@ -20,9 +20,11 @@ package com.wire.kalium.logic.feature.call.usecase
 
 import com.wire.kalium.logic.data.call.CallType
 import com.wire.kalium.logic.data.call.ConversationType
+import com.wire.kalium.logic.data.call.CALL_SUBCONVERSATION_ID
 import com.wire.kalium.logic.data.id.ConversationId
 import com.wire.kalium.logic.feature.call.CallManager
 import com.wire.kalium.logic.feature.call.usecase.StartCallUseCase.Result
+import com.wire.kalium.logic.feature.conversation.JoinSubconversationUseCase
 import com.wire.kalium.logic.functional.fold
 import com.wire.kalium.logic.sync.SyncManager
 import kotlin.Boolean
@@ -35,7 +37,8 @@ import kotlin.Lazy
  */
 class StartCallUseCase internal constructor(
     private val callManager: Lazy<CallManager>,
-    private val syncManager: SyncManager
+    private val syncManager: SyncManager,
+    private val joinSubconversationUseCase: JoinSubconversationUseCase
 ) {
 
     suspend operator fun invoke(
@@ -52,7 +55,20 @@ class StartCallUseCase internal constructor(
             conversationType = conversationType,
             isAudioCbr = isAudioCbr
         )
-        Result.Success
+
+        if (conversationType == ConversationType.ConferenceMls) {
+            // TODO update AVS on current epoch after joining the sub conversation
+            joinSubconversationUseCase(conversationId, CALL_SUBCONVERSATION_ID).fold(
+                {
+                    Result.SyncFailure
+                },
+                {
+                    Result.Success
+                }
+            )
+        } else {
+            Result.Success
+        }
     })
 
     sealed interface Result {
