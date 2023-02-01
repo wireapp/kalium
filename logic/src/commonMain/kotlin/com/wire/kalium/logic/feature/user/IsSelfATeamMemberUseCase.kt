874af6b5e1cbb0864c3ebec67b6aee764e1c7b33
Gonzalo Gran Crespo <gongracr@gmail.com>
2023-02-01 16:53:34 +0100

    Added docs

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/user/IsSelfATeamMemberUseCase.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/user/IsSelfATeamMemberUseCase.kt
@@ -21,16 +21,17 @@ package com.wire.kalium.logic.feature.user
 import com.wire.kalium.logic.feature.SelfTeamIdProvider
 import com.wire.kalium.logic.functional.fold
 
+/**
+ * Checks if the self user is a team member or not.
+ * @return true if the self user is a team member, false otherwise.
+ */
 fun interface IsSelfATeamMemberUseCase {
     suspend operator fun invoke(): Boolean
 }
 
-/**
- * Return if self user is part of a team or not
- */
 internal class IsSelfATeamMemberUseCaseImpl internal constructor(
     private val selfTeamIdProvider: SelfTeamIdProvider
-): IsSelfATeamMemberUseCase {
+) : IsSelfATeamMemberUseCase {
     override suspend operator fun invoke(): Boolean = selfTeamIdProvider().fold({ false }, {
         it != null
     })
