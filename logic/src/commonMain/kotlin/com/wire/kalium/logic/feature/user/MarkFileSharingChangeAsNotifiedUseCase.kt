8d98bdcd676dcc1096ccff5b253ff7ccb488c417
Mohamad Jaara <mohamad.jaara@wire.com>
2023-02-08 16:13:50 +0100

    add docs

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/user/MarkFileSharingChangeAsNotifiedUseCase.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/user/MarkFileSharingChangeAsNotifiedUseCase.kt
@@ -2,10 +2,15 @@ package com.wire.kalium.logic.feature.user
 
 import com.wire.kalium.logic.configuration.UserConfigRepository
 
+/**
+ * Mark the file sharing status as notified
+ * need to be called after notifying the user about the change
+ * e.g. after showing a dialog, or a toast etc.
+ */
 class MarkFileSharingChangeAsNotifiedUseCase(
     private val userConfigRepository: UserConfigRepository
 ) {
-    operator suspend fun invoke() {
+    suspend operator fun invoke() {
         userConfigRepository.setFileSharingAsNotified()
     }
 }
