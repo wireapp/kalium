f47af599fbc50ef4483380f38efd11ab50df51e5
yamilmedina <yamilmedina@users.noreply.github.com>
2023-02-10 12:02:16 -0300

    chore: add dokka exec, should remove later

--- scripts/release.mk
+++ scripts/release.mk
@@ -1,4 +1,4 @@
 # release related targets, ie: docs, changelog, build aar, etc.
 # dokka
 doc/generate-kdocs:
-	./gradlew --no-daemon --no-parallel dokkaHtmlMultiModule
+	./gradlew --no-daemon --no-parallel --stacktrace dokkaHtmlMultiModule
