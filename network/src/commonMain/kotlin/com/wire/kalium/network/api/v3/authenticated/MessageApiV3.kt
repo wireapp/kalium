05b6e6651f9843e65943857733d583c44deab614
yamilmedina <yamilmedina@users.noreply.github.com>
2023-02-12 16:07:42 -0300

    feat: move test to correct api version wip, moving modeñ

--- network/src/commonMain/kotlin/com/wire/kalium/network/api/v3/authenticated/MessageApiV3.kt
+++ network/src/commonMain/kotlin/com/wire/kalium/network/api/v3/authenticated/MessageApiV3.kt
@@ -20,9 +20,39 @@ package com.wire.kalium.network.api.v3.authenticated
 
 import com.wire.kalium.network.AuthenticatedNetworkClient
 import com.wire.kalium.network.api.base.authenticated.message.EnvelopeProtoMapper
+import com.wire.kalium.network.api.base.authenticated.message.MessageApi
+import com.wire.kalium.network.api.base.authenticated.message.QualifiedSendMessageResponse
+import com.wire.kalium.network.api.base.model.ConversationId
 import com.wire.kalium.network.api.v2.authenticated.MessageApiV2
+import com.wire.kalium.network.exceptions.ProteusClientsChangedError
+import com.wire.kalium.network.serialization.XProtoBuf
+import com.wire.kalium.network.utils.NetworkResponse
+import com.wire.kalium.network.utils.wrapKaliumResponse
+import io.ktor.client.call.body
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
 
 internal open class MessageApiV3 internal constructor(
-    authenticatedNetworkClient: AuthenticatedNetworkClient,
-    envelopeProtoMapper: EnvelopeProtoMapper
-) : MessageApiV2(authenticatedNetworkClient, envelopeProtoMapper)
+    private val authenticatedNetworkClient: AuthenticatedNetworkClient,
+    private val envelopeProtoMapper: EnvelopeProtoMapper
+) : MessageApiV2(authenticatedNetworkClient, envelopeProtoMapper) {
+
+    override suspend fun qualifiedSendMessage(
+        parameters: MessageApi.Parameters.QualifiedDefaultParameters,
+        conversationId: ConversationId
+    ): NetworkResponse<QualifiedSendMessageResponse> = wrapKaliumResponse<QualifiedSendMessageResponse.MessageSent>({
+        if (it.status != STATUS_CLIENTS_HAVE_CHANGED) null
+        else NetworkResponse.Error(
+            kException = ProteusClientsChangedError(
+                errorBody = it.body()
+            )
+        )
+    }) {
+        httpClient.post("$PATH_CONVERSATIONS/${conversationId.domain}/${conversationId.value}/$PATH_PROTEUS_MESSAGE") {
+            setBody(envelopeProtoMapper.encodeToProtobuf(parameters))
+            contentType(ContentType.Application.XProtoBuf)
+        }
+    }
+}
