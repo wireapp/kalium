f25644d9ba094a7d40c2e7e2a578cb1cbb6e8118
Mohamad Jaara <mohamad.jaara@wire.com>
2023-02-15 10:29:09 +0100

    not used imports

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/data/message/MessageRepository.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/data/message/MessageRepository.kt
@@ -40,8 +40,6 @@ import com.wire.kalium.logic.functional.Either
 import com.wire.kalium.logic.functional.flatMap
 import com.wire.kalium.logic.functional.fold
 import com.wire.kalium.logic.functional.map
-import com.wire.kalium.logic.functional.onFailure
-import com.wire.kalium.logic.functional.onSuccess
 import com.wire.kalium.logic.wrapApiRequest
 import com.wire.kalium.logic.wrapStorageRequest
 import com.wire.kalium.network.api.base.authenticated.message.MLSMessageApi
