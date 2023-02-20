ef7c3e765f1a88a48e80f47fe3384be95e48ad35
Michał Saleniuk <30429749+saleniuk@users.noreply.github.com>
2023-02-20 09:56:44 +0100

    fix(dev): crash loop when trying to create PendingMessageSenderWorker [AR-3041] (#1506)
    * fix: check if session still exists to break the crash loop and cancel scheduled worker when logging out
    
    * fix detekt issue
    
    * trigger build
    
    ---------
    
    Co-authored-by: Mohamad Jaara <mohamad.jaara@wire.com>

---
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/session/DoesValidSessionExistUseCase.kt
@@ -0,0 +1,49 @@
+/*
+ * Wire
+ * Copyright (C) 2023 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ */
+
+package com.wire.kalium.logic.feature.session
+
+import com.wire.kalium.logic.CoreFailure
+import com.wire.kalium.logic.StorageFailure
+import com.wire.kalium.logic.data.session.SessionRepository
+import com.wire.kalium.logic.data.user.UserId
+import com.wire.kalium.logic.functional.fold
+
+sealed class DoesValidSessionExistResult {
+    data class Success(val doesValidSessionExist: Boolean) : DoesValidSessionExistResult()
+
+    sealed class Failure : DoesValidSessionExistResult() {
+        @Suppress("UNUSED_PARAMETER") // It's used by consumers of Kalium
+        class Generic(coreFailure: CoreFailure) : Failure()
+    }
+}
+
+/**
+ * This use case will return the information whether the valid session exists for a given user id.
+ */
+class DoesValidSessionExistUseCase(private val sessionRepository: SessionRepository) {
+    suspend operator fun invoke(userId: UserId): DoesValidSessionExistResult =
+        sessionRepository.doesValidSessionExist(userId).fold({
+            when (it) {
+                StorageFailure.DataNotFound -> DoesValidSessionExistResult.Success(false)
+                is StorageFailure.Generic -> DoesValidSessionExistResult.Failure.Generic(it)
+            }
+        }, {
+            DoesValidSessionExistResult.Success(it)
+        })
+}
