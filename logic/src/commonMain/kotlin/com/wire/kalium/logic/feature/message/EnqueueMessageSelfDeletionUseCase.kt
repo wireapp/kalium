d1988768b1c47b87974d8dbb3a201535a47b1c42
Mateusz <m.pachulski94@gmail.com>
2023-02-20 16:46:42 +0100

    wip

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/message/EnqueueMessageSelfDeletionUseCase.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/message/EnqueueMessageSelfDeletionUseCase.kt
@@ -1,9 +1,14 @@
 package com.wire.kalium.logic.feature.message
 
 import com.wire.kalium.logic.data.id.ConversationId
+import com.wire.kalium.logic.data.message.Message
+import com.wire.kalium.logic.data.message.MessageRepository
+import com.wire.kalium.logic.functional.flatMap
+import com.wire.kalium.logic.functional.map
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.channels.ReceiveChannel
 import kotlinx.coroutines.channels.produce
+import kotlinx.coroutines.launch
 
 class EnqueueMessageSelfDeletionUseCase(coroutineScope: CoroutineScope) {
 
@@ -17,20 +22,31 @@ class EnqueueMessageSelfDeletionUseCase(coroutineScope: CoroutineScope) {
 
 
 internal class SelfDeletingMessageManager(
-    private val coroutineScope: CoroutineScope
+    private val coroutineScope: CoroutineScope,
+    private val messageRepository: MessageRepository
 ) : CoroutineScope by coroutineScope {
 
     // hot channel map
     private val map: MutableMap<Pair<ConversationId, String>, ReceiveChannel<String>> = mutableMapOf()
 
     fun enqueue(conversationId: ConversationId, messageId: String) {
-        val outgoingDeletionTimer = map[conversationId to messageId]
+        launch {
+            val requestedMessage = messageRepository.getMessageById(conversationId, messageId)
 
-        if (outgoingDeletionTimer == null) {
-            println("already outgoing")
-        } else {
-            map[conversationId to messageId] = produce {
-//                 val isObligateForDeletion =
+            requestedMessage.map { message ->
+                if (message is Message.Ephemeral) {
+                    val outgoingDeletionTimer = map[conversationId to messageId]
+
+                    if (outgoingDeletionTimer == null) {
+                        println("already outgoing")
+                    } else {
+                        map[conversationId to messageId] = produce {
+                            val isObligateForDeletion = message.expireAfterMillis
+                        }
+                    }
+                } else {
+                    throw IllegalStateException("Not an ephemeral message")
+                }
             }
         }
     }
