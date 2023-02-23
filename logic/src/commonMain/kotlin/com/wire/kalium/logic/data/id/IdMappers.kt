13e04af03f8a2fbc1a3a5184eee9caa587bc1390
Jacob Persson <7156+typfel@users.noreply.github.com>
2023-02-23 17:28:36 +0100

    feat: improve encryption performance by using batch API

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/data/id/IdMappers.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/data/id/IdMappers.kt
@@ -18,8 +18,10 @@
 
 package com.wire.kalium.logic.data.id
 
+import com.wire.kalium.cryptography.CryptoClientId
 import com.wire.kalium.cryptography.CryptoQualifiedID
 import com.wire.kalium.cryptography.MLSGroupId
+import com.wire.kalium.logic.data.conversation.ClientId
 import com.wire.kalium.network.api.base.model.UserAssetDTO
 import com.wire.kalium.persistence.dao.QualifiedIDEntity
 
@@ -37,9 +39,15 @@ internal fun QualifiedIDEntity.toApi(): NetworkQualifiedId = NetworkQualifiedId(
 internal fun NetworkQualifiedId.toModel(): QualifiedID = QualifiedID(value, domain)
 internal fun NetworkQualifiedId.toDao(): PersistenceQualifiedId = PersistenceQualifiedId(value, domain)
 
+internal fun CryptoQualifiedID.toModel() = QualifiedID(value, domain)
+
+internal fun CryptoClientId.toModel() = ClientId(value)
+
 internal fun UserAssetDTO.toDao(domain: String): QualifiedIDEntity = PersistenceQualifiedId(key, domain)
 internal fun UserAssetDTO.toModel(domain: String): QualifiedID = QualifiedID(key, domain)
 
 internal fun SubconversationId.toApi(): String = value
 
 internal fun GroupID.toCrypto(): MLSGroupId = value
+
+
