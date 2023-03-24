bf207fa80fb047e73ad66a8911379442a95b3752
Jacob Persson <7156+typfel@users.noreply.github.com>
2023-03-24 18:02:28 +0100

    feat(darwin): chunked AES encryption methods for better memory usage  (#1593)
    * feat: encrypt AES in chunks for lower memory usage
    
    * chore: improve documentation and naming inconsistencies
    
    * chore: fix detekt

--- cryptography/src/commonJvmAndroid/kotlin/com.wire.kalium.cryptography/utils/CryptoUtils.kt
+++ cryptography/src/commonJvmAndroid/kotlin/com.wire.kalium.cryptography/utils/CryptoUtils.kt
@@ -24,12 +24,12 @@ import okio.Source
 
 actual fun encryptDataWithAES256(data: PlainData, key: AES256Key): EncryptedData = AESEncrypt().encryptData(data, key)
 
-actual fun encryptFileWithAES256(assetDataSource: Source, key: AES256Key, outputSink: Sink) =
-    AESEncrypt().encryptFile(assetDataSource, key, outputSink)
+actual fun encryptFileWithAES256(source: Source, key: AES256Key, sink: Sink) =
+    AESEncrypt().encryptFile(source, key, sink)
 
 actual fun decryptDataWithAES256(data: EncryptedData, secretKey: AES256Key): PlainData = AESDecrypt(secretKey).decryptData(data)
 
-actual fun decryptFileWithAES256(encryptedDataSource: Source, decryptedDataSink: Sink, secretKey: AES256Key) =
-    AESDecrypt(secretKey).decryptFile(encryptedDataSource, decryptedDataSink)
+actual fun decryptFileWithAES256(source: Source, sink: Sink, secretKey: AES256Key) =
+    AESDecrypt(secretKey).decryptFile(source, sink)
 
 actual fun generateRandomAES256Key(): AES256Key = AESEncrypt().generateRandomAES256Key()
