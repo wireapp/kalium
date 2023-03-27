6fd9c98adc84c92e5480a4c0bc99d1a4a5b8adf3
yamilmedina <yamilmedina@users.noreply.github.com>
2023-03-27 18:48:17 +0200

    chore: increase memory for dokka

--- scripts/release.mk
+++ scripts/release.mk
@@ -1,4 +1,4 @@
 # release related targets, ie: docs, changelog, build aar, etc.
 # dokka
 doc/generate-kdocs:
-	./gradlew --no-daemon --no-parallel --stacktrace dokkaHtmlMultiModule
+	./gradlew -Dorg.gradle.jvmargs=-Xmx8g --stacktrace dokkaHtmlMultiModule
