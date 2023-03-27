d2524fc85a2dd45de68e51fdc0b9b8a581b96c6d
yamilmedina <yamilmedina@users.noreply.github.com>
2023-03-27 16:27:48 +0200

    chore: deletion of client, also delete it locally

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/data/client/ClientRepository.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/data/client/ClientRepository.kt
@@ -30,7 +30,6 @@ import com.wire.kalium.logic.data.user.UserMapper
 import com.wire.kalium.logic.di.MapperProvider
 import com.wire.kalium.logic.functional.Either
 import com.wire.kalium.logic.functional.flatMap
-import com.wire.kalium.logic.functional.fold
 import com.wire.kalium.logic.functional.map
 import com.wire.kalium.logic.functional.mapLeft
 import com.wire.kalium.logic.functional.onSuccess
