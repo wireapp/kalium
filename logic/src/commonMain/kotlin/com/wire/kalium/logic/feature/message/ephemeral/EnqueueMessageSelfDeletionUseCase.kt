9d38117b2bb3f8d768f2cd6e34c92e6ecc6f1013
Mateusz <m.pachulski94@gmail.com>
2023-03-29 17:10:42 +0200

    merge develop

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/message/ephemeral/EnqueueMessageSelfDeletionUseCase.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/message/ephemeral/EnqueueMessageSelfDeletionUseCase.kt
@@ -3,7 +3,7 @@ package com.wire.kalium.logic.feature.message.ephemeral
 import com.wire.kalium.logic.data.id.ConversationId
 
 /**
- * This use case enqueue the self deletion for a specific conversation id and message id
+ * This use case enqueue the self deletion of a [Message] for a specific conversation id and message id
  */
 interface EnqueueMessageSelfDeletionUseCase {
     operator fun invoke(conversationId: ConversationId, messageId: String)
