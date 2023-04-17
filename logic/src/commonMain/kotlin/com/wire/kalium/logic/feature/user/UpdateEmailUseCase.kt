79d5226dd88cc6862b2fcec542ff9c40b1391bda
Mohamad Jaara <mohamad.jaara@wire.com>
2023-04-17 11:35:38 +0200

    feat: UpdateEmailUseCase (#1632)
    * feat update email request
    
    * add repository function to update email
    
    * UpdateEmailUseCase
    
    * delete update email function form UserDAO
    
    * expose usecase
    
    * add cookie to the update email request path
    
    * StringTemplate
    
    * propagate is the email have been changed or not
    
    * add TODO to rename a usecase
    
    * detekt

---
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/user/UpdateEmailUseCase.kt
@@ -0,0 +1,77 @@
+/*
+ * Wire
+ * Copyright (C) 2023 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ */
+package com.wire.kalium.logic.feature.user
+
+import com.wire.kalium.logic.NetworkFailure
+import com.wire.kalium.logic.data.user.UserRepository
+import com.wire.kalium.logic.feature.user.UpdateEmailUseCase.Result
+import com.wire.kalium.logic.functional.fold
+import com.wire.kalium.network.exceptions.KaliumException
+import com.wire.kalium.network.exceptions.isInvalidEmail
+import com.wire.kalium.network.exceptions.isKeyExists
+
+/**
+ * Updates the user's email address.
+ * keep in mind that this use case does not update the email immediately, it only sends a request to the backend.
+ * the use need to confirm the email change by clicking on the link in an email they receive.
+ * @see [UserRepository.updateSelfEmail]
+ * @param email The new email address.
+ * @return [Result.Success] if the email was updated successfully.
+ * @return [Result.Failure.InvalidEmail] if the email is invalid.
+ * @return [Result.Failure.EmailAlreadyInUse] if the email is already in use.
+ * @return [Result.Failure.GenericFailure] if the email update failed for any other reason.
+ */
+class UpdateEmailUseCase internal constructor(
+    private val userRepository: UserRepository
+) {
+    suspend operator fun invoke(email: String): Result = userRepository.updateSelfEmail(email)
+        .fold(::onError, ::onSuccess)
+
+    private fun onError(error: NetworkFailure): Result.Failure {
+        return if (error is NetworkFailure.ServerMiscommunication && error.kaliumException is KaliumException.InvalidRequestError) {
+            when {
+                error.kaliumException.isKeyExists() -> Result.Failure.EmailAlreadyInUse
+                error.kaliumException.isInvalidEmail() -> Result.Failure.InvalidEmail
+                else -> Result.Failure.GenericFailure(error)
+            }
+        } else {
+            Result.Failure.GenericFailure(error)
+        }
+    }
+
+    private fun onSuccess(isEmailUpdated: Boolean): Result.Success {
+        return if (isEmailUpdated) {
+            Result.Success.VerificationEmailSent
+        } else {
+            Result.Success.NoChange
+        }
+    }
+
+    sealed interface Result {
+        sealed interface Success : Result {
+            object VerificationEmailSent : Success
+            object NoChange : Success
+        }
+
+        sealed interface Failure : Result {
+            object InvalidEmail : Failure
+            object EmailAlreadyInUse : Failure
+            data class GenericFailure(val error: NetworkFailure) : Failure
+        }
+    }
+}
---
+++ logic/src/commonTest/kotlin/com/wire/kalium/logic/feature/user/UpdateEmailUseCaseTest.kt
@@ -0,0 +1,154 @@
+/*
+ * Wire
+ * Copyright (C) 2023 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ */
+package com.wire.kalium.logic.feature.user
+
+import com.wire.kalium.logic.NetworkFailure
+import com.wire.kalium.logic.data.user.UserRepository
+import com.wire.kalium.logic.functional.Either
+import com.wire.kalium.network.api.base.model.ErrorResponse
+import com.wire.kalium.network.exceptions.KaliumException
+import io.mockative.Mock
+import io.mockative.any
+import io.mockative.eq
+import io.mockative.given
+import io.mockative.mock
+import io.mockative.once
+import io.mockative.verify
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import okio.IOException
+import kotlin.test.Test
+import kotlin.test.assertIs
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class UpdateEmailUseCaseTest {
+
+    @Test
+    fun givenUpdateEmailSuccess_whenInvoked_thenReturnsVerificationEmailSent() = runTest {
+        val (arrange, useCase) = Arrangement()
+            .withUpdateSelfEmailSuccess(true)
+            .arrange()
+        val result = useCase("email")
+        assertIs<UpdateEmailUseCase.Result.Success.VerificationEmailSent>(result)
+
+        verify(arrange.userRepository)
+            .suspendFunction(arrange.userRepository::updateSelfEmail)
+            .with(eq("email"))
+            .wasInvoked(exactly = once)
+    }
+
+    @Test
+    fun givenUpdateEmailSuccess_whenInvoked_thenReturnsSuccess() = runTest {
+        val (arrange, useCase) = Arrangement()
+            .withUpdateSelfEmailSuccess(false)
+            .arrange()
+        val result = useCase("email")
+        assertIs<UpdateEmailUseCase.Result.Success.NoChange>(result)
+
+        verify(arrange.userRepository)
+            .suspendFunction(arrange.userRepository::updateSelfEmail)
+            .with(eq("email"))
+            .wasInvoked(exactly = once)
+    }
+
+    @Test
+    fun givenUpdateEmailFailure_whenInvoked_thenReturnsFailure() = runTest {
+        val (arrange, useCase) = Arrangement()
+            .withUpdateSelfEmailFailure(NetworkFailure.ServerMiscommunication(KaliumException.GenericError(IOException())))
+            .arrange()
+        val result = useCase("email")
+        assertIs<UpdateEmailUseCase.Result.Failure.GenericFailure>(result)
+
+        verify(arrange.userRepository)
+            .suspendFunction(arrange.userRepository::updateSelfEmail)
+            .with(eq("email"))
+            .wasInvoked(exactly = once)
+    }
+
+    @Test
+    fun givenEmailAlreadyInUse_whenUpdatingEmail_thenEmailAlreadyInUseErrorIsReturned() = runTest {
+        val (arrange, useCase) = Arrangement()
+            .withUpdateSelfEmailFailure(
+                NetworkFailure.ServerMiscommunication(
+                    KaliumException.InvalidRequestError(
+                        ErrorResponse(
+                            400,
+                            "message",
+                            "key-exists"
+                        )
+                    )
+                )
+            )
+            .arrange()
+        val result = useCase("email")
+        assertIs<UpdateEmailUseCase.Result.Failure.EmailAlreadyInUse>(result)
+
+        verify(arrange.userRepository)
+            .suspendFunction(arrange.userRepository::updateSelfEmail)
+            .with(eq("email"))
+            .wasInvoked(exactly = once)
+    }
+
+    @Test
+    fun givenInvalidEmail_whenUpdatingEmail_thenInvalidEmailErrorIsReturned() = runTest {
+        val (arrange, useCase) = Arrangement()
+            .withUpdateSelfEmailFailure(
+                NetworkFailure.ServerMiscommunication(
+                    KaliumException.InvalidRequestError(
+                        ErrorResponse(
+                            400,
+                            "message",
+                            "invalid-email"
+                        )
+                    )
+                )
+            )
+            .arrange()
+        val result = useCase("email")
+        assertIs<UpdateEmailUseCase.Result.Failure.InvalidEmail>(result)
+
+        verify(arrange.userRepository)
+            .suspendFunction(arrange.userRepository::updateSelfEmail)
+            .with(eq("email"))
+            .wasInvoked(exactly = once)
+    }
+
+
+    private class Arrangement {
+        @Mock
+        val userRepository = mock(UserRepository::class)
+
+        private val useCase = UpdateEmailUseCase(userRepository)
+
+        fun withUpdateSelfEmailSuccess(isEmailUpdated: Boolean) = apply {
+            given(userRepository)
+                .suspendFunction(userRepository::updateSelfEmail)
+                .whenInvokedWith(any())
+                .thenReturn(Either.Right(isEmailUpdated))
+        }
+
+        fun withUpdateSelfEmailFailure(error: NetworkFailure) = apply {
+            given(userRepository)
+                .suspendFunction(userRepository::updateSelfEmail)
+                .whenInvokedWith(any())
+                .thenReturn(Either.Left(error))
+        }
+
+        fun arrange() = this to useCase
+    }
+}
