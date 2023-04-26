064d04497b01c062840a2170b217548c1e3a247a
vitorhugods <vitor@schwaab.xyz>
2023-04-26 15:50:34 +0200

    fix: crash during root verification

--- logic/src/androidMain/kotlin/com/wire/kalium/logic/feature/rootDetection/RootDetectorImpl.kt
+++ logic/src/androidMain/kotlin/com/wire/kalium/logic/feature/rootDetection/RootDetectorImpl.kt
@@ -4,21 +4,31 @@ import android.annotation.SuppressLint
 import java.io.File
 
 actual class RootDetectorImpl actual constructor() : RootDetector {
-    override fun isSystemRooted(): Boolean {
-        val releaseTagsExist = getSystemProperty("ro.build.tags").contains("release-keys")
-        val otacertsExist = File("/etc/security/otacerts.zip").exists()
-        val canRunSu = runCommand("su")
 
-        return !releaseTagsExist || !otacertsExist || canRunSu
+    override fun isSystemRooted(): Boolean {
+        // Lambdas so they can be executed lazily
+        val releaseTagsExist = {
+            // Fallback to true in case of failure
+            getSystemProperty("ro.build.tags")?.contains("release-keys") ?: true
+        }
+        val otacertsExist = { File("/etc/security/otacerts.zip").exists() }
+        val canRunSu = { runCommand("su") }
+
+        // Return eagerly on the first root detection
+        return !releaseTagsExist() || !otacertsExist() || canRunSu()
     }
 
     @SuppressLint("PrivateApi")
-    private fun getSystemProperty(key: String): List<String> {
-        val result = Class.forName("android.os.SystemProperties")
-            .getMethod("get", List::class.java as Class<out List<String>>)
-            .invoke(null, key) as? List<String>
-
-        return result ?: emptyList()
+    /**
+     * Attempts to read a system property based on the provided [key].
+     * @return The property's value, or null if it wasn't possible to read it.
+     */
+    private fun getSystemProperty(key: String): String? = try {
+        Class.forName("android.os.SystemProperties")
+            .getMethod("get", String::class.java)
+            .invoke(null, key) as? String
+    } catch (nsm: NoSuchMethodException) {
+        null
     }
 
     @Suppress("SwallowedException", "TooGenericExceptionCaught")
