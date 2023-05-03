6a5ecb94508cf21edb4be0b90821a3675faeb177
Jacob Persson <7156+typfel@users.noreply.github.com>
2023-05-03 18:04:53 +0200

    fix: user agent on native platforms

--- network/src/commonMain/kotlin/com/wire/kalium/network/networkContainer/KaliumUserAgentProvider.kt
+++ network/src/commonMain/kotlin/com/wire/kalium/network/networkContainer/KaliumUserAgentProvider.kt
@@ -2,7 +2,6 @@ package com.wire.kalium.network.networkContainer
 
 import kotlin.native.concurrent.ThreadLocal
 
-@ThreadLocal
 object KaliumUserAgentProvider {
     lateinit var userAgent: String
         private set
