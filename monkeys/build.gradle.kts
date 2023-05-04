341eb2bcf45edec612ffe393d08558ad32cc1e62
Vitor Hugo Schwaab <vitor@schwaab.dev>
2023-05-04 14:46:43 +0200

    chore: [WIP] start infinite monkeys

---
+++ monkeys/build.gradle.kts
@@ -0,0 +1,119 @@
+/*
+ * Wire
+ * Copyright (C) 2023 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ */
+
+import com.wire.kalium.plugins.commonDokkaConfig
+import com.wire.kalium.plugins.commonJvmConfig
+
+@Suppress("DSL_SCOPE_VIOLATION")
+
+plugins {
+    application
+    kotlin("multiplatform")
+}
+val mainFunctionClassName = "com.wire.kalium.monkeys.MainKt"
+
+application {
+    mainClass.set(mainFunctionClassName)
+}
+
+tasks.jar {
+    manifest.attributes["Main-Class"] = mainFunctionClassName
+    val dependencies = configurations
+        .runtimeClasspath
+        .get()
+        .map(::zipTree)
+    from(dependencies)
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+}
+
+kotlin {
+    val jvmTarget = jvm() {
+        commonJvmConfig(includeNativeInterop = false)
+        tasks.named("run", JavaExec::class) {
+            isIgnoreExitValue = true
+            standardInput = System.`in`
+            standardOutput = System.out
+        }
+    }
+    macosX64() {
+        binaries {
+            executable()
+        }
+    }
+    macosArm64() {
+        binaries {
+            executable()
+        }
+    }
+
+    sourceSets {
+        val commonMain by sourceSets.getting {
+            dependencies {
+                implementation(project(":network"))
+                implementation(project(":cryptography"))
+                implementation(project(":logic"))
+                implementation(project(":util"))
+
+                implementation(libs.cliKt)
+                implementation(libs.ktor.utils)
+                implementation(libs.coroutines.core)
+                implementation(libs.ktxDateTime)
+            }
+        }
+        val jvmMain by getting {
+            dependsOn(commonMain)
+
+             dependencies {
+                 implementation(libs.ktor.okHttp)
+                 implementation(libs.okhttp.loggingInterceptor)
+             }
+        }
+        val darwinMain by creating {
+            dependsOn(commonMain)
+
+            dependencies {
+                implementation(libs.ktor.iosHttp)
+            }
+        }
+        val macosX64Main by getting {
+            dependsOn(darwinMain)
+        }
+        val macosArm64Main by getting {
+            dependsOn(darwinMain)
+        }
+
+        tasks.withType<JavaExec> {
+            // code to make run task in kotlin multiplatform work
+            val compilation = jvmTarget.compilations.getByName<org.jetbrains.kotlin.gradle.plugin.mpp.KotlinJvmCompilation>("main")
+
+            val classes = files(
+                compilation.runtimeDependencyFiles,
+                compilation.output.allOutputs
+            )
+            classpath(classes)
+            setJvmArgs(listOf("-Djava.library.path=/usr/local/lib/:./native/libs"))
+        }
+    }
+}
+
+commonDokkaConfig()
+
+tasks.withType<Wrapper> {
+    gradleVersion = "7.3.1"
+    distributionType = Wrapper.DistributionType.BIN
+}
---
+++ monkeys/src/commonMain/kotlin/com/wire/kalium/monkeys/MonkeyApplication.kt
@@ -0,0 +1,75 @@
+/*
+ * Wire
+ * Copyright (C) 2023 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ */
+
+@file:Suppress("TooManyFunctions")
+package com.wire.kalium.monkeys
+
+import co.touchlab.kermit.LogWriter
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.flag
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.types.enum
+import com.wire.kalium.logger.KaliumLogLevel
+import com.wire.kalium.logic.CoreLogger
+import com.wire.kalium.logic.CoreLogic
+import com.wire.kalium.logic.featureFlags.KaliumConfigs
+import kotlinx.coroutines.runBlocking
+
+class MonkeyApplication : CliktCommand(allowMultipleSubcommands = true) {
+
+    private val logLevel by option(help = "log level").enum<KaliumLogLevel>().default(KaliumLogLevel.WARN)
+    private val logOutputFile by option(help = "output file for logs")
+    private val developmentApiEnabled by option(help = "use development API if supported by backend").flag(default = false)
+    private val encryptProteusStorage by option(help = "use encrypted storage for proteus sessions and identity").flag(default = false)
+    private val fileLogger: LogWriter by lazy { fileLogger(logOutputFile ?: "kalium.log") }
+
+    override fun run() = runBlocking {
+        val coreLogic = coreLogic(
+            rootPath = "$HOME_DIRECTORY/.kalium/accounts",
+            kaliumConfigs = KaliumConfigs(
+                developmentApiEnabled = developmentApiEnabled,
+                encryptProteusStorage = encryptProteusStorage
+            )
+        )
+
+        if (logOutputFile != null) {
+            CoreLogger.setLoggingLevel(logLevel, fileLogger)
+        } else {
+            CoreLogger.setLoggingLevel(logLevel)
+        }
+
+        coreLogic.updateApiVersionsScheduler.scheduleImmediateApiVersionUpdate()
+        runMonkey(coreLogic)
+    }
+
+    private fun runMonkey(coreLogic: CoreLogic) {
+
+    }
+
+    companion object {
+        val HOME_DIRECTORY: String = homeDirectory()
+    }
+
+}
+
+expect fun fileLogger(filePath: String): LogWriter
+
+expect fun homeDirectory(): String
+
+expect fun coreLogic(rootPath: String, kaliumConfigs: KaliumConfigs): CoreLogic
