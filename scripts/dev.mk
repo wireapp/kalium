903c2a3c8ac5d5b0418c6c6c75a23a74d7dffd84
Mohamad Jaara <mohamad.jaara@wire.com>
2023-05-14 18:21:34 +0200

    work around for the sqlDelight bug

--- scripts/dev.mk
+++ scripts/dev.mk
@@ -4,9 +4,19 @@ db/verify-global-migration:
 	./gradlew :persistence:verifyCommonMainGlobalDatabaseMigration
 
 db/verify-user-migration:
+	rm persistence/src/commonMain/db_user/schemas/2.db
+	for i in {1..33}; \
+	do \
+		rm persistence/src/commonMain/db_user/migrations/$$i.sqm; \
+	done
 	./gradlew :persistence:verifyCommonMainUserDatabaseMigration
 
 db/verify-all-migrations:
+	rm persistence/src/commonMain/db_user/schemas/2.db
+	for i in {1..33}; \
+	do \
+		rm persistence/src/commonMain/db_user/migrations/$$i.sqm; \
+	done
 	./gradlew :persistence:verifySqlDelightMigration
 
 # detekt
