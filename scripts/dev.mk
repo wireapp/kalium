a38cefec88317f3a409956372bdb9ced82d55aef
Mohamad Jaara <mohamad.jaara@wire.com>
2023-05-15 10:02:40 +0200

    add comment on why the files are deleted when running tests

--- scripts/dev.mk
+++ scripts/dev.mk
@@ -3,6 +3,10 @@
 db/verify-global-migration:
 	./gradlew :persistence:verifyCommonMainGlobalDatabaseMigration
 
+# the delete of 2.db and the migrations files from 1 -> 33 is a workaround for the following issue:
+# https://github.com/cashapp/sqldelight/issues/4154
+# TL,DR: the 33.sqm use UPDPATE table AS alias syntax which is a valid SQLtite syntax but sqldelight doesn't support it
+# it result to false nigaive when validating the migration
 db/verify-user-migration:
 	rm persistence/src/commonMain/db_user/schemas/2.db
 	for i in {1..33}; \
