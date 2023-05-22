b548f398497c144b297bd98c03a66a3b674b6daa
Mohamad Jaara <mohamad.jaara@wire.com>
2023-05-22 17:10:32 +0200

    remove TODO

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/service/ObserveAllServicesUseCase.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/service/ObserveAllServicesUseCase.kt
@@ -23,6 +23,7 @@ import com.wire.kalium.logic.functional.fold
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.fold
+import kotlinx.coroutines.flow.map
 
 /**
  * This use case returns all services currently in the database.
@@ -39,5 +40,10 @@ class ObserveAllServicesUseCaseImpl internal constructor(
     private val serviceRepository: ServiceRepository
 ) : ObserveAllServicesUseCase {
 
-    override suspend fun invoke(): Flow<List<ServiceDetails>> = TODO()
+    override suspend fun invoke(): Flow<List<ServiceDetails>> = serviceRepository.observeAllServices().map {
+        it.fold(
+            { emptyList() },
+            { it }
+        )
+    }
 }
