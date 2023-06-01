b8bda8cd16dd7283e7b81ca55e4518e8e7c807a7
Jacob Persson <7156+typfel@users.noreply.github.com>
2023-06-01 08:11:55 +0200

    feat(mls-migration): fetch migration configuration every 24 hours #1 (#1728)

--- logic/src/commonTest/kotlin/com/wire/kalium/logic/data/event/FeatureConfigMapperTest.kt
+++ logic/src/commonTest/kotlin/com/wire/kalium/logic/data/event/FeatureConfigMapperTest.kt
@@ -30,7 +30,9 @@ import com.wire.kalium.network.api.base.authenticated.featureConfigs.FeatureConf
 import com.wire.kalium.network.api.base.authenticated.featureConfigs.FeatureFlagStatusDTO
 import com.wire.kalium.network.api.base.authenticated.featureConfigs.MLSConfigDTO
 import com.wire.kalium.network.api.base.authenticated.featureConfigs.E2EIConfigDTO
+import com.wire.kalium.network.api.base.authenticated.featureConfigs.MLSMigrationConfigDTO
 import com.wire.kalium.network.api.base.authenticated.featureConfigs.SelfDeletingMessagesConfigDTO
+import kotlinx.datetime.Instant
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -62,6 +64,19 @@ class FeatureConfigMapperTest {
 
         assertEquals(Status.ENABLED, model.status)
         assertEquals(listOf(PlainId("someId")), model.allowedUsers)
+    }
+
+    @Test
+    fun givenApiModelResponse_whenMappingMLSMigrationStatusToModel_thenShouldBeMappedCorrectly() {
+        val (arrangement, mapper) = Arrangement().arrange()
+
+        val model = arrangement.featureConfigResponse.mlsMigration?.let { mapper.fromDTO(it) }
+
+        assertEquals(Status.ENABLED, model?.status)
+        assertEquals(Instant.DISTANT_FUTURE, model?.startTime)
+        assertEquals(Instant.DISTANT_FUTURE, model?.endTime)
+        assertEquals(100, model?.usersThreshold)
+        assertEquals(80, model?.clientsThreshold)
     }
 
     @Test
@@ -124,9 +139,13 @@ class FeatureConfigMapperTest {
     private class Arrangement {
         val featureConfigResponse = FeatureConfigResponse(
             FeatureConfigData.AppLock(
-                AppLockConfigDTO(true, 0), FeatureFlagStatusDTO.ENABLED
+                AppLockConfigDTO(true, 0),
+                FeatureFlagStatusDTO.ENABLED
             ),
-            FeatureConfigData.ClassifiedDomains(ClassifiedDomainsConfigDTO(listOf("wire.com")), FeatureFlagStatusDTO.ENABLED),
+            FeatureConfigData.ClassifiedDomains(
+                ClassifiedDomainsConfigDTO(listOf("wire.com")),
+                FeatureFlagStatusDTO.ENABLED
+            ),
             FeatureConfigData.ConferenceCalling(FeatureFlagStatusDTO.ENABLED),
             FeatureConfigData.ConversationGuestLinks(FeatureFlagStatusDTO.ENABLED),
             FeatureConfigData.DigitalSignatures(FeatureFlagStatusDTO.ENABLED),
@@ -148,6 +167,15 @@ class FeatureConfigMapperTest {
             FeatureConfigData.E2EI(
                 E2EIConfigDTO("url", 1_000_000L),
                 FeatureFlagStatusDTO.ENABLED
+            ),
+            FeatureConfigData.MLSMigration(
+                MLSMigrationConfigDTO(
+                    Instant.DISTANT_FUTURE,
+                    Instant.DISTANT_FUTURE,
+                    100,
+                    80
+                ),
+                FeatureFlagStatusDTO.ENABLED
             )
         )
 
@@ -155,5 +183,4 @@ class FeatureConfigMapperTest {
 
         fun arrange() = this to mapper
     }
-
 }
