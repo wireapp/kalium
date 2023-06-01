b66e9c6aa43ff8a4fee8974aee0a6e52f4306b43
Jacob Persson <7156+typfel@users.noreply.github.com>
2023-06-01 11:23:01 +0200

    feat(mls-migration): migrate proteus conversations to "mixed" protocol  #2 (#1729)
    * feat: add api support for updating the conversation protocol
    
    * fix: rollback when we lost the race to establish a MLS group
    
    * feat: add method for migrating from proteus to mixed
    
    * feat: allow controlling migration update interval in CLI
    
    * fix: use correct use case for checking if we can register an MLS client
    
    * chore: add debug response since BE is not implemented yet
    
    * test: add test for aborting when losing the race to establish an mls group
    
    * test: add network test for updating protocol
    
    * test: add MLSMigrationManager tests
    
    * test: add MLSMigrator tests
    
    * chore: fix detekt
    
    * chore: make method private
    
    * refactor: add sql query for fetching proteus team conversations

--- network/src/commonMain/kotlin/com/wire/kalium/network/api/v0/authenticated/ConversationApiV0.kt
+++ network/src/commonMain/kotlin/com/wire/kalium/network/api/v0/authenticated/ConversationApiV0.kt
@@ -21,6 +21,7 @@ package com.wire.kalium.network.api.v0.authenticated
 import com.wire.kalium.network.AuthenticatedNetworkClient
 import com.wire.kalium.network.api.base.authenticated.conversation.AddConversationMembersRequest
 import com.wire.kalium.network.api.base.authenticated.conversation.AddServiceRequest
+import com.wire.kalium.network.api.base.authenticated.conversation.ConvProtocol
 import com.wire.kalium.network.api.base.authenticated.conversation.ConversationApi
 import com.wire.kalium.network.api.base.authenticated.conversation.ConversationMemberAddedResponse
 import com.wire.kalium.network.api.base.authenticated.conversation.ConversationMemberRemovedResponse
@@ -36,6 +37,7 @@ import com.wire.kalium.network.api.base.authenticated.conversation.Subconversati
 import com.wire.kalium.network.api.base.authenticated.conversation.SubconversationResponse
 import com.wire.kalium.network.api.base.authenticated.conversation.UpdateConversationAccessRequest
 import com.wire.kalium.network.api.base.authenticated.conversation.UpdateConversationAccessResponse
+import com.wire.kalium.network.api.base.authenticated.conversation.UpdateConversationProtocolResponse
 import com.wire.kalium.network.api.base.authenticated.conversation.UpdateConversationReceiptModeResponse
 import com.wire.kalium.network.api.base.authenticated.conversation.messagetimer.ConversationMessageTimerDTO
 import com.wire.kalium.network.api.base.authenticated.conversation.model.ConversationCodeInfo
@@ -383,6 +385,12 @@ internal open class ConversationApiV0 internal constructor(
                 setBody(ConversationMessageTimerDTO(messageTimer))
             }
         }
+
+    override suspend fun updateProtocol(
+        conversationId: ConversationId,
+        protocol: ConvProtocol
+    ): NetworkResponse<UpdateConversationProtocolResponse> =
+        ConversationApi.getApiNotSupportError("updateProtocol")
 
     protected companion object {
         const val PATH_CONVERSATIONS = "conversations"
