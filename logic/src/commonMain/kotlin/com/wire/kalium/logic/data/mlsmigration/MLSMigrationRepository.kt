b4461576a04fa63c403b415b5cfd57b2621caf69
Jacob Persson <7156+typfel@users.noreply.github.com>
2023-06-01 08:11:55 +0200

    feat(mls-migration): fetch migration configuration every 24 hours #1 (#1728)

---
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/data/mlsmigration/MLSMigrationRepository.kt
@@ -0,0 +1,69 @@
+/*
+ * Wire
+ * Copyright (C) 2023 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ */
+package com.wire.kalium.logic.data.mlsmigration
+
+import com.wire.kalium.logic.CoreFailure
+import com.wire.kalium.logic.StorageFailure
+import com.wire.kalium.logic.data.featureConfig.FeatureConfigMapper
+import com.wire.kalium.logic.data.featureConfig.MLSMigrationModel
+import com.wire.kalium.logic.di.MapperProvider
+import com.wire.kalium.logic.functional.Either
+import com.wire.kalium.logic.functional.flatMap
+import com.wire.kalium.logic.functional.map
+import com.wire.kalium.logic.wrapApiRequest
+import com.wire.kalium.logic.wrapStorageRequest
+import com.wire.kalium.network.api.base.authenticated.featureConfigs.FeatureConfigApi
+import com.wire.kalium.network.api.base.authenticated.featureConfigs.FeatureConfigData
+import com.wire.kalium.persistence.dao.MetadataDAO
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+
+interface MLSMigrationRepository {
+    suspend fun fetchMigrationConfiguration(): Either<CoreFailure, Unit>
+    suspend fun setMigrationConfiguration(configuration: MLSMigrationModel): Either<StorageFailure, Unit>
+    suspend fun getMigrationConfiguration(): Either<StorageFailure, MLSMigrationModel>
+}
+
+internal class MLSMigrationRepositoryImpl(
+    private val featureConfigApi: FeatureConfigApi,
+    private val metadataDAO: MetadataDAO,
+    private val featureConfigMapper: FeatureConfigMapper = MapperProvider.featureConfigMapper()
+) : MLSMigrationRepository {
+
+    override suspend fun fetchMigrationConfiguration(): Either<CoreFailure, Unit> {
+        return wrapApiRequest { featureConfigApi.featureConfigs() }
+            .flatMap { it.mlsMigration?.let { setMigrationConfiguration(featureConfigMapper.fromDTO(it)) } ?: Either.Right(Unit) }
+    }
+
+    override suspend fun getMigrationConfiguration(): Either<StorageFailure, MLSMigrationModel> =
+        wrapStorageRequest {
+            metadataDAO.valueByKey(MLS_MIGRATION_CONFIGURATION_KEY)
+        }.map { encodedValue ->
+            featureConfigMapper.fromDTO(Json.decodeFromString<FeatureConfigData.MLSMigration>(encodedValue))
+        }
+
+    override suspend fun setMigrationConfiguration(configuration: MLSMigrationModel): Either<StorageFailure, Unit> =
+        wrapStorageRequest {
+            metadataDAO.insertValue(Json.encodeToString(featureConfigMapper.fromModel(configuration)), MLS_MIGRATION_CONFIGURATION_KEY)
+        }
+
+    companion object {
+        internal const val MLS_MIGRATION_CONFIGURATION_KEY = "mlsMigrationConfiguration"
+    }
+}
