09eaf11077efece418d408c3c49338344b48731f
Mohamad Jaara <mohamad.jaara@wire.com>
2023-06-08 17:15:31 +0200

    feat: use case for user domain lookup (#1784)
    * feat: use case for user domain lookup
    
    * SpacingAroundParens
    
    * expose domain lookup use case
    
    * feat: domain look up in sso login screen
    
    * detekt

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/UserSessionScope.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/UserSessionScope.kt
@@ -416,7 +416,8 @@ class UserSessionScope internal constructor(
     )
     val authenticationScope: AuthenticationScope = authenticationScopeProvider.provide(
         sessionManager.getServerConfig(),
-        sessionManager.getProxyCredentials()
+        sessionManager.getProxyCredentials(),
+         globalScope.serverConfigRepository
     )
 
     private val userConfigRepository: UserConfigRepository
