d0bb79da0a699b11f7d347d8c55dddece7574ad2
Jacob Persson <7156+typfel@users.noreply.github.com>
2023-06-20 13:18:34 +0200

    feat(mls-migration): update conversation protocol to MLS when every member supports MLS #6 (#1802)
    * feat: support fetching conversation which can be finalised
    
    * feat: add function for finalising migration of team group conversations
    
    * refactor: move methods for updating conversation protocol to the conversation repository
    
    * chore: fix copy & paste error
    
    * refactor: don't return a flow when fetching conversations for migration
    
    * chore: rebase clean up
    
    * performance: replace string matching by listing all cases

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/UserSessionScope.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/UserSessionScope.kt
@@ -960,9 +960,9 @@ class UserSessionScope internal constructor(
     private val mlsMigrator: MLSMigrator
         get() = MLSMigratorImpl(
             selfTeamId,
+            userRepository,
             conversationRepository,
-            mlsConversationRepository,
-            authenticatedNetworkContainer.conversationApi
+            mlsConversationRepository
         )
 
     internal val keyPackageManager: KeyPackageManager = KeyPackageManagerImpl(featureSupport,
