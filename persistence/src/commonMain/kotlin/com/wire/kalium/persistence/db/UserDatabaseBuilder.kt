445495b9f7cc57fed5fa88a507d214fc6a0ee6a9
Jacob Persson <7156+typfel@users.noreply.github.com>
2023-06-23 09:57:40 +0200

    feat(mls-migration): handle protocol changed events #7 (#1811)
    * feat: support persisting protocol changed system messages
    
    * feat: process protocol change events and insert system messages
    
    * feat: insert history lost system message if protocol change is discovered during slow sync
    
    * chore: add migration of new system message table
    
    * fix: check if protocol was updated inside sql query
    
    * fix: only fetch group conversations by protocol
    
    * chore: document return value
    
    * chore: fix test naming
    
    * refactor: rename SystemMessageBuilder to SystemMessageInserter
    
    * chore: fix logging inconsistency
    
    * fix: import
    
    * chore: fix compilation

--- persistence/src/commonMain/kotlin/com/wire/kalium/persistence/db/UserDatabaseBuilder.kt
+++ persistence/src/commonMain/kotlin/com/wire/kalium/persistence/db/UserDatabaseBuilder.kt
@@ -152,7 +152,8 @@ class UserDatabaseBuilder internal constructor(
         NewClientAdapter = TableMapper.newClientAdapter,
         MessageRecipientFailureAdapter = TableMapper.messageRecipientFailureAdapter,
         ButtonContentAdapter = TableMapper.buttonContentAdapter,
-        MessageFederationTerminatedContentAdapter = TableMapper.messageFederationTerminatedContentAdapter
+        MessageFederationTerminatedContentAdapter = TableMapper.messageFederationTerminatedContentAdapter,
+        MessageConversationProtocolChangedContentAdapter = TableMapper.messageConversationProtocolChangedContentAdapter
     )
 
     init {
