1ac4f8b2b4e7b940790ab7486abb2570bb84534d
Jacob Persson <7156+typfel@users.noreply.github.com>
2023-06-26 12:01:04 +0200

    feat(mls-migration): update self supported protocols during slow sync (#1826) #9

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/UserSessionScope.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/UserSessionScope.kt
@@ -868,6 +868,7 @@ class UserSessionScope internal constructor(
             eventRepository,
             syncSelfUser,
             syncFeatureConfigsUseCase,
+            users.updateSupportedProtocols,
             syncConversations,
             syncConnections,
             syncSelfTeamUseCase,
@@ -985,13 +986,21 @@ class UserSessionScope internal constructor(
                 mlsClientProvider, clientRepository, keyPackageRepository, keyPackageLimitsProvider
             )
         })
+
+    internal val mlsMigrationWorker get() =
+        MLSMigrationWorkerImpl(
+            mlsMigrationRepository,
+            mlsMigrator,
+            users.updateSupportedProtocols
+        )
+
     internal val mlsMigrationManager: MLSMigrationManager = MLSMigrationManagerImpl(
             kaliumConfigs,
             featureSupport,
             incrementalSyncRepository,
             lazy { clientRepository },
             lazy { users.timestampKeyRepository },
-            lazy { MLSMigrationWorkerImpl(mlsMigrationRepository, mlsMigrator) },
+            lazy { mlsMigrationWorker },
             lazy { mlsMigrationRepository }
     )
 
@@ -1412,7 +1421,6 @@ class UserSessionScope internal constructor(
             e2eiRepository,
             clientRepository,
             featureConfigRepository,
-            slowSyncRepository,
             team.isSelfATeamMember,
         )
     private val clearUserData: ClearUserDataUseCase get() = ClearUserDataUseCaseImpl(userStorage)
