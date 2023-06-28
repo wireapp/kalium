26800478bc7487d294f76bb450db8d6f6b4a836c
Jacob Persson <7156+typfel@users.noreply.github.com>
2023-06-28 11:35:55 +0200

    chore: remove global team conversations (#1840)

--- network/src/commonMain/kotlin/com/wire/kalium/network/api/v3/authenticated/ConversationApiV3.kt
+++ network/src/commonMain/kotlin/com/wire/kalium/network/api/v3/authenticated/ConversationApiV3.kt
@@ -18,15 +18,10 @@
 
 package com.wire.kalium.network.api.v3.authenticated
 
-import com.wire.kalium.util.DateTimeUtil
 import com.wire.kalium.network.AuthenticatedNetworkClient
-import com.wire.kalium.network.api.base.authenticated.conversation.ConvProtocol
-import com.wire.kalium.network.api.base.authenticated.conversation.ConversationMemberDTO
-import com.wire.kalium.network.api.base.authenticated.conversation.ConversationMembersResponse
 import com.wire.kalium.network.api.base.authenticated.conversation.ConversationResponse
 import com.wire.kalium.network.api.base.authenticated.conversation.ConversationResponseV3
 import com.wire.kalium.network.api.base.authenticated.conversation.CreateConversationRequest
-import com.wire.kalium.network.api.base.authenticated.conversation.GlobalTeamConversationResponse
 import com.wire.kalium.network.api.base.authenticated.conversation.SubconversationDeleteRequest
 import com.wire.kalium.network.api.base.authenticated.conversation.SubconversationResponse
 import com.wire.kalium.network.api.base.authenticated.conversation.UpdateConversationAccessRequest
@@ -37,8 +32,6 @@ import com.wire.kalium.network.api.base.model.ApiModelMapperImpl
 import com.wire.kalium.network.api.base.model.ConversationId
 import com.wire.kalium.network.api.base.model.QualifiedID
 import com.wire.kalium.network.api.base.model.SubconversationId
-import com.wire.kalium.network.api.base.model.TeamId
-import com.wire.kalium.network.api.base.model.UserId
 import com.wire.kalium.network.api.v2.authenticated.ConversationApiV2
 import com.wire.kalium.network.exceptions.KaliumException
 import com.wire.kalium.network.utils.NetworkResponse
@@ -86,36 +79,6 @@ internal open class ConversationApiV3 internal constructor(
                 "$PATH_CONVERSATIONS/${conversationId.domain}/${conversationId.value}/$PATH_GROUP_INFO"
             )
         }
-
-    override suspend fun fetchGlobalTeamConversationDetails(selfUserId: UserId, teamId: TeamId): NetworkResponse<ConversationResponse> {
-        return wrapKaliumResponse<GlobalTeamConversationResponse> {
-            httpClient.get("$PATH_TEAM/$teamId/$PATH_CONVERSATIONS/$PATH_GLOBAL")
-        }.mapSuccess { response ->
-            ConversationResponse(
-                response.creator ?: "",
-                ConversationMembersResponse(
-                    ConversationMemberDTO.Self(
-                        selfUserId,
-                        "wire_default",
-                    ),
-                    emptyList()
-                ),
-                response.name,
-                response.id,
-                response.groupId,
-                response.epoch,
-                ConversationResponse.Type.GLOBAL_TEAM,
-                0,
-                response.teamId,
-                ConvProtocol.MLS,
-                DateTimeUtil.currentIsoDateTimeString(),
-                response.mlsCipherSuiteTag,
-                response.access,
-                emptySet(),
-                response.receiptMode
-            )
-        }
-    }
 
     override suspend fun updateAccess(
         conversationId: ConversationId,
@@ -183,8 +146,6 @@ internal open class ConversationApiV3 internal constructor(
     }
 
     companion object {
-        const val PATH_TEAM = "team"
-        const val PATH_GLOBAL = "global"
         const val PATH_GROUP_INFO = "groupinfo"
     }
 }
