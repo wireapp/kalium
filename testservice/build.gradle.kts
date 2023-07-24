f99851fdbe9136c1a2cc3cb5a2c18e7cfb9bdd47
Jakub Żerko <iot.zerko@gmail.com>
2023-07-24 16:03:06 +0200

    chore: update to java 17 (#1902)
    * chore: update to java 17
    
    * ci: bump cryptobox image to 1.4.0
    
    * updated kotlin to jvm 17
    
    ---------
    
    Co-authored-by: Vitor Hugo Schwaab <vitor@schwaab.xyz>

--- testservice/build.gradle.kts
+++ testservice/build.gradle.kts
@@ -33,6 +33,17 @@ object Versions {
 
 val mainFunctionClassName = "com.wire.kalium.testservice.TestserviceApplication"
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
 application {
     mainClass.set(mainFunctionClassName)
 }
