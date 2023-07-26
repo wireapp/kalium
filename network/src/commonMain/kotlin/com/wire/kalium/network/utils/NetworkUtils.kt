0832d1885fc0927c7042dc004c390f29cdcaa9f8
yamilmedina <yamilmedina@users.noreply.github.com>
2023-07-26 16:33:34 +0200

    feat: detekt

--- network/src/commonMain/kotlin/com/wire/kalium/network/utils/NetworkUtils.kt
+++ network/src/commonMain/kotlin/com/wire/kalium/network/utils/NetworkUtils.kt
@@ -15,6 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see http://www.gnu.org/licenses/.
  */
+@file:Suppress("TooManyFunctions")
 
 package com.wire.kalium.network.utils
 
@@ -250,4 +251,3 @@ suspend fun <T : Any> wrapHandleFederationConflictOrDelegate(
     } else {
         delegatedHandler.invoke(response)
     }
-
