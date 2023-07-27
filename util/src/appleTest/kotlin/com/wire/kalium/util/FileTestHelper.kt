c7efc965a0266dfb88dee52a30ed8f1764895d9d
Jacob Persson <7156+typfel@users.noreply.github.com>
2023-07-27 12:13:49 +0200

    chore: add file helper methods

--- util/src/appleTest/kotlin/com/wire/kalium/util/FileTestHelper.kt
+++ util/src/appleTest/kotlin/com/wire/kalium/util/FileTestHelper.kt
@@ -1,4 +1,39 @@
-package com.wire.kalium.util.string
+/*
+ * Wire
+ * Copyright (C) 2023 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ */
+package com.wire.kalium.util
 
-class Foo {
+import platform.Foundation.NSFileManager
+import platform.Foundation.NSTemporaryDirectory
+import platform.Foundation.NSURL
+
+actual object FileTestHelper {
+    actual fun createRandomDirectory(): String {
+        val directory = NSURL.fileURLWithPath(NSTemporaryDirectory() + "/testDirectory", isDirectory = true)
+        NSFileManager.defaultManager.createDirectoryAtURL(directory, true, null, null)
+        return directory.absoluteString!!
+    }
+
+    actual fun createRandomFileAt(path: String) {
+        val directory = NSURL.fileURLWithPath(path + "/testFile", isDirectory = true)
+        NSFileManager.defaultManager.createDirectoryAtURL(directory, true, null, null)
+    }
+
+    actual fun directoryExists(path: String): Boolean {
+        return NSFileManager.defaultManager.fileExistsAtPath(path)
+    }
 }
--- util/src/commonTest/kotlin/com/wire/kalium/util/FileUtilTest.kt
+++ util/src/commonTest/kotlin/com/wire/kalium/util/FileUtilTest.kt
@@ -1,4 +1,67 @@
+/*
+ * Wire
+ * Copyright (C) 2023 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ */
 package com.wire.kalium.util
 
+import kotlin.test.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+expect object FileTestHelper {
+    fun createRandomDirectory(): String
+    fun createRandomFileAt(path: String)
+    fun directoryExists(path: String): Boolean
+}
+
 class FileUtilTest {
+
+    @Test
+    fun givenEmptyDirectory_thenIsDirectoryNonEmptyReturnsFalse() {
+        val path = FileTestHelper.createRandomDirectory()
+        assertFalse(FileUtil.isDirectoryNonEmpty(path))
+    }
+
+    @Test
+    fun givenNonExistingDirectory_thenIsDirectoryNonEmptyReturnsFalse() {
+        val path = "/non/existing/path"
+        assertFalse(FileUtil.isDirectoryNonEmpty(path))
+    }
+
+    @Test
+    fun givenNonEmptyDirectory_thenIsDirectoryNonEmptyReturnsTrue() {
+        val path = FileTestHelper.createRandomDirectory()
+        FileTestHelper.createRandomFileAt(path)
+        assertTrue(FileUtil.isDirectoryNonEmpty(path))
+    }
+
+    @Test
+    fun givenNonEmptyDirectory_whenCallingDeleteDirectory_thenDirectoryNoLongerExists() {
+        val path = FileTestHelper.createRandomDirectory()
+        assertTrue(FileTestHelper.directoryExists(path))
+        FileUtil.deleteDirectory(path)
+        assertFalse(FileTestHelper.directoryExists(path))
+    }
+
+    @Test
+    fun givenNonExistingDirectory_whenMkDir_thenDirectoryExists() {
+        val path = FileTestHelper.createRandomDirectory()
+        FileUtil.deleteDirectory(path)
+        assertFalse(FileTestHelper.directoryExists(path))
+        FileUtil.mkDirs(path)
+        assertTrue(FileTestHelper.directoryExists(path))
+    }
 }
