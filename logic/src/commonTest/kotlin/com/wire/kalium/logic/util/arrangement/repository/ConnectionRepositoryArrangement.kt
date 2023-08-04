d8d3c2e98bcf07f58be6ae1c226832ce9c895473
Jakub Żerko <iot.zerko@gmail.com>
2023-08-04 12:04:54 +0200

    Update logic/src/commonTest/kotlin/com/wire/kalium/logic/util/arrangement/repository/ConnectionRepositoryArrangement.kt
    Co-authored-by: Vitor Hugo Schwaab <vitor@schwaab.dev>

--- logic/src/commonTest/kotlin/com/wire/kalium/logic/util/arrangement/repository/ConnectionRepositoryArrangement.kt
+++ logic/src/commonTest/kotlin/com/wire/kalium/logic/util/arrangement/repository/ConnectionRepositoryArrangement.kt
@@ -27,7 +27,6 @@ import io.mockative.mock
 import kotlinx.coroutines.flow.Flow
 
 internal interface ConnectionRepositoryArrangement {
-    @Mock
     val connectionRepository: ConnectionRepository
 
     fun withGetConnections(result: Either<StorageFailure, Flow<List<ConversationDetails>>>)
