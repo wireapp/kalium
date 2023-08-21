af574aa57b434963b87725900d8771f5e95d9427
Oussama <oussama.has100@gmail.com>
2023-08-21 14:32:44 +0200

    chore: upgrade agp to 8.1

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -16,8 +16,6 @@
  * along with this program. If not, see http://www.gnu.org/licenses/.
  */
 
-enableFeaturePreview("VERSION_CATALOGS")
-
 dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
