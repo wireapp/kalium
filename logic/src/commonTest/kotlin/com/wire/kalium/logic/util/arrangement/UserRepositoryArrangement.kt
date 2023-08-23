f75c6e83a6efd0f1e830831b62a1367a34c5fd99
Vitor Hugo Schwaab <vitor@schwaab.dev>
2023-08-23 19:08:30 +0200

    feat: choose 1:1 protocol [WPB-2181] (#1993)
    Co-authored-by: Mojtaba Chenani <chenani@outlook.com>

---
+++ logic/src/commonTest/kotlin/com/wire/kalium/logic/util/arrangement/UserRepositoryArrangement.kt
@@ -0,0 +1,56 @@
+/*
+ * Wire
+ * Copyright (C) 2023 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ */
+package com.wire.kalium.logic.util.arrangement
+
+import com.wire.kalium.logic.CoreFailure
+import com.wire.kalium.logic.data.user.OtherUser
+import com.wire.kalium.logic.data.user.SelfUser
+import com.wire.kalium.logic.data.user.UserRepository
+import com.wire.kalium.logic.functional.Either
+import io.mockative.Mock
+import io.mockative.any
+import io.mockative.given
+import io.mockative.mock
+
+internal interface UserRepositoryArrangement {
+    val userRepository: UserRepository
+
+    fun withSelfUserReturning(selfUser: SelfUser?)
+
+    fun withUserByIdReturning(result: Either<CoreFailure, OtherUser>)
+}
+
+internal class UserRepositoryArrangementImpl: UserRepositoryArrangement {
+
+    @Mock
+    override val userRepository: UserRepository = mock(UserRepository::class)
+
+    override fun withSelfUserReturning(selfUser: SelfUser?) {
+        given(userRepository)
+            .suspendFunction(userRepository::getSelfUser)
+            .whenInvoked()
+            .thenReturn(selfUser)
+    }
+
+    override fun withUserByIdReturning(result: Either<CoreFailure, OtherUser>) {
+        given(userRepository)
+            .suspendFunction(userRepository::userById)
+            .whenInvokedWith(any())
+            .thenReturn(result)
+    }
+}
