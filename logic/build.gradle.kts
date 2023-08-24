b7d3d909e7d0f43110f4160dae3c8221e81ad573
Michał Saleniuk <saleniuk@gmail.com>
2023-08-24 12:21:16 +0200

    chore: run android unit tests on CI

--- logic/build.gradle.kts
+++ logic/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.util.suffixIfNot
+
 /*
  * Wire
  * Copyright (C) 2023 Wire Swiss GmbH
@@ -116,4 +118,21 @@ dependencies {
 
 ksp {
     arg("mockative.stubsUnitByDefault", "true")
+}
+
+android {
+    testOptions.unitTests.all { test ->
+        // only run tests that are different for the android platform, the rest is covered by the jvm tests
+        file("src/androidUnitTest/kotlin").let { dir ->
+            if (dir.exists() && dir.isDirectory) {
+                dir.walk().forEach {
+                    if (it.isFile && it.extension == "kt") {
+                        it.relativeToOrNull(dir)?.let {
+                            test.include(it.path.removeSuffix(".kt").suffixIfNot("*"))
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
