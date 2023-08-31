2f10cd97f899f586956bbe17d7e934a3a2831da8
Jacob Persson <7156+typfel@users.noreply.github.com>
2023-08-31 13:47:52 +0200

    feat: resolve one-on-one conversation to either proteus or mls (#2012)

--- logic/src/commonTest/kotlin/com/wire/kalium/logic/util/arrangement/repository/ConversationRepositoryArrangement.kt
+++ logic/src/commonTest/kotlin/com/wire/kalium/logic/util/arrangement/repository/ConversationRepositoryArrangement.kt
@@ -56,6 +56,8 @@ internal interface ConversationRepositoryArrangement {
     fun withGetConversationIdsReturning(result: Either<StorageFailure, List<QualifiedID>>)
 
     fun withGetOneOnOneConversationsWithOtherUserReturning(result: Either<StorageFailure, List<QualifiedID>>)
+
+    fun withGetConversationByIdReturning(result: Conversation?)
 }
 
 internal open class ConversationRepositoryArrangementImpl : ConversationRepositoryArrangement {
@@ -136,6 +138,13 @@ internal open class ConversationRepositoryArrangementImpl : ConversationReposito
     override fun withGetOneOnOneConversationsWithOtherUserReturning(result: Either<StorageFailure, List<QualifiedID>>) {
         given(conversationRepository)
             .suspendFunction(conversationRepository::getOneOnOneConversationsWithOtherUser)
+            .whenInvokedWith(any())
+            .thenReturn(result)
+    }
+
+    override fun withGetConversationByIdReturning(result: Conversation?) {
+        given(conversationRepository)
+            .suspendFunction(conversationRepository::getConversationById)
             .whenInvokedWith(any())
             .thenReturn(result)
     }
