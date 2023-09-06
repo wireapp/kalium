6425d555536c82f72d5614f96258660e6c3868f9
Jacob Persson <7156+typfel@users.noreply.github.com>
2023-09-06 13:19:41 +0200

    feat: resolve one-on-one conversaions when receiving a welcome message (#2033)

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/UserSessionScope.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/UserSessionScope.kt
@@ -1212,7 +1212,7 @@ class UserSessionScope internal constructor(
         )
     private val mlsWelcomeHandler: MLSWelcomeEventHandler
         get() = MLSWelcomeEventHandlerImpl(
-            mlsClientProvider, userStorage.database.conversationDAO, conversationRepository
+            mlsClientProvider, conversationRepository, oneOnOneResolver
         )
     private val renamedConversationHandler: RenamedConversationEventHandler
         get() = RenamedConversationEventHandlerImpl(
