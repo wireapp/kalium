c1fa26df57e9fab58d67360834e96694e7470f58
Jacob Persson <7156+typfel@users.noreply.github.com>
2023-09-07 21:09:00 +0200

    feat: resolve active 1-1 after accepting a connection request (#2042)

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/UserSessionScope.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/UserSessionScope.kt
@@ -1589,7 +1589,12 @@ class UserSessionScope internal constructor(
             kaliumConfigs
         )
 
-    val connection: ConnectionScope get() = ConnectionScope(connectionRepository, conversationRepository)
+    val connection: ConnectionScope
+        get() = ConnectionScope(
+            connectionRepository,
+            conversationRepository,
+            oneOnOneResolver
+        )
 
     val observeSecurityClassificationLabel: ObserveSecurityClassificationLabelUseCase
         get() = ObserveSecurityClassificationLabelUseCaseImpl(
