79206cad31ac882d905e39d025b5830e4182f84e
mohamad.jaara <mohamad.jaara@wire.com>
2023-09-13 16:07:56 +0200

    detekt

--- persistence/src/androidMain/kotlin/com/wire/kalium/persistence/db/support/SqliteCallback.kt
+++ persistence/src/androidMain/kotlin/com/wire/kalium/persistence/db/support/SqliteCallback.kt
@@ -24,7 +24,10 @@ import app.cash.sqldelight.db.QueryResult
 import app.cash.sqldelight.db.SqlSchema
 import app.cash.sqldelight.driver.android.AndroidSqliteDriver
 
-internal class SqliteCallback(schema: SqlSchema<QueryResult.Value<Unit>>, private val enableWAL: Boolean) : SupportSQLiteOpenHelper.Callback(schema.version.toInt()) {
+internal class SqliteCallback(
+    schema: SqlSchema<QueryResult.Value<Unit>>,
+    private val enableWAL: Boolean
+) : SupportSQLiteOpenHelper.Callback(schema.version.toInt()) {
     private val baseCallback = AndroidSqliteDriver.Callback(schema)
     override fun onCreate(db: SupportSQLiteDatabase) = baseCallback.onCreate(db)
 
