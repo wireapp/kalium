dcc34ce2176e992a604ba047e70ff6334450944c
Gonzalo Gran Crespo <gongracr@gmail.com>
2023-09-22 11:42:25 +0200

    feat: Add flag to decide whether to fetch archived conversations

--- cli/src/appleMain/kotlin/com/wire/kalium/cli/commands/Command.kt
+++ cli/src/appleMain/kotlin/com/wire/kalium/cli/commands/Command.kt
@@ -45,7 +45,7 @@ sealed class Command(
             }
 
         suspend fun prepare() {
-            conversations = userSession.conversations.observeConversationListDetails().first()
+            conversations = userSession.conversations.observeConversationListDetails(fetchArchivedConversations = true).first()
             updateFilter()
         }
 
