a3963e93edab851be13e6bb49ffe502bb381bc75
Vitor Hugo Schwaab <vitor@schwaab.dev>
2023-10-17 16:41:11 +0200

    fix: use correct auth token

--- network/src/commonMain/kotlin/com/wire/kalium/network/networkContainer/AuthenticatedNetworkContainer.kt
+++ network/src/commonMain/kotlin/com/wire/kalium/network/networkContainer/AuthenticatedNetworkContainer.kt
@@ -203,7 +203,7 @@ internal class AuthenticatedHttpClientProviderImpl(
 
     private val loadToken: suspend () -> BearerTokens? = {
         val session = sessionManager.session() ?: error("missing user session")
-        BearerTokens(accessToken = "Invalid", refreshToken = session.refreshToken)
+        BearerTokens(accessToken = session.accessToken, refreshToken = session.refreshToken)
     }
 
     private val refreshToken: suspend RefreshTokensParams.() -> BearerTokens = {
