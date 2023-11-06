66abde0880f969408a367f077456cb84b5713ae9
Yamil Medina <yamilmedina@users.noreply.github.com>
2023-11-06 12:04:51 -0300

    feat: configure supported min TLS versions (WPB-1810) (#2193)
    * fix: configure supported tls min versions
    
    * fix: configure supported tls min versions
    
    * fix: configure supported tls min versions
    
    * fix: configure supported tls min versions
    
    * Empty-Commitclear

---
+++ network/src/jvmTest/kotlin/com/wire/kalium/HttpClientConnectionSpecsTest.kt
@@ -0,0 +1,41 @@
+/*
+ * Wire
+ * Copyright (C) 2023 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ */
+package com.wire.kalium
+
+import com.wire.kalium.network.OkHttpSingleton
+import okhttp3.ConnectionSpec
+import okhttp3.TlsVersion
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class HttpClientConnectionSpecsTest {
+
+    @Test
+    fun givenTheHttpClientIsCreated_ThenEnsureOnlySupportedSpecsArePresent() {
+        val connectionSpecs = OkHttpSingleton.createNew {}.connectionSpecs
+        with(connectionSpecs[0]) {
+            tlsVersions?.let {
+                assertTrue(it.contains(TlsVersion.TLS_1_2) && it.contains(TlsVersion.TLS_1_3))
+                assertTrue(!it.contains(TlsVersion.TLS_1_1) && !it.contains(TlsVersion.TLS_1_0) && !it.contains(TlsVersion.SSL_3_0))
+            }
+        }
+
+        assertEquals(connectionSpecs[1], ConnectionSpec.CLEARTEXT)
+    }
+}
