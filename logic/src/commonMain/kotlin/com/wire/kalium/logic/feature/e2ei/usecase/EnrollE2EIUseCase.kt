bf09674563d2cc5247d0cf543c92f10422b51507
Mojtaba Chenani <chenani@outlook.com>
2023-11-14 13:40:48 +0100

    chore: refactor and added new error types for e2eiFailure

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/e2ei/usecase/EnrollE2EIUseCase.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/e2ei/usecase/EnrollE2EIUseCase.kt
@@ -25,7 +25,6 @@ import com.wire.kalium.logic.functional.Either
 import com.wire.kalium.logic.functional.fold
 import com.wire.kalium.logic.functional.onFailure
 import com.wire.kalium.logic.kaliumLogger
-import kotlinx.coroutines.delay
 
 /**
  * Issue an E2EI certificate and re-initiate the MLSClient
@@ -72,11 +71,9 @@ class EnrollE2EIUseCaseImpl internal constructor(
             return E2EIEnrollmentResult.Failed(E2EIEnrollmentResult.E2EIStep.AcmeNewAuthz, it).toEitherLeft()
         }, { it })
 
-        val oAuthTarget =
-            "https://idp.hogwash.work:5556/dex"// todo: replace with authzResponse.first.wireOidcChallenge!!.target after it fixed
-
-        val initializationResult =
-            E2EIEnrollmentResult.Initialized(oAuthTarget, authzResponse.first, authzResponse.second, newOrderResponse.third)
+        val initializationResult = E2EIEnrollmentResult.Initialized(
+            authzResponse.first.wireOidcChallenge!!.target, authzResponse.first, authzResponse.second, newOrderResponse.third
+        )
 
         kaliumLogger.i("E2EI Enrollment Initialization Result: $initializationResult")
 
@@ -106,28 +103,17 @@ class EnrollE2EIUseCaseImpl internal constructor(
 
         val dpopToken = e2EIRepository.getDPoPToken(wireNonce).fold({
             return E2EIEnrollmentResult.Failed(E2EIEnrollmentResult.E2EIStep.DPoPToken, it).toEitherLeft()
-        }, {
-            kaliumLogger.e("getDPoPToken:" + it.toString())
-            it
-        })
-
-        delay(5000)
+        }, { it })
 
         val wireAccessToken = e2EIRepository.getWireAccessToken(dpopToken).fold({
             return E2EIEnrollmentResult.Failed(E2EIEnrollmentResult.E2EIStep.WireAccessToken, it).toEitherLeft()
-        }, {
-            kaliumLogger.e("getWireAccessToken:" + it.toString())
-            it
-        })
+        }, { it })
 
         val dpopChallengeResponse = e2EIRepository.validateDPoPChallenge(
             wireAccessToken.token, prevNonce, authz.wireDpopChallenge!!
         ).fold({
             return E2EIEnrollmentResult.Failed(E2EIEnrollmentResult.E2EIStep.DPoPChallenge, it).toEitherLeft()
-        }, {
-            kaliumLogger.e("validateDPoPChallenge:" + it.toString())
-            it
-        })
+        }, { it })
 
         prevNonce = dpopChallengeResponse.nonce
 
@@ -135,37 +121,25 @@ class EnrollE2EIUseCaseImpl internal constructor(
             idToken, prevNonce, authz.wireOidcChallenge!!
         ).fold({
             return E2EIEnrollmentResult.Failed(E2EIEnrollmentResult.E2EIStep.OIDCChallenge, it).toEitherLeft()
-        }, {
-            kaliumLogger.e("validateOIDCChallenge:" + it.toString())
-            it
-        })
+        }, { it })
 
         prevNonce = oidcChallengeResponse.nonce
 
         val orderResponse = e2EIRepository.checkOrderRequest(orderLocation, prevNonce).fold({
             return E2EIEnrollmentResult.Failed(E2EIEnrollmentResult.E2EIStep.CheckOrderRequest, it).toEitherLeft()
-        }, {
-            kaliumLogger.e("checkOrderRequest:" + it.toString())
-            it
-        })
+        }, { it })
 
         prevNonce = orderResponse.first.nonce
 
         val finalizeResponse = e2EIRepository.finalize(orderResponse.second, prevNonce).fold({
             return E2EIEnrollmentResult.Failed(E2EIEnrollmentResult.E2EIStep.FinalizeRequest, it).toEitherLeft()
-        }, {
-            kaliumLogger.e("finalize:" + it.toString())
-            it
-        })
+        }, { it })
 
         prevNonce = finalizeResponse.first.nonce
 
         val certificateRequest = e2EIRepository.certificateRequest(finalizeResponse.second, prevNonce).fold({
             return E2EIEnrollmentResult.Failed(E2EIEnrollmentResult.E2EIStep.Certificate, it).toEitherLeft()
-        }, {
-            kaliumLogger.e("certificateRequest:" + it.toString())
-            it
-        })
+        }, { it })
 
         e2EIRepository.rotateKeysAndMigrateConversations(certificateRequest.response.decodeToString()).onFailure {
             return E2EIEnrollmentResult.Failed(E2EIEnrollmentResult.E2EIStep.ConversationMigration, it).toEitherLeft()
@@ -183,6 +157,7 @@ sealed interface E2EIEnrollmentResult {
         AcmeNewAccount,
         AcmeNewOrder,
         AcmeNewAuthz,
+        OAuth,
         WireNonce,
         DPoPToken,
         WireAccessToken,
@@ -203,6 +178,6 @@ sealed interface E2EIEnrollmentResult {
             return "E2EI enrollment failed at $step: with $failure"
         }
 
-        fun toEitherLeft() = Either.Left(E2EIFailure(Exception(this.toString())))
+        fun toEitherLeft() = Either.Left(E2EIFailure.Generic(Exception(this.toString())))
     }
 }
