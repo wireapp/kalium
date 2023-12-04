08c3f30d38415ead81ca159b777374e93bddf941
MohamadJaara <mohamad.jaara@wire.com>
2023-12-04 22:30:08 +0100

    add flag to fetchAllTeamMembersEagerly

--- testservice/src/main/kotlin/com/wire/kalium/testservice/managed/InstanceService.kt
+++ testservice/src/main/kotlin/com/wire/kalium/testservice/managed/InstanceService.kt
@@ -144,8 +144,7 @@ class InstanceService(
                 File.separator + ".testservice" + File.separator + instanceId
         log.info("Instance $instanceId: Creating $instancePath")
         val kaliumConfigs = KaliumConfigs(
-            developmentApiEnabled = instanceRequest.developmentApiEnabled ?: false,
-            fetchAllTeamMembersEagerly = instanceRequest.fetchAllTeamMembersEagerly ?: true
+            developmentApiEnabled = instanceRequest.developmentApiEnabled ?: false
         )
         val coreLogic = CoreLogic(instancePath, kaliumConfigs, userAgent)
         CoreLogger.init(KaliumLogger.Config(KaliumLogLevel.VERBOSE, listOf(KaliumLogWriter(instanceId))))
--- testservice/src/main/kotlin/com/wire/kalium/testservice/models/InstanceRequest.kt
+++ testservice/src/main/kotlin/com/wire/kalium/testservice/models/InstanceRequest.kt
@@ -31,5 +31,4 @@ data class InstanceRequest(
     val password: String = "",
     val verificationCode: String? = null,
     val developmentApiEnabled: Boolean? = false,
-    val fetchAllTeamMembersEagerly: Boolean? = true,
 )
