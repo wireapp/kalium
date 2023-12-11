c6008153d773888acdcee663b9011ab0dfcc103d
MohamadJaara <mohamad.jaara@wire.com>
2023-12-11 17:53:45 +0100

    fix tests

--- network/src/androidUnitTest/kotlin/com/wire/kalium/api/tools/HttpClientConnectionSpecsTest.kt
+++ network/src/androidUnitTest/kotlin/com/wire/kalium/api/tools/HttpClientConnectionSpecsTest.kt
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see http://www.gnu.org/licenses/.
  */
-package com.wire.kalium
+package com.wire.kalium.api.tools
 
 import com.wire.kalium.network.OkhttpClientFactory
 import okhttp3.CipherSuite
