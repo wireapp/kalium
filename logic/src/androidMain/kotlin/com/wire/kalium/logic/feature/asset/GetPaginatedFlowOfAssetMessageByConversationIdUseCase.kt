10e2eec72c639bd680db417f2c778266dd4520ba
Alexandre Ferris <ferris.alexandre@gmail.com>
2023-12-28 10:33:23 +0100

    feat: asset media files (WPB-5378) (#2322)
    * feat: add new query to get assets without images
    
    * feat: add new usecase for assets only without images
    
    * feat: add missing implementation of dao
    
    * feat: add property NOT IN in sql query and rename mapping function
    
    * feat: add nullable to AssetMessageEntity width and height of asset
    
    * feat: add nullable to AssetMessageEntity width and height of asset
    
    * chore: revert changes to nullable parameters
    
    * feat: get assets from message details view instead of assets due to need of user data
    
    * feat: adjust return type of usecase and repository
    
    * feat: add count query for asset messages without images
    
    * chore: remove get asset messages by conversation id without images
    
    * feat: add usecase and repository extension for paginated asset messages without images
    
    * chore: remove unused usecase from message scope
    
    * chore: remove unused usecase from message scope
    
    * chore: remove receiving mimetype to paginated usecase
    
    * chore: move unique asset scope to message scope
    
    * chore: add logs
    
    * chore: remove logs
    
    * chore: fix detekt
    
    * test: fix existing tests
    
    * fix: media image assets [WPB-5848] (#2332)
    
    * fix: load only downloaded image media assets
    
    ---------
    
    Co-authored-by: Jakub Żerko <iot.zerko@gmail.com>

---
+++ logic/src/androidMain/kotlin/com/wire/kalium/logic/feature/asset/GetPaginatedFlowOfAssetMessageByConversationIdUseCase.kt
@@ -0,0 +1,47 @@
+/*
+ * Wire
+ * Copyright (C) 2023 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ */
+package com.wire.kalium.logic.feature.asset
+
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.wire.kalium.logic.data.id.ConversationId
+import com.wire.kalium.logic.data.message.Message
+import com.wire.kalium.logic.data.message.MessageRepository
+import com.wire.kalium.util.KaliumDispatcher
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
+
+/**
+ * This use case will observe and return a flow of paginated asset messages for a given conversation.
+ * @see PagingData
+ * @see Message
+ */
+class GetPaginatedFlowOfAssetMessageByConversationIdUseCase internal constructor(
+    private val dispatcher: KaliumDispatcher,
+    private val messageRepository: MessageRepository
+) {
+    suspend operator fun invoke(
+        conversationId: ConversationId,
+        startingOffset: Long,
+        pagingConfig: PagingConfig
+    ): Flow<PagingData<Message.Standalone>> = messageRepository.extensions.getPaginatedMessageAssetsWithoutImageByConversationId(
+        conversationId = conversationId,
+        pagingConfig = pagingConfig,
+        startingOffset = startingOffset
+    ).flowOn(dispatcher.io)
+}
--- logic/src/commonTest/kotlin/com/wire/kalium/logic/feature/asset/GetAssetMessagesForConversationUseCaseTest.kt
+++ logic/src/commonTest/kotlin/com/wire/kalium/logic/feature/asset/GetAssetMessagesForConversationUseCaseTest.kt
@@ -75,7 +75,7 @@ class GetAssetMessagesForConversationUseCaseTest {
 
         // Then
         verify(arrangement.messageRepository)
-            .suspendFunction(arrangement.messageRepository::getAssetMessagesByConversationId)
+            .suspendFunction(arrangement.messageRepository::getImageAssetMessagesByConversationId)
             .with(eq(someConversationId), eq(limit), eq(offset))
             .wasInvoked(exactly = once)
     }
@@ -84,7 +84,7 @@ class GetAssetMessagesForConversationUseCaseTest {
         @Mock
         val messageRepository = mock(classOf<MessageRepository>())
 
-        val getAssetMessagesByConversationUseCase = GetAssetMessagesForConversationUseCaseImpl(
+        val getAssetMessagesByConversationUseCase = GetImageAssetMessagesForConversationUseCaseImpl(
             testDispatcher,
             messageRepository
         )
@@ -96,7 +96,7 @@ class GetAssetMessagesForConversationUseCaseTest {
             offset: Int
         ): Arrangement = apply {
             given(messageRepository)
-                .suspendFunction(messageRepository::getAssetMessagesByConversationId)
+                .suspendFunction(messageRepository::getImageAssetMessagesByConversationId)
                 .whenInvokedWith(eq(conversationId), eq(limit), eq(offset))
                 .thenReturn(assetList)
         }
