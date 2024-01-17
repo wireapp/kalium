1520b1fe232d786dcc7b47f3729ff1eed182e4ec
MohamadJaara <mohamad.jaara@wire.com>
2024-01-17 09:38:37 +0100

    expose the parser in search scope

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/search/SearchScope.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/search/SearchScope.kt
@@ -18,11 +18,15 @@
 package com.wire.kalium.logic.feature.search
 
 import com.wire.kalium.logic.data.publicuser.SearchUserRepository
+import com.wire.kalium.logic.data.session.SessionRepository
 import com.wire.kalium.logic.data.user.UserId
 
 class SearchScope internal constructor(
     private val searchUserRepository: SearchUserRepository,
+    private val sessionRepository: SessionRepository,
     private val selfUserId: UserId
 ) {
     val searchUsersUseCase: SearchUsersUseCase get() = SearchUsersUseCase(searchUserRepository, selfUserId)
+
+    val federatedSearchParser: FederatedSearchParser get() = FederatedSearchParser(sessionRepository, selfUserId)
 }
