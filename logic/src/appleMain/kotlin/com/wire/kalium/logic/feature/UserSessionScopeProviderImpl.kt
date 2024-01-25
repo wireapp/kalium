27f3e8e18b39ccfac473f694294a3c23fbd6e9fe
MohamadJaara <mohamad.jaara@wire.com>
2024-01-25 11:30:10 +0100

    fix ios target

--- logic/src/appleMain/kotlin/com/wire/kalium/logic/feature/UserSessionScopeProviderImpl.kt
+++ logic/src/appleMain/kotlin/com/wire/kalium/logic/feature/UserSessionScopeProviderImpl.kt
@@ -32,6 +32,7 @@ import com.wire.kalium.logic.feature.call.GlobalCallManager
 import com.wire.kalium.logic.featureFlags.KaliumConfigs
 import com.wire.kalium.network.NetworkStateObserver
 import com.wire.kalium.logic.sync.UserSessionWorkSchedulerImpl
+import com.wire.kalium.persistence.db.GlobalDatabaseProvider
 import com.wire.kalium.persistence.kmmSettings.GlobalPrefProvider
 
 @Suppress("LongParameterList")
@@ -42,6 +43,7 @@ internal actual class UserSessionScopeProviderImpl(
     private val kaliumConfigs: KaliumConfigs,
     private val globalPreferences: GlobalPrefProvider,
     private val globalCallManager: GlobalCallManager,
+    private val globalDatabaseProvider: GlobalDatabaseProvider,
     private val userStorageProvider: UserStorageProvider,
     private val networkStateObserver: NetworkStateObserver,
     userAgent: String
@@ -60,6 +62,7 @@ internal actual class UserSessionScopeProviderImpl(
             globalScope,
             globalCallManager,
             globalPreferences,
+            globalDatabaseProvider,
             authenticationScopeProvider,
             userSessionWorkScheduler,
             rootPathsProvider,
