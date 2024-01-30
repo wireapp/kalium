dd939876ff2d6ba76230cddb54152f70a68f25a3
Yamil Medina <yamilmedina@users.noreply.github.com>
2024-01-30 15:40:03 +0100

    chore: bring back architecture test after being ignored (#2431)

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/data/e2ei/E2EIRepository.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/data/e2ei/E2EIRepository.kt
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see http://www.gnu.org/licenses/.
  */
-@file:Suppress("TooManyFunctions")
+@file:Suppress("konsist.repositoriesShouldNotAccessFeaturePackageClasses", "TooManyFunctions")
 
 package com.wire.kalium.logic.data.e2ei
 
