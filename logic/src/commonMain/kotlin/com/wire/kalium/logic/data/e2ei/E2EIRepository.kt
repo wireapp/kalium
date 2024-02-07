84206b8844ce97f68d48abff56fb3597d6ff5c0b
Mojtaba Chenani <chenani@outlook.com>
2024-02-07 08:48:07 +0100

    remove un-needed changes

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/data/e2ei/E2EIRepository.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/data/e2ei/E2EIRepository.kt
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see http://www.gnu.org/licenses/.
  */
-@file:Suppress("konsist.repositoriesShouldNotAccessFeaturePackageClasses", "TooManyFunctions")
+@file:Suppress("TooManyFunctions")
 
 package com.wire.kalium.logic.data.e2ei
 
