56f1385a2985a8ba95f21fcb6f699694049ac9a4
MohamadJaara <mohamad.jaara@wire.com>
2024-02-17 09:41:33 +0100

    detekt

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/CoreFailure.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/CoreFailure.kt
@@ -192,8 +192,8 @@ interface MLSFailure : CoreFailure {
 
 interface E2EIFailure : CoreFailure {
 
-    data object Disabled: E2EIFailure
-    data object MissingDiscoveryUrl: E2EIFailure
+    data object Disabled : E2EIFailure
+    data object MissingDiscoveryUrl : E2EIFailure
     data class FailedInitialization(val step: E2EIEnrollmentResult.E2EIStep) : E2EIFailure
     data class FailedOAuth(val reason: String) : E2EIFailure
     data class FailedFinalization(val step: E2EIEnrollmentResult.E2EIStep) : E2EIFailure
