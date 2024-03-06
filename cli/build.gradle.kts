ad1a4920590b47a313d1b55afbf9d297c1b22115
Vitor Hugo Schwaab <vitor@schwaab.dev>
2024-03-06 15:06:17 +0100

    style: remove unused import

--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -18,7 +18,6 @@
 
 import com.wire.kalium.plugins.commonDokkaConfig
 import com.wire.kalium.plugins.commonJvmConfig
-import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 
 @Suppress("DSL_SCOPE_VIOLATION")
 
