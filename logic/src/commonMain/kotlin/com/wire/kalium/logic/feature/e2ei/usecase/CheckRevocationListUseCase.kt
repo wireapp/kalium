dad551e429a7b96419db582f124aaa2839cb0060
Boris Safonov <boris.safonov@appunite.com>
2024-03-14 16:45:50 +0200

    Fixed merge conflict

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/e2ei/usecase/CheckRevocationListUseCase.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/e2ei/usecase/CheckRevocationListUseCase.kt
@@ -22,10 +22,6 @@ import com.wire.kalium.logic.E2EIFailure
 import com.wire.kalium.logic.data.client.MLSClientProvider
 import com.wire.kalium.logic.data.e2ei.CertificateRevocationListRepository
 import com.wire.kalium.logic.data.id.CurrentClientIdProvider
-<<<<<<< HEAD
-import com.wire.kalium.logic.feature.conversation.MLSConversationsVerificationStatusesHandler
-=======
->>>>>>> 5286fd1d04 (fix: Crash by loop dependence (#2607))
 import com.wire.kalium.logic.feature.user.IsE2EIEnabledUseCase
 import com.wire.kalium.logic.functional.Either
 import com.wire.kalium.logic.functional.flatMap
