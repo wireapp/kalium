38737b00fa03e0b209fbc1d5c6ab8c90d848191f
yamilmedina <yamilmedina@users.noreply.github.com>
2024-05-04 14:57:12 +0200

    chore: cleanup

--- persistence/build.gradle.kts
+++ persistence/build.gradle.kts
@@ -86,7 +86,7 @@ kotlin {
         }
         val jvmMain by getting {
             dependencies {
-                implementation(libs.sqldelight.jvmDriver) // rename to something else ? as jvm should be parametrized
+                implementation(libs.sqldelight.jvmDriver)
                 implementation(libs.sqldelight.jdbcDriver)
                 implementation(libs.hikaricp)
                 implementation(libs.postgres.driver)
