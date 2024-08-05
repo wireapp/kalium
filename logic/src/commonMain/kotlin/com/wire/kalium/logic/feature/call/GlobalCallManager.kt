58b389664940651436132a1d23d2d44754350284
Oussama Hassine <oussama.has100@gmail.com>
2024-08-05 11:14:13 +0200

    feat: terminate the SFT OneOnOneCall once the other person hangup the call (WPB-7153) (#2923)
    * chore: resolve conflicts
    
    * chore: unit test
    
    * chore: resolve conflicts
    
    * chore: unit test
    
    * chore: unit test
    
    * feat: terminate the SFT OneOnOneCall once the other person hangup the call
    
    * chore: unit test
    
    * chore: unit test
    
    * chore: typo
    
    ---------
    
    Co-authored-by: Mohamad Jaara <mohamad.jaara@wire.com>

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/call/GlobalCallManager.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/call/GlobalCallManager.kt
@@ -19,10 +19,12 @@
 package com.wire.kalium.logic.feature.call
 
 import com.wire.kalium.logic.cache.SelfConversationIdProvider
+import com.wire.kalium.logic.configuration.UserConfigRepository
 import com.wire.kalium.logic.data.call.mapper.CallMapper
 import com.wire.kalium.logic.data.call.CallRepository
 import com.wire.kalium.logic.data.call.VideoStateChecker
 import com.wire.kalium.logic.data.conversation.ConversationRepository
+import com.wire.kalium.logic.data.conversation.SubconversationRepository
 import com.wire.kalium.logic.data.id.FederatedIdMapper
 import com.wire.kalium.logic.data.id.QualifiedID
 import com.wire.kalium.logic.data.id.QualifiedIdMapper
@@ -44,6 +46,8 @@ expect class GlobalCallManager {
         currentClientIdProvider: CurrentClientIdProvider,
         selfConversationIdProvider: SelfConversationIdProvider,
         conversationRepository: ConversationRepository,
+        subconversationRepository: SubconversationRepository,
+        userConfigRepository: UserConfigRepository,
         messageSender: MessageSender,
         callMapper: CallMapper,
         federatedIdMapper: FederatedIdMapper,
