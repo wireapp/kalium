b45ab2367a464cfe9f6ad9ada61e4d4a595f4bb6
yamilmedina <yamilmedina@users.noreply.github.com>
2024-11-11 18:18:37 +0100

    bump avs to v10 with simulcast support and extract Json serializer

--- data/src/commonMain/kotlin/com/wire/kalium/logic/data/call/CallClient.kt
+++ data/src/commonMain/kotlin/com/wire/kalium/logic/data/call/CallClient.kt
@@ -18,21 +18,28 @@
 
 package com.wire.kalium.logic.data.call
 
+import com.wire.kalium.util.serialization.LenientJsonSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.Json
 
 @Serializable
 data class CallClient(
     @SerialName("userid") val userId: String,
     @SerialName("clientid") val clientId: String,
-    @SerialName("in_subconv") val isMemberOfSubconversation: Boolean = false
+    @SerialName("in_subconv") val isMemberOfSubconversation: Boolean = false,
+    @SerialName("quality") val quality: CallQuality = CallQuality.ANY
 )
 
 @Serializable
 data class CallClientList(
     @SerialName("clients") val clients: List<CallClient>
 ) {
-    // TODO(optimization): Use a shared Json instance instead of creating one every time.
-    fun toJsonString(): String = Json { isLenient = true }.encodeToString(serializer(), this)
+    fun toJsonString(): String = LenientJsonSerializer.json.encodeToString(serializer(), this)
+}
+
+@Serializable
+enum class CallQuality {
+    ANY,
+    LOW,
+    HIGH
 }
