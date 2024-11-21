646017f2dbad2fad84f6d837436682d5aab07def
ohassine <oussama.has100@gmail.com>
2024-11-21 14:41:55 +0100

    chore: cleanup

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/user/UserScope.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/user/UserScope.kt
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see http://www.gnu.org/licenses/.
  */
-@file:Suppress("konsist.useCasesShouldNotAccessDaoLayerDirectly")
+@file:Suppress("konsist.useCasesShouldNotAccessDaoLayerDirectly", "konsist.useCasesShouldNotAccessNetworkLayerDirectly")
 
 package com.wire.kalium.logic.feature.user
 
