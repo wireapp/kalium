0b937572fd7415f3d30e69fb500e3570caebd264
ohassine <oussama.has100@gmail.com>
2024-11-21 13:57:26 +0100

    chore: cleanup

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/personaltoteamaccount/CanMigrateFromPersonalToTeamUseCase.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/personaltoteamaccount/CanMigrateFromPersonalToTeamUseCase.kt
@@ -15,6 +15,9 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see http://www.gnu.org/licenses/.
  */
+
+@file:Suppress("konsist.useCasesShouldNotAccessNetworkLayerDirectly")
+
 package com.wire.kalium.logic.feature.personaltoteamaccount
 
 import com.wire.kalium.logic.configuration.server.ServerConfigRepository
