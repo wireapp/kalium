28cc9661c0c0a7b2567f4daed48fb5ccb45a95e4
yamilmedina <yamilmedina@users.noreply.github.com>
2024-11-25 15:42:50 +0100

    chore: use case template

---
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/search/IsFederationSearchAllowedUseCase.kt
@@ -0,0 +1,66 @@
+/*
+ * Wire
+ * Copyright (C) 2024 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ */
+package com.wire.kalium.logic.feature.search
+
+import com.wire.kalium.logic.data.conversation.Conversation
+import com.wire.kalium.logic.data.id.ConversationId
+import com.wire.kalium.logic.data.mlspublickeys.MLSPublicKeysRepository
+import com.wire.kalium.logic.data.user.SupportedProtocol
+import com.wire.kalium.logic.feature.conversation.GetConversationProtocolInfoUseCase
+import com.wire.kalium.logic.feature.user.GetDefaultProtocolUseCase
+import com.wire.kalium.logic.functional.isRight
+import com.wire.kalium.util.KaliumDispatcher
+import com.wire.kalium.util.KaliumDispatcherImpl
+import kotlinx.coroutines.withContext
+
+interface IsFederationSearchAllowedUseCase {
+    suspend operator fun invoke(conversationId: ConversationId?): Boolean
+}
+
+@Suppress("FunctionNaming")
+internal fun IsFederationSearchAllowedUseCase(
+    mlsPublicKeysRepository: MLSPublicKeysRepository,
+    getDefaultProtocol: GetDefaultProtocolUseCase,
+    getConversationProtocolInfo: GetConversationProtocolInfoUseCase,
+    dispatcher: KaliumDispatcher = KaliumDispatcherImpl
+) = object : IsFederationSearchAllowedUseCase {
+
+    override suspend operator fun invoke(conversationId: ConversationId?): Boolean = withContext(dispatcher.io) {
+        val isMLSConfigured = mlsPublicKeysRepository.getKeys().isRight()
+
+//         when ()
+
+
+        true
+    }
+
+    private suspend fun isProtocolAbleToFederate(conversationId: ConversationId?): Boolean {
+        val isProteusTeam = getDefaultProtocol() == SupportedProtocol.PROTEUS
+        val isOtherDomainAllowed: Boolean = conversationId?.let {
+            when (val result = getConversationProtocolInfo(it)) {
+                is GetConversationProtocolInfoUseCase.Result.Failure -> !isProteusTeam
+
+                is GetConversationProtocolInfoUseCase.Result.Success ->
+                    !isProteusTeam && result.protocolInfo !is Conversation.ProtocolInfo.Proteus
+            }
+        } ?: !isProteusTeam
+
+        return isOtherDomainAllowed
+    }
+
+}
