25fd18f5c78efdcde894ea81f2ec71f062b3f131
Yamil Medina <yamilmedina@users.noreply.github.com>
2024-11-27 05:53:59 -0300

    feat: support Proteus federation if MLS not supported by backend (WPB-14250) (#3126)
    * chore: use case template
    
    * feat: usecase for allowing federated search
    
    * feat: add usecase to check for federation search allowance
    
    * feat: cleanup
    
    * feat: adjust tests to new implementation
    
    * feat: adjust tests to new implementation

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/UserSessionScope.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/UserSessionScope.kt
@@ -1903,6 +1903,9 @@ class UserSessionScope internal constructor(
 
     val search: SearchScope by lazy {
         SearchScope(
+            mlsPublicKeysRepository = mlsPublicKeysRepository,
+            getDefaultProtocol = getDefaultProtocol,
+            getConversationProtocolInfo = conversations.getConversationProtocolInfo,
             searchUserRepository = searchUserRepository,
             selfUserId = userId,
             sessionRepository = globalScope.sessionRepository,
