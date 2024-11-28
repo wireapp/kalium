c5c24685027a629022cabe70750ff62d7edc48ff
Yamil Medina <yamilmedina@users.noreply.github.com>
2024-11-28 11:22:47 -0300

    chore: bulletproofing crypto box to cc migration (WPB-14250) (🍒4.6) (#3136)
    * chore: manual cherry pick of #3129
    
    * fix: detekt
    
    * chore: test mockative older version
    
    * chore: empty commit bump
    
    * chore: test mockative older version
    
    * chore: update lib
    
    * chore: empty commit bump
    
    * chore: fix kalium ref
    
    * chore: fix test
    
    * chore: fix test

---
+++ logic/src/jvmTest/kotlin/com/wire/kalium/logic/data/client/ProteusClientProviderTest.kt
@@ -0,0 +1,80 @@
+package com.wire.kalium.logic.data.client
+
+import com.wire.kalium.cryptography.exceptions.ProteusStorageMigrationException
+import com.wire.kalium.logic.featureFlags.KaliumConfigs
+import com.wire.kalium.logic.framework.TestUser
+import com.wire.kalium.persistence.dbPassphrase.PassphraseStorage
+import com.wire.kalium.util.FileUtil
+import com.wire.kalium.util.KaliumDispatcherImpl
+import io.mockative.Mock
+import io.mockative.any
+import io.mockative.mock
+import io.mockative.once
+import io.mockative.verify
+import io.mockative.given
+import kotlinx.coroutines.test.runTest
+import org.junit.Ignore
+import org.junit.Test
+import java.nio.file.Paths
+import kotlin.io.path.createDirectory
+import kotlin.io.path.createFile
+import kotlin.io.path.exists
+
+class ProteusClientProviderTest {
+
+    @Ignore("Old version of the testing library, wont fix")
+    @Test
+    fun givenGettingOrCreatingAProteusClient_whenMigrationPerformedAndFails_thenCatchErrorAndStartRecovery() = runTest {
+        // given
+        val (arrangement, proteusClientProvider) = Arrangement()
+            .withCorruptedProteusStorage()
+            .arrange()
+
+        // when - then
+        try {
+            proteusClientProvider.getOrCreate()
+        } catch (e: ProteusStorageMigrationException) {
+            verify(arrangement.proteusMigrationRecoveryHandler)
+                .coroutine { clearClientData({}) }
+                .wasInvoked(exactly = once)
+        }
+    }
+
+    private class Arrangement {
+
+        @Mock
+        val passphraseStorage = mock(PassphraseStorage::class)
+
+        @Mock
+        val proteusMigrationRecoveryHandler = mock(ProteusMigrationRecoveryHandler::class)
+
+        init {
+            given(passphraseStorage)
+                .suspendFunction(passphraseStorage::getPassphrase)
+                .whenInvokedWith(any<String>())
+                .thenReturn("passphrase")
+        }
+
+        /**
+         * Corrupted because it's just an empty file called "prekeys".
+         * But nothing to migrate, this is just to test that we are calling recovery.
+         */
+        fun withCorruptedProteusStorage() = apply {
+            val rootProteusPath = Paths.get("/tmp/rootProteusPath")
+            if (rootProteusPath.exists()) {
+                FileUtil.deleteDirectory(rootProteusPath.toString())
+            }
+            rootProteusPath.createDirectory()
+            rootProteusPath.resolve("prekeys").createFile()
+        }
+
+        fun arrange() = this to ProteusClientProviderImpl(
+            rootProteusPath = "/tmp/rootProteusPath",
+            userId = TestUser.USER_ID,
+            passphraseStorage = passphraseStorage,
+            kaliumConfigs = KaliumConfigs(encryptProteusStorage = true),
+            dispatcher = KaliumDispatcherImpl,
+            proteusMigrationRecoveryHandler = proteusMigrationRecoveryHandler
+        )
+    }
+}
---
+++ logic/src/jvmTest/kotlin/com/wire/kalium/logic/feature/client/ProteusMigrationRecoveryHandlerTest.kt
@@ -0,0 +1,38 @@
+package com.wire.kalium.logic.feature.client
+
+import com.wire.kalium.logic.data.logout.LogoutReason
+import com.wire.kalium.logic.feature.auth.LogoutUseCase
+import io.mockative.Mock
+import io.mockative.mock
+import io.mockative.once
+import io.mockative.verify
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+
+class ProteusMigrationRecoveryHandlerTest {
+
+    @Test
+    fun givenGettingOrCreatingAProteusClient_whenMigrationPerformedAndFails_thenCatchErrorAndStartRecovery() = runTest {
+        // given
+        val (arrangement, proteusMigrationRecoveryHandler) = Arrangement().arrange()
+
+        // when
+        val clearLocalFiles: suspend () -> Unit = { }
+        proteusMigrationRecoveryHandler.clearClientData(clearLocalFiles)
+
+        // then
+        verify(arrangement.logoutUseCase)
+            .coroutine { invoke(LogoutReason.MIGRATION_TO_CC_FAILED, true) }
+            .wasInvoked(exactly = once)
+    }
+
+    private class Arrangement {
+
+        @Mock
+        val logoutUseCase = mock(LogoutUseCase::class)
+
+        fun arrange() = this to ProteusMigrationRecoveryHandlerImpl(
+            lazy { logoutUseCase }
+        )
+    }
+}
