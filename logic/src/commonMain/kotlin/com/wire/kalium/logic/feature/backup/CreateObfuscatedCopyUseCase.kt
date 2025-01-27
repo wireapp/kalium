0074cb07dbfc592fbc315c0134210231d3585d1f
Mohamad Jaara <mohamad.jaara@wire.com>
2025-01-27 11:36:56 +0100

    detekt

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/backup/CreateObfuscatedCopyUseCase.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/backup/CreateObfuscatedCopyUseCase.kt
@@ -15,6 +15,10 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see http://www.gnu.org/licenses/.
  */
+
+// will suppress since this class is for debugging only
+@file:Suppress("konsist.useCasesShouldNotAccessDaoLayerDirectly")
+
 package com.wire.kalium.logic.feature.backup
 
 import com.wire.kalium.cryptography.backup.BackupCoder
