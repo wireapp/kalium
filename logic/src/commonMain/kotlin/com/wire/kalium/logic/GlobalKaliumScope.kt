626a7adb1a4a0809080e5134ef4053568f021e05
Michał Saleniuk <30429749+saleniuk@users.noreply.github.com>
2025-01-30 17:01:54 +0100

    feat: add use case to observe valid sessions [WPB-14338] (#3263)
    * feat: add use case to observe valid sessions
    
    * revert name changes
    
    * fix detekt
    
    * revert name changes

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/GlobalKaliumScope.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/GlobalKaliumScope.kt
@@ -60,6 +60,7 @@ import com.wire.kalium.logic.feature.server.UpdateApiVersionsUseCaseImpl
 import com.wire.kalium.logic.feature.session.DeleteSessionUseCase
 import com.wire.kalium.logic.feature.session.DoesValidSessionExistUseCase
 import com.wire.kalium.logic.feature.session.GetSessionsUseCase
+import com.wire.kalium.logic.feature.session.ObserveSessionsUseCase
 import com.wire.kalium.logic.feature.session.SessionScope
 import com.wire.kalium.logic.feature.user.ObserveValidAccountsUseCase
 import com.wire.kalium.logic.feature.user.ObserveValidAccountsUseCaseImpl
@@ -136,6 +137,7 @@ class GlobalKaliumScope internal constructor(
         get() =
             AddAuthenticatedUserUseCase(sessionRepository, globalDatabase.serverConfigurationDAO)
     val getSessions: GetSessionsUseCase get() = GetSessionsUseCase(sessionRepository)
+    val observeSessions: ObserveSessionsUseCase get() = ObserveSessionsUseCase(sessionRepository)
     val doesValidSessionExist: DoesValidSessionExistUseCase get() = DoesValidSessionExistUseCase(sessionRepository)
     val observeValidAccounts: ObserveValidAccountsUseCase
         get() = ObserveValidAccountsUseCaseImpl(sessionRepository, userSessionScopeProvider.value)
