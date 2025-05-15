9db8eec1b8b0c8ebe2c7915ee90ef30562cb3c6c
sergei.bakhtiarov <sbakhtiarov@gmail.com>
2025-05-15 14:02:48 +0200

    chore: adjust backup file name, set read status for restored messages (#WPB-10579)

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/backup/BackupConstants.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/backup/BackupConstants.kt
@@ -19,7 +19,7 @@
 package com.wire.kalium.logic.feature.backup
 
 object BackupConstants {
-    const val BACKUP_FILE_NAME_PREFIX = "WBX"
+    const val BACKUP_FILE_NAME_PREFIX = "Wire"
     const val BACKUP_ENCRYPTED_FILE_NAME = "user-backup.cc20"
 
     // BACKUP_METADATA_FILE_NAME and BACKUP_USER_DB_NAME must not be changed
@@ -46,7 +46,7 @@ object BackupConstants {
     )
 
     fun createBackupFileName(userHandle: String?, timestampIso: String) = // file names cannot have special characters
-        "$BACKUP_FILE_NAME_PREFIX-$userHandle-${timestampIso.replace(":", "-")}.zip"
+        "$BACKUP_FILE_NAME_PREFIX-$userHandle-${timestampIso.replace(":", "-")}.wbu"
 
     val ACCEPTED_EXTENSIONS = listOf(
         BACKUP_ENCRYPTED_EXTENSION,
