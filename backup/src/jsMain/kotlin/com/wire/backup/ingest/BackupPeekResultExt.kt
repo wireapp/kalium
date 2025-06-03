25a1f323c2ab352c9d5c4425d4a70d8db83f950c
Sergey Bakhtiarov <sbakhtiarov@gmail.com>
2025-06-03 12:31:40 +0200

    feat: cross platform backup (#WPB-10579) (#3431)
    * feat: cross platform backup (#WPB-10579)
    
    * feat: cross platform backup unit tests and fixes (#WPB-10579)
    
    * chore: make MPBackupImporter and Exporter compatible with default-parameter constructors on ObjC
    
    * refactor(backup): replace kotlin-native-coroutines with skie
    
    Removed the use of `kotlin-native-coroutines` from multiple backup-related files and replaced it with `skie`. Updated the Gradle configuration accordingly to ensure compatibility with the new library.
    
    * docs: update backup sample
    
    * chore: cleanup after backup restore, updated error handling (#WPB-10579)
    
    * chore: adjust backup file name, set read status for restored messages (#WPB-10579)
    
    * fix(backup): pages are no longer closed before reading
    
    * test(backup): use fake file system
    
    * chore(samples): add okio dependency
    
    * chore(backup): bump to 0.0.2
    
    * chore(backup): bump to 0.0.3
    
    * feat(backup): add user verification on JS/TS
    
    * chore(backup): prepare for next release
    
    * feat: cross platform backup feature flag (#WPB-10579)
    
    ---------
    
    Co-authored-by: Vitor Hugo Schwaab <vitor@schwaab.dev>

--- backup/src/jsMain/kotlin/com/wire/backup/ingest/BackupPeekResultExt.kt
+++ backup/src/jsMain/kotlin/com/wire/backup/ingest/BackupPeekResultExt.kt
@@ -1,6 +1,6 @@
 /*
  * Wire
- * Copyright (C) 2024 Wire Swiss GmbH
+ * Copyright (C) 2025 Wire Swiss GmbH
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,15 +15,15 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see http://www.gnu.org/licenses/.
  */
-package com.wire.backup.dump
+package com.wire.backup.ingest
 
-import okio.Path
+import com.wire.backup.data.BackupQualifiedId
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.promise
+import kotlin.js.Promise
 
-public fun interface FileZipper {
-    /**
-     * Joins all file entries into a single zip archive.
-     * Returns the path to the zipped archived.
-     */
-    @Throws(Throwable::class)
-    public fun zip(entries: List<String>, outputDirectory: Path): String
+@JsExport
+@JsName("isCreatedBySameUser")
+public fun BackupPeekResult.Success.isFromSameUser(backupQualifiedId: BackupQualifiedId): Promise<Boolean> = GlobalScope.promise {
+    isCreatedBySameUser(backupQualifiedId)
 }
--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/backup/VerifyBackupUseCase.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/backup/VerifyBackupUseCase.kt
@@ -18,14 +18,23 @@
 
 package com.wire.kalium.logic.feature.backup
 
+import com.wire.backup.ingest.BackupPeekResult
+import com.wire.backup.ingest.isCreatedBySameUser
 import com.wire.kalium.common.error.CoreFailure
-import com.wire.kalium.logic.data.asset.KaliumFileSystem
 import com.wire.kalium.common.functional.fold
+import com.wire.kalium.common.logger.kaliumLogger
+import com.wire.kalium.logic.data.asset.KaliumFileSystem
+import com.wire.kalium.logic.data.user.UserId
+import com.wire.kalium.logic.feature.backup.mapper.toBackupQualifiedId
+import com.wire.kalium.logic.feature.backup.provider.MPBackupImporterProvider
+import com.wire.kalium.logic.feature.backup.provider.MPBackupImporterProviderImpl
 import com.wire.kalium.logic.util.checkIfCompressedFileContainsFileTypes
 import com.wire.kalium.util.KaliumDispatcher
 import com.wire.kalium.util.KaliumDispatcherImpl
 import kotlinx.coroutines.withContext
+import okio.FileSystem
 import okio.Path
+import okio.SYSTEM
 
 interface VerifyBackupUseCase {
     /**
@@ -37,44 +46,85 @@ interface VerifyBackupUseCase {
 }
 
 internal class VerifyBackupUseCaseImpl(
+    private val userId: UserId,
     private val kaliumFileSystem: KaliumFileSystem,
+    private val fileSystem: FileSystem = FileSystem.SYSTEM,
+    private val backupImporterProvider: MPBackupImporterProvider = MPBackupImporterProviderImpl(fileSystem),
     private val dispatchers: KaliumDispatcher = KaliumDispatcherImpl
 ) : VerifyBackupUseCase {
 
     override suspend operator fun invoke(compressedBackupFilePath: Path): VerifyBackupResult = withContext(dispatchers.io) {
-        checkIfCompressedFileContainsFileTypes(
-            compressedBackupFilePath,
-            kaliumFileSystem,
-            BackupConstants.ACCEPTED_EXTENSIONS
-        ).fold({
+        when (val result = verifyMpBackupFile(compressedBackupFilePath)) {
+            VerifyBackupResult.Failure.InvalidBackupFile -> verifyRegularBackupFile(compressedBackupFilePath)
+            else -> result
+        }
+    }
+
+    private fun verifyRegularBackupFile(
+        compressedBackupFilePath: Path,
+    ) = checkIfCompressedFileContainsFileTypes(
+        compressedBackupFilePath,
+        kaliumFileSystem,
+        BackupConstants.ACCEPTED_EXTENSIONS
+    ).fold(
+        {
             VerifyBackupResult.Failure.Generic(it)
-        }, {
+        },
+        { result ->
             when {
-                it.keys.any { it !in BackupConstants.ACCEPTED_EXTENSIONS } -> VerifyBackupResult.Failure.InvalidBackupFile
+                result.keys.any { it !in BackupConstants.ACCEPTED_EXTENSIONS } -> VerifyBackupResult.Failure.InvalidBackupFile
 
-                it[BackupConstants.BACKUP_ENCRYPTED_EXTENSION] == true ->
-                    VerifyBackupResult.Success.Encrypted
+                result[BackupConstants.BACKUP_ENCRYPTED_EXTENSION] == true ->
+                    VerifyBackupResult.AndroidBackup(true)
 
-                it[BackupConstants.BACKUP_DB_EXTENSION] == true && it[BackupConstants.BACKUP_METADATA_EXTENSION] == true ->
-                    VerifyBackupResult.Success.NotEncrypted
+                result[BackupConstants.BACKUP_DB_EXTENSION] == true && result[BackupConstants.BACKUP_METADATA_EXTENSION] == true ->
+                    VerifyBackupResult.AndroidBackup(false)
 
-                it[BackupConstants.BACKUP_METADATA_EXTENSION] == true -> VerifyBackupResult.Success.Web
-                else ->
-                    VerifyBackupResult.Failure.InvalidBackupFile
+                else -> VerifyBackupResult.Failure.InvalidBackupFile
             }
-        })
+        }
+    )
+
+    private suspend fun verifyMpBackupFile(
+        backupFilePath: Path,
+    ): VerifyBackupResult {
+
+        val mpBackupImporter = backupImporterProvider.providePeekImporter()
+        val peek = mpBackupImporter.peekBackupFile(backupFilePath.toString())
+
+        kaliumLogger.i("Peek result: $peek")
+
+        return when (peek) {
+            BackupPeekResult.Failure.UnknownFormat -> VerifyBackupResult.Failure.InvalidBackupFile
+            is BackupPeekResult.Failure.UnsupportedVersion -> VerifyBackupResult.Failure.UnsupportedVersion(peek.backupVersion)
+            is BackupPeekResult.Success ->
+                if (peek.isCreatedBySameUser(userId.toBackupQualifiedId())) {
+                    VerifyBackupResult.MultiPlatformBackup(peek.isEncrypted)
+                } else {
+                    VerifyBackupResult.Failure.InvalidUserId
+                }
+        }
     }
 }
 
+enum class BackupFileFormat {
+    ANDROID, MULTIPLATFORM
+}
+
 sealed class VerifyBackupResult {
-    sealed class Success : VerifyBackupResult() {
-        data object Encrypted : Success()
-        data object NotEncrypted : Success()
-        data object Web : Success()
+
+    @Suppress("FunctionName")
+    companion object {
+        internal fun AndroidBackup(isEncrypted: Boolean) = Success(BackupFileFormat.ANDROID, isEncrypted)
+        internal fun MultiPlatformBackup(isEncrypted: Boolean) = Success(BackupFileFormat.MULTIPLATFORM, isEncrypted)
     }
+
+    data class Success(val format: BackupFileFormat, val isEncrypted: Boolean) : VerifyBackupResult()
 
     sealed class Failure : VerifyBackupResult() {
         data object InvalidBackupFile : Failure()
+        data class UnsupportedVersion(val version: String) : Failure()
         data class Generic(val error: CoreFailure) : Failure()
+        data object InvalidUserId : Failure()
     }
 }
