3cfeb905c70df5a44d6130d2b9216b8c3823bce6
Vitor Hugo Schwaab <vitor@schwaab.dev>
2025-08-01 12:17:05 +0200

    chore: skip cells tests for ios

--- buildSrc/src/main/kotlin/OnlyAffectedTestTask.kt
+++ buildSrc/src/main/kotlin/OnlyAffectedTestTask.kt
@@ -127,10 +127,13 @@ open class OnlyAffectedTestTask : DefaultTask() {
      * @param taskName how the task will be named when registered
      * @param testTarget the target test task that would be wrapped in this "smart" execution
      */
-    enum class TestTaskConfiguration(val taskName: String, val testTarget: String) {
-        ANDROID_INSTRUMENTED_TEST_TASK("connectedAndroidOnlyAffectedTest", "connectedAndroidTest"),
-        ANDROID_UNIT_TEST_TASK("androidUnitOnlyAffectedTest", "testDebugUnitTest"),
-        IOS_TEST_TASK("iOSOnlyAffectedTest", "iosSimulatorArm64Test")
+    enum class TestTaskConfiguration(val taskName: String, val testTarget: String, val ignoredModules: List<String> = emptyList()) {
+        ANDROID_INSTRUMENTED_TEST_TASK("connectedAndroidOnlyAffectedTest", "connectedAndroidTest", IGNORED_MODULES),
+        ANDROID_UNIT_TEST_TASK("androidUnitOnlyAffectedTest", "testDebugUnitTest", IGNORED_MODULES),
+        IOS_TEST_TASK("iOSOnlyAffectedTest", "iosSimulatorArm64Test", IGNORED_MODULES + "cells");
     }
 
+    private companion object {
+        val IGNORED_MODULES = listOf("android", "protobuf", "protobuf-codegen")
+    }
 }
