1c0b48ac3e5a4b6a79cb5982fa2e77ecc37d2e8d
sergei.bakhtiarov <sbakhtiarov@gmail.com>
2025-08-14 14:31:25 +0200

    feat: cells feature flag (WPB-19476)

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/featureConfig/SyncFeatureConfigsUseCase.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/featureConfig/SyncFeatureConfigsUseCase.kt
@@ -38,6 +38,7 @@ import com.wire.kalium.logic.feature.featureConfig.handler.MLSMigrationConfigHan
 import com.wire.kalium.logic.feature.featureConfig.handler.SecondFactorPasswordChallengeConfigHandler
 import com.wire.kalium.logic.feature.featureConfig.handler.SelfDeletingMessagesConfigHandler
 import com.wire.kalium.logic.sync.receiver.handler.AllowedGlobalOperationsHandler
+import com.wire.kalium.logic.sync.receiver.handler.CellsConfigHandler
 import com.wire.kalium.network.exceptions.KaliumException
 import com.wire.kalium.network.exceptions.isNoTeam
 import io.mockative.Mockable
@@ -67,6 +68,7 @@ internal class SyncFeatureConfigsUseCaseImpl(
     private val channelsConfigHandler: ChannelsFeatureConfigurationHandler,
     private val consumableNotificationsConfigHandler: ConsumableNotificationsConfigHandler,
     private val allowedGlobalOperationsHandler: AllowedGlobalOperationsHandler,
+    private val cellsConfigHandler: CellsConfigHandler,
 ) : SyncFeatureConfigsUseCase {
     override suspend operator fun invoke(): Either<CoreFailure, Unit> =
         featureConfigRepository.getFeatureConfigs().flatMap { it ->
@@ -88,6 +90,7 @@ internal class SyncFeatureConfigsUseCaseImpl(
                 )
             }
             it.allowedGlobalOperationsModel?.let { model -> allowedGlobalOperationsHandler.handle(model) }
+            cellsConfigHandler.handle(it.cellsModel)
             Either.Right(Unit)
         }.onFailure { networkFailure ->
             if (
