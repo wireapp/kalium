e3b28f9f929aa8b1f670114e6b1cf8e12c5efb51
Mohamad Jaara <mohamad.jaara@wire.com>
2025-11-26 23:00:45 +0100

    chore: add WireCoreCrypto and WireCoreCryptoUniffi modules with version 9.1.1

---
+++ cryptography/frameworks/WireCoreCryptoUniffi.xcframework/ios-arm64-simulator/WireCoreCryptoUniffi.framework/Headers/WireCoreCryptoUniffi.h
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for WireCoreCryptoUniffi.
+FOUNDATION_EXPORT double WireCoreCryptoUniffiVersionNumber;
+
+//! Project version string for WireCoreCryptoUniffi.
+FOUNDATION_EXPORT const unsigned char WireCoreCryptoUniffiVersionString[];
+
+#import <WireCoreCryptoUniffi/core_crypto_ffiFFI.h>
