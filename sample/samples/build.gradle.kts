4cd304e2219480348fa4a2eaaa2c13b03bcf47c1
Mohamad Jaara <mohamad.jaara@wire.com>
2025-11-28 16:30:08 +0100

    chore: update iOS build configuration and restore cryptography dependency for non-JS targets

--- sample/samples/build.gradle.kts
+++ sample/samples/build.gradle.kts
@@ -32,7 +32,6 @@ kotlin {
         val commonMain by getting {
             dependencies {
                 implementation(projects.domain.backup)
-                implementation(projects.core.cryptography)
                 implementation(projects.data.protobuf)
                 implementation(projects.core.logger)
 
@@ -44,6 +43,7 @@ kotlin {
         val nonJsMain by creating {
             dependsOn(commonMain)
             dependencies {
+                implementation(projects.core.cryptography)
                 implementation(projects.data.network)
                 implementation(projects.data.persistence)
             }
