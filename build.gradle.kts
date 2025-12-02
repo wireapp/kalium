0e99d23aff96101b45fa3fcc9cfd501a53910181
Mohamad Jaara <mohamad.jaara@wire.com>
2025-12-02 17:15:53 +0100

    chore: configure garbage collection settings for iOS Simulator ARM64 tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -73,6 +73,21 @@ subprojects {
         reports.junitXml.required.set(false)
         reports.html.required.set(false)
     }
+
+    // Configure GC for iOS Simulator ARM64 tests only
+    pluginManager.withPlugin("org.jetbrains.kotlin.multiplatform") {
+        extensions.configure<org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension> {
+            targets.withType<org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget>()
+                .matching { it.name == "iosSimulatorArm64" }
+                .configureEach {
+                    binaries.all {
+                        if (this is org.jetbrains.kotlin.gradle.plugin.mpp.TestExecutable) {
+                            binaryOptions["gc"] = "stwms"
+                        }
+                    }
+                }
+        }
+    }
 }
 
 allprojects {
