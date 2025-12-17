f315068248d2c8ad8494b4bd4cf9a609cfd41126
Yamil Medina <yamilmedina@users.noreply.github.com>
2025-12-17 12:44:15 +0100

    fix: pattern expect/actual without implementations - logic pt.3 (WPB-22300) (#3773)
    * fix: kalium dispatcher hierarchy for native compilation
    
    * fix: backup classes hierarchy for full build
    
    * fix: logic related expect/actual classes pattern
    
    * fix: logic related expect/actual classes pattern
    
    * fix detekt

--- logic/src/androidMain/kotlin/com/wire/kalium/logic/CoreLogic.kt
+++ logic/src/androidMain/kotlin/com/wire/kalium/logic/CoreLogic.kt
@@ -52,12 +52,12 @@ actual class CoreLogic(
     kaliumConfigs: KaliumConfigs
 ) : CoreLogicCommon(rootPath, userAgent, kaliumConfigs) {
 
-    override val globalPreferences: GlobalPrefProvider = GlobalPrefProvider(
+    actual override val globalPreferences: GlobalPrefProvider = GlobalPrefProvider(
         appContext,
         kaliumConfigs.shouldEncryptData
     )
 
-    override val globalDatabaseBuilder: GlobalDatabaseBuilder = globalDatabaseProvider(
+    actual override val globalDatabaseBuilder: GlobalDatabaseBuilder = globalDatabaseProvider(
         platformDatabaseData = PlatformDatabaseData(appContext),
         queriesContext = KaliumDispatcherImpl.io,
         passphrase = if (kaliumConfigs.shouldEncryptData) {
@@ -68,28 +68,28 @@ actual class CoreLogic(
         enableWAL = true
     )
 
-    override fun getSessionScope(userId: UserId): UserSessionScope =
+    actual override fun getSessionScope(userId: UserId): UserSessionScope =
         userSessionScopeProvider.value.getOrCreate(userId)
 
-    override suspend fun deleteSessionScope(userId: UserId) {
+    actual override suspend fun deleteSessionScope(userId: UserId) {
         userSessionScopeProvider.value.get(userId)?.cancel()
         userSessionScopeProvider.value.delete(userId)
     }
 
-    override val globalCallManager: GlobalCallManager by lazy {
+    actual override val globalCallManager: GlobalCallManager by lazy {
         GlobalCallManager(
             appContext = PlatformContext(appContext),
             scope = getGlobalScope()
         )
     }
 
-    override val workSchedulerProvider: WorkSchedulerProvider = WorkSchedulerProviderImpl(appContext)
+    actual override val workSchedulerProvider: WorkSchedulerProvider = WorkSchedulerProviderImpl(appContext)
 
-    override val networkStateObserver: NetworkStateObserver = NetworkStateObserverImpl(
+    actual override val networkStateObserver: NetworkStateObserver = NetworkStateObserverImpl(
         appContext = appContext
     )
 
-    override val userSessionScopeProvider: Lazy<UserSessionScopeProvider> = lazy {
+    actual override val userSessionScopeProvider: Lazy<UserSessionScopeProvider> = lazy {
         UserSessionScopeProviderImpl(
             authenticationScopeProvider,
             rootPathsProvider,
@@ -106,7 +106,7 @@ actual class CoreLogic(
         )
     }
 
-    override val audioNormalizedLoudnessBuilder: AudioNormalizedLoudnessBuilder = AudioNormalizedLoudnessBuilderImpl(
+    actual override val audioNormalizedLoudnessBuilder: AudioNormalizedLoudnessBuilder = AudioNormalizedLoudnessBuilderImpl(
         dispatcher = KaliumDispatcherImpl.io,
         audioEffect = AudioEffect(appContext),
     )
