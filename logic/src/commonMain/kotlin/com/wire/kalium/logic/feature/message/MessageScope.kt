f080b5756cb72a13abc42e508742322394e2d239
Mathias Staab <mathias.staab@wire.com>
2026-01-18 14:21:25 +0100

    Add message search API for JVM/common targets
    - Add searchMessagesByText() to MessageDAO interface and implementation
    - Uses existing selectConversationMessagesFromSearch SQL query
    - Add searchMessagesByText() to MessageRepository interface and implementation
    - Add SearchMessagesInConversationUseCase for clean API access
    - Expose searchMessagesInConversation in MessageScope
    - Simple non-paginated API returning up to configurable limit of results
    - Useful for TUI and other clients that need simple text search

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/message/MessageScope.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/message/MessageScope.kt
@@ -534,6 +534,11 @@ public class MessageScope internal constructor(
             messageRepository = messageRepository
         )
 
+    public val searchMessagesInConversation: SearchMessagesInConversationUseCase
+        get() = SearchMessagesInConversationUseCaseImpl(
+            messageRepository = messageRepository
+        )
+
     public val observeAssetStatuses: ObserveAssetStatusesUseCase get() = ObserveAssetStatusesUseCaseImpl(messageRepository)
 
     public val saveMessageDraftUseCase: SaveMessageDraftUseCase
