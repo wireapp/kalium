042bd2be20c43b171821c08620773d7f0ac2dd7e
yamilmedina <yamilmedina@users.noreply.github.com>
2026-02-18 12:54:27 -0300

    feat: test coverage for db export func

--- logic/src/commonTest/kotlin/com/wire/kalium/logic/data/client/MLSClientProviderTest.kt
+++ logic/src/commonTest/kotlin/com/wire/kalium/logic/data/client/MLSClientProviderTest.kt
@@ -19,17 +19,19 @@ package com.wire.kalium.logic.data.client
 
 import com.wire.kalium.common.error.CoreFailure
 import com.wire.kalium.common.error.StorageFailure
+import com.wire.kalium.common.functional.left
+import com.wire.kalium.common.functional.right
+import com.wire.kalium.logic.data.conversation.EpochChangesObserver
 import com.wire.kalium.logic.data.featureConfig.FeatureConfigTest
 import com.wire.kalium.logic.data.featureConfig.MLSModel
 import com.wire.kalium.logic.data.featureConfig.Status
-import com.wire.kalium.logic.data.id.CurrentClientIdProvider
 import com.wire.kalium.logic.data.mls.CipherSuite
 import com.wire.kalium.logic.data.mls.SupportedCipherSuite
 import com.wire.kalium.logic.data.user.SupportedProtocol
 import com.wire.kalium.logic.data.user.UserId
-import com.wire.kalium.common.functional.left
-import com.wire.kalium.common.functional.right
-import com.wire.kalium.logic.data.conversation.EpochChangesObserver
+import com.wire.kalium.logic.framework.TestClient
+import com.wire.kalium.logic.util.arrangement.provider.CurrentClientIdProviderArrangement
+import com.wire.kalium.logic.util.arrangement.provider.CurrentClientIdProviderArrangementImpl
 import com.wire.kalium.logic.util.arrangement.repository.FeatureConfigRepositoryArrangement
 import com.wire.kalium.logic.util.arrangement.repository.FeatureConfigRepositoryArrangementImpl
 import com.wire.kalium.logic.util.arrangement.repository.UserConfigRepositoryArrangement
@@ -37,14 +39,17 @@ import com.wire.kalium.logic.util.arrangement.repository.UserConfigRepositoryArr
 import com.wire.kalium.logic.util.shouldFail
 import com.wire.kalium.logic.util.shouldSucceed
 import com.wire.kalium.persistence.dbPassphrase.PassphraseStorage
+import com.wire.kalium.util.FileUtil
 import io.ktor.util.reflect.instanceOf
+import io.mockative.any
 import io.mockative.coVerify
+import io.mockative.every
 import io.mockative.mock
 import io.mockative.once
-import io.mockative.verify
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runTest
+import kotlin.io.encoding.Base64
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -148,17 +153,90 @@ class MLSClientProviderTest {
             .wasNotInvoked()
     }
 
+    @Test
+    fun givenValidClient_whenExportingDB_thenReturnsExportData() = runTest {
+        val (arrangement, mlsClientProvider) = Arrangement(this).arrange {
+            withCurrentClientIdSuccess(TestClient.CLIENT_ID)
+            withCoreCryptoDatabaseExists()
+            withPassphraseStorage()
+        }
+
+        mlsClientProvider.exportCryptoDB().shouldSucceed {
+            assertEquals(
+                "${arrangement.rootKeyStorePath}/${TestClient.CLIENT_ID.value}/keystore/keystore",
+                it.dbPath
+            )
+            assertEquals(TestClient.CLIENT_ID, it.clientId)
+            assertEquals(
+                Base64.encode(ByteArray(32) { 0xAB.toByte() }),
+                Base64.encode(it.passphrase)
+            )
+        }
+    }
+
+    @Test
+    fun givenNoClient_whenExportingDB_thenReturnsFailure() = runTest {
+        val (_, mlsClientProvider) = Arrangement(this).arrange {
+            withCurrentClientIdFailure(StorageFailure.DataNotFound)
+        }
+
+        mlsClientProvider.exportCryptoDB().shouldFail {
+            it.instanceOf(StorageFailure.DataNotFound::class)
+        }
+    }
+
+    @Test
+    fun givenDBDoesNotExist_whenExportingDB_thenReturnsDataNotFound() = runTest {
+        val (arrangement, mlsClientProvider) = Arrangement(this).arrange {
+            withCurrentClientIdSuccess(TestClient.CLIENT_ID)
+            withPassphraseStorage()
+        }
+
+        mlsClientProvider.exportCryptoDB().shouldFail {
+            it.instanceOf(StorageFailure.DataNotFound::class)
+        }
+    }
+
     private class Arrangement(
         val processingScope: CoroutineScope
     ) : UserConfigRepositoryArrangement by UserConfigRepositoryArrangementImpl(),
-        FeatureConfigRepositoryArrangement by FeatureConfigRepositoryArrangementImpl() {
+        FeatureConfigRepositoryArrangement by FeatureConfigRepositoryArrangementImpl(),
+        CurrentClientIdProviderArrangement by CurrentClientIdProviderArrangementImpl() {
 
         val rootKeyStorePath: String = "rootKeyStorePath"
         val userId: UserId = UserId("userId", "domain")
-        val currentClientIdProvider: CurrentClientIdProvider = mock(CurrentClientIdProvider::class)
         val passphraseStorage: PassphraseStorage = mock(PassphraseStorage::class)
         val mlsTransportProvider: MLSTransportProvider = mock(MLSTransportProvider::class)
         val epochChangesObserver: EpochChangesObserver = mock(EpochChangesObserver::class)
+
+        init {
+            withCoreCryptoDatabaseDoesNotExists()
+        }
+
+        fun withCoreCryptoDatabaseDoesNotExists() {
+            val clientId = TestClient.CLIENT_ID
+            val location = "$rootKeyStorePath/${clientId.value}"
+            val rootDir = "$location/keystore"
+
+            FileUtil.deleteDirectory(rootDir)
+        }
+
+        /**
+         * Create the directory structure that exportCryptoDB expects
+         */
+        fun withCoreCryptoDatabaseExists() {
+            val clientId = TestClient.CLIENT_ID
+            val location = "$rootKeyStorePath/${clientId.value}"
+            val rootDir = "$location/keystore"
+            val dbPath = "$rootDir/keystore"
+
+            FileUtil.mkDirs(dbPath)
+        }
+
+        fun withPassphraseStorage() {
+            val passphraseBase64 = Base64.encode(ByteArray(32) { 0xAB.toByte() })
+            every { passphraseStorage.getPassphrase(any()) }.returns(passphraseBase64)
+        }
 
         fun arrange(block: suspend Arrangement.() -> Unit) = apply { runBlocking { block() } }.let {
             this to MLSClientProviderImpl(
