bd6e8cbe287f85fb977727fd1d16752bb9168467
yamilmedina <yamilmedina@users.noreply.github.com>
2026-02-26 10:11:56 -0300

    feat: add hook for crypto state mutation

---
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/feature/message/CryptoStateChangeHookRegistry.kt
@@ -0,0 +1,60 @@
+/*
+ * Wire
+ * Copyright (C) 2026 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ */
+
+package com.wire.kalium.logic.feature.message
+
+import com.wire.kalium.common.logger.kaliumLogger
+import com.wire.kalium.logic.data.user.UserId
+import com.wire.kalium.messaging.hooks.CryptoStateChangeHookNotifier
+import kotlin.concurrent.Volatile
+import kotlin.coroutines.cancellation.CancellationException
+
+internal class CryptoStateChangeHookRegistry : CryptoStateChangeHookNotifier {
+
+    @Volatile
+    private var hookNotifier: CryptoStateChangeHookNotifier? = null
+
+    fun register(hookNotifier: CryptoStateChangeHookNotifier) {
+        if (this.hookNotifier == null) {
+            this.hookNotifier = hookNotifier
+        } else {
+            error("Hook notifier already registered")
+        }
+    }
+
+    fun unregister(hookNotifier: CryptoStateChangeHookNotifier) {
+        if (this.hookNotifier === hookNotifier) {
+            this.hookNotifier = null
+        }
+    }
+
+    fun clear() {
+        hookNotifier = null
+    }
+
+    @Suppress("TooGenericExceptionCaught")
+    override suspend fun onCryptoStateChanged(userId: UserId) {
+        try {
+            hookNotifier?.onCryptoStateChanged(userId)
+        } catch (e: CancellationException) {
+            throw e
+        } catch (throwable: Exception) {
+            kaliumLogger.w("Crypto state change hook execution failed", throwable)
+        }
+    }
+}
