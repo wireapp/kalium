e200ed06f3104539ec3201c82dcd11d006d89e51
Jakub Zerko <iot.zerko@gmail.com>
2026-03-10 19:06:42 +0100

    fix: avoid iOS init-order crash in global call manager

--- logic/src/appleMain/kotlin/com/wire/kalium/logic/CoreLogic.kt
+++ logic/src/appleMain/kotlin/com/wire/kalium/logic/CoreLogic.kt
@@ -98,7 +98,9 @@ public actual class CoreLogic(
         userSessionScopeProvider.value.delete(userId)
     }
 
-    actual override val globalCallManager: GlobalCallManager = GlobalCallManager(getGlobalScope(), networkStateObserver)
+    actual override val globalCallManager: GlobalCallManager by lazy {
+        GlobalCallManager(getGlobalScope(), networkStateObserver)
+    }
     actual override val workSchedulerProvider: WorkSchedulerProvider = WorkSchedulerProviderImpl()
     public actual override val audioNormalizedLoudnessBuilder: AudioNormalizedLoudnessBuilder = AudioNormalizedLoudnessBuilderImpl()
 
