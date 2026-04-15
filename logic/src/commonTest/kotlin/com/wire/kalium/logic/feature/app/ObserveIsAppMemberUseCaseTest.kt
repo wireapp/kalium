23ce4e6e5e8e564aab5b568181db005891c94a0a
Alexandre Ferris <ferris.alexandre@gmail.com>
2026-04-15 14:31:11 +0200

    feat: Search for Apps - Part3 (Wiring Changes to UseCases) [WPB-20357] (#4041)
    * feat: Search for Apps - Part3 [WPB-20357]
    
    * Add AppScope
    * Add usecase to get an App by Id, returning ServiceDetails
    * Add usecase to observe all Apps returning a list of ServiceDetails
    * Add usecase to observe if App is member of a conversation returning its id
    * Add usecase to search Apps by name, returning a list of found ServiceDetails
    * Add tests for new usecases
    
    * feat: Search for Apps - Part3 [WPB-20357]
    
    * Change fold usage in ObserveIsAppMemberUseCase
    * Add failure testcases
    
    * feat: Search for Apps - Part3 [WPB-20357]
    
    * Remove extension function to map ServiceId to QualifiedID
    
    * feat: Search for Apps - Part3 [WPB-20357]
    
    * Migrate tests from Mockative to mokkery

---
+++ logic/src/commonTest/kotlin/com/wire/kalium/logic/feature/app/ObserveIsAppMemberUseCaseTest.kt
@@ -0,0 +1,119 @@
+/*
+ * Wire
+ * Copyright (C) 2026 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ */
+package com.wire.kalium.logic.feature.app
+
+import com.wire.kalium.common.error.StorageFailure
+import com.wire.kalium.common.functional.Either
+import com.wire.kalium.logic.data.app.AppRepository
+import com.wire.kalium.logic.data.id.ConversationId
+import com.wire.kalium.logic.data.id.QualifiedID
+import dev.mokkery.answering.returns
+import dev.mokkery.mock
+import dev.mokkery.everySuspend
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertIs
+import kotlin.uuid.Uuid
+
+class ObserveIsAppMemberUseCaseTest {
+
+    @Test
+    fun givenAppIdAndConversationId_whenObservingIsAppMember_thenReturnAppId() = runTest {
+        // given
+        val (_, observeIsAppMember) = Arrangement()
+            .withObserveIsAppMemberSuccess(
+                appId = Arrangement.APP_ID,
+                conversationId = Arrangement.CONVERSATION_ID
+            )
+            .arrange()
+
+        // when
+        val result = observeIsAppMember.invoke(
+            appId = Arrangement.APP_ID,
+            conversationId = Arrangement.CONVERSATION_ID
+        ).first()
+
+        // then
+        assertIs<ObserveIsAppMemberResult.Success>(result)
+        assertEquals(Arrangement.APP_ID, result.userId)
+    }
+
+    @Test
+    fun givenAppIdAndConversationId_whenObservingIsAppMemberAndAppDoesntExist_thenReturnStorageFailure() = runTest {
+        // given
+        val (_, observeIsAppMember) = Arrangement()
+            .withObserveIsAppMemberFailure(
+                appId = Arrangement.APP_ID,
+                conversationId = Arrangement.CONVERSATION_ID
+            )
+            .arrange()
+
+        // when
+        val result = observeIsAppMember.invoke(
+            appId = Arrangement.APP_ID,
+            conversationId = Arrangement.CONVERSATION_ID
+        ).first()
+
+        // then
+        assertIs<ObserveIsAppMemberResult.Failure>(result)
+        assertIs<StorageFailure.DataNotFound>(result.failure)
+    }
+
+    private class Arrangement {
+        private val appRepository = mock<AppRepository>()
+
+        private val useCase: ObserveIsAppMemberUseCase = ObserveIsAppMemberUseCaseImpl(
+            appRepository = appRepository
+        )
+
+        suspend fun withObserveIsAppMemberSuccess(
+            appId: QualifiedID,
+            conversationId: ConversationId
+        ) = apply {
+            everySuspend {
+                appRepository.observeIsAppMember(appId, conversationId)
+            } returns flowOf(Either.Right(APP_ID))
+        }
+
+        suspend fun withObserveIsAppMemberFailure(
+            appId: QualifiedID,
+            conversationId: ConversationId
+        ) = apply {
+            everySuspend {
+                appRepository.observeIsAppMember(appId, conversationId)
+            } returns flowOf(Either.Left(StorageFailure.DataNotFound))
+        }
+
+        fun arrange() = this to useCase
+
+        companion object {
+            val APP_ID = QualifiedID(
+                value = Uuid.random().toString(),
+                domain = "wire.com"
+            )
+
+            val CONVERSATION_ID = QualifiedID(
+                value = Uuid.random().toString(),
+                domain = "wire.com"
+            )
+        }
+    }
+}
