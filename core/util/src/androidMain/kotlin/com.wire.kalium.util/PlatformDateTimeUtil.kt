a6a87a6934854f890b94a03ce896b15ebcd37205
Mohamad Jaara <9083456+MohamadJaara@users.noreply.github.com>
2026-08-10 11:45:58 +0200

    restore the removed api 26 check

--- core/util/src/androidMain/kotlin/com.wire.kalium.util/PlatformDateTimeUtil.kt
+++ core/util/src/androidMain/kotlin/com.wire.kalium.util/PlatformDateTimeUtil.kt
@@ -18,20 +18,18 @@
 
 package com.wire.kalium.util
 
-import kotlinx.datetime.Clock
+import com.wire.kalium.util.DateTimeUtil.MILLISECONDS_DIGITS
 import kotlinx.datetime.Instant
+import kotlinx.datetime.toJavaInstant
 import java.text.SimpleDateFormat
-import java.util.Date
+import java.time.format.DateTimeFormatterBuilder
 import java.util.Locale
-import java.util.TimeZone
 
 actual open class PlatformDateTimeUtil actual constructor() {
 
-    private val isoDateTimeFormat = SimpleDateFormat(DateTimeUtil.iso8601Pattern, Locale.getDefault()).apply {
-        timeZone = TimeZone.getTimeZone("UTC")
-    }
-
     private val secondsDateTimeFormat = SimpleDateFormat(DateTimeUtil.simplePattern, Locale.getDefault())
+
+    private val isoDateTimeFormatter = DateTimeFormatterBuilder().appendInstant(MILLISECONDS_DIGITS).toFormatter()
 
     /**
      * Parse [kotlinx.datetime.Instant] into date-time string in ISO-8601 format.
@@ -42,13 +40,13 @@ actual open class PlatformDateTimeUtil actual constructor() {
      * @return date in ISO-8601 format (YYYY-MM-DDTHH:mm:ss.SSSZ)
      */
     actual fun fromInstantToIsoDateTimeString(instant: Instant): String =
-            isoDateTimeFormat.format(Date(instant.toEpochMilliseconds()))
+        isoDateTimeFormatter.format(instant.toJavaInstant())
 
     /**
      * Parse [kotlinx.datetime.Instant] into date-time string in simplified format with up to seconds precision.
      * @return date in simplified format (YYYY-MM-DD_HH:mm:ss)
      */
     actual fun fromInstantToSimpleDateTimeString(instant: Instant): String =
-        secondsDateTimeFormat.format(Date(Clock.System.now().toEpochMilliseconds()))
+        secondsDateTimeFormat.format(System.currentTimeMillis())
 
 }
