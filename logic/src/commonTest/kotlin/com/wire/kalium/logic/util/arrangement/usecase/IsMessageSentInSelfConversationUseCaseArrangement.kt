ddcdaba4c1034080f65eef9456cee1a23e512dac
Mohamad Jaara <9083456+MohamadJaara@users.noreply.github.com>
2026-08-29 17:03:14 +0200

    test(sync): add on-demand mutation coverage (#4427)

---
+++ logic/src/commonTest/kotlin/com/wire/kalium/logic/util/arrangement/usecase/IsMessageSentInSelfConversationUseCaseArrangement.kt
@@ -0,0 +1,40 @@
+/*
+ * Wire
+ * Copyright (C) 2026 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ */
+package com.wire.kalium.logic.util.arrangement.usecase
+
+import com.wire.kalium.logic.data.message.IsMessageSentInSelfConversationUseCase
+import dev.mokkery.MockMode
+import dev.mokkery.answering.returns
+import dev.mokkery.everySuspend
+import dev.mokkery.matcher.any
+import dev.mokkery.mock
+
+internal interface IsMessageSentInSelfConversationUseCaseArrangement {
+    val isMessageSentInSelfConversationUseCase: IsMessageSentInSelfConversationUseCase
+
+    suspend fun withMessageSentInSelfConversationReturning(result: Boolean)
+}
+
+internal class IsMessageSentInSelfConversationUseCaseArrangementImpl : IsMessageSentInSelfConversationUseCaseArrangement {
+    override val isMessageSentInSelfConversationUseCase =
+        mock<IsMessageSentInSelfConversationUseCase>(mode = MockMode.autoUnit)
+
+    override suspend fun withMessageSentInSelfConversationReturning(result: Boolean) {
+        everySuspend { isMessageSentInSelfConversationUseCase(any()) }.returns(result)
+    }
+}
